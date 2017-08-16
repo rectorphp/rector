fb7adfbfc3ffeb1802ba9e5e7bd3275ae24ee6ca
TomasVotruba <tomas.vot@gmail.com>
2017-08-16 09:48:48 +0200

    [Nette] add few more tests to HtmlAddMethodRector

--- tests/Rector/Contrib/Nette/HtmlAddMethodRector/Test.php
+++ tests/Rector/Contrib/Nette/HtmlAddMethodRector/Test.php
@@ -17,6 +17,14 @@ final class Test extends AbstractRectorTestCase
             __DIR__ . '/wrong/wrong2.php.inc',
             __DIR__ . '/correct/correct2.php.inc'
         );
+        $this->doTestFileMatchesExpectedContent(
+            __DIR__ . '/wrong/wrong3.php.inc',
+            __DIR__ . '/correct/correct3.php.inc'
+        );
+        $this->doTestFileMatchesExpectedContent(
+            __DIR__ . '/wrong/wrong4.php.inc',
+            __DIR__ . '/correct/correct4.php.inc'
+        );
     }
 
     /**
