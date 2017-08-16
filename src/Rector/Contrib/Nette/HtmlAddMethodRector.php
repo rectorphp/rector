a73ba4bb49033bb217e8e80be115c9ce37351824
TomasVotruba <tomas.vot@gmail.com>
2017-08-16 09:45:07 +0200

    [Nette] add simple type resolver for HtmlAddMethodRector

--- src/Rector/Contrib/Nette/HtmlAddMethodRector.php
+++ src/Rector/Contrib/Nette/HtmlAddMethodRector.php
@@ -4,13 +4,23 @@ namespace Rector\Rector\Contrib\Nette;
 
 use Nette\Utils\Html;
 use PhpParser\Node;
+use PhpParser\Node\Expr\Assign;
+use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Expr\New_;
 use PhpParser\Node\Expr\StaticCall;
+use PhpParser\Node\Expr\Variable;
 use PhpParser\Node\Identifier;
+use PhpParser\Node\Stmt\Expression;
 use Rector\Deprecation\SetNames;
 use Rector\Rector\AbstractRector;
 
 final class HtmlAddMethodRector extends AbstractRector
 {
+    /**
+     * @var string[]
+     */
+    private $variableTypes = [];
+
     public function getSetName(): string
     {
         return SetNames::NETTE;
@@ -23,23 +33,49 @@ final class HtmlAddMethodRector extends AbstractRector
 
     public function isCandidate(Node $node): bool
     {
-        if (! $this->isOnTypeCall($node, Html::class)) {
-            return false;
+        if ($this->isOnTypeCall($node, Html::class)) {
+            return true;
         }
 
-        if (! $this->isStaticCall($node)) {
-            return false;
+        if ($this->isStaticCall($node)) {
+            return true;
         }
 
-        return true;
+        return false;
     }
 
     /**
-     * @param StaticCall $node
+     * @param Node[] $nodes
+     */
+    public function beforeTraverse(array $nodes): void
+    {
+        foreach ($nodes as $node) {
+            if ($node instanceof Expression && $node->expr instanceof Assign) {
+                $assignNode = $node->expr;
+                $variableName = $assignNode->var->name;
+                if ($assignNode->expr instanceof New_) {
+                    $variableType = (string) $assignNode->expr->class;
+                }
+
+                $this->variableTypes[$variableName] = $variableType;
+            }
+        }
+    }
+
+    /**
+     * @param StaticCall|MethodCall $node
      */
     public function refactor(Node $node): ?Node
     {
-        $node->name->name = 'addHtml';
+        if ($node instanceof StaticCall) {
+            $node->name->name = 'addHtml';
+
+            return $node;
+        }
+
+        if ($node instanceof MethodCall) {
+            $node->name->name = 'addHtml';
+        }
 
         return $node;
     }
@@ -65,13 +101,29 @@ final class HtmlAddMethodRector extends AbstractRector
         return true;
     }
 
+    /**
+     * check elements type:
+     * inspire: https://github.com/phpstan/phpstan/blob/355060961eb4a33304c66dfbfc0cd32870a0b9d4/src/Rules/Methods/CallMethodsRule.php#L74
+     * https://github.com/phpstan/phpstan/blob/355060961eb4a33304c66dfbfc0cd32870a0b9d4/src/Analyser/Scope.php.
+     */
     private function isOnTypeCall(Node $node, string $class): bool
     {
-        dump($class);
-        die;
+        if (! $node instanceof MethodCall) {
+            return false;
+        }
 
-        # check elements type:
-        # inspire: https://github.com/phpstan/phpstan/blob/355060961eb4a33304c66dfbfc0cd32870a0b9d4/src/Rules/Methods/CallMethodsRule.php#L74
-        # local package?
+        if (! $node->var instanceof Variable) {
+            return false;
+        }
+
+        $varNode = $node->var;
+
+        if (isset($this->variableTypes[$varNode->name])) {
+            if ($this->variableTypes[$varNode->name] === $class) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
--- tests/Rector/Contrib/Nette/HtmlAddMethodRector/Test.php
+++ tests/Rector/Contrib/Nette/HtmlAddMethodRector/Test.php
@@ -9,10 +9,10 @@ final class Test extends AbstractRectorTestCase
 {
     public function test(): void
     {
-//        $this->doTestFileMatchesExpectedContent(
-//            __DIR__ . '/wrong/wrong.php.inc',
-//            __DIR__ . '/correct/correct.php.inc'
-//        );
+        $this->doTestFileMatchesExpectedContent(
+            __DIR__ . '/wrong/wrong.php.inc',
+            __DIR__ . '/correct/correct.php.inc'
+        );
         $this->doTestFileMatchesExpectedContent(
             __DIR__ . '/wrong/wrong2.php.inc',
             __DIR__ . '/correct/correct2.php.inc'
