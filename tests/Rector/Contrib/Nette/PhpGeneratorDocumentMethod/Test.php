0781228cc0a7cd23cc981f6c9c358bd16ba8a041
TomasVotruba <tomas.vot@gmail.com>
2017-08-24 12:00:04 +0200

    [Rector] PhpGeneratorDocument init

---
+++ tests/Rector/Contrib/Nette/PhpGeneratorDocumentMethod/Test.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+namespace Rector\Tests\Rector\Contrib\Nette\NetteObjectToSmartTraitRector;
+
+use Rector\Rector\Contrib\Nette\NetteObjectToSmartTraitRector;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+
+final class Test extends AbstractRectorTestCase
+{
+    public function test(): void
+    {
+        $this->doTestFileMatchesExpectedContent(
+            __DIR__ . '/Wrong/wrong.php.inc',
+            __DIR__ . '/Correct/correct.php.inc'
+        );
+        $this->doTestFileMatchesExpectedContent(
+            __DIR__ . '/Wrong/wrong2.php.inc',
+            __DIR__ . '/Correct/correct2.php.inc'
+        );
+        $this->doTestFileMatchesExpectedContent(
+            __DIR__ . '/Wrong/wrong3.php.inc',
+            __DIR__ . '/Correct/correct3.php.inc'
+        );
+        $this->doTestFileMatchesExpectedContent(
+            __DIR__ . '/Wrong/wrong4.php.inc',
+            __DIR__ . '/Correct/correct4.php.inc'
+        );
+    }
+
+    /**
+     * @return string[]
+     */
+    protected function getRectorClasses(): array
+    {
+        return [NetteObjectToSmartTraitRector::class];
+    }
+}
