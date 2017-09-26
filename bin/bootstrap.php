01d5d397e1d42d0649f217b83d4cab1b367050d3
TomasVotruba <tomas.vot@gmail.com>
2017-09-26 11:11:59 +0200

    bootstrap: add autoload paths comments

--- bin/bootstrap.php
+++ bin/bootstrap.php
@@ -4,7 +4,10 @@
  * This allows to load "vendor/autoload.php" both from
  * "composer create-project ..." and "composer require" installation.
  */
-$possibleAutoloadPaths = [__DIR__ . '/../vendor/autoload.php', __DIR__ . '/../../../vendor/autoload.php'];
+$possibleAutoloadPaths = [
+    __DIR__ . '/../vendor/autoload.php', // composer require ... | repository
+    __DIR__ . '/../../../vendor/autoload.php' // composer create-project
+];
 
 foreach ($possibleAutoloadPaths as $possibleAutoloadPath) {
     if (is_file($possibleAutoloadPath)) {
