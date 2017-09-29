22fa1089424fd2c30691186b3228628912a4d945
TomasVotruba <tomas.vot@gmail.com>
2017-09-29 17:54:55 +0200

    MagicMethodRector: use old @method detection from Nette\Object

--- src/Rector/Contrib/Nette/Utils/MagicMethodRector.php
+++ src/Rector/Contrib/Nette/Utils/MagicMethodRector.php
@@ -2,29 +2,146 @@
 
 namespace Rector\Rector\Contrib\Nette\Utils;
 
+use PhpParser\Comment\Doc;
 use PhpParser\Node;
+use PhpParser\Node\Stmt\Class_;
+use Rector\Builder\MethodBuilder;
+use Rector\Node\Attribute;
 use Rector\Rector\AbstractRector;
 
 /**
+ * Catches @method annotations at childs of Nette\Object
+ *
  * Covers @see https://github.com/RectorPHP/Rector/issues/49
+ *
+ * This would be nice test: https://github.com/Kdyby/Redis/blob/cbe29eef207ded876f41a40281e1fa746b3a2330/src/Kdyby/Redis/RedisClient.php#L29
  */
 final class MagicMethodRector extends AbstractRector
 {
-    public function isCandidate(Node $node): bool
+    /**
+     * @var string
+     */
+    private const MAGIC_METHODS_PATTERN = '~^
+			[ \t*]*  @method  [ \t]+
+			(?: [^\s(]+  [ \t]+ )?
+			(set|get|is|add)  ([A-Z]\w*)  [ \t]*
+			(?: \(  [ \t]* ([^)$\s]+)  )?
+		()~mx';
+
+    /**
+     * @var mixed[]
+     */
+    private $magicMethods = [];
+
+    /**
+     * @var MethodBuilder
+     */
+    private $methodBuilder;
+
+    public function __construct(MethodBuilder $methodBuilder)
     {
-        // class extending nette object
-        // or using smart trait
-        dump($node);
-        die;
-        // TODO: Implement isCandidate() method.
+        $this->methodBuilder = $methodBuilder;
     }
 
+    public function isCandidate(Node $node): bool
+    {
+        $this->magicMethods = [];
+
+        if (! $node instanceof Class_) {
+            return false;
+        }
+
+        if (! $this->isNetteObjectChild($node)) {
+            return false;
+        }
+
+        $docComments = $node->getAttribute('comments');
+        if ($docComments === null) {
+            return false;
+        }
+
+        /** @var Doc $docComment */
+        $docComment = $docComments[0];
+
+        // @todo consider NamespaceResolver NodeTraverser
+        $currentNamespace = $node->namespacedName->slice(0, -1)
+            ->toString();
+
+        $this->magicMethods = $this->matchMagicMethodsInDocComment($currentNamespace, $docComment->getText());
+
+        return (bool) count($this->magicMethods);
+    }
+
+    /**
+     * @param Class_ $node
+     */
     public function refactor(Node $node): ?Node
     {
-        // TODO: Implement refactor() method.
+        foreach ($this->magicMethods as $methodName => $methodSettings) {
+            $this->methodBuilder->addMethodToClass(
+                $node,
+                $methodName,
+                $methodSettings['propertyType'],
+                $methodSettings['propertyName']
+            );
+        }
 
-        // read annotation
-        // create public method
-        // add it
+        return $node;
+    }
+
+    private function isNetteObjectChild(Class_ $classNode): bool
+    {
+        if ($classNode->extends === null) {
+            return false;
+        }
+
+        $parentClassName = (string) $classNode->extends->getAttribute(Attribute::RESOLVED_NAME);
+
+        return $parentClassName === 'Nette\Object';
+    }
+
+    /**
+     * Mimics https://github.com/nette/utils/blob/v2.3/src/Utils/ObjectMixin.php#L285
+     * only without reflection.
+     *
+     * @return mixed[]
+     */
+    private function matchMagicMethodsInDocComment(string $currentNamespace, string $text): array
+    {
+        preg_match_all(self::MAGIC_METHODS_PATTERN, $text, $matches, PREG_SET_ORDER);
+
+        $methods = [];
+
+        foreach ($matches as $match) {
+            [, $op, $prop, $type] = $match;
+
+            $name = $op . $prop;
+            $prop = strtolower($prop[0]) . substr($prop, 1) . ($op === 'add' ? 's' : '');
+            // @todo: file aware BetterReflection? - FileLocator
+            // use CurrentFileProvider? load in ProcessCommand, enable here
+//            if ($rc->hasProperty($prop) && ($rp = $rc->getProperty($prop)) && !$rp->isStatic()) {
+//                $rp->setAccessible(TRUE);
+            if ($op === 'get' || $op === 'is') {
+                $type = null;
+                $op = 'get';
+            } elseif (                ! $type
+                && preg_match('#@var[ \t]+(\S+)' . ($op === 'add' ? '\[\]#' : '#'), $rp->getDocComment(), $match)
+            ) {
+                $type = $match[1];
+            }
+
+            if ($type && $currentNamespace && preg_match('#^[A-Z]\w+(\[|\||\z)#', $type)) {
+                $type = $currentNamespace . '\\' . $type;
+            }
+
+            $methods[$name] = [
+                    'propertyType' => $type,
+                    'propertyName' => $prop,
+                ];
+        }
+
+//        }
+
+        return $methods;
     }
 }
