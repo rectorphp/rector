ddd03ebcdac73b237cbc7669cd69d9f8d5039b48
TomasVotruba <tomas.vot@gmail.com>
2017-09-30 15:25:04 +0200

    BetterReflection: add support for file locator

--- packages/BetterReflection/src/SourceLocator/SourceLocatorFactory.php
+++ packages/BetterReflection/src/SourceLocator/SourceLocatorFactory.php
@@ -8,7 +8,9 @@ use Roave\BetterReflection\SourceLocator\Type\AutoloadSourceLocator;
 use Roave\BetterReflection\SourceLocator\Type\EvaledCodeSourceLocator;
 use Roave\BetterReflection\SourceLocator\Type\MemoizingSourceLocator;
 use Roave\BetterReflection\SourceLocator\Type\PhpInternalSourceLocator;
+use Roave\BetterReflection\SourceLocator\Type\SingleFileSourceLocator;
 use Roave\BetterReflection\SourceLocator\Type\SourceLocator;
+use SplFileInfo;
 
 final class SourceLocatorFactory
 {
@@ -24,10 +26,39 @@ final class SourceLocatorFactory
 
     public function create(): SourceLocator
     {
-        return new MemoizingSourceLocator(new AggregateSourceLocator([
+        return $this->wrapInMemoizinhSourceLocator($this->createCommonLocators());
+    }
+
+    public function createWithFile(SplFileInfo $fileInfo): SourceLocator
+    {
+        return $this->wrapInMemoizinhSourceLocator(
+            $this->createCommonLocators() +
+            [$this->createFileSourceLocator($fileInfo)]
+        );
+    }
+
+    private function createFileSourceLocator(SplFileInfo $fileInfo): SingleFileSourceLocator
+    {
+        return new SingleFileSourceLocator($fileInfo->getRealPath(), $this->locator);
+    }
+
+    /**
+     * @return SourceLocator[]
+     */
+    private function createCommonLocators(): array
+    {
+        return [
             new PhpInternalSourceLocator($this->locator),
             new EvaledCodeSourceLocator($this->locator),
             new AutoloadSourceLocator($this->locator),
-        ]));
+        ];
+    }
+
+    /**
+     * @param SourceLocator[] $sourceLocators
+     */
+    private function wrapInMemoizinhSourceLocator(array $sourceLocators): MemoizingSourceLocator
+    {
+        return new MemoizingSourceLocator(new AggregateSourceLocator($sourceLocators));
     }
 }
