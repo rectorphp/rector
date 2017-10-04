525f7ed9fb3799cc856d8be92bfb5c778c99c6ed
TomasVotruba <tomas.vot@gmail.com>
2017-10-04 19:05:23 +0200

    SetEntityToStatementRector finish [closes #50]

--- src/Rector/Contrib/Nette/DI/SetEntityToStatementRector.php
+++ src/Rector/Contrib/Nette/DI/SetEntityToStatementRector.php
@@ -3,8 +3,13 @@
 namespace Rector\Rector\Contrib\Nette\DI;
 
 use PhpParser\Node;
+use PhpParser\Node\Expr\Assign;
+use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Expr\New_;
+use PhpParser\Node\Expr\PropertyFetch;
+use PhpParser\Node\Identifier;
+use Rector\Node\Attribute;
 use Rector\NodeAnalyzer\MethodCallAnalyzer;
-use Rector\NodeFactory\NodeFactory;
 use Rector\Rector\AbstractRector;
 
 /**
@@ -18,24 +23,46 @@ use Rector\Rector\AbstractRector;
  */
 final class SetEntityToStatementRector extends AbstractRector
 {
+    /**
+     * @var MethodCallAnalyzer
+     */
+    private $methodCallAnalyzer;
+
+    public function __construct(MethodCallAnalyzer $methodCallAnalyzer)
+    {
+        $this->methodCallAnalyzer = $methodCallAnalyzer;
+    }
+
     public function isCandidate(Node $node): bool
     {
-        // ? is in class extending Nette\DI\CompilerExtension
-        // ...
+        $parentClassName = $node->getAttribute(Attribute::PARENT_CLASS_NAME);
+        if ($parentClassName !== 'Nette\DI\CompilerExtension') {
+            return false;
+        }
 
-        // is Definition?
-        // setEntity...
+        if (! $this->methodCallAnalyzer->isMethodCallMethod($node, 'setEntity')) {
+            return false;
+        }
+
+        return true;
     }
 
     /**
-     * @param Node\Expr\MethodCall $node
+     * @param MethodCall $methodCallNode
+     *
+     * Returns $variable = new Nette\DI\Statement($oldArg, $variable->arguments);
      */
-    public function refactor(Node $node): ?Node
+    public function refactor(Node $methodCallNode): ?Node
     {
-        // metdhod call to variable assign
-        $node->name->name = $this->newMethod;
-        $node->args = $this->nodeFactory->createArgs($this->newArguments);
-
-        return $node;
+        return new Assign(
+            $methodCallNode->var,
+            new New_(
+                new Identifier('Nette\DI\Statement'),
+                [
+                    $methodCallNode->args[0],
+                    new PropertyFetch($methodCallNode->var, 'arguments'),
+                ]
+            )
+        );
     }
 }
