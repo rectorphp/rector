0b60a9333172450beb8a148bdafcced792406ca1
TomasVotruba <tomas.vot@gmail.com>
2017-10-05 12:40:19 +0200

    ClassReplacerRector init

---
+++ src/Rector/ClassReplacerRector.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace Rector\Rector;
+
+final class ClassReplacerRector extends AbstractClassReplacerRector
+{
+    /**
+     * @var string[]
+     */
+    private $oldToNewClasses = [];
+
+    /**
+     * @param string[] $oldToNewClasses
+     */
+    public function __construct(array $oldToNewClasses)
+    {
+        $this->oldToNewClasses = $oldToNewClasses;
+    }
+
+    /**
+     * @return string[]
+     */
+    protected function getOldToNewClasses(): array
+    {
+        return $this->oldToNewClasses;
+    }
+}
