c80f831e4a3d70c54442ea50c73ca0a937c3f810
TomasVotruba <tomas.vot@gmail.com>
2017-10-06 09:30:34 +0200

    test UseStatement class rename duplication

--- src/Rector/Dynamic/ClassReplacerRector.php
+++ src/Rector/Dynamic/ClassReplacerRector.php
@@ -5,11 +5,15 @@ namespace Rector\Rector\Dynamic;
 use PhpParser\Node;
 use PhpParser\Node\Name;
 use PhpParser\Node\Name\FullyQualified;
+use PhpParser\Node\Stmt\Nop;
+use PhpParser\Node\Stmt\Use_;
+use PhpParser\Node\Stmt\UseUse;
+use Rector\Node\Attribute;
+use Rector\NodeTypeResolver\UseStatements;
 use Rector\Rector\AbstractRector;
 
 final class ClassReplacerRector extends AbstractRector
 {
-
     /**
      * @var string[]
      */
@@ -25,30 +29,61 @@ final class ClassReplacerRector extends AbstractRector
 
     public function isCandidate(Node $node): bool
     {
-        if (! $node instanceof Name) {
+        if (! $node instanceof Name && ! $node instanceof Use_) {
             return false;
         }
 
-        $fqnName = $node->toString();
+        $name = $this->resolveNameFromNode($node);
 
-        return isset($this->oldToNewClasses[$fqnName]);
+        return isset($this->oldToNewClasses[$name]);
     }
 
     /**
-     * @param Name $nameNode
+     * @param Name|UseUse $node
      */
-    public function refactor(Node $nameNode): ?Node
+    public function refactor(Node $node): ?Node
     {
-        $newName = $this->getNewName($nameNode->toString());
+        if ($node instanceof Name) {
+            $newName = $this->getNewName($node->toString());
 
-        // if already present use statement, just null it
-        // ... neturn Nop()
+            return new FullyQualified($newName);
+        }
 
-        return new FullyQualified($newName);
+        if ($node instanceof Use_) {
+            $name = $this->resolveNameFromNode($node);
+            $newName = $this->getNewName($name);
+
+            if ($this->isUseStatmenetAlreadyPresent($node, $newName)) {
+                $this->shouldRemoveNode = true;
+            }
+        }
+
+        return null;
     }
 
     private function getNewName(string $oldName): string
     {
         return $this->oldToNewClasses[$oldName];
+    }
+
+    private function isUseStatmenetAlreadyPresent(Use_ $useNode, string $newName): bool
+    {
+        /** @var UseStatements $useStatments */
+        $useStatments = $useNode->getAttribute(Attribute::USE_STATEMENTS);
+
+        return in_array($newName, $useStatments->getUseStatements(), true);
+    }
+
+    private function resolveNameFromNode(Node $node): string
+    {
+        if ($node instanceof Name) {
+            return $node->toString();
+        }
+
+        if ($node instanceof Use_) {
+            return $node->uses[0]->name->toString();
+        }
+
+        return '';
     }
 }
