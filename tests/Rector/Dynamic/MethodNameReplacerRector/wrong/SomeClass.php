093d64bd52e7291c06160f6bc4fca280ff9292ab
TomasVotruba <tomas.vot@gmail.com>
2017-10-06 14:10:14 +0200

    phpstan cleanups

--- tests/Rector/Dynamic/MethodNameReplacerRector/wrong/SomeClass.php
+++ tests/Rector/Dynamic/MethodNameReplacerRector/wrong/SomeClass.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Rector\Tests\Rector\Contrib\Nette\Utils\HtmlAddMethodRector\Wrong;
+namespace Rector\Tests\Rector\Dynamic\MethodNameReplacerRector\Wrong;
 
 use Nette\Utils\Html;
 
