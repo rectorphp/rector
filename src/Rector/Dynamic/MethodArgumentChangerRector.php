2c71f647f70e1fd8d9d6797503121cac5b42742f
TomasVotruba <tomas.vot@gmail.com>
2017-10-14 20:58:22 +0200

    MethodArgumentChangerRector init

--- src/Rector/Dynamic/MethodArgumentChangerRector.php
+++ src/Rector/Dynamic/MethodArgumentChangerRector.php
@@ -3,7 +3,10 @@
 namespace Rector\Rector\Dynamic;
 
 use PhpParser\Node;
+use PhpParser\Node\Expr\MethodCall;
+use Rector\NodeAnalyzer\MethodCallAnalyzer;
 use Rector\Rector\AbstractRector;
+use Rector\Rector\Dynamic\ArgumentWrapper\MethodChange;
 
 /**
  * @todo collect cases and prepare tests for them
@@ -15,8 +18,48 @@ use Rector\Rector\AbstractRector;
  */
 final class MethodArgumentChangerRector extends AbstractRector
 {
+    /**
+     * @var MethodChange[]
+     */
+    private $methodChanges = [];
+
+    /**
+     * @var MethodCallAnalyzer
+     */
+    private $methodCallAnalyzer;
+
+    /**
+     * @param mixed[] $methodChanges
+     */
+    public function __construct(array $methodChanges, MethodCallAnalyzer $methodCallAnalyzer)
+    {
+        foreach ($methodChanges as $methodChange) {
+            $this->methodChanges[] = MethodChange::createFromMethodChange($methodChange);
+        }
+
+        $this->methodCallAnalyzer = $methodCallAnalyzer;
+    }
+
     public function isCandidate(Node $node): bool
     {
+        if (! $node instanceof MethodCall) {
+            return false;
+        }
+
+        foreach ($this->methodChanges as $methodChange) {
+            if (! $this->methodCallAnalyzer->isMethodCallTypeAndMethod(
+                $node,
+                $methodChange->getClass(),
+                $methodChange->getMethod()
+            )) {
+                continue;
+            }
+        }
+
+        die;
+
+        // is method call...
+
         return true;
     }
 
