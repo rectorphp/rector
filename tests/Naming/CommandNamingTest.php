812d9e50efb050b82fdb84c9b4a4b99531c309e6
TomasVotruba <tomas.vot@gmail.com>
2017-10-16 22:42:00 +0200

    improve coverage

---
+++ tests/Naming/CommandNamingTest.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace Rector\Tests\Naming;
+
+use PHPUnit\Framework\TestCase;
+use Rector\Naming\CommandNaming;
+
+final class CommandNamingTest extends TestCase
+{
+    public function test(): void
+    {
+        $name = CommandNaming::classToName('SomeNameCommand');
+        $this->assertSame('some-name', $name);
+    }
+}
