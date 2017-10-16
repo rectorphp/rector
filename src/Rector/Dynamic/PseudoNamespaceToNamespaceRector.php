14cab79628987ee4b17169b5c73ac5defbc113de
TomasVotruba <tomas.vot@gmail.com>
2017-10-16 18:15:55 +0200

    PseudoNamespaceRector init

---
+++ src/Rector/Dynamic/PseudoNamespaceToNamespaceRector.php
@@ -0,0 +1,60 @@
+<?php declare(strict_types=1);
+
+
+namespace Rector\Rector\Dynamic;
+
+use Nette\Utils\Strings;
+use PhpParser\Node;
+use PhpParser\Node\Name;
+use Rector\Rector\AbstractRector;
+
+/**
+ * Basically inversion of https://github.com/nikic/PHP-Parser/blob/master/doc/2_Usage_of_basic_components.markdown#example-converting-namespaced-code-to-pseudo-namespaces
+ *
+ *
+ * Requested on SO: https://stackoverflow.com/questions/29014957/converting-pseudo-namespaced-classes-to-use-real-namespace
+ *
+ * @todo might handle current @see phpunit60.yml @see \Rector\Rector\Dynamic\NamespaceReplacerRector
+ */
+final class PseudoNamespaceToNamespaceRector extends AbstractRector
+{
+    /**
+     * @var string[]
+     */
+    private $pseudoNamespacePrefixes = [];
+
+    public function __construct(array $pseudoNamespacePrefixes)
+    {
+        $this->pseudoNamespacePrefixes = $pseudoNamespacePrefixes;
+    }
+
+    public function isCandidate(Node $node): bool
+    {
+        if (! $node instanceof Name) {
+            return false;
+        }
+
+        foreach ($this->pseudoNamespacePrefixes as $pseudoNamespacePrefix) {
+            if (Strings::startsWith($node->toString(), $pseudoNamespacePrefix)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @param Name $nameNode
+     * @return null|Node
+     */
+    public function refactor(Node $nameNode): ?Node
+    {
+        $oldName = $nameNode->toString();
+        $newName = str_replace('_', '\\', $oldName);
+        $newNameParts = explode('\\', $newName);
+
+        $nameNode->parts = $newNameParts;
+
+        return $nameNode;
+    }
+}
---
+++ tests/Rector/Dynamic/PseudoNamespaceToNamespaceRector/Test.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace Rector\Tests\Rector\Dynamic\PseudoNamespaceToNamespaceRector;
+
+use Rector\Rector\Dynamic\PseudoNamespaceToNamespaceRector;
+use Rector\Testing\PHPUnit\AbstractConfigurableRectorTestCase;
+
+final class Test extends AbstractConfigurableRectorTestCase
+{
+    public function test(): void
+    {
+        $this->doTestFileMatchesExpectedContent(
+            __DIR__ . '/wrong/wrong.php.inc',
+            __DIR__ . '/correct/correct.php.inc'
+        );
+    }
+
+    protected function provideConfig(): string
+    {
+        return __DIR__ . '/config/rector.yml';
+    }
+
+    /**
+     * @return string[]
+     */
+    protected function getRectorClasses(): array
+    {
+        return [PseudoNamespaceToNamespaceRector::class];
+    }
+}
