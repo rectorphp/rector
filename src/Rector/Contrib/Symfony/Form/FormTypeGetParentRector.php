bfc54761244327290b3a97925d3d2baa4ae8bc3b
TomasVotruba <tomas.vot@gmail.com>
2017-10-21 08:59:41 +0200

    [Symfony] decoupel FormTypeStringToTypeProvider

--- src/Rector/Contrib/Symfony/Form/FormTypeGetParentRector.php
+++ src/Rector/Contrib/Symfony/Form/FormTypeGetParentRector.php
@@ -7,6 +7,7 @@ use PhpParser\Node\Scalar\String_;
 use Rector\Node\Attribute;
 use Rector\Node\NodeFactory;
 use Rector\Rector\AbstractRector;
+use Rector\Rector\Contrib\Symfony\Form\Helper\FormTypeStringToTypeProvider;
 
 /**
  * Converts all:
@@ -28,50 +29,19 @@ use Rector\Rector\AbstractRector;
 final class FormTypeGetParentRector extends AbstractRector
 {
     /**
-     * @var string[]
-     */
-    private $nameToClassMap = [
-        'form' => 'Symfony\Component\Form\Extension\Core\Type\FormType',
-        'birthday' => 'Symfony\Component\Form\Extension\Core\Type\BirthdayType',
-        'checkbox' => 'Symfony\Component\Form\Extension\Core\Type\CheckboxType',
-        'collection' => 'Symfony\Component\Form\Extension\Core\Type\CollectionType',
-        'country' => 'Symfony\Component\Form\Extension\Core\Type\CountryType',
-        'currency' => 'Symfony\Component\Form\Extension\Core\Type\CurrencyType',
-        'date' => 'Symfony\Component\Form\Extension\Core\Type\DateType',
-        'datetime' => 'Symfony\Component\Form\Extension\Core\Type\DatetimeType',
-        'email' => 'Symfony\Component\Form\Extension\Core\Type\EmailType',
-        'file' => 'Symfony\Component\Form\Extension\Core\Type\FileType',
-        'hidden' => 'Symfony\Component\Form\Extension\Core\Type\HiddenType',
-        'integer' => 'Symfony\Component\Form\Extension\Core\Type\IntegerType',
-        'language' => 'Symfony\Component\Form\Extension\Core\Type\LanguageType',
-        'locale' => 'Symfony\Component\Form\Extension\Core\Type\LocaleType',
-        'money' => 'Symfony\Component\Form\Extension\Core\Type\MoneyType',
-        'number' => 'Symfony\Component\Form\Extension\Core\Type\NumberType',
-        'password' => 'Symfony\Component\Form\Extension\Core\Type\PasswordType',
-        'percent' => 'Symfony\Component\Form\Extension\Core\Type\PercentType',
-        'radio' => 'Symfony\Component\Form\Extension\Core\Type\RadioType',
-        'range' => 'Symfony\Component\Form\Extension\Core\Type\RangeType',
-        'repeated' => 'Symfony\Component\Form\Extension\Core\Type\RepeatedType',
-        'search' => 'Symfony\Component\Form\Extension\Core\Type\SearchType',
-        'textarea' => 'Symfony\Component\Form\Extension\Core\Type\TextareaType',
-        'text' => 'Symfony\Component\Form\Extension\Core\Type\TextType',
-        'time' => 'Symfony\Component\Form\Extension\Core\Type\TimeType',
-        'timezone' => 'Symfony\Component\Form\Extension\Core\Type\TimezoneType',
-        'url' => 'Symfony\Component\Form\Extension\Core\Type\UrlType',
-        'button' => 'Symfony\Component\Form\Extension\Core\Type\ButtonType',
-        'submit' => 'Symfony\Component\Form\Extension\Core\Type\SubmitType',
-        'reset' => 'Symfony\Component\Form\Extension\Core\Type\ResetType',
-        'entity' => 'Symfony\Bridge\Doctrine\Form\Type\EntityType',
-    ];
-
-    /**
      * @var NodeFactory
      */
     private $nodeFactory;
 
-    public function __construct(NodeFactory $nodeFactory)
+    /**
+     * @var FormTypeStringToTypeProvider
+     */
+    private $formTypeStringToTypeProvider;
+
+    public function __construct(NodeFactory $nodeFactory, FormTypeStringToTypeProvider $formTypeStringToTypeProvider)
     {
         $this->nodeFactory = $nodeFactory;
+        $this->formTypeStringToTypeProvider = $formTypeStringToTypeProvider;
     }
 
     public function isCandidate(Node $node): bool
@@ -80,7 +50,7 @@ final class FormTypeGetParentRector extends AbstractRector
             return false;
         }
 
-        if (! isset($this->nameToClassMap[$node->value])) {
+        if (! $this->formTypeStringToTypeProvider->hasClassForName($node->value)) {
             return false;
         }
 
@@ -100,7 +70,7 @@ final class FormTypeGetParentRector extends AbstractRector
      */
     public function refactor(Node $stringNode): ?Node
     {
-        $class = $this->nameToClassMap[$stringNode->value];
+        $class = $this->formTypeStringToTypeProvider->getClassForName($stringNode->value);
 
         return $this->nodeFactory->createClassConstantReference($class);
     }
