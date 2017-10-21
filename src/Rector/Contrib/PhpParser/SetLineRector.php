8e82b9ca9d5a67b1d70cf380c43dbea88ff87ba9
TomasVotruba <tomas.vot@gmail.com>
2017-10-22 01:10:53 +0200

    add SetLineRector

---
+++ src/Rector/Contrib/PhpParser/SetLineRector.php
@@ -0,0 +1,48 @@
+<?php declare(strict_types=1);
+
+namespace Rector\Rector\Contrib\PhpParser;
+
+use PhpParser\Node;
+use PhpParser\Node\Arg;
+use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Scalar\String_;
+use Rector\NodeAnalyzer\MethodCallAnalyzer;
+use Rector\Rector\AbstractRector;
+
+/**
+ * Before:
+ * - $node->setLine(5);
+ *
+ * After:
+ * - $node->setAttribute('line', 5);
+ */
+final class SetLineRector extends AbstractRector
+{
+    /**
+     * @var MethodCallAnalyzer
+     */
+    private $methodCallAnalyzer;
+
+    public function __construct(MethodCallAnalyzer $methodCallAnalyzer)
+    {
+        $this->methodCallAnalyzer = $methodCallAnalyzer;
+    }
+
+    public function isCandidate(Node $node): bool
+    {
+        return $this->methodCallAnalyzer->isTypeAndMethod($node, 'PhpParser\Node', 'setLine');
+    }
+
+    /**
+     * @param MethodCall $methodCallNode
+     */
+    public function refactor(Node $methodCallNode): ?Node
+    {
+        $methodCallNode->name = 'setAttribute';
+
+        $methodCallNode->args[1] = $methodCallNode->args[0];
+        $methodCallNode->args[0] = new Arg(new String_('line'));
+
+        return $methodCallNode;
+    }
+}
