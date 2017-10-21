14a156894d129a64b11987e55f346992cf0b1c34
TomasVotruba <tomas.vot@gmail.com>
2017-10-22 00:54:51 +0200

    fix ParamAndStaticVarNameRector

---
+++ src/Rector/Contrib/PhpParser/ParamAndStaticVarNameRector.php
@@ -0,0 +1,60 @@
+<?php declare(strict_types=1);
+
+namespace Rector\Rector\Contrib\PhpParser;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\PropertyFetch;
+use Rector\NodeAnalyzer\PropertyFetchAnalyzer;
+use Rector\Rector\AbstractRector;
+
+/**
+ * Before:
+ * - $paramNode->name
+ * - $staticVarNode->name
+ *
+ * After:
+ * - $paramNode->var->name
+ * - $staticVarNode->var->name
+ */
+final class ParamAndStaticVarNameRector extends AbstractRector
+{
+    /**
+     * @var PropertyFetchAnalyzer
+     */
+    private $propertyFetchAnalyzer;
+
+    /**
+     * @param string[][]
+     */
+    public function __construct(PropertyFetchAnalyzer $propertyFetchAnalyzer)
+    {
+        $this->propertyFetchAnalyzer = $propertyFetchAnalyzer;
+    }
+
+    public function isCandidate(Node $node): bool
+    {
+        $types = [
+            'PhpParser\Node\Param',
+            'PhpParser\Node\Stmt\StaticVar',
+        ];
+
+        if (! $this->propertyFetchAnalyzer->isTypes($node, $types)) {
+            return false;
+        }
+
+        /** @var PropertyFetch $node */
+        $propertyName = $node->name->name;
+
+        return $propertyName === 'name';
+    }
+
+    /**
+     * @param PropertyFetch $propertyFetchNode
+     */
+    public function refactor(Node $propertyFetchNode): ?Node
+    {
+        $propertyFetchNode->name->name = 'var';
+
+        return new PropertyFetch($propertyFetchNode, 'name');
+    }
+}
---
+++ tests/Rector/Contrib/PhpParser/ParamAndStaticVarNameRector/Test.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+namespace Rector\Tests\Rector\Contrib\PhpParser\ParamAndStaticVarNameRector;
+
+use Rector\Rector\Contrib\PhpParser\ParamAndStaticVarNameRector;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+
+final class Test extends AbstractRectorTestCase
+{
+    public function test(): void
+    {
+        $this->doTestFileMatchesExpectedContent(
+            __DIR__ . '/wrong/wrong.php.inc',
+            __DIR__ . '/correct/correct.php.inc'
+        );
+    }
+
+    /**
+     * @return string[]
+     */
+    protected function getRectorClasses(): array
+    {
+        return [ParamAndStaticVarNameRector::class];
+    }
+}
