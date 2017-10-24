f3381b015b29d5d26273f1ad7cbb2b87a77248fa
TomasVotruba <tomas.vot@gmail.com>
2017-10-24 22:26:13 +0200

    fix

--- packages/NodeTypeResolver/src/PerNodeTypeResolver/ParamTypeResolver.php
+++ packages/NodeTypeResolver/src/PerNodeTypeResolver/ParamTypeResolver.php
@@ -64,6 +64,9 @@ final class ParamTypeResolver implements PerNodeTypeResolverInterface, NodeTypeR
 
         // resolve param type from docblock
         $paramType = $this->docBlockAnalyzer->getParamTypeFor($classMethod, $variableName);
+        if ($paramType === null) {
+            return null;
+        }
 
         // resolve to FQN
         $paramType = $this->resolveTypeWithNamespaceAndUseStatments(
