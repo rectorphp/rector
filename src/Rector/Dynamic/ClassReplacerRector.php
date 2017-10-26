2ee2049cf18f7919678828d2e033d4ceefdcaae3
TomasVotruba <tomas.vot@gmail.com>
2017-10-26 12:42:56 +0200

    ClassReplacerRector - respect use statments import

--- src/Rector/Dynamic/ClassReplacerRector.php
+++ src/Rector/Dynamic/ClassReplacerRector.php
@@ -38,9 +38,9 @@ final class ClassReplacerRector extends AbstractRector
             return false;
         }
 
-        $name = $this->resolveNameFromNode($node);
+        $nameNode = $this->resolveNameNodeFromNode($node);
 
-        return isset($this->oldToNewClasses[$name]);
+        return isset($this->oldToNewClasses[$nameNode->toString()]);
     }
 
     /**
@@ -50,8 +50,9 @@ final class ClassReplacerRector extends AbstractRector
     {
         if ($node instanceof Name) {
             $newName = $this->resolveNewNameFromNode($node);
+            $newNameNode = new Name($newName);
 
-            return new FullyQualified($newName);
+            return new Name($newNameNode->getLast());
         }
 
         if ($node instanceof Use_) {
@@ -60,6 +61,10 @@ final class ClassReplacerRector extends AbstractRector
             if ($this->namespaceAnalyzer->isUseStatmenetAlreadyPresent($node, $newName)) {
                 $this->shouldRemoveNode = true;
             }
+
+            $node->uses[0]->name = new Name($newName);
+
+            return $node;
         }
 
         return null;
@@ -67,27 +72,27 @@ final class ClassReplacerRector extends AbstractRector
 
     private function resolveNewNameFromNode(Node $node): string
     {
-        $name = $this->resolveNameFromNode($node);
+        $nameNode = $this->resolveNameNodeFromNode($node);
 
-        return $this->oldToNewClasses[$name];
+        return $this->oldToNewClasses[$nameNode->toString()];
     }
 
-    private function resolveNameFromNode(Node $node): string
+    private function resolveNameNodeFromNode(Node $node): ?Name
     {
         if ($node instanceof Name) {
             // resolved name has priority, as it is FQN
             $resolvedName = $node->getAttribute(Attribute::RESOLVED_NAME);
             if ($resolvedName instanceof FullyQualified) {
-                return $resolvedName->toString();
+                return $resolvedName;
             }
 
-            return $node->toString();
+            return $node;
         }
 
         if ($node instanceof Use_) {
-            return $node->uses[0]->name->toString();
+            return $node->uses[0]->name;
         }
 
-        return '';
+        return null;
     }
 }
