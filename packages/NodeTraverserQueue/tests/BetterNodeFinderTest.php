5d1b918014978813fc1fc8336470ba3fc3b196bb
TomasVotruba <tomas.vot@gmail.com>
2017-10-26 20:35:57 +0200

    [NodeTraverserQueue] add BetterNodeFinder tests

---
+++ packages/NodeTraverserQueue/tests/BetterNodeFinderTest.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types=1);
+
+namespace Rector\NodeTraverserQueue;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\Array_;
+use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Stmt\Class_;
+use PhpParser\Node\Stmt\ClassLike;
+use Rector\NodeTypeResolver\Tests\AbstractNodeTypeResolverTest;
+
+final class BetterNodeFinderTest extends AbstractNodeTypeResolverTest
+{
+    /**
+     * @var Node[]
+     */
+    private $nodes = [];
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->nodes = $this->getNodesForFile(__DIR__ . '/BetterNodeFinderSource/SomeFile.php.inc');
+    }
+
+    public function testFindFirstAncestorInstanceOf(): void
+    {
+        $variableNode = $this->betterNodeFinder->findFirstInstanceOf($this->nodes, Variable::class);
+        $classNode = $this->betterNodeFinder->findFirstInstanceOf($this->nodes, Class_::class);
+
+        $classLikeNode = $this->betterNodeFinder->findFirstAncestorInstanceOf($variableNode,ClassLike::class);
+        $this->assertSame($classLikeNode, $classNode);
+    }
+
+    public function testFindMissingFirstAncestorInstanceOf(): void
+    {
+        $variableNode = $this->betterNodeFinder->findFirstInstanceOf($this->nodes, Variable::class);
+
+        $this->assertNull($this->betterNodeFinder->findFirstAncestorInstanceOf($variableNode,Array_::class));
+    }
+}
