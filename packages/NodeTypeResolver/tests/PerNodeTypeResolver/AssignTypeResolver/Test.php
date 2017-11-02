db0a959e5fdd0740e5ad82feb1b6791cdeeeca74
TomasVotruba <tomas.vot@gmail.com>
2017-11-02 14:18:26 +0100

    improve AssingTypeResolver test

--- packages/NodeTypeResolver/tests/PerNodeTypeResolver/AssignTypeResolver/Test.php
+++ packages/NodeTypeResolver/tests/PerNodeTypeResolver/AssignTypeResolver/Test.php
@@ -23,13 +23,28 @@ final class Test extends AbstractNodeTypeResolverTest
         );
     }
 
-    public function testMethodCall(): void
+    public function testNewTwo(): void
     {
-        $variableNodes = $this->getNodesForFileOfType(__DIR__ . '/Source/MethodCall.php.inc', Variable::class);
+        $variableNodes = $this->getNodesForFileOfType(__DIR__ . '/Source/New.php.inc', Variable::class);
 
         $this->assertSame(
-            ['Nette\DI\Container'],
-            $variableNodes[1]->getAttribute(Attribute::TYPES)
+            'Symfony\Component\DependencyInjection\ContainerBuilder',
+            $variableNodes[0]->getAttribute(Attribute::TYPES)[0]
+        );
+
+        $this->assertSame(
+            'Symfony\Component\DependencyInjection\ContainerBuilder',
+            $variableNodes[1]->getAttribute(Attribute::TYPES)[0]
         );
     }
+
+//    public function testMethodCall(): void
+//    {
+//        $variableNodes = $this->getNodesForFileOfType(__DIR__ . '/Source/MethodCall.php.inc', Variable::class);
+//
+//        $this->assertSame(
+//            ['Nette\DI\Container'],
+//            $variableNodes[1]->getAttribute(Attribute::TYPES)
+//        );
+//    }
 }
