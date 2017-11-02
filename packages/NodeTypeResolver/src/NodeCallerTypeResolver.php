683bf8331720fe76ac6ae266a13c4b84307fc5ec
TomasVotruba <tomas.vot@gmail.com>
2017-11-02 13:07:19 +0100

    drop unused code, fix cs

--- packages/NodeTypeResolver/src/NodeCallerTypeResolver.php
+++ packages/NodeTypeResolver/src/NodeCallerTypeResolver.php
@@ -4,13 +4,11 @@ namespace Rector\NodeTypeResolver;
 
 use PhpParser\Node;
 use PhpParser\Node\Expr\MethodCall;
-use PhpParser\Node\Expr\PropertyFetch;
 use PhpParser\Node\Expr\StaticCall;
 use PhpParser\Node\Expr\Variable;
 use PhpParser\Node\Name;
 use Rector\BetterReflection\Reflector\MethodReflector;
 use Rector\Node\Attribute;
-use Rector\NodeTraverserQueue\BetterNodeFinder;
 
 /**
  * This will tell the type of Node, which is calling this method
@@ -23,18 +21,12 @@ use Rector\NodeTraverserQueue\BetterNodeFinder;
 final class NodeCallerTypeResolver
 {
     /**
-     * @var BetterNodeFinder
-     */
-    private $betterNodeFinder;
-
-    /**
      * @var MethodReflector
      */
     private $methodReflector;
 
-    public function __construct(BetterNodeFinder $betterNodeFinder, MethodReflector $methodReflector)
+    public function __construct(MethodReflector $methodReflector)
     {
-        $this->betterNodeFinder = $betterNodeFinder;
         $this->methodReflector = $methodReflector;
     }
 
@@ -88,9 +80,14 @@ final class NodeCallerTypeResolver
         }
 
         $callerNodeTypes = $node->var->getAttribute(Attribute::TYPES);
-        $callerNodeType = $callerNodeTypes[0] ?? [];
+        $callerNodeType = $callerNodeTypes[0] ?? '';
 
         $methodName = $node->name->toString();
+
+        if (! $callerNodeType || ! $methodName) {
+            return [];
+        }
+
         $callerReturnType = $this->methodReflector->getMethodReturnType($callerNodeType, $methodName);
         if ($callerReturnType) {
             if ($callerReturnType === 'self') {
@@ -101,48 +98,5 @@ final class NodeCallerTypeResolver
         }
 
         return [];
-    }
-
-    /**
-     * @return string[]
-     */
-    private function processMethodCallNode(MethodCall $methodCallNode): array
-    {
-        // @todo extract to own method
-        $callerNode = $this->betterNodeFinder->findFirst(
-            $methodCallNode,
-            function (Node $node) use ($methodCallNode) {
-                if ($node instanceof Variable || $node instanceof PropertyFetch) {
-                    return $node;
-                }
-
-                if ($node instanceof MethodCall) {
-                    $returnTypes = $this->resolverMethodCallReturnTypes($node);
-                    if ($returnTypes) {
-                        $node->setAttribute(Attribute::CALLER_TYPES, $returnTypes);
-                    }
-
-                    return $node;
-                    // 2 options:
-                    // 1. self - fluent interface, first variable
-                    // 2. another object
-                }
-
-                if ($node === $methodCallNode) {
-                    return null;
-                }
-            }
-        );
-
-        if ($callerNode === null) {
-            return [];
-        }
-
-        $nodeTypes = (array) $callerNode->getAttribute(Attribute::TYPES);
-        if ($nodeTypes) {
-            return $nodeTypes;
-        }
-
-        return (array) $callerNode->getAttribute(Attribute::CALLER_TYPES);
     }
 }
