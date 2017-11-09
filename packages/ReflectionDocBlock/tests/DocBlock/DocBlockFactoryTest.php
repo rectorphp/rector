06e72d5c8e96453bf4942fd6be2965c55c6d129c
TomasVotruba <tomas.vot@gmail.com>
2017-11-09 14:53:38 +0100

    [ReflecitonDocBlock] remove unused annotations, to prevent exceptions due to hardcoded mailforms

--- packages/ReflectionDocBlock/tests/DocBlock/DocBlockFactoryTest.php
+++ packages/ReflectionDocBlock/tests/DocBlock/DocBlockFactoryTest.php
@@ -2,7 +2,6 @@
 
 namespace Rector\ReflectionDocBlock\Tests\DocBlock;
 
-use InvalidArgumentException;
 use phpDocumentor\Reflection\DocBlock;
 use PhpParser\Node\Scalar\String_;
 use Rector\Parser\Parser;
@@ -39,8 +38,7 @@ final class DocBlockFactoryTest extends AbstractContainerAwareTestCase
         $nodes = $this->parser->parseFile(__DIR__ . '/DocBlockFactorySource/SomeClassWithAuthor.php.inc');
         $classNode = $nodes[1];
 
-        // should be ok
-        $this->expectException(InvalidArgumentException::class);
-        $this->docBlockFactory->createFromNode($classNode);
+        $docBlock = $this->docBlockFactory->createFromNode($classNode);
+        $this->assertInstanceOf(DocBlock::class, $docBlock);
     }
 }
