7c64f4a5c9305804d9426872874e7fbaf1ef395d
TomasVotruba <tomas.vot@gmail.com>
2017-12-02 20:14:24 +0100

    fix namespace

--- packages/YamlParser/tests/Rector/Contrib/Symfony/NamedServiceToClassRector/NamedServiceToClassRectorTest.php
+++ packages/YamlParser/tests/Rector/Contrib/Symfony/NamedServiceToClassRector/NamedServiceToClassRectorTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Rector\YamlParser\Tests\Rector\Contrib\Symfony;
+namespace Rector\YamlParser\Tests\Rector\Contrib\Symfony\NamedServiceToClassRector;
 
 use Rector\YamlParser\Testing\PHPUnit\AbstractConfigurableYamlRectorTestCase;
 
