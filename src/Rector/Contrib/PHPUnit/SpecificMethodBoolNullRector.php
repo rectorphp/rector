18173376310b8d5b4cc93ff9cc30f2c14364cc31
TomasVotruba <tomas.vot@gmail.com>
2017-12-18 01:16:36 +0100

    [PHPUnit] SpecificMethodBoolNullRector added

---
+++ src/Rector/Contrib/PHPUnit/SpecificMethodBoolNullRector.php
@@ -0,0 +1,78 @@
+<?php declare(strict_types=1);
+
+namespace Rector\Rector\Contrib\PHPUnit;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\ConstFetch;
+use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Identifier;
+use Rector\NodeAnalyzer\MethodCallAnalyzer;
+use Rector\Rector\AbstractRector;
+
+/**
+ * Before:
+ * - $this->assertSame(null, $anything);
+ *
+ * After:
+ * - $this->assertNull($anything);
+ */
+final class SpecificMethodBoolNullRector extends AbstractRector
+{
+    /**
+     * @var MethodCallAnalyzer
+     */
+    private $methodCallAnalyzer;
+
+    /**
+     * @var string|null
+     */
+    private $activeFuncCallName;
+
+    public function __construct(MethodCallAnalyzer $methodCallAnalyzer)
+    {
+        $this->methodCallAnalyzer = $methodCallAnalyzer;
+    }
+
+    public function isCandidate(Node $node): bool
+    {
+        $this->activeFuncCallName = null;
+
+        if (! $this->methodCallAnalyzer->isTypesAndMethods(
+            $node,
+            ['PHPUnit\Framework\TestCase', 'PHPUnit_Framework_TestCase'],
+            ['assertSame']
+        )) {
+            return false;
+        }
+
+        /** @var MethodCall $methodCallNode */
+        $methodCallNode = $node;
+
+        $firstArgumentValue = $methodCallNode->args[0]->value;
+        if (! $firstArgumentValue instanceof ConstFetch) {
+            return false;
+        }
+
+        $costName = $firstArgumentValue->name->toString();
+        if ($costName === 'null') {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param MethodCall $methodCallNode
+     */
+    public function refactor(Node $methodCallNode): ?Node
+    {
+        $methodCallNode->name = new Identifier('assertNull');
+
+        $methodArguments = $methodCallNode->args;
+        array_shift($methodArguments);
+
+        $methodCallNode->args = $methodCallNode;
+
+        return $methodCallNode;
+    }
+}
