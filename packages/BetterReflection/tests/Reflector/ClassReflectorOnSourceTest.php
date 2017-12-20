544a7fee4b291cb1fa5c5256cfb4daa95a2f7fe9
TomasVotruba <tomas.vot@gmail.com>
2017-12-20 13:54:32 +0100

    test fix

--- packages/BetterReflection/tests/Reflector/ClassReflectorOnSourceTest.php
+++ packages/BetterReflection/tests/Reflector/ClassReflectorOnSourceTest.php
@@ -6,15 +6,9 @@ use Rector\BetterReflection\Reflection\ReflectionClass;
 use Rector\BetterReflection\Reflector\ClassReflector;
 use Rector\BetterReflection\Reflector\ClassReflectorFactory;
 use Rector\Tests\AbstractContainerAwareTestCase;
-use SplFileInfo;
 
 final class ClassReflectorOnSourceTest extends AbstractContainerAwareTestCase
 {
-    /**
-     * @var SplFileInfo
-     */
-    private $currentProcessedFileInfo;
-
     /**
      * @var ClassReflector
      */
@@ -25,20 +19,12 @@ final class ClassReflectorOnSourceTest extends AbstractContainerAwareTestCase
         /** @var ClassReflectorFactory $classReflectorFactory */
         $classReflectorFactory = $this->container->get(ClassReflectorFactory::class);
 
-        $this->currentProcessedFileInfo = new SplFileInfo(__DIR__ . '/NotLoadedSource/SomeClass.php');
-        $this->classReflector = $classReflectorFactory->createWithFile($this->currentProcessedFileInfo);
-    }
-
-    public function test(): void
-    {
-        $this->assertInstanceOf(ClassReflector::class, $this->classReflector);
+        $this->classReflector = $classReflectorFactory->createWithSource([__DIR__ . '/NotLoadedSource/SomeClass.php']);
     }
 
     public function testReflectClassThatIsNotLoaded(): void
     {
-        $className = 'NotLoadedSource\SomeClass';
-
-        $classReflection = $this->classReflector->reflect($className);
+        $classReflection = $this->classReflector->reflect('NotLoadedSource\SomeClass');
 
         $this->assertInstanceOf(ReflectionClass::class, $classReflection);
     }
