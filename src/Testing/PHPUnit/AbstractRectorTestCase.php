bb7225c62534edff0276a7470f01dcebf5558e7e
TomasVotruba <tomas.vot@gmail.com>
2017-12-20 14:43:48 +0100

    fix tests by making paramter provider aware of source

--- src/Testing/PHPUnit/AbstractRectorTestCase.php
+++ src/Testing/PHPUnit/AbstractRectorTestCase.php
@@ -8,6 +8,7 @@ use Rector\Application\FileProcessor;
 use Rector\DependencyInjection\ContainerFactory;
 use Rector\FileSystem\FileGuard;
 use SplFileInfo;
+use Symplify\PackageBuilder\Parameter\ParameterProvider;
 
 abstract class AbstractRectorTestCase extends TestCase
 {
@@ -21,17 +22,24 @@ abstract class AbstractRectorTestCase extends TestCase
      */
     protected $container;
 
+    /**
+     * @var ParameterProvider
+     */
+    protected $parameterProvider;
+
     protected function setUp(): void
     {
         $this->container = (new ContainerFactory())->createWithConfig(__DIR__ . '/../../config/levels.yml');
-
         $this->fileProcessor = $this->container->get(FileProcessor::class);
+        $this->parameterProvider = $this->container->get(ParameterProvider::class);
     }
 
     protected function doTestFileMatchesExpectedContent(string $file, string $reconstructedFile): void
     {
         FileGuard::ensureFileExists($file, get_called_class());
         FileGuard::ensureFileExists($reconstructedFile, get_called_class());
+
+        $this->parameterProvider->changeParameter('source', [$file]);
 
         $reconstructedFileContent = $this->fileProcessor->processFileWithRectorsToString(
             new SplFileInfo($file),
