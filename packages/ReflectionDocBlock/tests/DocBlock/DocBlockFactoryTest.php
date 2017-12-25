9bc11691e7d6bb52e839959cd2d696ca61ee7d11
TomasVotruba <tomas.vot@gmail.com>
2017-12-25 21:57:08 +0100

    simplify test

--- packages/ReflectionDocBlock/tests/DocBlock/DocBlockFactoryTest.php
+++ packages/ReflectionDocBlock/tests/DocBlock/DocBlockFactoryTest.php
@@ -35,16 +35,10 @@ final class DocBlockFactoryTest extends AbstractContainerAwareTestCase
 
     public function testMailformedAnnotations(): void
     {
-        $nodes = $this->parser->parseFile(__DIR__ . '/DocBlockFactorySource/SomeClassWithAuthor.php.inc');
+        $nodes = $this->parser->parseFile(__DIR__ . '/DocBlockFactorySource/SomeClassWithFalsyAnnotations.php.inc');
         $classNode = $nodes[1];
 
         $docBlock = $this->docBlockFactory->createFromNode($classNode);
-        $this->assertInstanceOf(DocBlock::class, $docBlock);
-
-        $nodes = $this->parser->parseFile(__DIR__ . '/DocBlockFactorySource/SomeClassWithReturn.php.inc');
-        $methodNode = $nodes[1]->stmts[0];
-
-        $docBlock = $this->docBlockFactory->createFromNode($methodNode);
         $this->assertInstanceOf(DocBlock::class, $docBlock);
     }
 }
