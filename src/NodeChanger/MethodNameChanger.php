2a066a424fd8cc7d5a7ea9c37d3d6a5c5f510e71
Gabriel Caruso <carusogabriel34@gmail.com>
2017-12-25 16:02:23 -0200

    renameNode does not need to return anything

--- src/NodeChanger/MethodNameChanger.php
+++ src/NodeChanger/MethodNameChanger.php
@@ -2,7 +2,6 @@
 
 namespace Rector\NodeChanger;
 
-use PhpParser\Node;
 use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Identifier;
 
@@ -11,12 +10,10 @@ final class MethodNameChanger
     /**
      * @param string[] $renameMethodMap
      */
-    public function renameNode(MethodCall $node, array $renameMethodMap): ?Node
+    public function renameNode(MethodCall $node, array $renameMethodMap): void
     {
         $oldNodeMethodName = $node->name->toString();
 
         $node->name = new Identifier($renameMethodMap[$oldNodeMethodName]);
-
-        return $node;
     }
 }
