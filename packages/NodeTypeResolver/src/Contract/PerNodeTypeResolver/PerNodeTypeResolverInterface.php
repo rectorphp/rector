8abcffdb80e07640cf78883b5fe499092d81deac
TomasVotruba <tomas.vot@gmail.com>
2017-12-26 02:17:59 +0100

    PerNodeTypeResolverInterface - add isPrimary() method, to separate helper resolvers and actually type bearers

--- packages/NodeTypeResolver/src/Contract/PerNodeTypeResolver/PerNodeTypeResolverInterface.php
+++ packages/NodeTypeResolver/src/Contract/PerNodeTypeResolver/PerNodeTypeResolverInterface.php
@@ -12,4 +12,10 @@ interface PerNodeTypeResolverInterface
      * @return string[]
      */
     public function resolve(Node $node): array;
+
+    /**
+     * If this resolves is subscribed to element that bears the type.
+     * E.g. name itself does not, bug object, new instance, property and method does.
+     */
+    public function isPrimary(): bool;
 }
