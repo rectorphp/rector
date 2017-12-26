71cfcf4b24c98f5a6070b45ee15f0ae998b3cba3
TomasVotruba <tomas.vot@gmail.com>
2017-12-26 23:02:54 +0100

    lazy-wip

---
+++ packages/NodeTypeResolver/tests/LazyTypeAttributeTest.php
@@ -0,0 +1,46 @@
+<?php declare(strict_types=1);
+
+namespace Rector\NodeTypeResolver\Tests;
+
+use PhpParser\Node\Expr\Variable;
+use Rector\Node\Attribute;
+
+final class LazyTypeAttributeTest extends AbstractNodeTypeResolverTest
+{
+    /**
+     * @var bool
+     */
+    private $isCalled = false;
+
+    /**
+     * @var Variable
+     */
+    private $variableNode;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $nodes = $this->getNodesForFileOfType(__DIR__ . '/LazyTypeAttributeSource/MethodCall.php.inc', Variable::class);
+        $this->assertCount(3, $nodes);
+
+        $this->variableNode = $nodes[2];
+    }
+
+    public function testLazyPrototype(): void
+    {
+        $this->variableNode->setAttribute('someKey', function () {
+            $this->isCalled = true;
+            return 5;
+        });
+        $this->assertFalse($this->isCalled);
+
+        $this->assertSame(5, $this->variableNode->getAttribute('someKey')());
+        $this->assertTrue($this->isCalled);
+    }
+
+    public function test(): void
+    {
+        $this->assertTrue($this->variableNode->hasAttribute(Attribute::TYPES));
+    }
+}
