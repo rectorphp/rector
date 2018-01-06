a92c63f85b80a5e6cf59ffa9013115ee25acf710
TomasVotruba <tomas.vot@gmail.com>
2018-01-06 23:19:33 +0100

    AdditionalAutoloader added, inspired by PHPStan

--- src/Autoloading/AdditionalAutoloader.php
+++ src/Autoloading/AdditionalAutoloader.php
@@ -2,17 +2,35 @@
 
 namespace Rector\Autoloading;
 
+use Nette\Loaders\RobotLoader;
 use Rector\Console\Command\ProcessCommand;
 use Symfony\Component\Console\Input\InputInterface;
+use Symplify\PackageBuilder\Parameter\ParameterProvider;
 
 final class AdditionalAutoloader
 {
-    public function autoloadWithInput(InputInterface $input): void
+    /**
+     * @var string
+     */
+    private const AUTOLOAD_DIRECTORIES_PARAMETER = 'autoload_directories';
+
+    /**
+     * @var ParameterProvider
+     */
+    private $parameterProvider;
+
+    public function __construct(ParameterProvider $parameterProvider)
     {
-        $this->autoloadFile($input);
+        $this->parameterProvider = $parameterProvider;
     }
 
-    private function autoloadFile(InputInterface $input): void
+    public function autoloadWithInput(InputInterface $input): void
+    {
+        $this->autoloadFileFromInput($input);
+        $this->autoloadDirectoriesFromParameter($this->parameterProvider);
+    }
+
+    private function autoloadFileFromInput(InputInterface $input): void
     {
         /** @var string|null $autoloadFile */
         $autoloadFile = $input->getOption(ProcessCommand::OPTION_AUTOLOAD_FILE);
@@ -25,5 +43,23 @@ final class AdditionalAutoloader
         }
 
         require_once $autoloadFile;
+    }
+
+    private function autoloadDirectoriesFromParameter(ParameterProvider $parameterProvider): void
+    {
+        $autoloadDirectories = $parameterProvider->provideParameter(self::AUTOLOAD_DIRECTORIES_PARAMETER);
+        if ($autoloadDirectories === null) {
+            return;
+        }
+
+        $robotLoader = new RobotLoader();
+        $robotLoader->ignoreDirs = ['*Fixtures'] + $robotLoader->ignoreDirs;
+        $robotLoader->setTempDirectory(sys_get_temp_dir() . '/_rector_robot_loader');
+
+        foreach ($autoloadDirectories as $autoloadDirectory) {
+            $robotLoader->addDirectory($autoloadDirectory);
+        }
+
+        $robotLoader->register();
     }
 }
