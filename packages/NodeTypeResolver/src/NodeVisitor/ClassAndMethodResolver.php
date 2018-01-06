e389f1c1257216b470623942b7ecf43310f9ef59
TomasVotruba <tomas.vot@gmail.com>
2018-01-06 23:32:05 +0100

    cleanup

--- packages/NodeTypeResolver/src/NodeVisitor/ClassAndMethodResolver.php
+++ packages/NodeTypeResolver/src/NodeVisitor/ClassAndMethodResolver.php
@@ -14,13 +14,14 @@ use PhpParser\NodeVisitorAbstract;
 use Rector\Node\Attribute;
 
 /**
- * Adds attribute to all nodes inside:
- * - 'className' with current class name
- * - 'classNode' with current class node
- * - 'parentClassName' with current class node
- * - 'methodName' with current method name
- * - 'methodNode' with current method node
- * - 'methodCall' with current method call
+ * Adds attributes to all nested nodes:
+ *
+ * - @see Attribute::CLASS_NAME with current class name
+ * - @see Attribute::CLASS_NODE with current class node
+ * - @see Attribute::PARENT_CLASS_NAME with current class node
+ * - @see Attribute::METHOD_NAME with current method name
+ * - @see Attribute::METHOD_NODE with current method node
+ * - @see Attribute::METHOD_CALL with current method call
  */
 final class ClassAndMethodResolver extends NodeVisitorAbstract
 {
@@ -68,6 +69,19 @@ final class ClassAndMethodResolver extends NodeVisitorAbstract
             return;
         }
 
+        $this->processClass($node);
+        $this->processMethod($node);
+    }
+
+    public function leaveNode(Node $node): void
+    {
+        if ($node instanceof Expression) {
+            $this->methodCall = null;
+        }
+    }
+
+    private function processClass(Node $node): void
+    {
         if ($node instanceof ClassLike) {
             $this->classNode = $node;
             $this->className = $node->namespacedName->toString();
@@ -78,30 +92,6 @@ final class ClassAndMethodResolver extends NodeVisitorAbstract
 
         if ($this->classNode instanceof Class_) {
             $this->setParentClassName($this->classNode, $node);
-        }
-
-        if ($node instanceof ClassMethod) {
-            $this->methodNode = $node;
-
-            /** @var Identifier $identifierNode */
-            $identifierNode = $node->name;
-
-            $this->methodName = $identifierNode->toString();
-        }
-
-        if ($node instanceof MethodCall && $node->name instanceof Identifier) {
-            $this->methodCall = $node->name->toString();
-        }
-
-        $node->setAttribute(Attribute::METHOD_NAME, $this->methodName);
-        $node->setAttribute(Attribute::METHOD_NODE, $this->methodNode);
-        $node->setAttribute(Attribute::METHOD_CALL, $this->methodCall);
-    }
-
-    public function leaveNode(Node $node): void
-    {
-        if ($node instanceof Expression) {
-            $this->methodCall = null;
         }
     }
 
@@ -118,5 +108,21 @@ final class ClassAndMethodResolver extends NodeVisitorAbstract
         }
 
         $node->setAttribute(Attribute::PARENT_CLASS_NAME, $parentClassResolvedName);
+    }
+
+    private function processMethod(Node $node): void
+    {
+        if ($node instanceof ClassMethod) {
+            $this->methodNode = $node;
+            $this->methodName = $node->name->toString();
+        }
+
+        if ($node instanceof MethodCall && $node->name instanceof Identifier) {
+            $this->methodCall = $node->name->toString();
+        }
+
+        $node->setAttribute(Attribute::METHOD_NAME, $this->methodName);
+        $node->setAttribute(Attribute::METHOD_NODE, $this->methodNode);
+        $node->setAttribute(Attribute::METHOD_CALL, $this->methodCall);
     }
 }
