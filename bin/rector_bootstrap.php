2d09efcf484321098c3d586bbf7d40209655bd0a
TomasVotruba <tomas.vot@gmail.com>
2018-01-06 21:45:09 +0100

    bin/retor: load nearest vendor

--- bin/rector_bootstrap.php
+++ bin/rector_bootstrap.php
@@ -1,13 +1,9 @@
 <?php declare(strict_types=1);
 
-/**
- * This allows to load "vendor/autoload.php" both from
- * "composer create-project ..." and "composer require" installation.
- */
 $possibleAutoloadPaths = [
+    // load from nearest vendor
+    getcwd() . '/vendor/autoload.php',
     // repository
-    __DIR__ . '/../vendor/autoload.php',
-    // composer create-project
     __DIR__ . '/../vendor/autoload.php',
     // composer require
     __DIR__ . '/../../../../vendor/autoload.php',
