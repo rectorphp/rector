7142b38b0a40f57ff4cc78316d5e4ba709e7ca5e
Gabriel Caruso <carusogabriel34@gmail.com>
2018-01-09 07:50:08 -0200

    Add vendor-bin to $possibleAutoloadPaths

--- bin/rector_bootstrap.php
+++ bin/rector_bootstrap.php
@@ -1,6 +1,8 @@
 <?php declare(strict_types=1);
 
 $possibleAutoloadPaths = [
+    // load from vendor-bin
+    getcwd() . '/vendor-bin/rector/vendor/autoload.php',
     // load from nearest vendor
     getcwd() . '/vendor/autoload.php',
     // repository
