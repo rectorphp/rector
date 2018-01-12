7bc13c799d929485346395c5a05143f5fe359cea
Gabriel Caruso <carusogabriel34@gmail.com>
2018-01-12 08:10:42 -0200

    Identify `return $this`

--- src/Rector/Dynamic/FluentReplaceRector.php
+++ src/Rector/Dynamic/FluentReplaceRector.php
@@ -3,15 +3,29 @@
 namespace Rector\Rector\Dynamic;
 
 use PhpParser\Node;
+use PhpParser\Node\Stmt\Return_;
+use PhpParser\Node\Expr\Variable;
 use Rector\Rector\AbstractRector;
 
 final class FluentReplaceRector extends AbstractRector
 {
     public function isCandidate(Node $node): bool
     {
+        if (! $node instanceof Return_) {
+            return false;
+        }
+
+        $returnExpr = $node->expr;
+
+        if (! $returnExpr instanceof Variable) {
+            return false;
+        }
+
+        return $returnExpr->name === 'this';
     }
 
-    public function refactor(Node $node): Node
+    public function refactor(Node $node): ?Node
     {
+        return null;
     }
 }
