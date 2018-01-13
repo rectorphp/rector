4ca2743a292d8b7c0aba1f558b4a92a0763b66af
TomasVotruba <tomas.vot@gmail.com>
2018-01-13 23:11:21 +0100

    add relatedTypesAndMethods property to collect defluented methods

--- src/Rector/Dynamic/FluentReplaceRector.php
+++ src/Rector/Dynamic/FluentReplaceRector.php
@@ -3,15 +3,18 @@
 namespace Rector\Rector\Dynamic;
 
 use PhpParser\Node;
-use PhpParser\Node\Stmt\Nop;
 use PhpParser\Node\Stmt\Return_;
 use PhpParser\Node\Expr\Variable;
-use PhpParser\NodeTraverser;
 use Rector\Node\Attribute;
 use Rector\Rector\AbstractRector;
 
 final class FluentReplaceRector extends AbstractRector
 {
+    /**
+     * @var string[][]
+     */
+    private $relatedTypesAndMethods;
+
     public function isCandidate(Node $node): bool
     {
         if (! $node instanceof Return_) {
@@ -30,6 +33,12 @@ final class FluentReplaceRector extends AbstractRector
     public function refactor(Node $node): ?Node
     {
         $this->removeNode = true;
+
+        $className = $node->getAttribute(Attribute::CLASS_NAME);
+        $methodName = $node->getAttribute(Attribute::METHOD_NAME);
+
+        $this->relatedTypesAndMethods[$className][] = $methodName;
+
         return null;
     }
 }
