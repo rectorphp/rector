b010d0a60385a995cd3c1baec0c9dc121a0af058
TomasVotruba <tomas.vot@gmail.com>
2018-01-13 23:14:05 +0100

    add description

--- src/Rector/Dynamic/FluentReplaceRector.php
+++ src/Rector/Dynamic/FluentReplaceRector.php
@@ -3,17 +3,34 @@
 namespace Rector\Rector\Dynamic;
 
 use PhpParser\Node;
+use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Stmt\Return_;
 use PhpParser\Node\Expr\Variable;
 use Rector\Node\Attribute;
+use Rector\NodeAnalyzer\MethodCallAnalyzer;
 use Rector\Rector\AbstractRector;
 
+/**
+ * This Rector handles 2 things:
+ * - removes "$return this;" method bodies
+ * - changes fluent calls to standalone calls
+ */
 final class FluentReplaceRector extends AbstractRector
 {
     /**
      * @var string[][]
      */
     private $relatedTypesAndMethods;
+
+    /**
+     * @var MethodCallAnalyzer
+     */
+    private $methodCallAnalyzer;
+
+    public function __construct(MethodCallAnalyzer $methodCallAnalyzer)
+    {
+        $this->methodCallAnalyzer = $methodCallAnalyzer;
+    }
 
     public function isCandidate(Node $node): bool
     {
@@ -27,11 +44,21 @@ final class FluentReplaceRector extends AbstractRector
             return $returnExpr->name === 'this';
         }
 
+        if ($node instanceof MethodCall) {
+            foreach ($this->relatedTypesAndMethods as $type => $methods) {
+                if (! $this->methodCallAnalyzer->isTypeAndMethods($node, $type, $methods)) {
+                    continue;
+                }
+
+                return true;
+            }
+        }
+
         return false;
     }
 
     /**
-     * @param Return_ $node
+     * @param Return_|MethodCall $node
      */
     public function refactor(Node $node): ?Node
     {
@@ -44,6 +71,9 @@ final class FluentReplaceRector extends AbstractRector
             $this->relatedTypesAndMethods[$className][] = $methodName;
 
             return null;
+        }
+
+        if ($node instanceof MethodCall) {
         }
 
         return $node;
