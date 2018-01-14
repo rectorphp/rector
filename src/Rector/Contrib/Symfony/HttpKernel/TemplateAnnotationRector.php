47d3368bc3882cddca13d5db74b0550454dd41bd
TomasVotruba <tomas.vot@gmail.com>
2018-01-14 16:51:15 +0100

    isCandidate()

--- src/Rector/Contrib/Symfony/HttpKernel/TemplateAnnotationRector.php
+++ src/Rector/Contrib/Symfony/HttpKernel/TemplateAnnotationRector.php
@@ -5,6 +5,7 @@ namespace Rector\Rector\Contrib\Symfony\HttpKernel;
 use PhpParser\Node;
 use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Scalar\String_;
+use PhpParser\Node\Stmt\ClassMethod;
 use Rector\Builder\Class_\ClassPropertyCollector;
 use Rector\Contract\Bridge\ServiceTypeForNameProviderInterface;
 use Rector\Naming\PropertyNaming;
@@ -12,6 +13,7 @@ use Rector\Node\Attribute;
 use Rector\Node\PropertyFetchNodeFactory;
 use Rector\NodeAnalyzer\Contrib\Symfony\ContainerCallAnalyzer;
 use Rector\Rector\AbstractRector;
+use Rector\ReflectionDocBlock\NodeAnalyzer\DocBlockAnalyzer;
 
 /**
  * Converts all:
@@ -25,9 +27,27 @@ use Rector\Rector\AbstractRector;
  */
 final class TemplateAnnotationRector extends AbstractRector
 {
+    /**
+     * @var DocBlockAnalyzer
+     */
+    private $docBlockAnalyzer;
+
+    public function __construct(DocBlockAnalyzer $docBlockAnalyzer)
+    {
+        $this->docBlockAnalyzer = $docBlockAnalyzer;
+    }
+
     public function isCandidate(Node $node): bool
     {
-        // is class method with @template annotation
+        if (! $node instanceof ClassMethod) {
+            return false;
+        }
+
+        if (! $this->docBlockAnalyzer->hasAnnotation($node, 'template')) {
+            return false;
+        }
+
+        return true;
     }
 
     /**
@@ -35,6 +55,9 @@ final class TemplateAnnotationRector extends AbstractRector
      */
     public function refactor(Node $methodCallNode): ?Node
     {
+        dump($methodCallNode);
+        die;
+
         // 1.remove annotation
         // 2. derive template name
         // 3. add $this->render method call with template
