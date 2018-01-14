132b2a494838f8bc3f43ef85ed142259a23d336a
mssimi <mssimi@seznam.cz>
2018-01-14 22:47:59 +0100

    better tests

--- tests/Rector/Contrib/Symfony/HttpKernel/TemplateAnnotationRector/TemplateAnnotationRectorTest.php
+++ tests/Rector/Contrib/Symfony/HttpKernel/TemplateAnnotationRector/TemplateAnnotationRectorTest.php
@@ -24,6 +24,7 @@ final class TemplateAnnotationRectorTest extends AbstractConfigurableRectorTestC
             [__DIR__ . '/Wrong/wrong.php.inc', __DIR__ . '/Correct/correct.php.inc'],
             [__DIR__ . '/Wrong/wrong2.php.inc', __DIR__ . '/Correct/correct2.php.inc'],
             [__DIR__ . '/Wrong/wrong3.php.inc', __DIR__ . '/Correct/correct3.php.inc'],
+            [__DIR__ . '/Wrong/wrong4.php.inc', __DIR__ . '/Correct/correct4.php.inc'],
         ];
     }
 
