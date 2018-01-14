bdb3eff4cf21038516327878e17eaf1d046827b7
TomasVotruba <tomas.vot@gmail.com>
2018-01-14 01:06:48 +0100

    move method one call up + add separate call node

--- src/Rector/Dynamic/FluentReplaceRector.php
+++ src/Rector/Dynamic/FluentReplaceRector.php
@@ -4,9 +4,10 @@ namespace Rector\Rector\Dynamic;
 
 use PhpParser\Node;
 use PhpParser\Node\Expr\MethodCall;
-use PhpParser\Node\Stmt\Return_;
 use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Stmt\Return_;
 use Rector\Node\Attribute;
+use Rector\Node\MethodCallNodeFactory;
 use Rector\NodeAnalyzer\MethodCallAnalyzer;
 use Rector\Rector\AbstractRector;
 
@@ -20,20 +21,31 @@ final class FluentReplaceRector extends AbstractRector
     /**
      * @var string[][]
      */
-    private $relatedTypesAndMethods;
+    private $relatedTypesAndMethods = [];
 
     /**
      * @var MethodCallAnalyzer
      */
     private $methodCallAnalyzer;
 
-    public function __construct(MethodCallAnalyzer $methodCallAnalyzer)
+    /**
+     * @var MethodCallNodeFactory
+     */
+    private $methodCallNodeFactory;
+
+    public function __construct(MethodCallAnalyzer $methodCallAnalyzer, MethodCallNodeFactory $methodCallNodeFactory)
     {
         $this->methodCallAnalyzer = $methodCallAnalyzer;
+        $this->methodCallNodeFactory = $methodCallNodeFactory;
     }
 
     public function isCandidate(Node $node): bool
     {
+        // skip just added calls
+        if ($node->getAttribute(Attribute::ORIGINAL_NODE) === null) {
+            return false;
+        }
+
         if ($node instanceof Return_) {
             $returnExpr = $node->expr;
 
@@ -45,8 +57,13 @@ final class FluentReplaceRector extends AbstractRector
         }
 
         if ($node instanceof MethodCall) {
+            // is chain method call
+            if (! $node->var instanceof MethodCall) {
+                return false;
+            }
+
             foreach ($this->relatedTypesAndMethods as $type => $methods) {
-                if (! $this->methodCallAnalyzer->isTypeAndMethods($node, $type, $methods)) {
+                if (! $this->methodCallAnalyzer->isTypeAndMethods($node->var, $type, $methods)) {
                     continue;
                 }
 
@@ -74,6 +91,26 @@ final class FluentReplaceRector extends AbstractRector
         }
 
         if ($node instanceof MethodCall) {
+            // metod call to prepend
+            /** @var Node $parentVarName */
+            $parentVarName = $node->var->var;
+            $parentVarName->setAttribute(Attribute::ORIGINAL_NODE, null);
+
+            $nextMethodCallNode = $this->methodCallNodeFactory->createWithVariableAndMethodName(
+                $parentVarName,
+                $node->name->toString()
+            );
+
+            $this->addNodeAfterNode($nextMethodCallNode, $node->var);
+
+            // move method call one up
+            $node->name = $node->var->name;
+            $node->var = $node->var->var;
+
+            // to clear indent
+            $node->setAttribute(Attribute::ORIGINAL_NODE, null);
+
+            return $node;
         }
 
         return $node;
