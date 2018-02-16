cd85f7a5a0adff4a799c0b159c7399d3bc48cbdc
TomasVotruba <tomas.vot@gmail.com>
2018-02-16 13:17:00 +0100

    scoper: drop unused whitelist config section

--- scoper.inc.php
+++ scoper.inc.php
@@ -40,18 +40,4 @@ return [
             ->files()
             ->in(__DIR__ . '/vendor/friendsofphp/php-cs-fixer/tests/Test')
     ],
-
-
-    // PHP-Scoper's goal is to make sure that all code for a project lies in a distinct PHP namespace. However, you
-    // may want to share a common API between the bundled code of your PHAR and the consumer code. For example if
-    // you have a PHPUnit PHAR with isolated code, you still want the PHAR to be able to understand the
-    // PHPUnit\Framework\TestCase class.
-    //
-    // A way to achieve this is by specifying a list of classes to not prefix with the following configuration key. Note
-    // that this does not work with functions or constants neither with classes belonging to the global namespace.
-    //
-    // Fore more see https://github.com/humbug/php-scoper#whitelist
-    'whitelist' => [
-        'PHPUnit\Framework\TestCase',
-    ],
 ];
