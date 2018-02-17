82334445282eb410af6bea17f53eead39ed957d9
TomasVotruba <tomas.vot@gmail.com>
2018-02-17 16:12:41 +0100

    tests: add Phar process test

--- tests/Phar/PharTest.php
+++ tests/Phar/PharTest.php
@@ -3,11 +3,31 @@
 namespace Rector\Tests\Phar;
 
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Process\Process;
 
 final class PharTest extends TestCase
 {
-    public function test(): void
+    public function testPhpScoper(): void
     {
-        $this->assertFileExists(__DIR__ . '/../../rector.phar');
+        $rectorPrefixedBinLocation = __DIR__ . '/../../build/bin/rector';
+        $this->assertFileExists($rectorPrefixedBinLocation);
+
+        $process = new Process($rectorPrefixedBinLocation);
+        $exitCode = $process->run();
+
+        $this->assertSame('', $process->getErrorOutput());
+        $this->assertSame(1, $exitCode);
+    }
+
+    public function testBox(): void
+    {
+        $rectorPharLocation = __DIR__ . '/../../rector.phar';
+        $this->assertFileExists($rectorPharLocation);
+
+        $process = new Process($rectorPharLocation);
+        $exitCode = $process->run();
+
+        $this->assertSame('', $process->getErrorOutput());
+        $this->assertSame(1, $exitCode);
     }
 }
