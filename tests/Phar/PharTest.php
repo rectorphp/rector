02aa370afe5c0f8f7175da03f194aacc3762a406
TomasVotruba <tomas.vot@gmail.com>
2018-02-17 16:38:09 +0100

    tests: make Phar bin files executable

--- tests/Phar/PharTest.php
+++ tests/Phar/PharTest.php
@@ -5,6 +5,10 @@ namespace Rector\Tests\Phar;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Process\Process;
 
+/**
+ * Run after `rector.phar` is generated to the root,
+ * before removing `/build` temp directory
+ */
 final class PharTest extends TestCase
 {
     public function testPhpScoper(): void
@@ -12,6 +16,7 @@ final class PharTest extends TestCase
         $rectorPrefixedBinLocation = __DIR__ . '/../../build/bin/rector';
         $this->assertFileExists($rectorPrefixedBinLocation);
 
+        $this->makeFileExecutable($rectorPrefixedBinLocation);
         $process = new Process($rectorPrefixedBinLocation);
         $exitCode = $process->run();
 
@@ -24,10 +29,17 @@ final class PharTest extends TestCase
         $rectorPharLocation = __DIR__ . '/../../rector.phar';
         $this->assertFileExists($rectorPharLocation);
 
+        $this->makeFileExecutable($rectorPharLocation);
         $process = new Process($rectorPharLocation);
         $exitCode = $process->run();
 
         $this->assertSame('', $process->getErrorOutput());
         $this->assertSame(1, $exitCode);
+    }
+
+    private function makeFileExecutable(string $file): void
+    {
+        $process = new Process(['chmod', '+x', $file]);
+        $process->run();
     }
 }
