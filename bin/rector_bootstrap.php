47e02f3f28f3d950c1c6d24f626054680301335a
TomasVotruba <tomas.vot@gmail.com>
2018-02-20 11:01:33 +0100

    rector_bootstrap: add scoper autoload

--- bin/rector_bootstrap.php
+++ bin/rector_bootstrap.php
@@ -1,6 +1,8 @@
 <?php declare(strict_types=1);
 
 $possibleAutoloadPaths = [
+    // Is scoped (in PHAR or build directory)
+    __DIR__.'/../scoper-autoload.php',
     // repository
     __DIR__ . '/../vendor/autoload.php',
     // composer require
