866398ffede57fe3147f2cdf486ffbe30856e27f
TomasVotruba <tomas.vot@gmail.com>
2018-02-21 16:10:56 +0100

    run build phar only, without scoper

--- tests/Phar/PharTest.php
+++ tests/Phar/PharTest.php
@@ -6,40 +6,19 @@ use PHPUnit\Framework\TestCase;
 use Symfony\Component\Process\Process;
 
 /**
- * Run after `rector.phar` is generated to the root,
- * before removing `/build` temp directory
+ * Run after `rector.phar` is generated to the root
  */
 final class PharTest extends TestCase
 {
-    public function testPhpScoper(): void
-    {
-        $rectorPrefixedBinLocation = __DIR__ . '/../../build/bin/rector';
-        $this->assertFileExists($rectorPrefixedBinLocation);
-
-        $this->makeFileExecutable($rectorPrefixedBinLocation);
-        $process = new Process($rectorPrefixedBinLocation);
-        $exitCode = $process->run();
-
-        $this->assertSame('', $process->getErrorOutput());
-        $this->assertSame(1, $exitCode);
-    }
-
     public function testBox(): void
     {
         $rectorPharLocation = __DIR__ . '/../../rector.phar';
         $this->assertFileExists($rectorPharLocation);
 
-        $this->makeFileExecutable($rectorPharLocation);
         $process = new Process($rectorPharLocation);
         $exitCode = $process->run();
 
         $this->assertSame('', $process->getErrorOutput());
         $this->assertSame(1, $exitCode);
-    }
-
-    private function makeFileExecutable(string $file): void
-    {
-        $process = new Process(['chmod', '+x', $file]);
-        $process->run();
     }
 }
