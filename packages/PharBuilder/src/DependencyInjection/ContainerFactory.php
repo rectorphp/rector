304c8c77ae07f1b44f1263ffb262c9b168057483
TomasVotruba <tomas.vot@gmail.com>
2018-02-23 11:07:01 +0100

    ContainerFactory: fix debug Kernel param verbosity

--- packages/PharBuilder/src/DependencyInjection/ContainerFactory.php
+++ packages/PharBuilder/src/DependencyInjection/ContainerFactory.php
@@ -11,6 +11,10 @@ final class ContainerFactory
         $appKernel = new PharBuilderKernel();
         $appKernel->boot();
 
+        // this is require to keep CLI verbosity independent on AppKernel dev/prod mode
+        // see: https://github.com/symfony/symfony/blob/c12c07865a6fe3a8e0edd8540ac3ab9a3bc75543/src/Symfony/Component/HttpKernel/Kernel.php#L115
+        putenv('SHELL_VERBOSITY=0');
+
         return $appKernel->getContainer();
     }
 }
