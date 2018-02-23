9c2176739ba1d5f0fedbf3b89907abe73996b104
TomasVotruba <tomas.vot@gmail.com>
2018-02-23 20:04:45 +0100

    add Symfony\ExpressionLanguage

--- packages/RectorBuilder/src/DependencyInjection/CompilerPass/RectorProvidersCompilerPass.php
+++ packages/RectorBuilder/src/DependencyInjection/CompilerPass/RectorProvidersCompilerPass.php
@@ -6,6 +6,7 @@ use Rector\Rector\RectorCollector;
 use Rector\RectorBuilder\Contract\RectorProviderInterface;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\ExpressionLanguage\Expression;
 use Symplify\PackageBuilder\DependencyInjection\DefinitionFinder;
 
 final class RectorProvidersCompilerPass implements CompilerPassInterface
@@ -15,8 +16,11 @@ final class RectorProvidersCompilerPass implements CompilerPassInterface
         $rectorCollectorDefinition = DefinitionFinder::getByType($containerBuilder, RectorCollector::class);
         $rectorProviderDefinitions = DefinitionFinder::findAllByType($containerBuilder, RectorProviderInterface::class);
 
+        // see https://symfony.com/doc/current/service_container/expression_language.html
         foreach ($rectorProviderDefinitions as $rectorProviderDefinition) {
-            $methodCallArgument = sprintf("@=service('%s').provide()", $rectorProviderDefinition->getClass());
+            $methodCallArgument = new Expression(
+                sprintf('service("%s").provide()', $rectorProviderDefinition->getClass())
+            );
             $rectorCollectorDefinition->addMethodCall('addRector', [$methodCallArgument]);
         }
     }
