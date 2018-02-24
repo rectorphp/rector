d464401aae2758f1e6b55d2014494db98b8aa954
TomasVotruba <tomas.vot@gmail.com>
2018-02-24 12:05:49 +0100

    move ValidateControlRector to Provider

---
+++ src/Rector/Contrib/Nette/Application/ValidateControlRectorProvider.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+namespace Rector\Rector\Contrib\Nette\Application;
+
+use Rector\Contract\Rector\RectorInterface;
+use Rector\RectorBuilder\BuilderRectorFactory;
+use Rector\RectorBuilder\Contract\RectorProviderInterface;
+
+final class ValidateControlRectorProvider implements RectorProviderInterface
+{
+    /**
+     * @var BuilderRectorFactory
+     */
+    private $builderRectorFactory;
+
+    public function __construct(BuilderRectorFactory $builderRectorFactory)
+    {
+        $this->builderRectorFactory = $builderRectorFactory;
+    }
+
+    /**
+     * Before::
+     * - $myControl->validateControl(?$snippet)
+     *
+     * After:
+     * - $myControl->redrawControl(?$snippet, false);
+     */
+    public function provide(): RectorInterface
+    {
+        return $this->builderRectorFactory->create()
+            ->matchMethodCallByType('Nette\Application\UI\Control')
+            ->matchMethodName('validateControl')
+            ->changeMethodNameTo('redrawControl')
+            ->addArgument(1, false);
+    }
+}
