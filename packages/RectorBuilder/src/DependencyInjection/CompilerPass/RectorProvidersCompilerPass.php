f178e3c4aac0a6099330dd5d4cd776855b86722f
TomasVotruba <tomas.vot@gmail.com>
2018-02-24 22:50:42 +0100

    cleanup CompilerPass

--- packages/RectorBuilder/src/DependencyInjection/CompilerPass/RectorProvidersCompilerPass.php
+++ packages/RectorBuilder/src/DependencyInjection/CompilerPass/RectorProvidersCompilerPass.php
@@ -6,22 +6,17 @@ use Rector\NodeTraverser\RectorNodeTraverser;
 use Rector\RectorBuilder\Contract\RectorProviderInterface;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\ExpressionLanguage\Expression;
-use Symplify\PackageBuilder\DependencyInjection\DefinitionFinder;
+use Symplify\PackageBuilder\DependencyInjection\DefinitionCollector;
 
 final class RectorProvidersCompilerPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $containerBuilder): void
     {
-        $rectorNodeTraverserDefinition = $containerBuilder->getDefinition(RectorNodeTraverser::class);
-
-        $rectorProviderDefinitions = DefinitionFinder::findAllByType(
+        DefinitionCollector::loadCollectorWithType(
             $containerBuilder,
-            RectorProviderInterface::class
+            RectorNodeTraverser::class,
+            RectorProviderInterface::class,
+            'addRectorProvider'
         );
-
-        foreach ($rectorProviderDefinitions as $rectorProviderDefinition) {
-            $rectorNodeTraverserDefinition->addMethodCall('addRectorProvider', ['@' . $rectorProviderDefinition->getClass()]);
-        }
     }
 }
