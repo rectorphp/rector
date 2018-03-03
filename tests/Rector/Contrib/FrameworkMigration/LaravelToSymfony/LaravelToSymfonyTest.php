de29708927688aba11473c950674715afded218f
TomasVotruba <tomas.vot@gmail.com>
2018-03-04 00:34:40 +0100

    [FrameworkMigration] init LaravelToSymfony

---
+++ tests/Rector/Contrib/FrameworkMigration/LaravelToSymfony/LaravelToSymfonyTest.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace Rector\Tests\Rector\Contrib\Symfony\FrameworkMigration\LaravelToSymfony;
+
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+
+final class LaravelToSymfonyTest extends AbstractRectorTestCase
+{
+    /**
+     * @dataProvider provideWrongToFixedFiles()
+     */
+    public function test(string $wrong, string $fixed): void
+    {
+        $this->doTestFileMatchesExpectedContent($wrong, $fixed);
+    }
+
+    /**
+     * @return string[][]
+     */
+    public function provideWrongToFixedFiles(): array
+    {
+        return [
+            [__DIR__ . '/Wrong/wrong.php.inc', __DIR__ . '/Correct/correct.php.inc'],
+        ];
+    }
+
+    protected function provideConfig(): string
+    {
+        return __DIR__ . '/config.yml';
+    }
+}
