768a4554517308565fc451c28a7a03b6a2ec3018
TomasVotruba <tomas.vot@gmail.com>
2018-03-04 14:18:43 +0100

    move migration one level up

--- tests/Rector/FrameworkMigration/LaravelToSymfony/LaravelToSymfonyTest.php
+++ tests/Rector/FrameworkMigration/LaravelToSymfony/LaravelToSymfonyTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Rector\Tests\Rector\Contrib\FrameworkMigration\LaravelToSymfony;
+namespace Rector\Tests\Rector\FrameworkMigration\LaravelToSymfony;
 
 use Rector\Testing\PHPUnit\AbstractRectorTestCase;
 
