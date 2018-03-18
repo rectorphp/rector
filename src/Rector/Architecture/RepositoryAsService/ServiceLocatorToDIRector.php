7356ffe4e1107db12d09c28dc61d11b13a02c3af
mssimi <mssimi@seznam.cz>
2018-03-18 23:37:20 +0100

    fqn alias

--- src/Rector/Architecture/RepositoryAsService/ServiceLocatorToDIRector.php
+++ src/Rector/Architecture/RepositoryAsService/ServiceLocatorToDIRector.php
@@ -2,9 +2,11 @@
 
 namespace Rector\Rector\Architecture\RepositoryAsService;
 
+use Nette\Utils\Strings;
 use PhpParser\Node;
 use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Stmt\Class_;
 use Rector\Builder\Class_\VariableInfo;
 use Rector\Builder\ConstructorMethodBuilder;
 use Rector\Contract\Bridge\RepositoryForDoctrineEntityProviderInterface;
@@ -60,7 +62,13 @@ final class ServiceLocatorToDIRector extends AbstractRector
             return false;
         }
 
-        return true;
+        $className = $node->getAttribute(Attribute::CLASS_NAME);
+
+        if($className === null){
+            return false;
+        }
+
+        return ! Strings::endsWith($node->getAttribute(Attribute::CLASS_NAME), 'Repository');
     }
 
     public function refactor(Node $node): ?Node
@@ -87,25 +95,25 @@ final class ServiceLocatorToDIRector extends AbstractRector
         $repositoryArgument = $node->args[0]->value;
 
         if($repositoryArgument->class === null){
-            return 'todo';
+            $fqnOrAlias = $repositoryArgument->value;
         }
 
         if($repositoryArgument->class instanceof Node\Name){
-            $fullyQualifiedName = $repositoryArgument->class->getAttribute(Attribute::TYPES)[0];
+            $fqnOrAlias = $repositoryArgument->class->getAttribute(Attribute::TYPES)[0];
         }
 
         if($repositoryArgument->class instanceof Node\Name\FullyQualified){
-            $fullyQualifiedName = $repositoryArgument->class->toString();
+            $fqnOrAlias = $repositoryArgument->class->toString();
         }
 
         $repositoryClassName = $this->repositoryForDoctrineEntityProvider->provideRepositoryForEntity(
-            $fullyQualifiedName
+            $fqnOrAlias
         );
 
         if ($repositoryClassName === null) {
             throw new RectorProviderException(sprintf(
                 'A repository was not provided for "%s" entity by your "%s" class.',
-                $repositoryArgument->class->toString(),
+                $fqnOrAlias,
                 \get_class($this->repositoryForDoctrineEntityProvider)
             ));
         }
