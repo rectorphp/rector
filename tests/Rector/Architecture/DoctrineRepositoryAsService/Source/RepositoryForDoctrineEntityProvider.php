08f3d86556a147401a2fd713c4350542bbc3f78c
mssimi <mssimi@seznam.cz>
2018-03-18 23:37:20 +0100

    fqn alias

--- tests/Rector/Architecture/DoctrineRepositoryAsService/Source/RepositoryForDoctrineEntityProvider.php
+++ tests/Rector/Architecture/DoctrineRepositoryAsService/Source/RepositoryForDoctrineEntityProvider.php
@@ -15,6 +15,31 @@ final class RepositoryForDoctrineEntityProvider implements RepositoryForDoctrine
 
     public function provideRepositoryForEntity(string $name): ?string
     {
+        if($this->isAlias($name)){
+            return $this->resoleFromAlias($name);
+        }
+
         return $this->map[$name] ?? null;
+
+    }
+
+    private function isAlias(string $name): bool
+    {
+        return strpos($name, ':') !== false;
+    }
+
+    private function resoleFromAlias(string $name): ?string
+    {
+        [$namespaceAlias, $simpleClassName] = explode(':', $name, 2);
+
+        $pattern = sprintf('/(%s{1}.*%s)/', $namespaceAlias, $simpleClassName);
+
+        foreach($this->map as $key => $value) {
+            if (preg_match($pattern, $key)){
+                return $value;
+            }
+        }
+
+        return null;
     }
 }
