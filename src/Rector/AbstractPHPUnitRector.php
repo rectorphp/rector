20f0bca837f18830ee0b9ff4cb7768c884c10aa1
Gabriel Caruso <carusogabriel34@gmail.com>
2018-03-20 16:34:17 -0300

    [PHPUnit] Safe way to check if a class is a Test class

--- src/Rector/AbstractPHPUnitRector.php
+++ src/Rector/AbstractPHPUnitRector.php
@@ -2,21 +2,16 @@
 
 namespace Rector\Rector;
 
-use Nette\Utils\Strings;
 use PhpParser\Node;
+use PHPUnit\Framework\TestCase;
 use Rector\Node\Attribute;
 
 abstract class AbstractPHPUnitRector extends AbstractRector
 {
-    /**
-     * Check if the file is a PHPUnit TestCase. By default, it should end with "Test", as it is the standard.
-     *
-     * @see https://phpunit.de/getting-started-with-phpunit.html
-     */
     protected function isInTestClass(Node $node): bool
     {
-        $className = (string) $node->getAttribute(Attribute::CLASS_NAME);
+        $parentClassName = (string) $node->getAttribute(Attribute::PARENT_CLASS_NAME);
 
-        return Strings::endsWith($className, 'Test');
+        return in_array($parentClassName, [TestCase::class, 'PHPUnit_Framework_TestCase'], true);
     }
 }
