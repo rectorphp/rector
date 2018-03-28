8648452dbd4d290afe99a4139cd624f0f847e9b4
mssimi <mssimi@seznam.cz>
2018-03-28 16:15:28 +0200

    revert autoload, enable phar composer

--- bin/rector_bootstrap.php
+++ bin/rector_bootstrap.php
@@ -1,6 +1,10 @@
 <?php declare(strict_types=1);
 
 $possibleAutoloadPaths = [
+    // repository
+    __DIR__ . '/../vendor/autoload.php',
+    // composer require
+    __DIR__ . '/../../../../vendor/autoload.php',
     // load from nearest vendor
     getcwd() . '/vendor/autoload.php',
 ];
@@ -8,6 +12,11 @@ $possibleAutoloadPaths = [
 foreach ($possibleAutoloadPaths as $possibleAutoloadPath) {
     if (is_file($possibleAutoloadPath)) {
         require_once $possibleAutoloadPath;
+
+        if(!class_exists('Rector\Parser\ParserFactory',true)){
+            die('autoload broken');
+        }
+
         return;
     }
 }
