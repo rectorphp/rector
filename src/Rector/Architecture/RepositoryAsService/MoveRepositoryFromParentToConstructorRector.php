8a185e93ece77d925ab072ff731b81b4c3e19cdc
Tomas Votruba <tomas.vot@gmail.com>
2018-04-08 17:37:45 +0200

    add definition

--- src/Rector/Architecture/RepositoryAsService/MoveRepositoryFromParentToConstructorRector.php
+++ src/Rector/Architecture/RepositoryAsService/MoveRepositoryFromParentToConstructorRector.php
@@ -82,7 +82,38 @@ final class MoveRepositoryFromParentToConstructorRector extends AbstractRector
 
     public function getDefinition(): RectorDefinition
     {
-        return new RectorDefinition('...', [new CodeSample('...', '...')]);
+        return new RectorDefinition('Turns parent EntityRepository class to constructor dependency', [
+            new CodeSample(
+                <<<'CODE_SAMPLE'
+namespace App\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+final class PostRepository extends EntityRepository
+{
+}
+CODE_SAMPLE
+                ,
+                <<<'CODE_SAMPLE'
+namespace App\Repository;
+
+use App\Entity\Post;
+use Doctrine\ORM\EntityRepository;
+
+final class PostRepository
+{
+    /**
+     * @var \Doctrine\ORM\EntityRepository
+     */
+    private $repository;
+    public function __construct(\Doctrine\ORM\EntityManager $entityManager)
+    {
+        $this->repository = $entityManager->getRepository(\App\Entity\Post::class);
+    }
+}
+CODE_SAMPLE
+            ),
+        ]);
     }
 
     /**
