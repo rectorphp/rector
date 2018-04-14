ca66e806a6e193da4b8d0cc4f2f0496a7325c964
Tomas Votruba <tomas.vot@gmail.com>
2018-04-14 20:17:59 +0200

    fix () in Yield

--- src/Printer/BetterStandardPrinter.php
+++ src/Printer/BetterStandardPrinter.php
@@ -2,6 +2,7 @@
 
 namespace Rector\Printer;
 
+use PhpParser\Node\Expr\Yield_;
 use PhpParser\PrettyPrinter\Standard;
 
 final class BetterStandardPrinter extends Standard
@@ -18,5 +19,20 @@ final class BetterStandardPrinter extends Standard
     protected function pSingleQuotedString(string $string): string
     {
         return '\'' . preg_replace("/'|\\\\(?=[\\\\']|$)/", '\\\\$0', $string) . '\'';
+    }
+
+    /**
+     * Do not add () on Expressions
+     * @see https://github.com/rectorphp/rector/pull/401#discussion_r181487199
+     */
+    protected function pExpr_Yield(Yield_ $yieldNode)
+    {
+        if ($yieldNode->value === null) {
+            return 'yield';
+        } else {
+            return 'yield '
+                . ($yieldNode->key !== null ? $this->p($yieldNode->key) . ' => ' : '')
+                . $this->p($yieldNode->value);
+        }
     }
 }
