0c8b0751a41095f5cfba16a8a051b06fa6929876
Tomas Votruba <tomas.vot@gmail.com>
2018-04-25 19:47:44 +0200

    init ReturnTypehintRector

---
+++ tests/Rector/Dynamic/ReturnTypehintRector/ReturnTypehintRectorTest.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace Rector\Tests\Rector\Dynamic\ReturnTypehintRector;
+
+use Iterator;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+
+/**
+ * @see \Rector\Rector\Dynamic\ReturnTypehintRector
+ */
+final class ReturnTypehintRectorTest extends AbstractRectorTestCase
+{
+    /**
+     * @dataProvider provideWrongToFixedFiles()
+     */
+    public function test(string $wrong, string $fixed): void
+    {
+        $this->doTestFileMatchesExpectedContent($wrong, $fixed);
+    }
+
+    public function provideWrongToFixedFiles(): Iterator
+    {
+        yield [__DIR__ . '/Wrong/wrong.php.inc', __DIR__ . '/Correct/correct.php.inc'];
+    }
+
+    protected function provideConfig(): string
+    {
+        return __DIR__ . '/config.yml';
+    }
+}
