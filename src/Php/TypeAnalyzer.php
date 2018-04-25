a0397df9b7e7b3b369a851cff53351ede90d2950
Tomas Votruba <tomas.vot@gmail.com>
2018-04-25 20:09:09 +0200

    add nullable type

--- src/Php/TypeAnalyzer.php
+++ src/Php/TypeAnalyzer.php
@@ -2,10 +2,17 @@
 
 namespace Rector\Php;
 
+use Nette\Utils\Strings;
+
 final class TypeAnalyzer
 {
     public function isPhpReservedType(string $type): bool
     {
         return in_array($type, ['string', 'bool', 'mixed', 'object', 'iterable', 'array'], true);
+    }
+
+    public function isNullableType(string $type): bool
+    {
+        return Strings::startsWith($type, '?');
     }
 }
