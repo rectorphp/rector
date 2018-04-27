cbcb9106c2094f7b8685e70fdae827c7f2239db1
mssimi <mssimi@seznam.cz>
2018-04-27 23:07:50 +0200

    fix reviews

--- src/Rector/Contrib/Doctrine/AliasToClassRector.php
+++ src/Rector/Contrib/Doctrine/AliasToClassRector.php
@@ -3,9 +3,8 @@
 namespace Rector\Rector\Contrib\Doctrine;
 
 use PhpParser\Node;
-use PhpParser\Node\Scalar\String_;
-use Rector\Node\Attribute;
 use Rector\Node\NodeFactory;
+use Rector\NodeAnalyzer\MethodArgumentAnalyzer;
 use Rector\NodeAnalyzer\MethodCallAnalyzer;
 use Rector\Rector\AbstractRector;
 use Rector\RectorDefinition\CodeSample;
@@ -29,52 +28,23 @@ final class AliasToClassRector extends AbstractRector
     private $nodeFactory;
 
     /**
+     * @var MethodArgumentAnalyzer
+     */
+    private $methodArgumentAnalyzer;
+
+    /**
      * @param string[] $aliasesToNamespaces
      */
     public function __construct(
         array $aliasesToNamespaces,
         MethodCallAnalyzer $methodCallAnalyzer,
+        MethodArgumentAnalyzer $methodArgumentAnalyzer,
         NodeFactory $nodeFactory
     ) {
         $this->methodCallAnalyzer = $methodCallAnalyzer;
         $this->aliasesToNamespaces = $aliasesToNamespaces;
         $this->nodeFactory = $nodeFactory;
-    }
-
-    public function isCandidate(Node $node): bool
-    {
-        if (! $this->methodCallAnalyzer->isMethod($node, 'getRepository')) {
-            return false;
-        }
-
-        $className = $node->getAttribute(Attribute::CLASS_NAME);
-
-        if ($className === null) {
-            return false;
-        }
-
-        /** @var MethodCall $node */
-        if (count($node->args) !== 1) {
-            return false;
-        }
-
-        if (! $node->args[0]->value instanceof String_) {
-            return false;
-        }
-
-        /** @var String_ $string */
-        $string = $node->args[0]->value;
-
-        return $this->isAlias($string->value) && $this->entityFqn($string->value) !== '';
-    }
-
-    public function refactor(Node $node): ?Node
-    {
-        $node->args[0]->value = $this->nodeFactory->createClassConstantReference(
-            $this->entityFqn($node->args[0]->value->value)
-        );
-
-        return $node;
+        $this->methodArgumentAnalyzer = $methodArgumentAnalyzer;
     }
 
     public function getDefinition(): RectorDefinition
@@ -84,19 +54,47 @@ final class AliasToClassRector extends AbstractRector
         ]);
     }
 
+    public function isCandidate(Node $node): bool
+    {
+        if (! $this->methodCallAnalyzer->isMethod($node, 'getRepository')) {
+            return false;
+        }
+
+        if (! $this->methodArgumentAnalyzer->isMethodFirstArgumentString($node)) {
+            return false;
+        }
+
+        return $this->isAliasWithConfiguredEntity($node->args[0]->value->value);
+    }
+
+    public function refactor(Node $node): ?Node
+    {
+        $node->args[0]->value = $this->nodeFactory->createClassConstantReference(
+            $this->convertAliasToFqn($node->args[0]->value->value)
+        );
+
+        return $node;
+    }
+
     private function isAlias(string $name): bool
     {
         return strpos($name, ':') !== false;
     }
 
-    private function entityFqn(string $name): string
+    private function hasAlias(string $name): bool
+    {
+        return isset($this->aliasesToNamespaces[strtok($name, ':')]);
+    }
+
+    private function isAliasWithConfiguredEntity(string $name): bool
+    {
+        return $this->isAlias($name) && $this->hasAlias($name);
+    }
+
+    private function convertAliasToFqn(string $name): string
     {
         [$namespaceAlias, $simpleClassName] = explode(':', $name, 2);
 
-        if (array_key_exists($namespaceAlias, $this->aliasesToNamespaces)) {
-            return sprintf('%s\%s', $this->aliasesToNamespaces[$namespaceAlias], $simpleClassName);
-        }
-
-        return '';
+        return sprintf('%s\%s', $this->aliasesToNamespaces[$namespaceAlias], $simpleClassName);
     }
 }
