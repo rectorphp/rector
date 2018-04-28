99431d7bb4f4368811b68b2dc446aa61b1b2b124
Tomas Votruba <tomas.vot@gmail.com>
2018-04-28 21:27:51 +0200

    fix definition for GetRequestRector

--- src/Rector/Contrib/Symfony/HttpKernel/GetRequestRector.php
+++ src/Rector/Contrib/Symfony/HttpKernel/GetRequestRector.php
@@ -14,18 +14,6 @@ use Rector\Rector\AbstractRector;
 use Rector\RectorDefinition\CodeSample;
 use Rector\RectorDefinition\RectorDefinition;
 
-/**
- * Converts all:
- *     public action()
- *     {
- *         $this->getRequest()->...();
- *
- * into:
- *     public action(Request $request)
- *     {
- *         $request->...();
- *     }
- */
 final class GetRequestRector extends AbstractRector
 {
     /**
@@ -67,27 +55,23 @@ final class GetRequestRector extends AbstractRector
             [
                 new CodeSample(
                     <<<'CODE_SAMPLE'
-class MyCommand extends ContainerAwareCommand
+class SomeController
 {
-    public function someMethod()
+    public function someAction()
     {
-        // ...
-        $this->get('some_service');
+        $this->getRequest()->...();
     }
 }
 CODE_SAMPLE
                     ,
                     <<<'CODE_SAMPLE'
-class MyCommand extends Command
-{
-    public function __construct(SomeService $someService)
-    {
-        $this->someService = $someService;
-    }
+use Symfony\Component\HttpFoundation\Request;
 
-    public function someMethod()
+class SomeController
+{
+    public action(Request $request)
     {
-        $this->someService;
+        $request->...();
     }
 }
 CODE_SAMPLE
