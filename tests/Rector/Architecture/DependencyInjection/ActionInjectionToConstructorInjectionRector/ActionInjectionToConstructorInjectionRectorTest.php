90bfbcf1f06cb52a07d9e12b9c8591b48db4abc0
Tomas Votruba <tomas.vot@gmail.com>
2018-05-07 09:46:00 +0200

    add test case for non-services

--- tests/Rector/Architecture/DependencyInjection/ActionInjectionToConstructorInjectionRector/ActionInjectionToConstructorInjectionRectorTest.php
+++ tests/Rector/Architecture/DependencyInjection/ActionInjectionToConstructorInjectionRector/ActionInjectionToConstructorInjectionRectorTest.php
@@ -21,6 +21,7 @@ final class ActionInjectionToConstructorInjectionRectorTest extends AbstractRect
     public function provideWrongToFixedFiles(): Iterator
     {
         yield [__DIR__ . '/Wrong/wrong.php.inc', __DIR__ . '/Correct/correct.php.inc'];
+        yield [__DIR__ . '/Wrong/wrong2.php.inc', __DIR__ . '/Correct/correct2.php.inc'];
     }
 
     protected function provideConfig(): string
