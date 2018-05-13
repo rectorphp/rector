654fea839ce2ddf10c222822ef1d3fecbc3d54a5
Tomas Votruba <tomas.vot@gmail.com>
2018-05-13 23:13:33 +0200

    prefer

--- bin/publish-prefixed-rector.sh
+++ bin/publish-prefixed-rector.sh
@@ -19,6 +19,6 @@ git fetch origin            # Updates origin/master
 #git rebase origin/master    # Rebases current branch onto origin/master
 
 # https://demisx.github.io/git/rebase/2015/07/02/git-rebase-keep-my-branch-changes.html
-git rebase -Xours origin/master    # Rebases current branch onto origin/master, prefers our "master" in conflicts
+git rebase -Xtheirs origin/master    # Rebases current branch onto origin/master, prefers our "master" in conflicts
 
 git push origin master
