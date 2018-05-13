32a70dce1c94db9d3a5bed93547e3abe04696da5
Tomas Votruba <tomas.vot@gmail.com>
2018-05-13 23:10:54 +0200

    fix cs

--- bin/build-prefixed-rector-composer-json.php
+++ bin/build-prefixed-rector-composer-json.php
@@ -10,16 +10,15 @@ $composerJsonPath = __DIR__ . '/../build/composer.json';
 $composerContent = Json::decode(file_get_contents($composerJsonPath), Json::FORCE_ARRAY);
 
 // remove unused sections
-unset($composerContent['require-dev'], $composerContent['scripts'], $composerContent['config'], $composerContent['autoload-dev']);
+unset($composerContent['require-dev'], $composerContent['scripts'], $composerContent['config'], $composerContent['autoload-dev'], $composerContent['authors']);
 
 // change name
 $composerContent['name'] = 'rector/rector-prefixed';
 
 // keep only requirements on PHP 7.1+
 $composerContent['require'] = [
-    'php' => '^7.1'
+    'php' => '^7.1',
 ];
 
 // save
 file_put_contents($composerJsonPath, Json::encode($composerContent, Json::PRETTY));
-
