8a3bbec4304dae66f8cb26bbe87764dac9f1c528
Tomas Votruba <tomas.vot@gmail.com>
2018-05-28 17:34:39 +0200

    resolve double autoload loading

---
+++ bin/build-prefixed-rector-bin.php
@@ -0,0 +1,20 @@
+#!/usr/bin/env php
+<?php declare(strict_types=1);
+
+use Nette\Utils\Strings;
+
+require_once __DIR__ . '/rector_bootstrap.php';
+
+$binPath = __DIR__ . '/../build/bin/rector';
+
+$binContent = file_get_contents($binPath);
+
+// add constant to make clear the define('RECTOR_PREFIXED', true)
+$binContent = Strings::replace(
+        $binContent,
+        '#namespace RectorPrefixed;#',
+        'namespace RectorPrefixed;' . PHP_EOL . "define('RECTOR_PREFIXED', true);"
+);
+
+// save
+file_put_contents($binPath, $binContent);
--- bin/rector_bootstrap.php
+++ bin/rector_bootstrap.php
@@ -1,24 +1,22 @@
 <?php declare(strict_types=1);
 
 $possibleAutoloadPaths = [
-    // repository
+    // dev repository
     __DIR__ . '/../vendor/autoload.php',
     // composer require
     __DIR__ . '/../../../../vendor/autoload.php',
-    // load from nearest vendor
-    getcwd() . '/vendor/autoload.php',
 ];
 
-$isAutoloadFileLoaded = false;
+// load the project with Prefixed Rector
+if (defined('RECTOR_PREFIXED')) {
+    getcwd() . '/vendor/autoload.php';
+}
+
 foreach ($possibleAutoloadPaths as $possibleAutoloadPath) {
     if (is_file($possibleAutoloadPath)) {
         require_once $possibleAutoloadPath;
-        $isAutoloadFileLoaded = true;
+        return;
     }
-}
-
-if ($isAutoloadFileLoaded) {
-    return;
 }
 
 die(sprintf(
