b214651653fc333d76529250ac6c8b0d1ebdfc95
Tomas Votruba <tomas.vot@gmail.com>
2018-05-28 17:43:30 +0200

    do not load 2 autoloads in dev repository

--- bin/rector_bootstrap.php
+++ bin/rector_bootstrap.php
@@ -1,16 +1,21 @@
 <?php declare(strict_types=1);
 
+$isDevRepository = file_exists(__DIR__ . '/../vendor/autoload.php');
+
+// load the project with Prefixed Rector
+if (defined('RECTOR_PREFIXED') && ! $isDevRepository) {
+    $projectAutoload = getcwd() . '/vendor/autoload.php';
+    if (is_file($projectAutoload)) {
+        require_once $projectAutoload;
+    }
+}
+
 $possibleAutoloadPaths = [
     // dev repository
     __DIR__ . '/../vendor/autoload.php',
     // composer require
     __DIR__ . '/../../../../vendor/autoload.php',
 ];
-
-// load the project with Prefixed Rector
-if (defined('RECTOR_PREFIXED')) {
-    getcwd() . '/vendor/autoload.php';
-}
 
 foreach ($possibleAutoloadPaths as $possibleAutoloadPath) {
     if (is_file($possibleAutoloadPath)) {
