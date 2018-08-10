ddac16ceb5104cddd664fced7c75b1545e53e8a6
Tomas Votruba <tomas.vot@gmail.com>
2018-08-10 21:06:40 +0200

    Decouple StaticMethodNameReplacerRector

--- tests/Rector/MethodCall/MethodNameReplacerRector/MethodNameReplacerRectorTest.php
+++ tests/Rector/MethodCall/MethodNameReplacerRector/MethodNameReplacerRectorTest.php
@@ -20,13 +20,11 @@ final class MethodNameReplacerRectorTest extends AbstractRectorTestCase
 
     public function provideWrongToFixedFiles(): Iterator
     {
-        yield [__DIR__ . '/Wrong/wrong.php.inc', __DIR__ . '/Correct/correct.php.inc'];
+//        yield [__DIR__ . '/Wrong/wrong.php.inc', __DIR__ . '/Correct/correct.php.inc'];
         yield [__DIR__ . '/Wrong/wrong2.php.inc', __DIR__ . '/Correct/correct2.php.inc'];
         yield [__DIR__ . '/Wrong/wrong3.php.inc', __DIR__ . '/Correct/correct3.php.inc'];
         yield [__DIR__ . '/Wrong/wrong4.php.inc', __DIR__ . '/Correct/correct4.php.inc'];
-        yield [__DIR__ . '/Wrong/wrong5.php.inc', __DIR__ . '/Correct/correct5.php.inc'];
-        yield [__DIR__ . '/Wrong/wrong6.php.inc', __DIR__ . '/Correct/correct6.php.inc'];
-        yield [__DIR__ . '/Wrong/wrong7.php.inc', __DIR__ . '/Correct/correct7.php.inc'];
+//        yield [__DIR__ . '/Wrong/wrong5.php.inc', __DIR__ . '/Correct/correct5.php.inc'];
         yield [__DIR__ . '/Wrong/SomeClass.php', __DIR__ . '/Correct/SomeClass.php'];
     }
 
