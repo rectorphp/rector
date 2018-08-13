8dc06a625f78aafee1fe77024a0a36fd27a5aba9
Tomas Votruba <tomas.vot@gmail.com>
2018-08-13 20:40:51 +0200

    improve README

--- bin/clean_levels.sh
+++ bin/clean_levels.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # trailing whitespaces
-sed -i -E 's#\s+$##g' config/level/*/*.yml docs/*.md README.md
+sed -i -E 's#\s+$##g' config/level/*/*.yml docs/*.md packages/*/README.md README.md
