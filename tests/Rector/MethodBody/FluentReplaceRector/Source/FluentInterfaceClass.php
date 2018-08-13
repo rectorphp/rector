930b7ba089c68b86716982e48df84f99de1298b3
Tomas Votruba <tomas.vot@gmail.com>
2018-08-13 12:18:27 +0200

    decouple FluentReplaceRector test and cleanup usage

---
+++ tests/Rector/MethodBody/FluentReplaceRector/Source/FluentInterfaceClass.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types=1);
+
+namespace Rector\Tests\Rector\MethodBody\FluentReplaceRector\Source;
+
+final class FluentInterfaceClass
+{
+
+}
--- tests/Rector/MethodBody/ReturnThisRemoveRector/ReturnThisRemoveRectorTest.php
+++ tests/Rector/MethodBody/ReturnThisRemoveRector/ReturnThisRemoveRectorTest.php
@@ -1,14 +1,14 @@
 <?php declare(strict_types=1);
 
-namespace Rector\Tests\Rector\MethodBody\FluentReplaceRector;
+namespace Rector\Tests\Rector\MethodBody\ReturnThisRemoveRector;
 
 use Iterator;
 use Rector\Testing\PHPUnit\AbstractRectorTestCase;
 
 /**
- * @see \Rector\Rector\MethodBody\FluentReplaceRector
+ * @see \Rector\Rector\MethodBody\ReturnThisRemoveRector
  */
-final class FluentReplaceRectorTest extends AbstractRectorTestCase
+final class ReturnThisRemoveRectorTest extends AbstractRectorTestCase
 {
     /**
      * @dataProvider provideWrongToFixedFiles()
@@ -21,6 +21,8 @@ final class FluentReplaceRectorTest extends AbstractRectorTestCase
     public function provideWrongToFixedFiles(): Iterator
     {
         yield [__DIR__ . '/Wrong/wrong.php.inc', __DIR__ . '/Correct/correct.php.inc'];
+        yield [__DIR__ . '/Wrong/wrong2.php.inc', __DIR__ . '/Correct/correct2.php.inc'];
+        yield [__DIR__ . '/Wrong/wrong3.php.inc', __DIR__ . '/Correct/correct3.php.inc'];
     }
 
     protected function provideConfig(): string
