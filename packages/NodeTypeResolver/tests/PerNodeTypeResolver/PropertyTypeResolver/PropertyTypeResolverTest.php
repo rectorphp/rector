0a127e79743ef0fe6ed8df1269e06b1ff877b1f9
Tomas Votruba <tomas.vot@gmail.com>
2018-08-13 23:24:23 +0200

    [NodeTypeResolver] Add test for property

---
+++ packages/NodeTypeResolver/tests/PerNodeTypeResolver/PropertyTypeResolver/PropertyTypeResolverTest.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace Rector\NodeTypeResolver\Tests\PerNodeTypeResolver\PropertyTypeResolver;
+
+use Iterator;
+use PhpParser\Node\Stmt\Property;
+use Rector\NodeTypeResolver\Tests\PerNodeTypeResolver\AbstractNodeTypeResolverTest;
+use Rector\NodeTypeResolver\Tests\PerNodeTypeResolver\PropertyTypeResolver\Source\Html;
+
+/**
+ * @covers \Rector\NodeTypeResolver\PerNodeTypeResolver\PropertyTypeResolver
+ */
+final class PropertyTypeResolverTest extends AbstractNodeTypeResolverTest
+{
+    /**
+     * @dataProvider provideData()
+     * @param string[] $expectedTypes
+     */
+    public function test(string $file, int $nodePosition, array $expectedTypes): void
+    {
+        $propertyNodes = $this->getNodesForFileOfType($file, Property::class);
+        $this->assertSame($expectedTypes, $this->nodeTypeResolver->resolve($propertyNodes[$nodePosition]));
+    }
+
+    public function provideData(): Iterator
+    {
+        yield [__DIR__ . '/Source/ClassWithProperties.php', 0, [Html::class]];
+    }
+}
---
+++ packages/NodeTypeResolver/tests/PerNodeTypeResolver/PropertyTypeResolver/Source/ClassWithProperties.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+namespace Rector\NodeTypeResolver\Tests\PerNodeTypeResolver\PropertyTypeResolver\Source;
+
+final class MethodParamDocBlock
+{
+    /**
+     * @var Html
+     */
+    public $htmlProperty;
+}
