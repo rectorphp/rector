972e134625a96aca07dd820d4dce5005be19a434
Tomas Votruba <tomas.vot@gmail.com>
2018-08-29 19:22:59 +0200

    use sys_get_temp_dir instead of relative random path

--- src/Autoloading/AdditionalAutoloader.php
+++ src/Autoloading/AdditionalAutoloader.php
@@ -74,7 +74,7 @@ final class AdditionalAutoloader
         $robotLoader = new RobotLoader();
         $robotLoader->ignoreDirs[] = '*Fixtures';
         // last argument is workaround: https://github.com/nette/robot-loader/issues/12
-        $robotLoader->setTempDirectory(__DIR__ . '/../../temp/_rector_robot_loader');
+        $robotLoader->setTempDirectory(sys_get_temp_dir() . '/_rector_robot_loader');
 
         foreach ($directories as $autoloadDirectory) {
             $robotLoader->addDirectory($autoloadDirectory);
