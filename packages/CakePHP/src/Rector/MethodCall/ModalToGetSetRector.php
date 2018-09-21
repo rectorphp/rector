5e533766e6dd5a2d98ea7746278c57a277055d14
Tomas Votruba <tomas.vot@gmail.com>
2018-09-21 08:00:36 +0200

    add ref

--- packages/CakePHP/src/Rector/MethodCall/ModalToGetSetRector.php
+++ packages/CakePHP/src/Rector/MethodCall/ModalToGetSetRector.php
@@ -6,6 +6,9 @@ use PhpParser\Node;
 use Rector\Rector\AbstractRector;
 use Rector\RectorDefinition\RectorDefinition;
 
+/**
+ * @see https://book.cakephp.org/3.0/en/appendices/3-4-migration-guide.html#deprecated-combined-get-set-methods
+ */
 final class ModalToGetSetRector extends AbstractRector
 {
     public function getDefinition(): RectorDefinition
