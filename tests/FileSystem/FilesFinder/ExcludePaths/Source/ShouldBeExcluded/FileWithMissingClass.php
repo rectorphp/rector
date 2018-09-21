73c663b711bf63d96bf6b116e71657c5b67f1da8
Tomas Votruba <tomas.vot@gmail.com>
2018-09-21 13:44:52 +0200

    fix paths

--- tests/FileSystem/FilesFinder/ExcludePaths/Source/ShouldBeExcluded/FileWithMissingClass.php
+++ tests/FileSystem/FilesFinder/ExcludePaths/Source/ShouldBeExcluded/FileWithMissingClass.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Rector\Tests\Configuration\ExcludePaths\ShouldBeExcludedSource;
+namespace Rector\Tests\FileSystem\FilesFinder\ExcludePaths\Source\ShouldBeExcluded;
 
 final class FileWithMissingClass extends ThisClassIsNotHere
 {
--- tests/FileSystem/FilesFinder/ExcludePaths/Source/ThisShouldBeHere.php
+++ tests/FileSystem/FilesFinder/ExcludePaths/Source/ThisShouldBeHere.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Rector\Tests\Configuration\ExcludePaths\Source;
+namespace Rector\Tests\FileSystem\FilesFinder\ExcludePaths\Source;
 
 final class ThisShouldBeHere
 {
