f0ddbbec979fdea00190733829b28f466e54f362
Ben Davies <ben@owsy.com>
2018-09-27 13:21:13 +0100

    add a few test to highlight issues with AnnotatedPropertyInjectToConstructorInjectionRector

---
+++ tests/Rector/Architecture/DependencyInjection/AnnotatedPropertyInjectToConstructorInjectionRector/Source/SomeInterface.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types=1);
+
+namespace Rector\Tests\Rector\Architecture\DependencyInjection\AnnotatedPropertyInjectToConstructorInjectionRector\Source;
+
+interface SomeInterface
+{
+}
