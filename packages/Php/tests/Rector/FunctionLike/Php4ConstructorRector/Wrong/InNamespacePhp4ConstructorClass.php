fb6a3d818bf720111c88e120886984a63d7d6eca
Tomas Votruba <tomas.vot@gmail.com>
2018-10-05 17:13:04 +0800

    [PHP] Add Php4ConstructorRector

---
+++ packages/Php/tests/Rector/FunctionLike/Php4ConstructorRector/Wrong/InNamespacePhp4ConstructorClass.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Rector\Php\Tests\Rector\FunctionLike\Php4ConstructorRector\Wrong;
+
+final class InNamespacePhp4ConstructorClass
+{
+    public function InNamespacePhp4ConstructorClass()
+    {
+    }
+}
