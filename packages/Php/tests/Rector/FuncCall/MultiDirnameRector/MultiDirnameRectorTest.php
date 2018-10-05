9b5a6fb6e055a91e7755c80d34c1ccce32b81a7b
Tomas Votruba <tomas.vot@gmail.com>
2018-10-05 22:09:00 +0800

    [PHP] Add MultiDirnameRector

---
+++ packages/Php/tests/Rector/FuncCall/MultiDirnameRector/MultiDirnameRectorTest.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+namespace Rector\Php\Tests\Rector\FuncCall\MultiDirnameRector;
+
+use Iterator;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+
+/**
+ * @covers \Rector\Php\Rector\FuncCall\MultiDirnameRector
+ *
+ * Some tests copied from:
+ * - https://github.com/FriendsOfPHP/PHP-CS-Fixer/pull/3826/files
+ */
+final class MultiDirnameRectorTest extends AbstractRectorTestCase
+{
+    /**
+     * @dataProvider provideWrongToFixedFiles()
+     */
+    public function test(string $wrong, string $fixed): void
+    {
+        $this->doTestFileMatchesExpectedContent($wrong, $fixed);
+    }
+
+    public function provideWrongToFixedFiles(): Iterator
+    {
+        yield [__DIR__ . '/Wrong/wrong.php.inc', __DIR__ . '/Correct/correct.php.inc'];
+        yield [__DIR__ . '/Wrong/wrong2.php.inc', __DIR__ . '/Correct/correct2.php.inc'];
+        yield [__DIR__ . '/Wrong/wrong3.php.inc', __DIR__ . '/Correct/correct3.php.inc'];
+    }
+
+    protected function provideConfig(): string
+    {
+        return __DIR__ . '/config.yml';
+    }
+}
