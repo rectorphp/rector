1035b04658a2ab56d465e1ef1ca0590e0cc25236
Tomas Votruba <tomas.vot@gmail.com>
2018-10-10 20:51:22 +0800

    [PHP] Add CountOnNullRector

--- packages/NodeTypeResolver/src/NodeTypeAnalyzer.php
+++ packages/NodeTypeResolver/src/NodeTypeAnalyzer.php
@@ -2,11 +2,16 @@
 
 namespace Rector\NodeTypeResolver;
 
+use Countable;
 use PhpParser\Node;
 use PhpParser\Node\Expr;
 use PHPStan\Analyser\Scope;
+use PHPStan\Type\Constant\ConstantArrayType;
+use PHPStan\Type\ObjectType;
 use PHPStan\Type\StringType;
+use PHPStan\Type\Type;
 use Rector\NodeTypeResolver\Node\Attribute;
+use function Safe\class_implements;
 
 final class NodeTypeAnalyzer
 {
@@ -16,11 +21,39 @@ final class NodeTypeAnalyzer
             return false;
         }
 
+        $nodeType = $this->getNodeType($node);
+
+        return $nodeType instanceof StringType;
+    }
+
+    public function isCountableType(Node $node): bool
+    {
+        if (! $node instanceof Expr) {
+            return false;
+        }
+
+        $nodeType = $this->getNodeType($node);
+        if ($nodeType instanceof ConstantArrayType) {
+            return true;
+        }
+
+        if ($nodeType instanceof ObjectType) {
+            $className = $nodeType->getClassName();
+            if (in_array(Countable::class, class_implements($className), true)) {
+                return true;
+            }
+
+            return false;
+        }
+
+        return false;
+    }
+
+    private function getNodeType(Expr $node): Type
+    {
         /** @var Scope $nodeScope */
         $nodeScope = $node->getAttribute(Attribute::SCOPE);
 
-        $nodeType = $nodeScope->getType($node);
-
-        return $nodeType instanceof StringType;
+        return $nodeScope->getType($node);
     }
 }
---
+++ packages/Php/src/Rector/FuncCall/CountOnNullRector.php
@@ -0,0 +1,98 @@
+<?php declare(strict_types=1);
+
+namespace Rector\Php\Rector\FuncCall;
+
+use PhpParser\Node;
+use PhpParser\Node\Arg;
+use PhpParser\Node\Expr\BinaryOp\BooleanOr;
+use PhpParser\Node\Expr\FuncCall;
+use PhpParser\Node\Expr\Instanceof_;
+use PhpParser\Node\Expr\Ternary;
+use PhpParser\Node\Name;
+use PhpParser\Node\Name\FullyQualified;
+use PhpParser\Node\Scalar\LNumber;
+use Rector\NodeTypeResolver\Node\Attribute;
+use Rector\NodeTypeResolver\NodeTypeAnalyzer;
+use Rector\Rector\AbstractRector;
+use Rector\RectorDefinition\CodeSample;
+use Rector\RectorDefinition\RectorDefinition;
+
+/**
+ * @see https://3v4l.org/Bndc9
+ */
+final class CountOnNullRector extends AbstractRector
+{
+    /**
+     * @var NodeTypeAnalyzer
+     */
+    private $nodeTypeAnalyzer;
+
+    public function __construct(NodeTypeAnalyzer $nodeTypeAnalyzer)
+    {
+        $this->nodeTypeAnalyzer = $nodeTypeAnalyzer;
+    }
+
+    public function getDefinition(): RectorDefinition
+    {
+        return new RectorDefinition(
+            'Changes count() on null to safe ternary check',
+            [new CodeSample(
+<<<'CODE_SAMPLE'
+$values = null;
+$count = count($values);
+CODE_SAMPLE
+                ,
+<<<'CODE_SAMPLE'
+$values = null;
+$count = is_array($values) || $values instanceof Countable ? count($values) : 0;
+CODE_SAMPLE
+            )]
+        );
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [FuncCall::class];
+    }
+
+    /**
+     * @param FuncCall $funcCallNode
+     */
+    public function refactor(Node $funcCallNode): ?Node
+    {
+        if ((string) $funcCallNode->name !== 'count') {
+            return $funcCallNode;
+        }
+
+        // check if it has some condition before already, if so, probably it's already handled
+        $parentNode = $funcCallNode->getAttribute(Attribute::PARENT_NODE);
+        if ($parentNode instanceof Ternary) {
+            return $funcCallNode;
+        }
+
+        if (! isset($funcCallNode->args[0])) {
+            return $funcCallNode;
+        }
+
+        $countedNode = $funcCallNode->args[0]->value;
+
+        if ($this->nodeTypeAnalyzer->isCountableType($countedNode)) {
+            return $funcCallNode;
+        }
+
+        $conditionNode = new BooleanOr(
+            new FuncCall(new Name('is_array'), [new Arg($countedNode)]),
+            new Instanceof_($countedNode, new FullyQualified('Countable'))
+        );
+
+        $ternaryNode = new Ternary($conditionNode, $funcCallNode, new LNumber(0));
+
+        // needed to prevent infinity loop re-resolution
+        $funcCallNode->setAttribute(Attribute::PARENT_NODE, $ternaryNode);
+
+        return $ternaryNode;
+    }
+}
