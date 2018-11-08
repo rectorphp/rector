01dab609a328b0d584387cc29c658a53b1677f0e
Tomas Votruba <tomas.vot@gmail.com>
2018-11-08 13:15:00 +0100

    [CodeQuality] Add DeadCodeRemovingRector

---
+++ packages/CodeQuality/src/Rector/Stmt/DeadCodeRemovingRector.php
@@ -0,0 +1,55 @@
+<?php declare(strict_types=1);
+
+namespace Rector\CodeQuality\Rector\Stmt;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\ArrayDimFetch;
+use PhpParser\Node\Expr\PropertyFetch;
+use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Stmt\Expression;
+use Rector\NodeTypeResolver\Node\Attribute;
+use Rector\Rector\AbstractRector;
+use Rector\RectorDefinition\CodeSample;
+use Rector\RectorDefinition\RectorDefinition;
+
+final class DeadCodeRemovingRector extends AbstractRector
+{
+    public function getDefinition(): RectorDefinition
+    {
+        return new RectorDefinition('Removes dead code that is nowhere run', [
+            new CodeSample(
+                <<<'CODE_SAMPLE'
+$value = 5;
+$value;
+CODE_SAMPLE
+                ,
+                <<<'CODE_SAMPLE'
+$value = 5;
+CODE_SAMPLE
+            ),
+        ]);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [Variable::class, PropertyFetch::class, ArrayDimFetch::class];
+    }
+
+    /**
+     * @param Variable $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        $parentNode = $node->getAttribute(Attribute::PARENT_NODE);
+        if (! $parentNode instanceof Expression) {
+            return null;
+        }
+
+        $this->removeNode($node);
+
+        return null;
+    }
+}
