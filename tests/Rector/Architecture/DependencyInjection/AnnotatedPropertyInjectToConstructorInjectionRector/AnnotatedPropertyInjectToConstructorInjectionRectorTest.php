7ac6f1003953e6531eedb995255c0697fb2f9990
Tomas Votruba <tomas.vot@gmail.com>
2018-12-04 14:53:52 +0100

    refactor /tests by JoinTestAndProvideFilesRector
    Signed-off-by: Tomas Votruba <tomas.vot@gmail.com>

--- tests/Rector/Architecture/DependencyInjection/AnnotatedPropertyInjectToConstructorInjectionRector/AnnotatedPropertyInjectToConstructorInjectionRectorTest.php
+++ tests/Rector/Architecture/DependencyInjection/AnnotatedPropertyInjectToConstructorInjectionRector/AnnotatedPropertyInjectToConstructorInjectionRectorTest.php
@@ -2,7 +2,6 @@
 
 namespace Rector\Tests\Rector\Architecture\DependencyInjection\AnnotatedPropertyInjectToConstructorInjectionRector;
 
-use Iterator;
 use Rector\Testing\PHPUnit\AbstractRectorTestCase;
 
 /**
@@ -10,26 +9,26 @@ use Rector\Testing\PHPUnit\AbstractRectorTestCase;
  */
 final class AnnotatedPropertyInjectToConstructorInjectionRectorTest extends AbstractRectorTestCase
 {
-    /**
-     * @dataProvider provideFiles()
-     */
-    public function test(string $wrong, string $fixed): void
+    public function test(): void
     {
-        $this->doTestFileMatchesExpectedContent($wrong, $fixed);
-    }
-
-    public function provideFiles(): Iterator
-    {
-        yield [__DIR__ . '/Wrong/wrong.php.inc', __DIR__ . '/Correct/correct.php.inc'];
-        yield [__DIR__ . '/Wrong/wrong2.php.inc', __DIR__ . '/Correct/correct2.php.inc'];
-        yield [__DIR__ . '/Wrong/wrong3.php.inc', __DIR__ . '/Correct/correct3.php.inc'];
-        yield [__DIR__ . '/Wrong/wrong4.php.inc', __DIR__ . '/Correct/correct4.php.inc'];
-        yield [__DIR__ . '/Wrong/wrong5.php.inc', __DIR__ . '/Correct/correct5.php.inc'];
-        yield [__DIR__ . '/Wrong/wrong6.php.inc', __DIR__ . '/Correct/correct6.php.inc'];
-        yield [__DIR__ . '/Wrong/wrong7.php.inc', __DIR__ . '/Correct/correct7.php.inc'];
-        yield [__DIR__ . '/Wrong/wrong8.php.inc', __DIR__ . '/Correct/correct8.php.inc'];
-        yield [__DIR__ . '/Wrong/wrong9.php.inc', __DIR__ . '/Correct/correct9.php.inc'];
-        yield [__DIR__ . '/Wrong/wrong10.php.inc', __DIR__ . '/Correct/correct10.php.inc'];
+        $this->doTestFiles(
+            [[__DIR__ . '/Wrong/wrong.php.inc', __DIR__ . '/Correct/correct.php.inc'], [
+                __DIR__ . '/Wrong/wrong2.php.inc',
+                __DIR__ . '/Correct/correct2.php.inc',
+            ], [
+                __DIR__ . '/Wrong/wrong3.php.inc',
+                __DIR__ . '/Correct/correct3.php.inc',
+            ], [__DIR__ . '/Wrong/wrong4.php.inc', __DIR__ . '/Correct/correct4.php.inc'], [
+                __DIR__ . '/Wrong/wrong5.php.inc',
+                __DIR__ . '/Correct/correct5.php.inc',
+            ], [__DIR__ . '/Wrong/wrong6.php.inc', __DIR__ . '/Correct/correct6.php.inc'], [
+                __DIR__ . '/Wrong/wrong7.php.inc',
+                __DIR__ . '/Correct/correct7.php.inc',
+            ], [__DIR__ . '/Wrong/wrong8.php.inc', __DIR__ . '/Correct/correct8.php.inc'], [
+                __DIR__ . '/Wrong/wrong9.php.inc',
+                __DIR__ . '/Correct/correct9.php.inc',
+            ], [__DIR__ . '/Wrong/wrong10.php.inc', __DIR__ . '/Correct/correct10.php.inc']]
+        );
     }
 
     protected function provideConfig(): string
--- tests/Rector/Architecture/DoctrineRepositoryAsService/DoctrineRepositoryAsServiceTest.php
+++ tests/Rector/Architecture/DoctrineRepositoryAsService/DoctrineRepositoryAsServiceTest.php
@@ -2,7 +2,6 @@
 
 namespace Rector\Tests\Rector\Architecture\DoctrineRepositoryAsService;
 
-use Iterator;
 use Rector\Testing\PHPUnit\AbstractRectorTestCase;
 
 /**
@@ -12,18 +11,14 @@ use Rector\Testing\PHPUnit\AbstractRectorTestCase;
  */
 final class DoctrineRepositoryAsServiceTest extends AbstractRectorTestCase
 {
-    /**
-     * @dataProvider provideFiles()
-     */
-    public function test(string $wrong, string $fixed): void
+    public function test(): void
     {
-        $this->doTestFileMatchesExpectedContent($wrong, $fixed);
-    }
-
-    public function provideFiles(): Iterator
-    {
-        yield [__DIR__ . '/Wrong/wrong.php.inc', __DIR__ . '/Correct/correct.php.inc'];
-        yield [__DIR__ . '/Wrong/PostController.php', __DIR__ . '/Correct/correct2.php.inc'];
+        $this->doTestFiles(
+            [[__DIR__ . '/Wrong/wrong.php.inc', __DIR__ . '/Correct/correct.php.inc'], [
+                __DIR__ . '/Wrong/PostController.php',
+                __DIR__ . '/Correct/correct2.php.inc',
+            ]]
+        );
     }
 
     protected function provideConfig(): string
