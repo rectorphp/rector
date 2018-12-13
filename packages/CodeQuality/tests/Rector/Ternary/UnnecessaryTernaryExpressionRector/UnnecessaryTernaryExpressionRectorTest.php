b22b3e65e6eb753b80a5f8177c87282e9de41048
Tomas Votruba <tomas.vot@gmail.com>
2018-12-13 19:52:49 +0100

    add bool and non-bool non-binary cases to UnnecessaryTernaryExpressionRector

--- packages/CodeQuality/tests/Rector/Ternary/UnnecessaryTernaryExpressionRector/UnnecessaryTernaryExpressionRectorTest.php
+++ packages/CodeQuality/tests/Rector/Ternary/UnnecessaryTernaryExpressionRector/UnnecessaryTernaryExpressionRectorTest.php
@@ -9,7 +9,11 @@ final class UnnecessaryTernaryExpressionRectorTest extends AbstractRectorTestCas
 {
     public function test(): void
     {
-        $this->doTestFiles([__DIR__ . '/Fixture/fixture.php.inc', __DIR__ . '/Fixture/fixture2.php.inc']);
+        $this->doTestFiles([
+            __DIR__ . '/Fixture/fixture.php.inc',
+            __DIR__ . '/Fixture/fixture2.php.inc',
+            __DIR__ . '/Fixture/fixture3.php.inc',
+        ]);
     }
 
     public function getRectorClass(): string
