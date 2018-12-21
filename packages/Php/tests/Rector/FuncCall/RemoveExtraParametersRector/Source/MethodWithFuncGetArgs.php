67647fd16e810d4fe1dfce2f2d57e29ca05df43c
Tomas Votruba <tomas.vot@gmail.com>
2018-12-21 19:03:46 +0100

    add external variadic fix + detect root function name only

---
+++ packages/Php/tests/Rector/FuncCall/RemoveExtraParametersRector/Source/MethodWithFuncGetArgs.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace Rector\Php\Tests\Rector\FuncCall\RemoveExtraParametersRector\Source;
+
+final class MethodWithFuncGetArgs
+{
+    public static function call($me)
+    {
+        $us = func_get_args();
+    }
+
+    public static function betterCall($me)
+    {
+        $us = better_func_get_args();
+    }
+}
