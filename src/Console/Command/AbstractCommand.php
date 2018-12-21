bbf118fb693a8e23f920a87ddd23d739436a0542
Tomas Votruba <tomas.vot@gmail.com>
2018-12-21 11:25:27 +0100

    describe command on invalid argument

--- src/Console/Command/AbstractCommand.php
+++ src/Console/Command/AbstractCommand.php
@@ -2,12 +2,45 @@
 
 namespace Rector\Console\Command;
 
+use Nette\Utils\Strings;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Descriptor\TextDescriptor;
+use Symfony\Component\Console\Exception\RuntimeException;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symplify\PackageBuilder\Console\ShellCode;
 
 abstract class AbstractCommand extends Command
 {
+    /**
+     * @var TextDescriptor
+     */
+    private $textDescriptor;
+
+    /**
+     * @required
+     */
+    public function setDescriptor(TextDescriptor $textDescriptor): void
+    {
+        $this->textDescriptor = $textDescriptor;
+    }
+
+    public function run(InputInterface $input, OutputInterface $output): int
+    {
+        // show help on arguments fail
+        try {
+            return parent::run($input, $output);
+        } catch (RuntimeException $throwable) {
+            if (Strings::match($throwable->getMessage(), '#Not enough arguments#')) {
+                $this->textDescriptor->describe($output, $this);
+
+                return ShellCode::SUCCESS;
+            }
+
+            throw $throwable;
+        }
+    }
+
     protected function initialize(InputInterface $input, OutputInterface $output): void
     {
         if ($input->getOption('debug')) {
