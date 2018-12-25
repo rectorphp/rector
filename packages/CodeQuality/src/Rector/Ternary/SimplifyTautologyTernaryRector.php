1c093821a348631cce1f7d122e1418bae3070ef7
Tomas Votruba <tomas.vot@gmail.com>
2018-12-25 18:51:46 +0100

    [CodeQuality] Add SimplifyTautologyTernaryRector

---
+++ packages/CodeQuality/src/Rector/Ternary/SimplifyTautologyTernaryRector.php
@@ -0,0 +1,69 @@
+<?php declare(strict_types=1);
+
+namespace Rector\CodeQuality\Rector\Ternary;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\BinaryOp\Identical;
+use PhpParser\Node\Expr\BinaryOp\NotIdentical;
+use PhpParser\Node\Expr\Ternary;
+use Rector\PhpParser\Node\Maintainer\BinaryOpMaintainer;
+use Rector\Rector\AbstractRector;
+use Rector\RectorDefinition\CodeSample;
+use Rector\RectorDefinition\RectorDefinition;
+
+final class SimplifyTautologyTernaryRector extends AbstractRector
+{
+    /**
+     * @var BinaryOpMaintainer
+     */
+    private $binaryOpMaintainer;
+
+    public function __construct(BinaryOpMaintainer $binaryOpMaintainer)
+    {
+        $this->binaryOpMaintainer = $binaryOpMaintainer;
+    }
+
+    public function getDefinition(): RectorDefinition
+    {
+        return new RectorDefinition('Simplify tautology ternary to value', [
+            new CodeSample(
+                '$value = ($fullyQualifiedTypeHint !== $typeHint) ? $fullyQualifiedTypeHint : $typeHint;',
+                '$value = $fullyQualifiedTypeHint;'
+            ),
+        ]);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [Ternary::class];
+    }
+
+    /**
+     * @param Ternary $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if (! $node->cond instanceof NotIdentical && ! $node->cond instanceof Identical) {
+            return null;
+        }
+
+        $isMatch = $this->binaryOpMaintainer->matchFirstAndSecondConditionNode(
+            $node->cond,
+            function (Node $leftNode) use ($node) {
+                return $this->areNodesEqual($leftNode, $node->if);
+            },
+            function (Node $leftNode) use ($node) {
+                return $this->areNodesEqual($leftNode, $node->else);
+            }
+        );
+
+        if ($isMatch === null) {
+            return null;
+        }
+
+        return $node->if;
+    }
+}
