f9a94edb26d4b36b0b7f950e479b6871dbbe5309
Tomas Votruba <tomas.vot@gmail.com>
2019-01-02 22:24:00 +0100

    fix cs

--- packages/CodingStyle/tests/Rector/Use_/RemoveUnusedAliasRector/RemoveUnusedAliasRectorTest.php
+++ packages/CodingStyle/tests/Rector/Use_/RemoveUnusedAliasRector/RemoveUnusedAliasRectorTest.php
@@ -9,10 +9,7 @@ final class RemoveUnusedAliasRectorTest extends AbstractRectorTestCase
 {
     public function test(): void
     {
-        $this->doTestFiles([
-            __DIR__ . '/Fixture/fixture.php.inc',
-            __DIR__ . '/Fixture/used.php.inc',
-        ]);
+        $this->doTestFiles([__DIR__ . '/Fixture/fixture.php.inc', __DIR__ . '/Fixture/used.php.inc']);
     }
 
     protected function getRectorClass(): string
