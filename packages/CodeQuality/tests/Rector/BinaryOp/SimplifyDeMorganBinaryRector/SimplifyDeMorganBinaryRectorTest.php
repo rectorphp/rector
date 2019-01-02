6d7ed86cb8cbccd0e51b853aae739b1d5d40ab72
Tomas Votruba <tomas.vot@gmail.com>
2019-01-02 14:04:29 +0100

    Fix empty class in SimplifyDeMorganBinaryRector [closes #915]

--- packages/CodeQuality/tests/Rector/BinaryOp/SimplifyDeMorganBinaryRector/SimplifyDeMorganBinaryRectorTest.php
+++ packages/CodeQuality/tests/Rector/BinaryOp/SimplifyDeMorganBinaryRector/SimplifyDeMorganBinaryRectorTest.php
@@ -9,7 +9,7 @@ final class SimplifyDeMorganBinaryRectorTest extends AbstractRectorTestCase
 {
     public function test(): void
     {
-        $this->doTestFiles([__DIR__ . '/Fixture/fixture.php.inc']);
+        $this->doTestFiles([__DIR__ . '/Fixture/fixture.php.inc', __DIR__ . '/Fixture/binary_and.php.inc']);
     }
 
     protected function getRectorClass(): string
