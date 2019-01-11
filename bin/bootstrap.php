e9037cf180a3034e258062d3c4f6219d100f943f
Lars Moelleken <lars.moelleken@vdmg-it.com>
2019-01-11 18:57:55 +0100

    [+]: try to fix issue #942 v3

--- bin/bootstrap.php
+++ bin/bootstrap.php
@@ -1,5 +1,25 @@
 <?php declare(strict_types=1);
 
+function includeProjectsAutoload(string $composerJsonPath, string $cwd): void
+{
+    $contents = file_get_contents($composerJsonPath);
+
+    $composerSettings = json_decode($contents, true);
+    if (!is_array($composerSettings)) {
+        fwrite(STDERR, "Failed to load '${composerJsonPath}'\n");
+        exit(1);
+    }
+
+    $vendorPath = $composerSettings['config']['vendor-dir'] ?? $cwd . '/vendor';
+    if (!is_dir($vendorPath)) {
+        fwrite(STDERR, "Please check if 'composer.phar install' was run already (expected to find '${vendorPath}')\n");
+        exit(1);
+    }
+
+    /** @noinspection PhpIncludeInspection */
+    require $vendorPath . '/autoload.php';
+}
+
 $cwd = getcwd();
 
 $projectAutoload = $cwd . '/vendor/autoload.php';
@@ -25,29 +45,17 @@ foreach ($possibleAutoloadPaths as $possibleAutoloadPath) {
     }
 }
 
-$composer_json_path = $cwd . '/composer.json';
-if (file_exists($composer_json_path)) {
-    $contents = file_get_contents($composer_json_path);
-
-    $composer_settings = json_decode($contents, true);
-    if (!is_array($composer_settings)) {
-        fwrite(STDERR, "Failed to load '$composer_json_path'\n");
-        exit(1);
-    }
-
-    $vendor_path = $composer_settings['config']['vendor-dir'] ?? $cwd . '/vendor';
-    if (!is_dir($vendor_path)) {
-        fwrite(STDERR, "Please check if 'composer.phar install' was run already (expected to find '$vendor_path')\n");
-        exit(1);
-    }
-
-    /** @noinspection PhpIncludeInspection */
-    require $vendor_path . '/autoload.php';
+$composerJsonPath = $cwd . '/composer.json';
+if (file_exists($composerJsonPath)) {
+    includeProjectsAutoload($composerJsonPath, $cwd);
     return;
 }
 
-fwrite(STDERR, sprintf(
-    'Composer autoload.php was not found in paths "%s". Have you ran "composer update"?',
-    implode('", "', $possibleAutoloadPaths)
-));
+fwrite(
+    STDERR,
+    sprintf(
+        'Composer autoload.php was not found in paths "%s". Have you ran "composer update"?',
+        implode('", "', $possibleAutoloadPaths)
+    )
+);
 exit(1);
