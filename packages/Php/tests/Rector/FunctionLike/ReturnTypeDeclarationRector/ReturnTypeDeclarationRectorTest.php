09cd48568f6911ff2c4846d040bc86b11f8692d1
Tomas Votruba <tomas.vot@gmail.com>
2019-01-21 02:07:22 +0100

    Add void to ReturnTypeDeclarationRector

--- packages/Php/tests/Rector/FunctionLike/ReturnTypeDeclarationRector/ReturnTypeDeclarationRectorTest.php
+++ packages/Php/tests/Rector/FunctionLike/ReturnTypeDeclarationRector/ReturnTypeDeclarationRectorTest.php
@@ -11,6 +11,8 @@ final class ReturnTypeDeclarationRectorTest extends AbstractRectorTestCase
     {
         $integrationFiles = [
             // static types
+            __DIR__ . '/Fixture/void_type.php.inc',
+            __DIR__ . '/Fixture/no_void_abstract.php.inc',
             __DIR__ . '/Fixture/code_over_doc_priority.php.inc',
             __DIR__ . '/Fixture/known_static.php.inc',
             __DIR__ . '/Fixture/known_static_conflicts.php.inc',
@@ -20,26 +22,21 @@ final class ReturnTypeDeclarationRectorTest extends AbstractRectorTestCase
             // various
             __DIR__ . '/Fixture/known_static_nullable.php.inc',
             __DIR__ . '/Fixture/known_float.php.inc',
-            __DIR__ . '/Fixture/variadic.php.inc',
-            __DIR__ . '/Fixture/mixed.php.inc',
             __DIR__ . '/Fixture/trait_interface.php.inc',
             __DIR__ . '/Fixture/this.php.inc',
             __DIR__ . '/Fixture/false.php.inc',
             __DIR__ . '/Fixture/complex_array.php.inc',
             // php cs fixer return set - https://github.com/Slamdunk/PHP-CS-Fixer/blob/d7a409c10d0e21bc847efb26552aa65bb3c61547/tests/Fixer/FunctionNotation/PhpdocToReturnTypeFixerTest.php
-            __DIR__ . '/Fixture/php-cs-fixer-return/no_doc_return.php.inc',
             __DIR__ . '/Fixture/php-cs-fixer-return/invalid_class.php.inc',
             __DIR__ . '/Fixture/php-cs-fixer-return/invalid_return.php.inc',
             __DIR__ . '/Fixture/php-cs-fixer-return/blacklisted_class_methods.php.inc',
             __DIR__ . '/Fixture/php-cs-fixer-return/various.php.inc',
             __DIR__ . '/Fixture/php-cs-fixer-return/various_2.php.inc',
-            __DIR__ . '/Fixture/php-cs-fixer-return/various_3.php.inc',
             __DIR__ . '/Fixture/php-cs-fixer-return/self_static.php.inc',
             __DIR__ . '/Fixture/php-cs-fixer-return/arrays.php.inc',
             __DIR__ . '/Fixture/php-cs-fixer-return/skip.php.inc',
             __DIR__ . '/Fixture/php-cs-fixer-return/nullables.php.inc',
             // nikic set - https://github.com/nikic/TypeUtil/
-            __DIR__ . '/Fixture/nikic/basic.php.inc',
             __DIR__ . '/Fixture/nikic/inheritance.php.inc',
             __DIR__ . '/Fixture/nikic/iterable.php.inc',
             __DIR__ . '/Fixture/nikic/name_resolution.php.inc',
@@ -52,12 +49,10 @@ final class ReturnTypeDeclarationRectorTest extends AbstractRectorTestCase
             __DIR__ . '/Fixture/nikic/self_parent_static.php.inc',
             __DIR__ . '/Fixture/nikic/unsupported_types.php.inc',
             // dunglas set - https://github.com/dunglas/phpdoc-to-typehint/
-            __DIR__ . '/Fixture/dunglas/array_no_types.php.inc',
             __DIR__ . '/Fixture/dunglas/BarInterface.php.inc',
             __DIR__ . '/Fixture/dunglas/BazTrait.php.inc',
             __DIR__ . '/Fixture/dunglas/Child.php.inc',
             __DIR__ . '/Fixture/dunglas/nullable_types.php.inc',
-            __DIR__ . '/Fixture/dunglas/param_no_type.php.inc',
         ];
 
         $this->doTestFiles($integrationFiles);
