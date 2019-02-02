81493d3cae0ea8c90d58f215ddbeb8ebeabe567f
Tomas Votruba <tomas.vot@gmail.com>
2019-02-02 01:33:39 +0100

    add .phpstorm.meta.php

---
+++ .phpstorm.meta.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types=1);
+
+// see https://confluence.jetbrains.com/display/PhpStorm/PhpStorm+Advanced+Metadata
+namespace PHPSTORM_META;
+
+// $container->get(Type::class) → instance of "Type"
+override(\Psr\Container\ContainerInterface::get(0), type(0));
