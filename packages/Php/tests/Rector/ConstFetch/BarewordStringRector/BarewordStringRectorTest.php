59bba92f5c7a3e7749ccc186a72b58c488334bef
Tomas Votruba <tomas.vot@gmail.com>
2019-02-19 16:48:29 +0100

    add define test [closes #1068]

--- packages/Php/tests/Rector/ConstFetch/BarewordStringRector/BarewordStringRectorTest.php
+++ packages/Php/tests/Rector/ConstFetch/BarewordStringRector/BarewordStringRectorTest.php
@@ -9,7 +9,10 @@ final class BarewordStringRectorTest extends AbstractRectorTestCase
 {
     public function test(): void
     {
-        $this->doTestFilesWithoutAutoload([__DIR__ . '/Fixture/fixture.php.inc']);
+        $this->doTestFilesWithoutAutoload([
+            __DIR__ . '/Fixture/fixture.php.inc',
+            __DIR__ . '/Fixture/define.php.inc',
+        ]);
     }
 
     public function getRectorClass(): string
