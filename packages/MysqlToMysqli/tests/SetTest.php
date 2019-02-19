5764c81de646e80bd3f9d3344776397230d0073e
Tomas Votruba <tomas.vot@gmail.com>
2019-02-19 16:09:53 +0100

    add SetTest

---
+++ packages/MysqlToMysqli/tests/SetTest.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace Rector\MysqlToMysqli\Tests;
+
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+
+final class SetTest extends AbstractRectorTestCase
+{
+    public function test(): void
+    {
+        $this->doTestFiles([
+            __DIR__ . '/Fixture/SetFixture.php.inc',
+        ]);
+    }
+
+    protected function provideConfig(): string
+    {
+        return __DIR__ . '/../../../config/level/database-migration/mysql-to-mysqli.yaml';
+    }
+}
