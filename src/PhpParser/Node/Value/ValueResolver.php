87d3333b0980d09822ecc9228ebf2a776c72c79e
Kevin R <22906111+cgkkevinr@users.noreply.github.com>
2019-02-27 15:07:22 +0100

    Change type hint for resolveClassConstFetch to mixed
    Rector throws an exception when it has to inspect a constant with an
    integer value. See: #1127.
    
    Signed-off-by: Kevin R <22906111+cgkkevinr@users.noreply.github.com>

--- src/PhpParser/Node/Value/ValueResolver.php
+++ src/PhpParser/Node/Value/ValueResolver.php
@@ -50,7 +50,7 @@ final class ValueResolver
             return $this->constExprEvaluator;
         }
 
-        $this->constExprEvaluator = new ConstExprEvaluator(function (Expr $expr): ?string {
+        $this->constExprEvaluator = new ConstExprEvaluator(function (Expr $expr) {
             if ($expr instanceof Dir) {
                 // __DIR__
                 return $this->resolveDirConstant($expr);
@@ -92,7 +92,14 @@ final class ValueResolver
         return $fileInfo->getPathname();
     }
 
-    private function resolveClassConstFetch(ClassConstFetch $classConstFetch): string
+    /**
+     * @param ClassConstFetch $classConstFetch
+     *
+     * @return mixed
+     * @throws ShouldNotHappenException
+     * @throws \PhpParser\ConstExprEvaluationException
+     */
+    private function resolveClassConstFetch(ClassConstFetch $classConstFetch)
     {
         $class = $this->nameResolver->resolve($classConstFetch->class);
         $constant = $this->nameResolver->resolve($classConstFetch->name);
