e7cc751849dc4db5186ff23ebb7a3a9d1aaae5b4
Tomas Votruba <tomas.vot@gmail.com>
2019-03-10 15:31:14 +0000

    [Laravel] Add request() to injection

--- packages/Laravel/tests/Rector/StaticCall/RequestStaticValidateToInjectRector/RequestStaticValidateToInjectRectorTest.php
+++ packages/Laravel/tests/Rector/StaticCall/RequestStaticValidateToInjectRector/RequestStaticValidateToInjectRectorTest.php
@@ -9,7 +9,7 @@ final class RequestStaticValidateToInjectRectorTest extends AbstractRectorTestCa
 {
     public function test(): void
     {
-        $this->doTestFiles([__DIR__ . '/Fixture/fixture.php.inc']);
+        $this->doTestFiles([__DIR__ . '/Fixture/fixture.php.inc', __DIR__ . '/Fixture/function.php.inc']);
     }
 
     protected function getRectorClass(): string
