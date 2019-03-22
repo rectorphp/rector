5de72390b43873293ebbccfc2818fb16f03839c9
Tomas Votruba <tomas.vot@gmail.com>
2019-03-22 21:30:15 +0100

    add PHPUnit void type check

---
+++ packages/PhpSpecToPHPUnit/src/PHPUnitTypeDeclarationDecorator.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace Rector\PhpSpecToPHPUnit;
+
+use PhpParser\Node\Identifier;
+use PhpParser\Node\Stmt\ClassMethod;
+use ReflectionMethod;
+
+/**
+ * Decorate setUp() and tearDown() with "void" when local TestClass class uses them
+ */
+final class PHPUnitTypeDeclarationDecorator
+{
+    public function decorate(ClassMethod $classMethod): void
+    {
+        if (! class_exists('PHPUnit\Framework\TestCase')) {
+            return;
+        }
+
+        $reflectionMethod = new ReflectionMethod('PHPUnit\Framework\TestCase', 'setUp');
+        if (! $reflectionMethod->hasReturnType()) {
+            return;
+        }
+
+        $returnType = (string) $reflectionMethod->getReturnType();
+        $classMethod->returnType = new Identifier($returnType);
+    }
+}
