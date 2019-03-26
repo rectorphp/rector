368a57256344a5ba0f1af7e26e6568df942e4816
Tomas Votruba <tomas.vot@gmail.com>
2019-03-26 12:38:07 +0100

    [DeadCode] Improve RemoveDuplicatedArrayKeyRector to clear items, that are ignored

--- packages/DeadCode/tests/Rector/Array_/RemoveDuplicatedArrayKeyRector/RemoveDuplicatedArrayKeyRectorTest.php
+++ packages/DeadCode/tests/Rector/Array_/RemoveDuplicatedArrayKeyRector/RemoveDuplicatedArrayKeyRectorTest.php
@@ -9,7 +9,7 @@ final class RemoveDuplicatedArrayKeyRectorTest extends AbstractRectorTestCase
 {
     public function test(): void
     {
-        $this->doTestFiles([__DIR__ . '/Fixture/fixture.php.inc']);
+        $this->doTestFiles([__DIR__ . '/Fixture/fixture.php.inc', __DIR__ . '/Fixture/last_man_standing.php.inc']);
     }
 
     protected function getRectorClass(): string
