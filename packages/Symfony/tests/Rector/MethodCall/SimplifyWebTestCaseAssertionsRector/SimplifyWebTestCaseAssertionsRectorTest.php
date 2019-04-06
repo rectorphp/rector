18b09f1aef453181740feddfcb9950cfad75115c
Tomas Votruba <tomas.vot@gmail.com>
2019-04-06 20:35:41 +0200

    [Symfony 4.3] Add SimplifyWebTestCaseAssertionsRector

---
+++ packages/Symfony/tests/Rector/MethodCall/SimplifyWebTestCaseAssertionsRector/SimplifyWebTestCaseAssertionsRectorTest.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+namespace Rector\Symfony\Tests\Rector\MethodCall\SimplifyWebTestCaseAssertionsRector;
+
+use Rector\Symfony\Rector\MethodCall\SimplifyWebTestCaseAssertionsRector;
+use Rector\Symfony\Tests\Rector\MethodCall\SimplifyWebTestCaseAssertionsRector\Source\FixtureWebTestCase;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+
+final class SimplifyWebTestCaseAssertionsRectorTest extends AbstractRectorTestCase
+{
+    public function test(): void
+    {
+        $this->doTestFiles([
+            __DIR__ . '/Fixture/fixture.php.inc',
+            __DIR__ . '/Fixture/response_code_same.php.inc',
+        ]);
+    }
+
+    protected function getRectorClass(): string
+    {
+        return SimplifyWebTestCaseAssertionsRector::class;
+    }
+
+    /**
+     * @return mixed[]|null
+     */
+    protected function getRectorConfiguration(): ?array
+    {
+        return [
+            '$webTestCaseClass' => FixtureWebTestCase::class,
+        ];
+    }
+}
