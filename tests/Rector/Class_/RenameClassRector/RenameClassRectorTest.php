9958a44ec351e0e471b6d595ddb59bd157a7465b
Jan Mikeš <j.mikes@me.com>
2019-05-03 16:14:53 +0200

    Added support for renaming class and it’s namespace

--- tests/Rector/Class_/RenameClassRector/RenameClassRectorTest.php
+++ tests/Rector/Class_/RenameClassRector/RenameClassRectorTest.php
@@ -2,6 +2,7 @@
 
 namespace Rector\Tests\Rector\Class_\RenameClassRector;
 
+use Iterator;
 use Manual\Twig\TwigFilter;
 use Manual_Twig_Filter;
 use Rector\Rector\Class_\RenameClassRector;
@@ -17,17 +18,31 @@ use Rector\Tests\Rector\Class_\RenameClassRector\Source\OldClassWithTypo;
  */
 final class RenameClassRectorTest extends AbstractRectorTestCase
 {
-    public function test(): void
+    /**
+     * @dataProvider provideTestFiles
+     */
+    public function test(string $filePath): void
     {
-        $this->doTestFiles([
-            __DIR__ . '/Fixture/class_to_new.php.inc',
-            __DIR__ . '/Fixture/class_to_interface.php.inc',
-            __DIR__ . '/Fixture/interface_to_class.php.inc',
-            __DIR__ . '/Fixture/name_insensitive.php.inc',
-            __DIR__ . '/Fixture/twig_case.php.inc',
-            __DIR__ . '/Fixture/underscore_doc.php.inc',
-            __DIR__ . '/Fixture/keep_return_tag.php.inc',
-        ]);
+        $this->doTestFile($filePath);
+    }
+
+    public function provideTestFiles(): Iterator
+    {
+        yield [__DIR__ . '/Fixture/class_to_new.php.inc'];
+        yield [__DIR__ . '/Fixture/class_to_interface.php.inc'];
+        yield [__DIR__ . '/Fixture/interface_to_class.php.inc'];
+        yield [__DIR__ . '/Fixture/name_insensitive.php.inc'];
+        yield [__DIR__ . '/Fixture/twig_case.php.inc'];
+        yield [__DIR__ . '/Fixture/underscore_doc.php.inc'];
+        yield [__DIR__ . '/Fixture/keep_return_tag.php.inc'];
+
+        // Renaming class itself and its namespace
+        yield [__DIR__ . '/Fixture/rename_class_without_namespace.php.inc'];
+        yield [__DIR__ . '/Fixture/rename_class.php.inc'];
+        yield [__DIR__ . '/Fixture/rename_interface.php.inc'];
+        yield [__DIR__ . '/Fixture/rename_trait.php.inc'];
+        yield [__DIR__ . '/Fixture/rename_class_without_namespace_to_class_without_namespace.php.inc'];
+        yield [__DIR__ . '/Fixture/rename_class_to_class_without_namespace.php.inc'];
     }
 
     /**
@@ -44,6 +59,13 @@ final class RenameClassRectorTest extends AbstractRectorTestCase
                 Manual_Twig_Filter::class => TwigFilter::class,
                 'Twig_AbstractManualExtension' => AbstractManualExtension::class,
                 'Twig_Extension_Sandbox' => 'Twig\Extension\SandboxExtension',
+                // Renaming class itself and its namespace
+                'MyNamespace\MyClass' => 'MyNewNamespace\MyNewClass',
+                'MyNamespace\MyTrait' => 'MyNewNamespace\MyNewTrait',
+                'MyNamespace\MyInterface' => 'MyNewNamespace\MyNewInterface',
+                'MyOldClass' => 'MyNamespace\MyNewClass',
+                'AnotherMyOldClass' => 'AnotherMyNewClass',
+                'MyNamespace\AnotherMyClass' => 'MyNewClassWithoutNamespace',
             ],
         ]];
     }
