08868e457d19837161414fbf6020dbd61c83f4c4
Tomas Votruba <tomas.vot@gmail.com>
2019-05-26 01:15:21 +0200

    [CodingStyle] Add CatchExceptionNameMatchingTypeRector

--- packages/CodingStyle/src/Naming/ClassNaming.php
+++ packages/CodingStyle/src/Naming/ClassNaming.php
@@ -3,14 +3,38 @@
 namespace Rector\CodingStyle\Naming;
 
 use Nette\Utils\Strings;
+use PhpParser\Node\Name;
+use Rector\Exception\ShouldNotHappenException;
+use Rector\PhpParser\Node\Resolver\NameResolver;
 
 final class ClassNaming
 {
-    public function getShortName(string $fullyQualifiedName): string
-    {
-        $fullyQualifiedName = trim($fullyQualifiedName, '\\');
+    /**
+     * @var NameResolver
+     */
+    private $nameResolver;
 
-        return Strings::after($fullyQualifiedName, '\\', -1) ?: $fullyQualifiedName;
+    public function __construct(NameResolver $nameResolver)
+    {
+        $this->nameResolver = $nameResolver;
+    }
+
+    /**
+     * @param string|Name $name
+     * @return string
+     */
+    public function getShortName($name): string
+    {
+        if ($name instanceof Name) {
+            $name = $this->nameResolver->resolve($name);
+            if ($name === null) {
+                throw new ShouldNotHappenException();
+            }
+        }
+
+        $name = trim($name, '\\');
+
+        return Strings::after($name, '\\', -1) ?: $name;
     }
 
     public function getNamespace(string $fullyQualifiedName): ?string
