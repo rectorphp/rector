16c60344a678cd96ad13cc8424d4b910ca62d618
Tomas Votruba <tomas.vot@gmail.com>
2019-06-05 14:10:39 +0300

    [DeadCode] Add RemoveConcatAutocastRector

---
+++ packages/DeadCode/src/Rector/Concat/RemoveConcatAutocastRector.php
@@ -0,0 +1,63 @@
+<?php declare(strict_types=1);
+
+namespace Rector\DeadCode\Rector\Concat;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\BinaryOp\Concat;
+use Rector\Rector\AbstractRector;
+use Rector\RectorDefinition\CodeSample;
+use Rector\RectorDefinition\RectorDefinition;
+
+final class RemoveConcatAutocastRector extends AbstractRector
+{
+    public function getDefinition(): RectorDefinition
+    {
+        return new RectorDefinition('Remove (string) casting when it comes to concat, that does this by default', [
+            new CodeSample(
+                <<<'CODE_SAMPLE'
+class SomeConcatingClass
+{
+    public function run($value)
+    {
+        return 'hi ' . (string) $value;
+    }
+}
+CODE_SAMPLE
+                ,
+                <<<'CODE_SAMPLE'
+class SomeConcatingClass
+{
+    public function run($value)
+    {
+        return 'hi ' . $value;
+    }
+}
+CODE_SAMPLE
+            ),
+        ]);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [Concat::class];
+    }
+
+    /**
+     * @param Concat $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        $node->left = $this->removeStringCast($node->left);
+        $node->right = $this->removeStringCast($node->right);
+
+        return $node;
+    }
+
+    private function removeStringCast(Node\Expr $expr): Node
+    {
+        return $expr instanceof Node\Expr\Cast\String_ ? $expr->expr : $expr;
+    }
+}
