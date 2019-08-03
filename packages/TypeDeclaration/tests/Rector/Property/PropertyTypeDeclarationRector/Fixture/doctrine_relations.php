cbdd92e98354101e87599188760b4cb0a3529744
Ben Davies <ben.davies@gmail.com>
2019-08-03 21:40:01 +0100

    add failing testcase for PropertyTypeDeclarationRector

---
+++ packages/TypeDeclaration/tests/Rector/Property/PropertyTypeDeclarationRector/Fixture/doctrine_relations.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Rector\TypeDeclaration\Tests\Rector\FunctionLike\PropertyTypeDeclarationRector\Fixture {
+    class Product
+    {
+    }
+
+    class Car
+    {
+    }
+}
+
+namespace Rector\TypeDeclaration\Tests\Rector\FunctionLike\PropertyTypeDeclarationRector\Fixture\Entity {
+    class Company
+    {
+    }
+}
--- packages/TypeDeclaration/tests/Rector/Property/PropertyTypeDeclarationRector/PropertyTypeDeclarationRectorTest.php
+++ packages/TypeDeclaration/tests/Rector/Property/PropertyTypeDeclarationRector/PropertyTypeDeclarationRectorTest.php
@@ -17,6 +17,7 @@ final class PropertyTypeDeclarationRectorTest extends AbstractRectorTestCase
             __DIR__ . '/Fixture/default_value.php.inc',
             __DIR__ . '/Fixture/doctrine_column.php.inc',
             __DIR__ . '/Fixture/doctrine_relation.php.inc',
+            __DIR__ . '/Fixture/doctrine_relation_imported.php.inc',
             // get and set
             __DIR__ . '/Fixture/complex.php.inc',
             __DIR__ . '/Fixture/single_nullable_return.php.inc',
