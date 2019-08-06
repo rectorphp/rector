9341f4fca5eb5cae3cd099e21d456c904913e923
Tomas Votruba <tomas.vot@gmail.com>
2019-08-06 17:57:09 +0200

    skip abstract parent methods in RemoveUnusedDoctrineEntityMethodAndPropertyRector

--- packages/DeadCode/src/UnusedNodeResolver/ClassUnusedPrivateClassMethodResolver.php
+++ packages/DeadCode/src/UnusedNodeResolver/ClassUnusedPrivateClassMethodResolver.php
@@ -7,6 +7,7 @@ use PhpParser\Node\Stmt\Class_;
 use Rector\NodeContainer\ParsedNodesByType;
 use Rector\PhpParser\Node\Manipulator\ClassManipulator;
 use Rector\PhpParser\Node\Resolver\NameResolver;
+use ReflectionMethod;
 
 final class ClassUnusedPrivateClassMethodResolver
 {
@@ -61,7 +62,9 @@ final class ClassUnusedPrivateClassMethodResolver
 
         $unusedMethods = $this->filterOutSystemMethods($unusedMethods);
 
-        return $this->filterOutInterfaceRequiredMethods($class, $unusedMethods);
+        $unusedMethods = $this->filterOutInterfaceRequiredMethods($class, $unusedMethods);
+
+        return $this->filterOutParentAbstractMethods($class, $unusedMethods);
     }
 
     /**
@@ -102,5 +105,44 @@ final class ClassUnusedPrivateClassMethodResolver
         }
 
         return array_diff($unusedMethods, $interfaceMethods);
+    }
+
+    /**
+     * @param string[] $unusedMethods
+     * @return string[]
+     */
+    private function filterOutParentAbstractMethods(Class_ $class, array $unusedMethods): array
+    {
+        if ($class->extends === null) {
+            return $unusedMethods;
+        }
+
+        $parentClasses = class_parents($class);
+
+        $parentAbstractMethods = [];
+
+        foreach ($parentClasses as $parentClass) {
+            foreach ($unusedMethods as $unusedMethod) {
+                if (in_array($unusedMethod, $parentAbstractMethods, true)) {
+                    continue;
+                }
+
+                if ($this->isMethodAbstract($parentClass, $unusedMethod)) {
+                    $parentAbstractMethods[] = $unusedMethod;
+                }
+            }
+        }
+
+        return array_diff($unusedMethods, $parentAbstractMethods);
+    }
+
+    private function isMethodAbstract(string $class, string $method): bool
+    {
+        if (! method_exists($class, $method)) {
+            return false;
+        }
+        $methodReflection = new ReflectionMethod($class, $method);
+
+        return $methodReflection->isAbstract();
     }
 }
