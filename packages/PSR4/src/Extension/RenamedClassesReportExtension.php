987645516355fc39eb3b704894b9f8bd864c4925
Tomas Votruba <tomas.vot@gmail.com>
2019-08-21 11:52:12 +0200

    [PSR4] Improve renamed classes collector to sort by highest parent

--- packages/PSR4/src/Extension/RenamedClassesReportExtension.php
+++ packages/PSR4/src/Extension/RenamedClassesReportExtension.php
@@ -3,7 +3,6 @@
 namespace Rector\PSR4\Extension;
 
 use Nette\Utils\FileSystem;
-use Nette\Utils\Strings;
 use PhpParser\Node\Arg;
 use PhpParser\Node\Expr\FuncCall;
 use PhpParser\Node\Name;
@@ -12,6 +11,7 @@ use PhpParser\Node\Stmt\Expression;
 use Rector\Contract\Extension\ReportingExtensionInterface;
 use Rector\PhpParser\Printer\BetterStandardPrinter;
 use Rector\PSR4\Collector\RenamedClassesCollector;
+use Rector\PSR4\ValueObject\ClassRenameValueObject;
 use Rector\Rector\Class_\RenameClassRector;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Yaml\Yaml;
@@ -45,72 +45,64 @@ final class RenamedClassesReportExtension implements ReportingExtensionInterface
 
     public function run(): void
     {
-        if ($this->renamedClassesCollector->getRenamedClasses() === []) {
+        if (! $this->renamedClassesCollector->hasClassRenames()) {
             return;
         }
+
+        // 1. dump rector config for RenameClassRector
+        $rectorYamlContent = $this->createRectorYamlContent();
+        FileSystem::write(getcwd() . '/rename-classes-rector.yaml', $rectorYamlContent);
+
+        // 2. dump class aliases
+        $renameClassesAliasesContent = $this->createRenameClassAliasContent();
+        FileSystem::write(getcwd() . DIRECTORY_SEPARATOR . 'rename-classes-aliases.php', $renameClassesAliasesContent);
+
+        // 3. tell user what to do next
+        $this->symfonyStyle->warning(
+            'Run: "vendor/bin/rector process src tests --config rename-classes-rector.yaml --autoload-file rename-classes-aliases.php" to finish the process'
+        );
+    }
+
+    /**
+     * @param ClassRenameValueObject[] $classRenames
+     * @return Expression[]
+     */
+    private function createClassAliasNodes(array $classRenames): array
+    {
+        $nodes = [];
+        foreach ($classRenames as $classRename) {
+            $classAlias = new FuncCall(new Name('class_alias'));
+            $classAlias->args[] = new Arg(new String_($classRename->getNewClass()));
+            $classAlias->args[] = new Arg(new String_($classRename->getOldClass()));
+
+            $nodes[] = new Expression($classAlias);
+        }
+
+        return $nodes;
+    }
+
+    private function createRectorYamlContent(): string
+    {
+        $oldToNewClasses = $this->renamedClassesCollector->getOldToNewClassesSortedByHighestParentsAsString();
 
         $data = [
             # rector.yaml
             'services' => [
                 RenameClassRector::class => [
-                    '$oldToNewClasses' => $this->renamedClassesCollector->getRenamedClasses(),
+                    '$oldToNewClasses' => $oldToNewClasses,
                 ],
             ],
         ];
 
-        // 1.
-        $yaml = Yaml::dump($data, 5);
-        FileSystem::write(getcwd() . '/rename-classes-rector.yaml', $yaml);
-
-        $nodes = [];
-
-        $renamedClasses = $this->sortInterfaceFirst($this->renamedClassesCollector->getRenamedClasses());
-        foreach ($renamedClasses as $oldClass => $newClass) {
-            $classAlias = new FuncCall(new Name('class_alias'));
-            $classAlias->args[] = new Arg(new String_($newClass));
-            $classAlias->args[] = new Arg(new String_($oldClass));
-
-            $nodes[] = new Expression($classAlias);
-        }
-
-        // 2.
-        $aliasesContent = '<?php' . PHP_EOL . PHP_EOL . $this->betterStandardPrinter->print($nodes);
-        FileSystem::write(getcwd() . '/rename-classes-aliases.php', $aliasesContent);
-
-        // @todo shell exec!?
-        $this->symfonyStyle->warning(
-            'Run: "vendor/bin/rector process tests --config rename-classes-rector.yaml --autoload-file rename-classes-aliases.php" to finish the process'
-        );
+        return Yaml::dump($data, 10);
     }
 
-    /**
-     * Interfaces needs to be aliased first, as aliased class needs to have an aliased interface that implements.
-     * PHP will crash with not very helpful "missing interface" error otherwise.
-     *
-     * Also abstract classes and traits, as other code depens on them.
-     *
-     * @param string[] $types
-     * @return string[]
-     */
-    private function sortInterfaceFirst(array $types): array
+    private function createRenameClassAliasContent(): string
     {
-        $interfaces = [];
-        $abstractClasses = [];
-        $traits = [];
-        $classes = [];
+        $classRenames = $this->renamedClassesCollector->getClassRenamesSortedByHighestParents();
 
-        foreach ($types as $oldType => $newType) {
-            if (Strings::endsWith($oldType, 'Interface')) {
-                $interfaces[$oldType] = $newType;
-            } elseif (Strings::contains($oldType, 'Abstract')) {
-                $abstractClasses[$oldType] = $newType;
-            } elseif (Strings::contains($oldType, 'Trait')) {
-                $traits[$oldType] = $newType;
-            } else {
-                $classes[$oldType] = $newType;
-            }
-        }
+        $classAliasContent = $this->betterStandardPrinter->print($this->createClassAliasNodes($classRenames));
 
-        return array_merge($interfaces, $traits, $abstractClasses, $classes);
+        return '<?php' . PHP_EOL . PHP_EOL . $classAliasContent;
     }
 }
