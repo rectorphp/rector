f492007f37eb810875a9cf8c5a8ff7a49f98f044
Tomas Votruba <tomas.vot@gmail.com>
2019-08-24 12:44:41 +0200

    move Contributor tools to utils

---
+++ utils/ContributorTools/src/Command/DumpRectorsCommand.php
@@ -0,0 +1,65 @@
+<?php declare(strict_types=1);
+
+namespace Rector\Utils\ContributorTools\Command;
+
+use Rector\Console\Command\AbstractCommand;
+use Rector\Console\Shell;
+use Rector\Utils\ContributorTools\Contract\OutputFormatter\DumpRectorsOutputFormatterInterface;
+use Rector\Utils\ContributorTools\Finder\RectorsFinder;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symplify\PackageBuilder\Console\Command\CommandNaming;
+
+final class DumpRectorsCommand extends AbstractCommand
+{
+    /**
+     * @var RectorsFinder
+     */
+    private $rectorsFinder;
+
+    /**
+     * @var DumpRectorsOutputFormatterInterface[]
+     */
+    private $dumpRectorsOutputFormatterInterfaces = [];
+
+    /**
+     * @param DumpRectorsOutputFormatterInterface[] $dumpRectorsOutputFormatterInterfaces
+     */
+    public function __construct(RectorsFinder $rectorsFinder, array $dumpRectorsOutputFormatterInterfaces)
+    {
+        parent::__construct();
+
+        $this->rectorsFinder = $rectorsFinder;
+        $this->dumpRectorsOutputFormatterInterfaces = $dumpRectorsOutputFormatterInterfaces;
+    }
+
+    protected function configure(): void
+    {
+        $this->setName(CommandNaming::classToName(self::class));
+        $this->setDescription('Dump overview of all Rectors');
+        $this->addOption(
+            'output-format',
+            'o',
+            InputOption::VALUE_REQUIRED,
+            'Output format for Rectors [json, markdown]',
+            'markdown'
+        );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $packageRectors = $this->rectorsFinder->findInDirectory(__DIR__ . '/../../../../packages');
+        $generalRectors = $this->rectorsFinder->findInDirectory(__DIR__ . '/../../../../src');
+
+        foreach ($this->dumpRectorsOutputFormatterInterfaces as $outputFormatter) {
+            if ($outputFormatter->getName() !== $input->getOption('output-format')) {
+                continue;
+            }
+
+            $outputFormatter->format($generalRectors, $packageRectors);
+        }
+
+        return Shell::CODE_SUCCESS;
+    }
+}
---
+++ utils/ContributorTools/src/Contract/OutputFormatter/DumpRectorsOutputFormatterInterface.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace Rector\Utils\ContributorTools\Contract\OutputFormatter;
+
+use Rector\Contract\Rector\RectorInterface;
+
+interface DumpRectorsOutputFormatterInterface
+{
+    public function getName(): string;
+
+    /**
+     * @param RectorInterface[] $genericRectors
+     * @param RectorInterface[] $packageRectors
+     */
+    public function format(array $genericRectors, array $packageRectors): void;
+}
