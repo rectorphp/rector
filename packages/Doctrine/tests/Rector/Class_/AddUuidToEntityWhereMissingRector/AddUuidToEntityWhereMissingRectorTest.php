71893e27253c7285c37be573b2ca41af88b70ae7
Tomas Votruba <tomas.vot@gmail.com>
2019-09-10 11:53:30 +0200

    use data provider [skip ci]

--- packages/Doctrine/tests/Rector/Class_/AddUuidToEntityWhereMissingRector/AddUuidToEntityWhereMissingRectorTest.php
+++ packages/Doctrine/tests/Rector/Class_/AddUuidToEntityWhereMissingRector/AddUuidToEntityWhereMissingRectorTest.php
@@ -7,19 +7,28 @@ use Rector\Testing\PHPUnit\AbstractRectorTestCase;
 
 final class AddUuidToEntityWhereMissingRectorTest extends AbstractRectorTestCase
 {
-    public function test(): void
+    /**
+     * @dataProvider provideDataForTest()
+     */
+    public function test(string $file): void
     {
-        $this->doTestFiles([
-            __DIR__ . '/Fixture/fixture.php.inc',
-            __DIR__ . '/Fixture/already_has_constructor.php.inc',
-            __DIR__ . '/Fixture/process_string_id.php.inc',
-            __DIR__ . '/Fixture/with_parent_constructor.php.inc',
-            __DIR__ . '/Fixture/add_single_table_inheritance.php.inc',
-            __DIR__ . '/Fixture/add_single_table_inheritance_with_identifier.php.inc',
-            // skip
-            __DIR__ . '/Fixture/skip_id_with_uuid_type.php.inc',
-            __DIR__ . '/Fixture/skip_id_with_uuid_binary_type.php.inc',
-        ]);
+        $this->doTestFile($file);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function provideDataForTest(): iterable
+    {
+        yield [__DIR__ . '/Fixture/fixture.php.inc'];
+        yield [__DIR__ . '/Fixture/already_has_constructor.php.inc'];
+        yield [__DIR__ . '/Fixture/process_string_id.php.inc'];
+        yield [__DIR__ . '/Fixture/with_parent_constructor.php.inc'];
+        yield [__DIR__ . '/Fixture/add_single_table_inheritance.php.inc'];
+        yield [__DIR__ . '/Fixture/add_single_table_inheritance_with_identifier.php.inc'];
+        // skip
+        yield [__DIR__ . '/Fixture/skip_id_with_uuid_type.php.inc'];
+        yield [__DIR__ . '/Fixture/skip_id_with_uuid_binary_type.php.inc'];
     }
 
     protected function getRectorClass(): string
