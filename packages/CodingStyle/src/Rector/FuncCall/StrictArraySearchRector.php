4b9120c952ec64631a07797f80baf68b669c0111
Jeroen Herczeg <jeroen@herczeg.be>
2019-09-28 00:34:34 +0200

    Added StrictArraySearchRector - Issue #2009

---
+++ packages/CodingStyle/src/Rector/FuncCall/StrictArraySearchRector.php
@@ -0,0 +1,46 @@
+<?php declare(strict_types=1);
+
+namespace Rector\CodingStyle\Rector\FuncCall;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\FuncCall;
+use Rector\Rector\AbstractRector;
+use Rector\RectorDefinition\CodeSample;
+use Rector\RectorDefinition\RectorDefinition;
+
+/**
+ * @see \Rector\CodingStyle\Tests\Rector\FuncCall\StrictArraySearchRector\StrictArraySearchRectorTest
+ */
+final class StrictArraySearchRector extends AbstractRector
+{
+    public function getDefinition(): RectorDefinition
+    {
+        return new RectorDefinition('Makes array_search search for identical elements', [
+            new CodeSample('array_search($value, $items);', 'array_search($value, $items, true);'),
+        ]);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [FuncCall::class];
+    }
+
+    /**
+     * @param FuncCall $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if (! $this->isName($node, 'array_search')) {
+            return null;
+        }
+
+        if (count($node->args) === 2) {
+            $node->args[2] = $this->createArg($this->createTrue());
+        }
+
+        return $node;
+    }
+}
