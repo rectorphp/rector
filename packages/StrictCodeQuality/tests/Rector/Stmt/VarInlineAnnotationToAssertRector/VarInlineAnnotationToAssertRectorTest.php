ff5403aa8b8c9dafc5c2e82a13943fbfc8781b2b
Tomas Votruba <tomas.vot@gmail.com>
2019-10-08 18:58:32 +0100

    skip property and missing  name

--- packages/StrictCodeQuality/tests/Rector/Stmt/VarInlineAnnotationToAssertRector/VarInlineAnnotationToAssertRectorTest.php
+++ packages/StrictCodeQuality/tests/Rector/Stmt/VarInlineAnnotationToAssertRector/VarInlineAnnotationToAssertRectorTest.php
@@ -21,6 +21,8 @@ final class VarInlineAnnotationToAssertRectorTest extends AbstractRectorTestCase
         yield [__DIR__ . '/Fixture/fixture.php.inc'];
         yield [__DIR__ . '/Fixture/scalar.php.inc'];
         yield [__DIR__ . '/Fixture/assign_fresh_var.php.inc'];
+        yield [__DIR__ . '/Fixture/skip_missing_variable.php.inc'];
+        yield [__DIR__ . '/Fixture/skip_property.php.inc'];
     }
 
     protected function getRectorClass(): string
