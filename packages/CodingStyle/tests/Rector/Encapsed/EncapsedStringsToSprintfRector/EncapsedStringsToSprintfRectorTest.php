1cf50c6aaf416e6fe2205a4e4eaf37cceebc3454
Dorian Villet <gnutix@gmail.com>
2019-10-09 14:59:30 +0200

    Add a failing test for EncapsedStringsToSprintfRector.

--- packages/CodingStyle/tests/Rector/Encapsed/EncapsedStringsToSprintfRector/EncapsedStringsToSprintfRectorTest.php
+++ packages/CodingStyle/tests/Rector/Encapsed/EncapsedStringsToSprintfRector/EncapsedStringsToSprintfRectorTest.php
@@ -19,6 +19,7 @@ final class EncapsedStringsToSprintfRectorTest extends AbstractRectorTestCase
     public function provideDataForTest(): Iterator
     {
         yield [__DIR__ . '/Fixture/fixture.php.inc'];
+        yield [__DIR__ . '/Fixture/fixture2.php.inc'];
         yield [__DIR__ . '/Fixture/numberz.php.inc'];
     }
 
