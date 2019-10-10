a35f94474768641c77f1dedb9163c649207b4758
Tomas Votruba <tomas.vot@gmail.com>
2019-10-10 17:32:18 +0100

    add ScreenFileCommand

---
+++ sample/ScreenSample.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace App;
+
+final class ScreenSample
+{
+    public function run()
+    {
+        $this->mute();
+    }
+
+    public function mute()
+    {
+    }
+}
