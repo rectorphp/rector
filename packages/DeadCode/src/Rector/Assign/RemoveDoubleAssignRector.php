b7a1f130b54f330602edf52719a8d8e6fd1eeaa7
Jeroen Smit <j.smit@hypotheekbond.nl>
2019-11-01 21:53:08 +0100

    Added typehint

--- packages/DeadCode/src/Rector/Assign/RemoveDoubleAssignRector.php
+++ packages/DeadCode/src/Rector/Assign/RemoveDoubleAssignRector.php
@@ -160,7 +160,7 @@ PHP
 
     private function isSelfReferencing(Assign $assign): bool
     {
-        return (bool) $this->betterNodeFinder->findFirst($assign->expr, function ($subNode) use ($assign) {
+        return (bool) $this->betterNodeFinder->findFirst($assign->expr, function ($subNode) use ($assign) : bool {
             return $this->areNodesEqual($assign->var, $subNode);
         });
     }
