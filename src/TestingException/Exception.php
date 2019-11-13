5a3ee90421548e20ed4e00b0e27de21cf2d90b3d
Jan Mikeš <j.mikes@me.com>
2019-11-13 21:47:20 +0100

    Test weird behaviour

---
+++ src/TestingException/Exception.php
@@ -0,0 +1,7 @@
+<?php declare (strict_types=1);
+
+namespace Rector\TestingException;
+
+final class Exception extends \Exception
+{
+}
---
+++ src/TestingException/SomeException.php
@@ -0,0 +1,8 @@
+<?php declare (strict_types=1);
+
+namespace Rector\TestingException;
+
+final class SomeException
+{
+
+}
