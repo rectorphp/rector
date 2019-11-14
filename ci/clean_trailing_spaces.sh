ea35ed9568242fe9357adf8a8e40410c6ed47f08
Jeroen Smit <j.smit@hypotheekbond.nl>
2019-11-14 15:02:18 +0100

    Fixed sed command

--- ci/clean_trailing_spaces.sh
+++ ci/clean_trailing_spaces.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 # trailing whitespaces
-sed -i -E 's#\s+$##g' config/set/*/*.yaml docs/*.md README.md
+
+#see https://stackoverflow.com/questions/2320564/sed-i-command-for-in-place-editing-to-work-with-both-gnu-sed-and-bsd-osx
+if sed --version >/dev/null 2>&1; then
+  #GNU sed (common to linux)
+  sed -i -E 's#\\s+$##g' config/set/*/*.yaml docs/*.md README.md;
+else
+  #BSD sed (common to osX)
+  sed -i '' -E 's#\\s+$##g' config/set/*/*.yaml docs/*.md README.md
+fi
