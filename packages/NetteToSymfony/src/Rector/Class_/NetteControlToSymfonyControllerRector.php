f2260c1d521e620424dd9a059b36fe56d6571613
TomasVotruba <tomas.vot@gmail.com>
2019-11-24 12:42:21 +0100

    add support for assign cleanup

--- packages/NetteToSymfony/src/Rector/Class_/NetteControlToSymfonyControllerRector.php
+++ packages/NetteToSymfony/src/Rector/Class_/NetteControlToSymfonyControllerRector.php
@@ -14,15 +14,19 @@ use PhpParser\Node\Expr\ArrayItem;
 use PhpParser\Node\Expr\Assign;
 use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Expr\PropertyFetch;
+use PhpParser\Node\Expr\Variable;
 use PhpParser\Node\Identifier;
 use PhpParser\Node\Name\FullyQualified;
 use PhpParser\Node\Scalar\String_;
 use PhpParser\Node\Stmt\Class_;
 use PhpParser\Node\Stmt\ClassMethod;
-use PhpParser\Node\Stmt\Expression;
+use PhpParser\Node\Stmt\Return_;
 use Rector\Rector\AbstractRector;
 use Rector\RectorDefinition\CodeSample;
 use Rector\RectorDefinition\RectorDefinition;
+use Rector\ValueObject\PhpVersionFeature;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @see https://doc.nette.org/en/2.4/components
@@ -61,12 +65,14 @@ PHP
                 ,
                 <<<'PHP'
 use Nette\Application\UI\Control;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
-class SomeController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
+class SomeController extends AbstractController
 {
-     public function some()
+     public function some(): Response
      {
-         $this->render(__DIR__ . '/poll.latte', ['param' => 'some value']);
+         return $this->render(__DIR__ . '/poll.latte', ['param' => 'some value']);
      }
 }
 PHP
@@ -92,20 +98,22 @@ PHP
             return null;
         }
 
+        // skip presenter
+        if ($this->isName($node->name, '*Presenter')) {
+            return null;
+        }
+
         if (! $this->isObjectType($node, Control::class)) {
             return null;
         }
 
         $className = $node->name->toString();
 
-        // @todo decouple to removeSuffix, removePrefix
-        if (Strings::endsWith($className, 'Control')) {
-            $className = Strings::substring($className, 0, -Strings::length('Control'));
-        }
+        $className = $this->removeSuffix($className, 'Control');
         $className .= 'Controller';
-        $node->name = new Identifier($className);
 
-        $node->extends = new FullyQualified('Symfony\Bundle\FrameworkBundle\Controller\AbstractController');
+        $node->name = new Identifier($className);
+        $node->extends = new FullyQualified(AbstractController::class);
 
         $renderMethod = $node->getMethod('render');
         if ($renderMethod !== null) {
@@ -120,7 +128,7 @@ PHP
         // rename method - @todo pick
         $classMethod->name = new Identifier('action');
 
-        $this->collectTemplateFileNameAndVariables($classMethod);
+        $this->collectTemplateFileNameAndVariablesAndRemoveDeadCode($classMethod);
 
         $thisRenderMethod = $this->createMethodCall('this', 'render');
 
@@ -132,29 +140,27 @@ PHP
             $thisRenderMethod->args[1] = new Arg($this->createTemplateVariablesArray());
         }
 
-        $classMethod->stmts[] = new Expression($thisRenderMethod);
+        // add return in the end
+        $return = new Return_($thisRenderMethod);
+        $classMethod->stmts[] = $return;
+
+        if ($this->isAtLeastPhpVersion(PhpVersionFeature::SCALAR_TYPES)) {
+            $classMethod->returnType = new FullyQualified(Response::class);
+        }
     }
 
-    private function collectTemplateFileNameAndVariables(ClassMethod $classMethod): void
+    private function collectTemplateFileNameAndVariablesAndRemoveDeadCode(ClassMethod $classMethod): void
     {
         $this->templateFileExpr = null;
         $this->templateVariables = [];
 
         $this->traverseNodesWithCallable((array) $classMethod->stmts, function (Node $node): void {
-            if ($node instanceof MethodCall && $this->isName($node->name, 'render')) {
-                $this->templateFileExpr = $node->args[0]->value;
-                $this->removeNode($node);
+            if ($node instanceof MethodCall) {
+                $this->collectTemplateFileExpr($node);
             }
 
-            if ($node instanceof Assign && $node->var instanceof PropertyFetch) {
-                if (! $this->isName($node->var->var, 'template')) {
-                    return;
-                }
-
-                $variableName = $this->getName($node->var);
-                $this->templateVariables[$variableName] = $node->expr;
-
-                $this->removeNode($node);
+            if ($node instanceof Assign) {
+                $this->collectVariableFromAssign($node);
             }
         });
     }
@@ -162,10 +168,79 @@ PHP
     private function createTemplateVariablesArray(): Array_
     {
         $array = new Array_();
+
         foreach ($this->templateVariables as $name => $node) {
             $array->items[] = new ArrayItem($node, new String_($name));
         }
 
         return $array;
+    }
+
+    /**
+     * Looks for:
+     * $this->template
+     */
+    private function isTemplatePropertyFetch(Expr $expr): bool
+    {
+        if (! $expr instanceof PropertyFetch) {
+            return false;
+        }
+
+        if (! $expr->var instanceof Variable) {
+            return false;
+        }
+
+        if (! $this->isName($expr->var, 'this')) {
+            return false;
+        }
+
+        return $this->isName($expr->name, 'template');
+    }
+
+    private function collectTemplateFileExpr(MethodCall $methodCall): void
+    {
+        if ($this->isName($methodCall->name, 'render')) {
+            if (isset($methodCall->args[0])) {
+                $this->templateFileExpr = $methodCall->args[0]->value;
+            }
+
+            $this->removeNode($methodCall);
+        }
+
+        if ($this->isName($methodCall->name, 'setFile')) {
+            $this->templateFileExpr = $methodCall->args[0]->value;
+            $this->removeNode($methodCall);
+        }
+    }
+
+    private function collectVariableFromAssign(Assign $assign): void
+    {
+        // $this->template = x
+        if ($assign->var instanceof PropertyFetch) {
+            if (! $this->isName($assign->var->var, 'template')) {
+                return;
+            }
+
+            $variableName = $this->getName($assign->var);
+            $this->templateVariables[$variableName] = $assign->expr;
+
+            $this->removeNode($assign);
+        }
+
+        // $x = $this->template
+        if ($assign->var instanceof Variable) {
+            if ($this->isTemplatePropertyFetch($assign->expr)) {
+                $this->removeNode($assign);
+            }
+        }
+    }
+
+    private function removeSuffix(string $content, string $suffix): string
+    {
+        if (! Strings::endsWith($content, $suffix)) {
+            return $content;
+        }
+
+        return Strings::substring($content, 0, -Strings::length($suffix));
     }
 }
