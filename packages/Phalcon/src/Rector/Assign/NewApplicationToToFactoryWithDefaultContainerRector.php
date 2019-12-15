1b4dc68a81787b312b3d355e54dc85790b863ceb
TomasVotruba <tomas.vot@gmail.com>
2019-12-15 23:26:13 +0100

    [Phalcon 4.0] Add NewApplicationToToFactoryWithDefaultContainerRector

---
+++ packages/Phalcon/src/Rector/Assign/NewApplicationToToFactoryWithDefaultContainerRector.php
@@ -0,0 +1,106 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Phalcon\Rector\Assign;
+
+use PhpParser\Node;
+use PhpParser\Node\Arg;
+use PhpParser\Node\Expr;
+use PhpParser\Node\Expr\Assign;
+use PhpParser\Node\Expr\New_;
+use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Name\FullyQualified;
+use Rector\Rector\AbstractRector;
+use Rector\RectorDefinition\CodeSample;
+use Rector\RectorDefinition\RectorDefinition;
+
+/**
+ * @see https://github.com/rectorphp/rector/issues/2408
+ *
+ * @see \Rector\Phalcon\Tests\Rector\Assign\NewApplicationToToFactoryWithDefaultContainerRector\NewApplicationToToFactoryWithDefaultContainerRectorTest
+ */
+final class NewApplicationToToFactoryWithDefaultContainerRector extends AbstractRector
+{
+    public function getDefinition(): RectorDefinition
+    {
+        return new RectorDefinition('Change new application to default factory with application', [
+            new CodeSample(
+                <<<'PHP'
+class SomeClass
+{
+    public function run($di)
+    {
+        $application = new \Phalcon\Mvc\Application($di);
+
+        $response = $application->handle();
+    }
+}
+PHP
+,
+                <<<'PHP'
+class SomeClass
+{
+    public function run($di)
+    {
+        $container = new \Phalcon\Di\FactoryDefault();
+        $application = new \Phalcon\Mvc\Application($container);
+
+        $response = $application->handle($_SERVER["REQUEST_URI"]);
+    }
+}
+PHP
+
+            ),
+        ]);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [Assign::class];
+    }
+
+    /**
+     * @param Assign $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if (! $this->isNewApplication($node->expr)) {
+            return null;
+        }
+
+        if (! $node->expr instanceof New_) {
+            return null;
+        }
+
+        $containerVariable = new Variable('container');
+        $factoryAssign = $this->createNewContainerToFactoryDefaultAssign($containerVariable);
+
+        $node->expr->args = [new Arg($containerVariable)];
+
+        $this->addNodeBeforeNode($factoryAssign, $node);
+
+        return $node;
+    }
+
+    private function isNewApplication(Expr $expr): bool
+    {
+        if (! $expr instanceof New_) {
+            return false;
+        }
+
+        if (! $this->isName($expr->class, 'Phalcon\Mvc\Application')) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private function createNewContainerToFactoryDefaultAssign(Variable $variable): Assign
+    {
+        return new Assign($variable, new New_(new FullyQualified('Phalcon\Di\FactoryDefault')));
+    }
+}
