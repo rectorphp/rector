8cbccfe6614dea6da79a7a7a33c27122d62c15e9
TomasVotruba <tomas.vot@gmail.com>
2019-12-18 10:44:56 +0100

    [TypeDeclaration] Add AddParamTypeDeclarationRector

---
+++ packages/TypeDeclaration/src/Rector/ClassMethod/AddParamTypeDeclarationRector.php
@@ -0,0 +1,175 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\TypeDeclaration\Rector\ClassMethod;
+
+use PhpParser\Node;
+use PhpParser\Node\Param;
+use PhpParser\Node\Stmt\Class_;
+use PhpParser\Node\Stmt\ClassLike;
+use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\Node\Stmt\Interface_;
+use PhpParser\Node\Stmt\Trait_;
+use Rector\NodeTypeResolver\Node\AttributeKey;
+use Rector\Rector\AbstractRector;
+use Rector\RectorDefinition\ConfiguredCodeSample;
+use Rector\RectorDefinition\RectorDefinition;
+
+/**
+ * @see \Rector\TypeDeclaration\Tests\Rector\ClassMethod\AddParamTypeDeclarationRector\AddParamTypeDeclarationRectorTest
+ */
+final class AddParamTypeDeclarationRector extends AbstractRector
+{
+    /**
+     * @var mixed[]
+     */
+    private $typehintForParameterByMethodByClass = [];
+
+    /**
+     * @param mixed[] $typehintForParameterByMethodByClass
+     */
+    public function __construct(array $typehintForParameterByMethodByClass = [])
+    {
+        $this->typehintForParameterByMethodByClass = $typehintForParameterByMethodByClass;
+    }
+
+    public function getDefinition(): RectorDefinition
+    {
+        return new RectorDefinition('Add param types where needed', [
+            new ConfiguredCodeSample(
+                <<<'PHP'
+class SomeClass
+{
+    public function process($name)
+    {
+    }
+}
+PHP
+,
+                <<<'PHP'
+class SomeClass
+{
+    public function process(string $name)
+    {
+    }
+}
+PHP
+
+            , [
+                '$typehintForParameterByMethodByClass' => [
+                    'SomeClass' => [
+                        'process' => [
+                            0 => 'string'
+                        ]
+                    ]
+                ]
+            ])
+        ]);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [ClassMethod::class];
+    }
+
+    /**
+     * @param ClassMethod $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if ($this->shouldSkip($node)) {
+            return null;
+        }
+
+        /** @var ClassLike $class */
+        $class = $node->getAttribute(AttributeKey::CLASS_NODE);
+
+        foreach ($this->typehintForParameterByMethodByClass as $objectType => $typehintForParameterByMethod) {
+            if (! $this->isObjectType($class, $objectType)) {
+                continue;
+            }
+
+            foreach ($typehintForParameterByMethod as $methodName => $typehintByParameterPosition) {
+                if (! $this->isName($node, $methodName)) {
+                    continue;
+                }
+
+                $this->refactorClassMethodWithTypehintByParameterPosition($node, $typehintByParameterPosition);
+            }
+        }
+
+        return $node;
+    }
+
+    private function shouldSkip(ClassMethod $classMethod): bool
+    {
+        // skip class methods without args
+        if (count((array) $classMethod->params) === 0) {
+            return true;
+        }
+
+        /** @var ClassLike|null $class */
+        $class = $classMethod->getAttribute(AttributeKey::CLASS_NODE);
+        if ($class === null) {
+            return true;
+        }
+
+        // skip traits
+        if ($class instanceof Trait_) {
+            return true;
+        }
+
+        // skip class without parents/interfaces
+        if ($class instanceof Class_) {
+            if ($class->implements) {
+                return false;
+            }
+
+            if ($class->extends) {
+                return false;
+            }
+
+            return true;
+        }
+
+        // skip interface without parents
+        return ! (bool) $class->extends;
+    }
+
+    private function refactorParameter(Param $param, string $newType): void
+    {
+        // already set → no change
+        if ($param->type && $this->isName($param->type, $newType)) {
+            return;
+        }
+
+        // remove it
+        if ($newType === '') {
+            $param->type = null;
+            return;
+        }
+
+        $returnTypeNode = $this->staticTypeMapper->mapStringToPhpParserNode($newType);
+        if ($returnTypeNode === null) {
+            return;
+        }
+
+        $param->type = $returnTypeNode;
+    }
+
+    private function refactorClassMethodWithTypehintByParameterPosition(Node $node, $typehintByParameterPosition): void
+    {
+        foreach ($typehintByParameterPosition as $parameterPosition => $type) {
+            if (!isset($node->params[$parameterPosition])) {
+                continue;
+            }
+
+            $parameter = $node->params[$parameterPosition];
+            $this->refactorParameter($parameter, $type);
+        }
+    }
+}
---
+++ packages/TypeDeclaration/tests/Rector/ClassMethod/AddParamTypeDeclarationRector/AddParamTypeDeclarationRectorTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\TypeDeclaration\Tests\Rector\ClassMethod\AddParamTypeDeclarationRector;
+
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddParamTypeDeclarationRector;
+
+final class AddParamTypeDeclarationRectorTest extends AbstractRectorTestCase
+{
+    /**
+     * @dataProvider provideDataForTest()
+     */
+    public function test(string $file): void
+    {
+        $this->doTestFile($file);
+    }
+
+    public function provideDataForTest(): \Iterator
+    {
+        return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
+    }
+
+    protected function getRectorsWithConfiguration(): array
+    {
+        return [
+            AddParamTypeDeclarationRector::class => [
+                '$typehintForParameterByMethodByClass' => [
+                    ParentInterfaceWithChangeType::class => [
+                        'process' => [
+                            0 => 'string'
+                        ]
+                    ]
+                ]
+            ]
+        ];
+    }
+}
