d61a911e6ad7dbc04b976eb81895a7041d13cf58
TomasVotruba <tomas.vot@gmail.com>
2019-12-26 18:02:05 +0100

    [NetteCodeQuality] Add TemplateMagicAssignToExplicitVariableArrayRector

---
+++ packages/Nette/src/ValueObject/MagicTemplatePropertyCalls.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Nette\ValueObject;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr;
+
+final class MagicTemplatePropertyCalls
+{
+    /**
+     * @var Expr|null
+     */
+    private $templateFileExpr;
+
+    /**
+     * @var Node[]
+     */
+    private $templateVariables = [];
+
+    /**
+     * @var Node[]
+     */
+    private $nodesToRemove = [];
+
+    /**
+     * @param Node[] $templateVariables
+     * @param Node[] $nodesToRemove
+     */
+    public function __construct(?Expr $templateFileExpr, array $templateVariables, array $nodesToRemove)
+    {
+        $this->templateFileExpr = $templateFileExpr;
+        $this->templateVariables = $templateVariables;
+        $this->nodesToRemove = $nodesToRemove;
+    }
+
+    public function getTemplateFileExpr(): ?Expr
+    {
+        return $this->templateFileExpr;
+    }
+
+    /**
+     * @return Node[]
+     */
+    public function getTemplateVariables(): array
+    {
+        return $this->templateVariables;
+    }
+
+    /**
+     * @return Node[]
+     */
+    public function getNodesToRemove(): array
+    {
+        return $this->nodesToRemove;
+    }
+}
