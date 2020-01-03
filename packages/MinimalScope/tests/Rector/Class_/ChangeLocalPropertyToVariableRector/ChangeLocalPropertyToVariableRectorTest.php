268480b2fcaa617c14da0dc612e941edee4564c8
TomasVotruba <tomas.vot@gmail.com>
2020-01-04 00:36:19 +0100

    [MinimalScope] Add ChangeLocalPropertyToVariableRector

---
+++ packages/MinimalScope/tests/Rector/Class_/ChangeLocalPropertyToVariableRector/ChangeLocalPropertyToVariableRectorTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\MinimalScope\Tests\Rector\Class_\ChangeLocalPropertyToVariableRector;
+
+use Iterator;
+use Rector\MinimalScope\Rector\Class_\ChangeLocalPropertyToVariableRector;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+
+final class ChangeLocalPropertyToVariableRectorTest extends AbstractRectorTestCase
+{
+    /**
+     * @dataProvider provideDataForTest()
+     */
+    public function test(string $file): void
+    {
+        $this->doTestFile($file);
+    }
+
+    public function provideDataForTest(): Iterator
+    {
+        return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
+    }
+
+    protected function getRectorClass(): string
+    {
+        return ChangeLocalPropertyToVariableRector::class;
+    }
+}
