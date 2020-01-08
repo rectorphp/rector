88519c0cd9024f4c4ecb7b9e564cd1f92e9675ed
Dorian Villet <gnutix@gmail.com>
2020-01-08 21:28:25 +0100

    Add support for PHPStan 0.12+ @template annotation.

---
+++ packages/BetterPhpDocParser/src/Attributes/Ast/PhpDoc/AttributeAwareTemplateTagValueNode.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\BetterPhpDocParser\Attributes\Ast\PhpDoc;
+
+use PHPStan\PhpDocParser\Ast\PhpDoc\TemplateTagValueNode;
+use Rector\BetterPhpDocParser\Attributes\Attribute\AttributeTrait;
+use Rector\BetterPhpDocParser\Contract\PhpDocNode\AttributeAwareNodeInterface;
+
+final class AttributeAwareTemplateTagValueNode extends TemplateTagValueNode implements AttributeAwareNodeInterface
+{
+    use AttributeTrait;
+}
