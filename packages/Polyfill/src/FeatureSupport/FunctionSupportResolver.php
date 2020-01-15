564d05e381cd44288e99e0fd31be77521679368c
Markus Staab <maggus.staab@googlemail.com>
2020-01-15 20:57:39 +0100

    Added list functions supported by the symfony/polyfill* components

--- packages/Polyfill/src/FeatureSupport/FunctionSupportResolver.php
+++ packages/Polyfill/src/FeatureSupport/FunctionSupportResolver.php
@@ -12,8 +12,12 @@ final class FunctionSupportResolver
      * @var string[][]
      */
     private const FUNCTIONS_BY_VERSION = [
-        '5.6' => ['session_abort'],
-        '7.0' => ['random_int', 'random_bytes'],
+        '5.6' => ['session_abort', 'hash_equals', 'ldap_escape'],
+        '7.0' => ['random_int', 'random_bytes', 'intdiv', 'preg_replace_callback_array', 'error_clear_last'],
+        '7.1' => ['is_iterable'],
+        '7.2' => ['spl_object_id', 'stream_isatty'],
+        '7.3' => ['array_key_first', 'array_key_last', 'hrtime', 'is_countable'],
+        '7.4' => ['get_mangled_object_vars', 'mb_str_split', 'password_algos']
     ];
 
     /**
