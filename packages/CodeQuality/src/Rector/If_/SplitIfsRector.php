63bf2330a5de712dc4b7289f8c0a55176aac28a4
Jeroen Smit <j.smit@hypotheekbond.nl>
2020-01-15 21:47:23 +0100

    style

--- packages/CodeQuality/src/Rector/If_/SplitIfsRector.php
+++ packages/CodeQuality/src/Rector/If_/SplitIfsRector.php
@@ -4,19 +4,17 @@ declare(strict_types=1);
 
 namespace Rector\CodeQuality\Rector\If_;
 
+use PhpParser\Node;
+use PhpParser\Node\Expr\Exit_;
+use PhpParser\Node\Stmt\Expression;
 use PhpParser\Node\Stmt\If_;
 use PhpParser\Node\Stmt\Return_;
 use PhpParser\Node\Stmt\Throw_;
-use PhpParser\Node\Stmt\Expression;
-use PhpParser\Node\Expr\Exit_;
-use PhpParser\Node;
-use PhpParser\NodeDumper;
 use Rector\Rector\AbstractRector;
 use Rector\RectorDefinition\CodeSample;
 use Rector\RectorDefinition\RectorDefinition;
 
 /**
-
  * @see \Rector\CodeQuality\Tests\Rector\If_\SplitIfsRector\SplitIfsRectorTest
  */
 final class SplitIfsRector extends AbstractRector
@@ -51,7 +49,7 @@ class SomeClass {
 }
 PHP
 
-            )
+            ),
         ]);
     }
 
@@ -70,7 +68,7 @@ PHP
     {
         $lastStmt = end($node->stmts);
 
-        if (!($lastStmt instanceof Return_
+        if (! ($lastStmt instanceof Return_
             || $lastStmt instanceof Throw_
             || ($lastStmt instanceof Expression && $lastStmt->expr instanceof Exit_))
         ) {
@@ -79,7 +77,7 @@ PHP
 
         if ($node->elseifs) {
             $newNode = new If_($node->cond);
-            $newNode->stmts =  $node->stmts;
+            $newNode->stmts = $node->stmts;
             $this->addNodeBeforeNode($newNode, $node);
             /** @var Node\Stmt\ElseIf_ $firstElseIf */
             $firstElseIf = array_shift($node->elseifs);
