eafea9d1c7d9ba19bdbb2688e67e3d461c3e795c
TomasVotruba <tomas.vot@gmail.com>
2020-01-17 00:40:38 +0100

    [PHPUnit] Add GetMockBuilderGetMockToCreateMockRector

---
+++ packages/PHPUnit/src/Rector/MethodCall/GetMockBuilderGetMockToCreateMockRector.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\PHPUnit\Rector\MethodCall;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\MethodCall;
+use Rector\Rector\AbstractRector;
+use Rector\RectorDefinition\CodeSample;
+use Rector\RectorDefinition\RectorDefinition;
+
+/**
+ * @see https://github.com/lmc-eu/steward/pull/187/files#diff-c7e8c65e59b8b4ff8b54325814d4ba55L80
+ *
+ * @see \Rector\PHPUnit\Tests\Rector\MethodCall\GetMockBuilderGetMockToCreateMockRector\GetMockBuilderGetMockToCreateMockRectorTest
+ */
+final class GetMockBuilderGetMockToCreateMockRector extends AbstractRector
+{
+    public function getDefinition(): RectorDefinition
+    {
+        return new RectorDefinition('Remove getMockBuilder() to createMock()', [
+            new CodeSample(
+                <<<'PHP'
+class SomeTest extends \PHPUnit\Framework\TestCase
+{
+    public function test()
+    {
+        $applicationMock = $this->getMockBuilder('SomeClass')
+           ->disableOriginalConstructor()
+           ->getMock();
+    }
+}
+PHP
+,
+                <<<'PHP'
+class SomeTest extends \PHPUnit\Framework\TestCase
+{
+    public function test()
+    {
+        $applicationMock = $this->createMock('SomeClass');
+    }
+}
+PHP
+
+            ),
+        ]);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [MethodCall::class];
+    }
+
+    /**
+     * @param MethodCall $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if (! $this->isName($node->name, 'getMock')) {
+            return null;
+        }
+
+        if (! $node->var instanceof MethodCall) {
+            return null;
+        }
+
+        if ($this->isName($node->var->name, 'disableOriginalConstructor')) {
+            $getMockBuilderMethodCall = $node->var->var; // null;
+        } else {
+            $getMockBuilderMethodCall = $node->var;
+        }
+
+        /** @var MethodCall|null $getMockBuilderMethodCall */
+        if ($getMockBuilderMethodCall === null) {
+            return null;
+        }
+
+        if (! $this->isName($getMockBuilderMethodCall->name, 'getMockBuilder')) {
+            return null;
+        }
+
+        $args = $getMockBuilderMethodCall->args;
+        $thisVariable = $getMockBuilderMethodCall->var;
+
+        return new MethodCall($thisVariable, 'createMock', $args);
+    }
+}
