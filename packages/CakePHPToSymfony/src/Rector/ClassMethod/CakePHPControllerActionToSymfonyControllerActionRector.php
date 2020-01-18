9fdfbac687cc7e3169941bdf458137f16906cc2c
TomasVotruba <tomas.vot@gmail.com>
2020-01-18 22:13:12 +0100

    [CakePHPToSymfony] Add CakePHPControllerRenderToSymfonyRector

--- packages/CakePHPToSymfony/src/Rector/ClassMethod/CakePHPControllerActionToSymfonyControllerActionRector.php
+++ packages/CakePHPToSymfony/src/Rector/ClassMethod/CakePHPControllerActionToSymfonyControllerActionRector.php
@@ -5,19 +5,9 @@ declare(strict_types=1);
 namespace Rector\CakePHPToSymfony\Rector\ClassMethod;
 
 use PhpParser\Node;
-use PhpParser\Node\Arg;
-use PhpParser\Node\Expr\Array_;
-use PhpParser\Node\Expr\ArrayItem;
-use PhpParser\Node\Expr\FuncCall;
-use PhpParser\Node\Expr\MethodCall;
-use PhpParser\Node\Expr\Variable;
 use PhpParser\Node\Name\FullyQualified;
-use PhpParser\Node\Scalar\String_;
 use PhpParser\Node\Stmt\ClassMethod;
-use PhpParser\Node\Stmt\Return_;
 use Rector\CakePHPToSymfony\Rector\AbstractCakePHPRector;
-use Rector\CakePHPToSymfony\Rector\TemplatePathResolver;
-use Rector\CodeQuality\CompactConverter;
 use Rector\RectorDefinition\CodeSample;
 use Rector\RectorDefinition\RectorDefinition;
 
@@ -32,22 +22,6 @@ use Rector\RectorDefinition\RectorDefinition;
  */
 final class CakePHPControllerActionToSymfonyControllerActionRector extends AbstractCakePHPRector
 {
-    /**
-     * @var CompactConverter
-     */
-    private $compactConverter;
-
-    /**
-     * @var TemplatePathResolver
-     */
-    private $templatePathResolver;
-
-    public function __construct(CompactConverter $compactConverter, TemplatePathResolver $templatePathResolver)
-    {
-        $this->compactConverter = $compactConverter;
-        $this->templatePathResolver = $templatePathResolver;
-    }
-
     public function getDefinition(): RectorDefinition
     {
         return new RectorDefinition('Migrate CakePHP 2.4 Controller action to Symfony 5', [
@@ -58,7 +32,6 @@ class HomepageController extends \AppController
     public function index()
     {
         $value = 5;
-        $this->set('name', $value);
     }
 }
 PHP
@@ -71,9 +44,6 @@ class HomepageController extends \AppController
     public function index(): Response
     {
         $value = 5;
-        return $this->renderResponse('homepage/index.twig', [
-            'name' => $value
-        ]);
     }
 }
 PHP
@@ -103,88 +73,8 @@ PHP
             return null;
         }
 
-        $methodCall = $this->createThisRenderMethodCall($node);
-        $return = new Return_($methodCall);
-        $node->stmts[] = $return;
-
         $node->returnType = new FullyQualified('Symfony\Component\HttpFoundation\Response');
 
         return $node;
-    }
-
-    /**
-     * @return Arg[][]
-     */
-    private function collectAndRemoveSetMethodCallArgs(array $stmts): array
-    {
-        $setMethodCallArgs = [];
-
-        $this->traverseNodesWithCallable($stmts, function (Node $node) use (&$setMethodCallArgs) {
-            if (! $node instanceof MethodCall) {
-                return null;
-            }
-
-            if (! $this->isName($node->name, 'set')) {
-                return null;
-            }
-
-            $setMethodCallArgs[] = $node->args;
-            $this->removeNode($node);
-
-            return null;
-        });
-
-        return $setMethodCallArgs;
-    }
-
-    /**
-     * @param Arg[][] $setValues
-     */
-    private function createArrayFromSetValues(array $setValues): Array_
-    {
-        $arrayItems = [];
-
-        foreach ($setValues as $setValue) {
-            if (count($setValue) > 1) {
-                $arrayItems[] = new ArrayItem($setValue[1]->value, $setValue[0]->value);
-            } elseif ($this->isCompactFuncCall($setValue[0]->value)) {
-                /** @var FuncCall $compactFuncCall */
-                $compactFuncCall = $setValue[0]->value;
-
-                return $this->compactConverter->convertToArray($compactFuncCall);
-            }
-        }
-
-        return new Array_($arrayItems);
-    }
-
-    private function isCompactFuncCall(Node $node): bool
-    {
-        if (! $node instanceof FuncCall) {
-            return false;
-        }
-
-        return $this->isName($node, 'compact');
-    }
-
-    /**
-     * Creates "$this->render('...', [...])";
-     */
-    private function createThisRenderMethodCall(ClassMethod $classMethod): MethodCall
-    {
-        $thisVariable = new Variable('this');
-        $thisRenderMethodCall = new MethodCall($thisVariable, 'render');
-
-        $templateName = $this->templatePathResolver->resolveForClassMethod($classMethod);
-        $thisRenderMethodCall->args[] = new Arg(new String_($templateName));
-
-        $setValues = $this->collectAndRemoveSetMethodCallArgs((array) $classMethod->stmts);
-
-        if ($setValues !== []) {
-            $parametersArray = $this->createArrayFromSetValues($setValues);
-            $thisRenderMethodCall->args[] = new Arg($parametersArray);
-        }
-
-        return $thisRenderMethodCall;
     }
 }
---
+++ packages/CakePHPToSymfony/src/Rector/Template/TemplateMethodCallManipulator.php
@@ -0,0 +1,130 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\CakePHPToSymfony\Rector\Template;
+
+use Nette\Utils\Strings;
+use PhpParser\Node;
+use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Scalar\String_;
+use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\Node\Stmt\Expression;
+use PhpParser\Node\Stmt\Return_;
+use Rector\CakePHPToSymfony\Rector\TemplatePathResolver;
+use Rector\PhpParser\Node\Resolver\NameResolver;
+use Rector\PhpParser\Node\Value\ValueResolver;
+use Rector\PhpParser\NodeTraverser\CallableNodeTraverser;
+
+final class TemplateMethodCallManipulator
+{
+    /**
+     * @var ValueResolver
+     */
+    private $valueResolver;
+
+    /**
+     * @var TemplatePathResolver
+     */
+    private $templatePathResolver;
+
+    /**
+     * @var CallableNodeTraverser
+     */
+    private $callableNodeTraverser;
+
+    /**
+     * @var NameResolver
+     */
+    private $nameResolver;
+
+    public function __construct(
+        ValueResolver $valueResolver,
+        TemplatePathResolver $templatePathResolver,
+        CallableNodeTraverser $callableNodeTraverser,
+        NameResolver $nameResolver
+    ) {
+        $this->valueResolver = $valueResolver;
+        $this->templatePathResolver = $templatePathResolver;
+        $this->callableNodeTraverser = $callableNodeTraverser;
+        $this->nameResolver = $nameResolver;
+    }
+
+    public function refactorExistingRenderMethodCall(ClassMethod $classMethod): void
+    {
+        $controllerNamePart = $this->templatePathResolver->resolveClassNameTemplatePart($classMethod);
+
+        $this->callableNodeTraverser->traverseNodesWithCallable($classMethod, function (Node $node) use (
+            $controllerNamePart
+        ) {
+            $renderMethodCall = $this->matchThisRenderMethodCallBareOrInReturn($node);
+            if ($renderMethodCall === null) {
+                return null;
+            }
+
+            return $this->refactorRenderTemplateName($renderMethodCall, $controllerNamePart);
+        });
+    }
+
+    private function refactorRenderTemplateName(Node $node, string $controllerNamePart): ?Return_
+    {
+        /** @var MethodCall $node */
+        $renderArgumentValue = $this->valueResolver->getValue($node->args[0]->value);
+
+        /** @var string|mixed $renderArgumentValue */
+        if (! is_string($renderArgumentValue)) {
+            return null;
+        }
+
+        if (Strings::contains($renderArgumentValue, '/')) {
+            $templateName = $renderArgumentValue . '.twig';
+        } else {
+            // add explicit controller
+            $templateName = $controllerNamePart . '/' . $renderArgumentValue . '.twig';
+        }
+
+        $node->args[0]->value = new String_($templateName);
+
+        return new Return_($node);
+    }
+
+    private function isThisRenderMethodCall(Node $node): bool
+    {
+        if (! $node instanceof MethodCall) {
+            return false;
+        }
+
+        if (! $this->nameResolver->isName($node->var, 'this')) {
+            return false;
+        }
+
+        return $this->nameResolver->isName($node->name, 'render');
+    }
+
+    private function matchThisRenderMethodCallBareOrInReturn(Node $node): ?MethodCall
+    {
+        if ($node instanceof Return_) {
+            $nodeExpr = $node->expr;
+            if ($nodeExpr === null) {
+                return null;
+            }
+
+            if (! $this->isThisRenderMethodCall($nodeExpr)) {
+                return null;
+            }
+
+            /** @var MethodCall $nodeExpr */
+            return $nodeExpr;
+        }
+
+        if ($node instanceof Expression) {
+            if (! $this->isThisRenderMethodCall($node->expr)) {
+                return null;
+            }
+
+            return $node->expr;
+        }
+
+        return null;
+    }
+}
