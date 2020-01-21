559f2c05e7561b20e737d8064b11b52a02772731
TomasVotruba <tomas.vot@gmail.com>
2020-01-21 19:39:39 +0100

    remove autoload, already in composer.json

--- helpers/rector_dump_function.php
+++ helpers/rector_dump_function.php
@@ -2,8 +2,6 @@
 
 declare(strict_types=1);
 
-require __DIR__ . '/../vendor/autoload.php';
-
 if (!function_exists('rd')) {
     function rd($var) {
         array_map([Tracy\Debugger::class, 'dump'], func_get_args());
