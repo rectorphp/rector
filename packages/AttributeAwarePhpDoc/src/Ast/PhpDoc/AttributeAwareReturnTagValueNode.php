21f906d77c33d83d82bad81bf7b88003f12e7629
TomasVotruba <tomas.vot@gmail.com>
2020-02-04 09:22:07 +0100

    use type interface in custom nodes

--- packages/AttributeAwarePhpDoc/src/Ast/PhpDoc/AttributeAwareReturnTagValueNode.php
+++ packages/AttributeAwarePhpDoc/src/Ast/PhpDoc/AttributeAwareReturnTagValueNode.php
@@ -7,8 +7,9 @@ namespace Rector\AttributeAwarePhpDoc\Ast\PhpDoc;
 use PHPStan\PhpDocParser\Ast\PhpDoc\ReturnTagValueNode;
 use Rector\BetterPhpDocParser\Attributes\Attribute\AttributeTrait;
 use Rector\BetterPhpDocParser\Contract\PhpDocNode\AttributeAwareNodeInterface;
+use Rector\BetterPhpDocParser\Contract\PhpDocNode\TypeAwareTagValueNodeInterface;
 
-final class AttributeAwareReturnTagValueNode extends ReturnTagValueNode implements AttributeAwareNodeInterface
+final class AttributeAwareReturnTagValueNode extends ReturnTagValueNode implements AttributeAwareNodeInterface, TypeAwareTagValueNodeInterface
 {
     use AttributeTrait;
 }
--- packages/AttributeAwarePhpDoc/src/Ast/PhpDoc/AttributeAwareVarTagValueNode.php
+++ packages/AttributeAwarePhpDoc/src/Ast/PhpDoc/AttributeAwareVarTagValueNode.php
@@ -7,8 +7,9 @@ namespace Rector\AttributeAwarePhpDoc\Ast\PhpDoc;
 use PHPStan\PhpDocParser\Ast\PhpDoc\VarTagValueNode;
 use Rector\BetterPhpDocParser\Attributes\Attribute\AttributeTrait;
 use Rector\BetterPhpDocParser\Contract\PhpDocNode\AttributeAwareNodeInterface;
+use Rector\BetterPhpDocParser\Contract\PhpDocNode\TypeAwareTagValueNodeInterface;
 
-final class AttributeAwareVarTagValueNode extends VarTagValueNode implements AttributeAwareNodeInterface
+final class AttributeAwareVarTagValueNode extends VarTagValueNode implements AttributeAwareNodeInterface, TypeAwareTagValueNodeInterface
 {
     use AttributeTrait;
 }
