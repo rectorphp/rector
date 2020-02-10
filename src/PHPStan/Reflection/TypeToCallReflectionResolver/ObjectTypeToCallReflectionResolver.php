11c0655718e98f061b5cbf4c43bdf0fabd9c9434
Lctrs <jerome@prmntr.me>
2020-02-10 14:00:23 +0100

    Decouple PHPStan Type to function resolver logic

---
+++ src/PHPStan/Reflection/TypeToCallReflectionResolver/ObjectTypeToCallReflectionResolver.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Core\PHPStan\Reflection\TypeToCallReflectionResolver;
+
+use PHPStan\Reflection\ClassMemberAccessAnswerer;
+use PHPStan\Reflection\ReflectionProvider;
+use PHPStan\Type\ObjectType;
+use PHPStan\Type\Type;
+
+/**
+ * @see https://github.com/phpstan/phpstan-src/blob/b1fd47bda2a7a7d25091197b125c0adf82af6757/src/Type/ObjectType.php#L705
+ */
+final class ObjectTypeToCallReflectionResolver implements TypeToCallReflectionResolverInterface
+{
+    /**
+     * @var ReflectionProvider
+     */
+    private $reflectionProvider;
+
+    public function __construct(ReflectionProvider $reflectionProvider)
+    {
+        $this->reflectionProvider = $reflectionProvider;
+    }
+
+    public function supports(Type $type): bool
+    {
+        return $type instanceof ObjectType;
+    }
+
+    /**
+     * @param ObjectType $type
+     */
+    public function resolve(Type $type, ClassMemberAccessAnswerer $classMemberAccessAnswerer)
+    {
+        $className = $type->getClassName();
+        if (! $this->reflectionProvider->hasClass($className)) {
+            return null;
+        }
+
+        $classReflection = $this->reflectionProvider->getClass($className);
+
+        if (! $classReflection->hasNativeMethod('__invoke')) {
+            return null;
+        }
+
+        return $classReflection->getNativeMethod('__invoke');
+    }
+}
---
+++ src/PHPStan/Reflection/TypeToCallReflectionResolver/TypeToCallReflectionResolverRegistry.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Core\PHPStan\Reflection\TypeToCallReflectionResolver;
+
+use PHPStan\Reflection\ClassMemberAccessAnswerer;
+use PHPStan\Reflection\FunctionReflection;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Type\Type;
+
+final class TypeToCallReflectionResolverRegistry
+{
+    /**
+     * @var TypeToCallReflectionResolverInterface[]
+     */
+    private $resolvers = [];
+
+    /**
+     * @param TypeToCallReflectionResolverInterface[] $resolvers
+     */
+    public function __construct(array $resolvers)
+    {
+        $this->resolvers = $resolvers;
+    }
+
+    /**
+     * @return FunctionReflection|MethodReflection|null
+     */
+    public function resolve(Type $type, ClassMemberAccessAnswerer $classMemberAccessAnswerer)
+    {
+        foreach ($this->resolvers as $resolver) {
+            if (! $resolver->supports($type)) {
+                continue;
+            }
+
+            return $resolver->resolve($type, $classMemberAccessAnswerer);
+        }
+
+        return null;
+    }
+}
