15f97ae2f38629e4710be3167efb9869a877416c
Aerendir <hello@aerendir.me>
2020-02-17 16:52:59 +0100

    `AnnotateThrowablesRector`: Improve organization of tests.

--- rules/coding-style/tests/Rector/Throw_/AnnotateThrowablesRector/Source/TheException.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Rector\CodingStyle\Tests\Rector\Throw_\AnnotateThrowablesRector\Source;
-
-class TheException extends \RuntimeException {
-}
--- rules/coding-style/tests/Rector/Throw_/AnnotateThrowablesRector/Source/TheExceptionTheThird.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Rector\CodingStyle\Tests\Rector\Throw_\AnnotateThrowablesRector\Source;
-
-class TheExceptionTheThird extends \RuntimeException {}
