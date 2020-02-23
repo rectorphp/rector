f7aefc68b83dcc14c47568b42cc62627573f6184
TomasVotruba <tomas.vot@gmail.com>
2020-02-23 22:14:24 +0100

    [DeadCode] Add RemoveUnusedVariableAssignRector

---
+++ rules/dead-code/src/Rector/Assign/RemoveUnusedVariableAssignRector.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\DeadCode\Rector\Assign;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\Assign;
+use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\FunctionLike;
+use Rector\Core\Rector\AbstractRector;
+use Rector\Core\RectorDefinition\CodeSample;
+use Rector\Core\RectorDefinition\RectorDefinition;
+use Rector\NodeTypeResolver\Node\AttributeKey;
+
+/**
+ * @see \Rector\DeadCode\Tests\Rector\Assign\RemoveUnusedVariableAssignRector\RemoveUnusedVariableAssignRectorTest
+ */
+final class RemoveUnusedVariableAssignRector extends AbstractRector
+{
+    public function getDefinition(): RectorDefinition
+    {
+        return new RectorDefinition('Remove unused assigns to variables', [
+            new CodeSample(
+                <<<'PHP'
+class SomeClass
+{
+    public function run()
+    {
+        $value = 5;
+    }
+}
+PHP
+,
+                <<<'PHP'
+class SomeClass
+{
+    public function run()
+    {
+    }
+}
+PHP
+
+            ),
+        ]);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [Assign::class];
+    }
+
+    /**
+     * @param Assign $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        $functionLike = $node->getAttribute(AttributeKey::METHOD_NODE);
+        if (! $functionLike instanceof FunctionLike) {
+            return null;
+        }
+
+        if (! $node->var instanceof Variable) {
+            return null;
+        }
+
+        // variable is used
+        $variableUsages = $this->findVariableUsages($functionLike, $node);
+        if (count($variableUsages) > 0) {
+            return null;
+        }
+
+        $this->removeNode($node);
+
+        return $node;
+    }
+
+    /**
+     * @return Variable[]
+     */
+    private function findVariableUsages(FunctionLike $functionLike, Assign $assign): array
+    {
+        return $this->betterNodeFinder->find((array) $functionLike->getStmts(), function (Node $node) use ($assign) {
+            if (! $node instanceof Variable) {
+                return false;
+            }
+
+            // skip assign value
+            return $assign->var !== $node;
+        });
+    }
+}
