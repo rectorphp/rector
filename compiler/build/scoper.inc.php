aa7835fb7483d3d42f750d3c3aa0b3e2bc2eb3e5
TomasVotruba <tomas.vot@gmail.com>
2020-02-26 15:54:01 +0100

    debug scoper

--- compiler/build/scoper.inc.php
+++ compiler/build/scoper.inc.php
@@ -21,6 +21,8 @@ $stubFinder->files()->name('*.php')
 
 foreach ($stubFinder->getIterator() as $fileInfo) {
     // mirrors https://github.com/phpstan/phpstan-src/commit/04f777bc4445725d17dac65c989400485454b145
+    dump($fileInfo->getPathName());
+
     if ($fileInfo->getPathName() === '../../vendor/jetbrains/phpstorm-stubs/PhpStormStubsMap.php') {
         continue;
     }
