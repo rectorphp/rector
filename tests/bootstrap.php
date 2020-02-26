7ba752378ca84d617f4edac2e38a2b94e1d523da
TomasVotruba <tomas.vot@gmail.com>
2020-02-26 18:27:54 +0100

    remove manual load of bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,8 +6,6 @@ use Rector\Core\Stubs\StubLoader;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-require_once 'phar://vendor/phpstan/phpstan/phpstan.phar/vendor/jetbrains/phpstorm-stubs/PhpStormStubsMap.php';
-
 // silent deprecations, since we test them
 error_reporting(E_ALL ^ E_DEPRECATED);
 
