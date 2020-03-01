96ebef4c2ee2e9e36f89b57daa683d5a1f3200d7
TomasVotruba <tomas.vot@gmail.com>
2020-03-01 01:43:35 +0100

    fix array shape type

--- packages/attribute-aware-php-doc/src/AttributeAwareNodeFactory/Type/AttributeAwareArrayShapeNodeFactory.php
+++ packages/attribute-aware-php-doc/src/AttributeAwareNodeFactory/Type/AttributeAwareArrayShapeNodeFactory.php
@@ -7,11 +7,18 @@ namespace Rector\AttributeAwarePhpDoc\AttributeAwareNodeFactory\Type;
 use PHPStan\PhpDocParser\Ast\Node;
 use PHPStan\PhpDocParser\Ast\Type\ArrayShapeNode;
 use Rector\AttributeAwarePhpDoc\Ast\Type\AttributeAwareArrayShapeNode;
+use Rector\AttributeAwarePhpDoc\Contract\AttributeNodeAwareFactory\AttributeAwareNodeFactoryAwareInterface;
 use Rector\AttributeAwarePhpDoc\Contract\AttributeNodeAwareFactory\AttributeNodeAwareFactoryInterface;
+use Rector\BetterPhpDocParser\Attributes\Ast\AttributeAwareNodeFactory;
 use Rector\BetterPhpDocParser\Contract\PhpDocNode\AttributeAwareNodeInterface;
 
-final class AttributeAwareArrayShapeNodeFactory implements AttributeNodeAwareFactoryInterface
+final class AttributeAwareArrayShapeNodeFactory implements AttributeNodeAwareFactoryInterface, AttributeAwareNodeFactoryAwareInterface
 {
+    /**
+     * @var AttributeAwareNodeFactory
+     */
+    private $attributeAwareNodeFactory;
+
     public function getOriginalNodeClass(): string
     {
         return ArrayShapeNode::class;
@@ -27,6 +34,17 @@ final class AttributeAwareArrayShapeNodeFactory implements AttributeNodeAwareFac
      */
     public function create(Node $node, string $docContent): AttributeAwareNodeInterface
     {
-        return new AttributeAwareArrayShapeNode($node->items);
+        $items = [];
+        foreach ($node->items as $item) {
+            $item->valueType = $this->attributeAwareNodeFactory->createFromNode($item->valueType, $docContent);
+            $items[] = $item;
+        }
+
+        return new AttributeAwareArrayShapeNode($items);
+    }
+
+    public function setAttributeAwareNodeFactory(AttributeAwareNodeFactory $attributeAwareNodeFactory): void
+    {
+        $this->attributeAwareNodeFactory = $attributeAwareNodeFactory;
     }
 }
