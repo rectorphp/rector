8b5c4f57263726a35bf170cc24649d4ba03779b3
TomasVotruba <tomas.vot@gmail.com>
2020-03-01 09:53:08 +0100

    [DeadCode] Fix shifted value

---
+++ rules/dead-code/src/NodeCollector/ModifiedVariableNamesCollector.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\DeadCode\NodeCollector;
+
+use PhpParser\Node;
+use PhpParser\Node\Arg;
+use PhpParser\Node\Expr\Assign;
+use PhpParser\Node\Expr\FuncCall;
+use PhpParser\Node\Expr\Variable;
+use Rector\Core\PhpParser\NodeTraverser\CallableNodeTraverser;
+use Rector\NodeNameResolver\NodeNameResolver;
+use Rector\NodeTypeResolver\Node\AttributeKey;
+
+final class ModifiedVariableNamesCollector
+{
+    /**
+     * @var CallableNodeTraverser
+     */
+    private $callableNodeTraverser;
+
+    /**
+     * @var NodeNameResolver
+     */
+    private $nodeNameResolver;
+
+    public function __construct(CallableNodeTraverser $callableNodeTraverser, NodeNameResolver $nodeNameResolver)
+    {
+        $this->callableNodeTraverser = $callableNodeTraverser;
+        $this->nodeNameResolver = $nodeNameResolver;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function collectModifiedVariableNames(Node $node): array
+    {
+        $modifiedVariableNames = [];
+
+        $this->callableNodeTraverser->traverseNodesWithCallable($node, function (Node $node) use (
+            &$modifiedVariableNames
+        ) {
+            if ($this->isVariableOverriddenInAssign($node)) {
+                /** @var Assign $node */
+                $variableName = $this->nodeNameResolver->getName($node->var);
+                if ($variableName === null) {
+                    return null;
+                }
+
+                $modifiedVariableNames[] = $variableName;
+            }
+
+            if ($this->isVariableChangedInReference($node)) {
+                /** @var Arg $node */
+                $variableName = $this->nodeNameResolver->getName($node->value);
+                if ($variableName === null) {
+                    return null;
+                }
+
+                $modifiedVariableNames[] = $variableName;
+            }
+        });
+
+        return $modifiedVariableNames;
+    }
+
+    private function isVariableOverriddenInAssign(Node $node): bool
+    {
+        if (! $node instanceof Assign) {
+            return false;
+        }
+
+        return $node->var instanceof Variable;
+    }
+
+    /**
+     * @todo decouple to changed variable service
+     */
+    private function isVariableChangedInReference(Node $node): bool
+    {
+        if (! $node instanceof Arg) {
+            return false;
+        }
+
+        $parentNode = $node->getAttribute(AttributeKey::PARENT_NODE);
+        if (! $parentNode instanceof FuncCall) {
+            return false;
+        }
+
+        return $this->nodeNameResolver->isNames($parentNode, ['array_shift', 'array_pop']);
+    }
+}
