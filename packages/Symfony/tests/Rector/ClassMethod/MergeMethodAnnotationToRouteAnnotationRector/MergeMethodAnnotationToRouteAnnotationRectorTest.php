9f7ba49b991a1dbd3e69b4766b40d36ebd567f8c
Stefan Blanke <blanke@enterio.de>
2020-03-04 13:55:44 +0100

    Resolve conflict

--- packages/Symfony/tests/Rector/ClassMethod/MergeMethodAnnotationToRouteAnnotationRector/MergeMethodAnnotationToRouteAnnotationRectorTest.php
+++ packages/Symfony/tests/Rector/ClassMethod/MergeMethodAnnotationToRouteAnnotationRector/MergeMethodAnnotationToRouteAnnotationRectorTest.php
@@ -5,24 +5,22 @@ declare(strict_types=1);
 namespace Rector\Symfony\Tests\Rector\ClassMethod\MergeMethodAnnotationToRouteAnnotationRector;
 
 use Iterator;
+use Rector\Core\Testing\PHPUnit\AbstractRectorTestCase;
 use Rector\Symfony\Rector\ClassMethod\MergeMethodAnnotationToRouteAnnotationRector;
-use Rector\Testing\PHPUnit\AbstractRectorTestCase;
 
 final class MergeMethodAnnotationToRouteAnnotationRectorTest extends AbstractRectorTestCase
 {
     /**
-     * @dataProvider provideDataForTest()
+     * @dataProvider provideData()
      */
     public function test(string $file): void
     {
         $this->doTestFile($file);
     }
 
-    public function provideDataForTest(): Iterator
+    public function provideData(): Iterator
     {
-        yield [__DIR__ . '/Fixture/fixture.php.inc'];
-        yield [__DIR__ . '/Fixture/fixture_with_template_annotation.php.inc'];
-        yield [__DIR__ . '/Fixture/fixture_with_template_annotation2.php.inc'];
+        return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
     }
 
     protected function getRectorClass(): string
