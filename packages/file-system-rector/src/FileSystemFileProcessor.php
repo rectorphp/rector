15283616a40b74bbfcfa0608f8ff299585621a77
TomasVotruba <tomas.vot@gmail.com>
2020-03-09 17:53:16 +0100

    add current file info provide to file system file processor

--- packages/file-system-rector/src/FileSystemFileProcessor.php
+++ packages/file-system-rector/src/FileSystemFileProcessor.php
@@ -5,10 +5,16 @@ declare(strict_types=1);
 namespace Rector\FileSystemRector;
 
 use Rector\FileSystemRector\Contract\FileSystemRectorInterface;
+use Rector\NodeTypeResolver\FileSystem\CurrentFileInfoProvider;
 use Symplify\SmartFileSystem\SmartFileInfo;
 
 final class FileSystemFileProcessor
 {
+    /**
+     * @var CurrentFileInfoProvider
+     */
+    private $currentFileInfoProvider;
+
     /**
      * @var FileSystemRectorInterface[]
      */
@@ -17,13 +23,16 @@ final class FileSystemFileProcessor
     /**
      * @param FileSystemRectorInterface[] $fileSystemRectors
      */
-    public function __construct(array $fileSystemRectors = [])
+    public function __construct(CurrentFileInfoProvider $currentFileInfoProvider, array $fileSystemRectors = [])
     {
+        $this->currentFileInfoProvider = $currentFileInfoProvider;
         $this->fileSystemRectors = $fileSystemRectors;
     }
 
     public function processFileInfo(SmartFileInfo $smartFileInfo): void
     {
+        $this->currentFileInfoProvider ->setCurrentFileInfo($smartFileInfo);
+
         foreach ($this->fileSystemRectors as $fileSystemRector) {
             $fileSystemRector->refactor($smartFileInfo);
         }
