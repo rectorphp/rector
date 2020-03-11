5cab1397f5554ef2c5d9a0fc052d0b664c1bc05f
Jan Mikeš <j.mikes@me.com>
2020-03-11 21:43:16 +0100

    Dogfood class

---
+++ src/DogFood/DogFoodClass.php
@@ -0,0 +1,12 @@
+<?php declare (strict_types=1);
+
+namespace Rector\Core\DogFood;
+
+final class DogFoodClass
+{
+    public function test(): int
+    {
+        return 0;
+        return 1;
+    }
+}
