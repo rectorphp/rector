4e5c09fbb6d7110776cb746d80ab9b1abe101610
Jeroen Thora <jeroenthora@gmail.com>
2020-04-07 12:47:47 +0200

    Make sure doctrine alias rector works in symfony controllers

--- stubs/Symfony/Bundle/FrameworkBundle/Controller/AbstractController.php
+++ stubs/Symfony/Bundle/FrameworkBundle/Controller/AbstractController.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 
 namespace Symfony\Bundle\FrameworkBundle\Controller;
 
+use Doctrine\Common\Persistence\ManagerRegistry;
+
 if (class_exists('Symfony\Bundle\FrameworkBundle\Controller\AbstractController')) {
     return;
 }
 
 abstract class AbstractController
 {
-
+    public function getDoctrine(): ManagerRegistry
+    {
+    }
 }
