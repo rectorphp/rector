c84f5c171b4f0ca582d9688ee1a84cd9a39ee8c3
TomasVotruba <tomas.vot@gmail.com>
2020-04-24 01:21:05 +0200

    simplify throws class resolving

--- src/PhpDoc/PhpDocTagsFinder.php
+++ src/PhpDoc/PhpDocTagsFinder.php
@@ -5,11 +5,6 @@ declare(strict_types=1);
 namespace Rector\Core\PhpDoc;
 
 use Nette\Utils\Strings;
-use PhpParser\Node;
-use Rector\BetterPhpDocParser\PhpDocInfo\PhpDocInfo;
-use Rector\NodeTypeResolver\Node\AttributeKey;
-use Rector\PHPStan\Type\FullyQualifiedObjectType;
-use Rector\PHPStan\Type\ShortenedObjectType;
 
 final class PhpDocTagsFinder
 {
@@ -39,29 +34,5 @@ final class PhpDocTagsFinder
         $matchingTags = array_map($explode, $matchingTags);
 
         return array_merge(...$matchingTags);
-    }
-
-    public function extractTagsThrowsFromNode(Node $node): array
-    {
-        $throwsTags = [];
-
-        /** @var PhpDocInfo $phpDocInfo */
-        $phpDocInfo = $node->getAttribute(AttributeKey::PHP_DOC_INFO);
-        foreach ($phpDocInfo->getThrowsTypes() as $throwsType) {
-            $thrownClass = null;
-            if ($throwsType instanceof ShortenedObjectType) {
-                $thrownClass = $throwsType->getFullyQualifiedName();
-            }
-
-            if ($throwsType instanceof FullyQualifiedObjectType) {
-                $thrownClass = $throwsType->getClassName();
-            }
-
-            if ($thrownClass !== null) {
-                $throwsTags[] = $thrownClass;
-            }
-        }
-
-        return $throwsTags;
     }
 }
