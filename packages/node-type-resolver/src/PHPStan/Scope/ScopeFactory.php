67cd31faa1fd8d67558d77b7147b8819d7c7a93b
Ondrej Mirtes <ondrej@mirtes.cz>
2020-05-31 14:03:19 +0200

    Future-proof ScopeFactory

--- packages/node-type-resolver/src/PHPStan/Scope/ScopeFactory.php
+++ packages/node-type-resolver/src/PHPStan/Scope/ScopeFactory.php
@@ -4,77 +4,25 @@ declare(strict_types=1);
 
 namespace Rector\NodeTypeResolver\PHPStan\Scope;
 
-use PHPStan\Analyser\MutatingScope;
 use PHPStan\Analyser\Scope;
 use PHPStan\Analyser\ScopeContext;
 use PHPStan\Analyser\ScopeFactory as PHPStanScopeFactory;
-use PHPStan\Analyser\TypeSpecifier;
-use PHPStan\DependencyInjection\Type\DynamicReturnTypeExtensionRegistryProvider;
-use PHPStan\DependencyInjection\Type\OperatorTypeSpecifyingExtensionRegistryProvider;
-use PHPStan\Reflection\ReflectionProvider;
-use PHPStan\Rules\Properties\PropertyReflectionFinder;
-use Rector\Core\PhpParser\Printer\BetterStandardPrinter;
 use Symplify\SmartFileSystem\SmartFileInfo;
 
 final class ScopeFactory
 {
     /**
-     * @var ReflectionProvider
-     */
-    private $reflectionProvider;
-
-    /**
-     * @var TypeSpecifier
-     */
-    private $typeSpecifier;
-
-    /**
      * @var PHPStanScopeFactory
      */
     private $phpStanScopeFactory;
 
-    /**
-     * @var BetterStandardPrinter
-     */
-    private $betterStandardPrinter;
-
-    /**
-     * @var DynamicReturnTypeExtensionRegistryProvider
-     */
-    private $dynamicReturnTypeExtensionRegistryProvider;
-
-    /**
-     * @var OperatorTypeSpecifyingExtensionRegistryProvider
-     */
-    private $operatorTypeSpecifyingExtensionRegistryProvider;
-
-    public function __construct(
-        ReflectionProvider $reflectionProvider,
-        TypeSpecifier $typeSpecifier,
-        PHPStanScopeFactory $phpStanScopeFactory,
-        BetterStandardPrinter $betterStandardPrinter,
-        DynamicReturnTypeExtensionRegistryProvider $dynamicReturnTypeExtensionRegistryProvider,
-        OperatorTypeSpecifyingExtensionRegistryProvider $operatorTypeSpecifyingExtensionRegistryProvider
-    ) {
-        $this->reflectionProvider = $reflectionProvider;
-        $this->typeSpecifier = $typeSpecifier;
+    public function __construct(PHPStanScopeFactory $phpStanScopeFactory)
+    {
         $this->phpStanScopeFactory = $phpStanScopeFactory;
-        $this->betterStandardPrinter = $betterStandardPrinter;
-        $this->dynamicReturnTypeExtensionRegistryProvider = $dynamicReturnTypeExtensionRegistryProvider;
-        $this->operatorTypeSpecifyingExtensionRegistryProvider = $operatorTypeSpecifyingExtensionRegistryProvider;
     }
 
     public function createFromFile(SmartFileInfo $fileInfo): Scope
     {
-        return new MutatingScope(
-            $this->phpStanScopeFactory,
-            $this->reflectionProvider,
-            $this->dynamicReturnTypeExtensionRegistryProvider->getRegistry(),
-            $this->operatorTypeSpecifyingExtensionRegistryProvider->getRegistry(),
-            $this->betterStandardPrinter,
-            $this->typeSpecifier,
-            new PropertyReflectionFinder(),
-            ScopeContext::create($fileInfo->getRealPath())
-        );
+        return $this->phpStanScopeFactory->create(ScopeContext::create($fileInfo->getRealPath()));
     }
 }
