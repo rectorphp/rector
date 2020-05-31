ae5ee7d09e519604e73d0bb23a9a422038230617
TomasVotruba <tomas.vot@gmail.com>
2020-05-31 16:29:27 +0200

    [PHP 5.3] Add ClearReturnNewByReferenceRector

---
+++ rules/php53/src/Rector/Assign/ClearReturnNewByReferenceRector.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Php53\Rector\Assign;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\Assign;
+use PhpParser\Node\Expr\AssignRef;
+use PhpParser\Node\Expr\New_;
+use Rector\Core\Rector\AbstractRector;
+use Rector\Core\RectorDefinition\CodeSample;
+use Rector\Core\RectorDefinition\RectorDefinition;
+
+/**
+ * @sponsor Thanks https://twitter.com/afilina for sponsoring this rule
+
+ * @see https://3v4l.org/UJN6H
+ * @see \Rector\Php53\Rector\Assign\ClearReturnNewByReferenceRector
+ */
+final class ClearReturnNewByReferenceRector extends AbstractRector
+{
+    public function getDefinition(): RectorDefinition
+    {
+        return new RectorDefinition('Remove reference from "$assign = &new Value;"', [
+            new CodeSample(
+                <<<'CODE_SAMPLE'
+$assign = &new Value;
+CODE_SAMPLE
+                ,
+                <<<'CODE_SAMPLE'
+$assign = new Value;
+CODE_SAMPLE
+            ), ]);
+    }
+
+    public function getNodeTypes(): array
+    {
+        return [AssignRef::class];
+    }
+
+    /**
+     * @param AssignRef $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if (! $node->expr instanceof New_) {
+            return null;
+        }
+
+        return new Assign($node->var, $node->expr);
+    }
+}
