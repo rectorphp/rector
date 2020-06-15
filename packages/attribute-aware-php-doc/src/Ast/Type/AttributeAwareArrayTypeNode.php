9135bff8ebece9e5b42d3db77a010ab295d51d90
TomasVotruba <tomas.vot@gmail.com>
2020-06-15 16:19:01 +0200

    add brackets in case of array of callables in php doc

--- packages/attribute-aware-php-doc/src/Ast/Type/AttributeAwareArrayTypeNode.php
+++ packages/attribute-aware-php-doc/src/Ast/Type/AttributeAwareArrayTypeNode.php
@@ -5,10 +5,22 @@ declare(strict_types=1);
 namespace Rector\AttributeAwarePhpDoc\Ast\Type;
 
 use PHPStan\PhpDocParser\Ast\Type\ArrayTypeNode;
+use PHPStan\PhpDocParser\Ast\Type\CallableTypeNode;
 use Rector\BetterPhpDocParser\Attributes\Attribute\AttributeTrait;
 use Rector\BetterPhpDocParser\Contract\PhpDocNode\AttributeAwareNodeInterface;
 
 final class AttributeAwareArrayTypeNode extends ArrayTypeNode implements AttributeAwareNodeInterface
 {
     use AttributeTrait;
+
+    public function __toString(): string
+    {
+        $typeAsString = (string) $this->type;
+
+        if ($this->type instanceof CallableTypeNode) {
+            return sprintf('(%s)[]', $typeAsString);
+        }
+
+        return $typeAsString . '[]';
+    }
 }
