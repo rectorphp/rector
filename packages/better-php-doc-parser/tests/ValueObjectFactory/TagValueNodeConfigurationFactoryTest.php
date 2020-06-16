749c608badb8a21b4babeac2dcc9199adee22393
TomasVotruba <tomas.vot@gmail.com>
2020-06-16 13:50:34 +0200

    add TagValueNodeConfigurationFactoryTest

---
+++ packages/better-php-doc-parser/tests/ValueObjectFactory/TagValueNodeConfigurationFactoryTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\BetterPhpDocParser\Tests\ValueObjectFactory;
+
+use PHPUnit\Framework\TestCase;
+use Rector\BetterPhpDocParser\PhpDocNode\Symfony\SymfonyRouteTagValueNode;
+use Rector\BetterPhpDocParser\ValueObjectFactory\TagValueNodeConfigurationFactory;
+
+final class TagValueNodeConfigurationFactoryTest extends TestCase
+{
+    /**
+     * @var TagValueNodeConfigurationFactory
+     */
+    private $tagValueNodeConfigurationFactory;
+
+    protected function setUp(): void
+    {
+        $this->tagValueNodeConfigurationFactory = new TagValueNodeConfigurationFactory();
+    }
+
+    public function test(): void
+    {
+        $tagValueNodeConfiguration = $this->tagValueNodeConfigurationFactory->createFromOriginalContent(
+            '...',
+            new SymfonyRouteTagValueNode([])
+        );
+
+        $this->assertSame('=', $tagValueNodeConfiguration->getArrayEqualSign());
+    }
+}
