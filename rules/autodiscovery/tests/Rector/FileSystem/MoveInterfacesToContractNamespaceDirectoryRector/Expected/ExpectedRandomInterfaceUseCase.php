9abc75916a9a04c6b950e9b053df4d0fb4f2b459
TomasVotruba <tomas.vot@gmail.com>
2020-06-18 23:01:11 +0200

    fix namespace

--- rules/autodiscovery/tests/Rector/FileSystem/MoveInterfacesToContractNamespaceDirectoryRector/Expected/ExpectedRandomInterfaceUseCase.php
+++ rules/autodiscovery/tests/Rector/FileSystem/MoveInterfacesToContractNamespaceDirectoryRector/Expected/ExpectedRandomInterfaceUseCase.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace Rector\Autodiscovery\Tests\Rector\FileSystem\MoveInterfacesToContractNamespaceDirectoryRector\Source;
 
-use Rector\Autodiscovery\Tests\Rector\FileSystem\MoveEntitiesToEntityDirectoryRector\Source\Contract\RandomInterface;
+use Rector\Autodiscovery\Tests\Rector\FileSystem\MoveInterfacesToContractNamespaceDirectoryRector\Source\Contract\RandomInterface;
 
 class SomeFactory
 {
-    public function create(): \Rector\Autodiscovery\Tests\Rector\FileSystem\MoveEntitiesToEntityDirectoryRector\Source\Contract\RandomInterface
+    public function create(): \Rector\Autodiscovery\Tests\Rector\FileSystem\MoveInterfacesToContractNamespaceDirectoryRector\Source\Contract\RandomInterface
     {
     }
 }
