09fd6fee8aed1e83e5cd2d83e9d826078de78282
Derrick Schoen <derrickschoen@dealerfocused.ca>
2020-06-24 18:12:15 -0400

    Found a bug where if there is a nested if with an "OR" and all conditions evaluate to true, the rector changes the output.
    When calling invertCondition() with a BooleanOr, it returns a BooleanAnd that needs a BooleanNot added.
    
    Attempting to rebase and squanch to one commit.

--- rules/solid/src/NodeTransformer/ConditionInverter.php
+++ rules/solid/src/NodeTransformer/ConditionInverter.php
@@ -6,6 +6,7 @@ namespace Rector\SOLID\NodeTransformer;
 
 use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\BinaryOp;
+use PhpParser\Node\Expr\BinaryOp\BooleanAnd;
 use PhpParser\Node\Expr\BooleanNot;
 use Rector\Core\PhpParser\Node\Manipulator\BinaryOpManipulator;
 
@@ -26,7 +27,9 @@ final class ConditionInverter
         // inverse condition
         if ($expr instanceof BinaryOp) {
             $inversedCondition = $this->binaryOpManipulator->invertCondition($expr);
-            if ($inversedCondition === null) {
+
+            // When calling invertCondition() with a BooleanOr, it returns a BooleanAnd that needs a BooleanNot added.
+            if ($inversedCondition === null || $inversedCondition instanceof BooleanAnd) {
                 return new BooleanNot($expr);
             }
 
