0e2b52000db410b3afb8692c9d9c405f6d2e73d8
TomasVotruba <tomas.vot@gmail.com>
2020-07-02 14:19:23 +0200

    note about hashes in tests compoarision

--- src/Testing/PHPUnit/AbstractRectorTestCase.php
+++ src/Testing/PHPUnit/AbstractRectorTestCase.php
@@ -305,6 +305,7 @@ abstract class AbstractRectorTestCase extends AbstractGenericRectorTestCase
                 FileSystem::write($fixtureFileInfo->getRealPath(), $newOriginalContent);
             }
 
+            // if not exact match, check the regex version (useful for generated hashes/uuids in the code)
             $this->assertStringMatchesFormat($expectedFileContent, $changedContent, $causedByFixtureMessage);
         }
     }
