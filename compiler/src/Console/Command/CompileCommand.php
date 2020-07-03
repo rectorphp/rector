743171f8819a0c8d5e6ad35b4785bad1c93ad468
TomasVotruba <tomas.vot@gmail.com>
2020-07-03 14:59:29 +0200

    swap file/content

--- compiler/src/Console/Command/CompileCommand.php
+++ compiler/src/Console/Command/CompileCommand.php
@@ -107,8 +107,9 @@ final class CompileCommand extends Command
         // fix require path first
         $transformSourceFilePath = __DIR__ . '/../../../../vendor/phpstan/phpstan-src/bin/transform-source.php';
         $transformSourceFileContent = FileSystem::read($transformSourceFilePath);
+
         $transformSourceFileContent = str_replace("__DIR__ . '/../vendor/autoload.php'", "__DIR__ . '/../../../../vendor/autoload.php'", $transformSourceFileContent);
-        FileSystem::write($transformSourceFileContent, $transformSourceFilePath);
+        FileSystem::write($transformSourceFilePath, $transformSourceFileContent);
 
         $process = new Process(['php', 'vendor/phpstan/phpstan-src/bin/transform-source.php'], $this->buildDir);
         $process->mustRun(static function (string $type, string $buffer) use ($output): void {
