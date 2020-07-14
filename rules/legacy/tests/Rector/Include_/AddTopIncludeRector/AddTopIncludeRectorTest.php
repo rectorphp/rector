4b90133374fb97de4afd60d775ebddb24b0e7283
Bruce Wells <brucekwells@gmail.com>
2020-07-14 13:04:31 -0400

    AddTopIncludeRector

---
+++ rules/legacy/tests/Rector/Include_/AddTopIncludeRector/AddTopIncludeRectorTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Legacy\Tests\Rector\Include_\AddTopIncludeRector;
+
+use Iterator;
+use Rector\Core\Testing\PHPUnit\AbstractFileSystemRectorTestCase;
+use Rector\Legacy\Rector\Include_\AddTopIncludeRector;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class AddTopIncludeRectorTest extends AbstractFileSystemRectorTestCase
+{
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(SmartFileInfo $fileInfo): void
+    {
+        $this->doTestFileInfo($fileInfo);
+    }
+
+    public function provideData(): Iterator
+    {
+        return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
+    }
+
+    protected function getRectorsWithConfiguration(): array
+    {
+        return [
+            AddTopIncludeRector::class => [
+                '$type' => 'TYPE_INCLUDE',
+                '$file' => '__DIR__ . "/../autoloader.php"',
+            ],
+        ];
+    }
+}
