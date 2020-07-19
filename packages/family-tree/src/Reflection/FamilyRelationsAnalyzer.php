e9fea42472e604adec50d3c11e3dfc41fcd4e1a0
TomasVotruba <tomas.vot@gmail.com>
2020-07-19 20:02:37 +0200

    add known parent classes

--- packages/family-tree/src/Reflection/FamilyRelationsAnalyzer.php
+++ packages/family-tree/src/Reflection/FamilyRelationsAnalyzer.php
@@ -4,8 +4,15 @@ declare(strict_types=1);
 
 namespace Rector\FamilyTree\Reflection;
 
+use PhpParser\Node\Expr;
+
 final class FamilyRelationsAnalyzer
 {
+    /**
+     * @var string[]
+     */
+    private const KNOWN_PARENT_CLASSES = [\PhpParser\Node::class, Expr::class];
+
     /**
      * @return string[]
      */
@@ -29,6 +36,10 @@ final class FamilyRelationsAnalyzer
 
     public function isParentClass(string $class): bool
     {
+        if (in_array($class, self::KNOWN_PARENT_CLASSES, true)) {
+            return true;
+        }
+
         foreach (get_declared_classes() as $declaredClass) {
             if ($declaredClass === $class) {
                 continue;
