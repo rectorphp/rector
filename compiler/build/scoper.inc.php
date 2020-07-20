a7984b759c539666e603519a999338fdef1c63b2
TomasVotruba <tomas.vot@gmail.com>
2020-07-21 01:59:31 +0200

    prefix set list

--- compiler/build/scoper.inc.php
+++ compiler/build/scoper.inc.php
@@ -20,7 +20,7 @@ return [
     'files-whitelist' => $whitelistedStubsProvider->provide(),
     'patchers' => [
         function (string $filePath, string $prefix, string $content): string {
-            if ($filePath !== 'bin/rector' /* && ! Strings::contains($filePath, 'config/')*/) {
+            if ($filePath !== 'bin/rector' && ! $filePath !== 'packages/set/src/SetProvider.php') {
                 return $content;
             }
 
