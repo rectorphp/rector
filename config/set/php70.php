dc4d5adc9d3028d44abd4e9af7596885340fe8e9
TomasVotruba <tomas.vot@gmail.com>
2020-07-21 02:01:14 +0200

    correct path

--- config/set/php70.php
+++ config/set/php70.php
@@ -23,7 +23,7 @@ use Rector\Php70\Rector\Ternary\TernaryToSpaceshipRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(__DIR__ . '/../database-migration/mysql-to-mysqli.php');
+    $containerConfigurator->import(__DIR__ . '/mysql-to-mysqli.php');
 
     $services = $containerConfigurator->services();
 
