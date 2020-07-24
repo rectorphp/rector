9c08060925288824a690e338d79e37fb15c7de89
TomasVotruba <tomas.vot@gmail.com>
2020-07-24 13:14:32 +0200

    [Injection] Add StaticCallToAnotherServiceConstructorInjectionRector

---
+++ rules/injection/tests/Rector/StaticCall/StaticCallToAnotherServiceConstructorInjectionRector/StaticCallToAnotherServiceConstructorInjectionRectorTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Injection\Tests\Rector\StaticCall\StaticCallToAnotherServiceConstructorInjectionRector;
+
+use Iterator;
+use Rector\Core\Testing\PHPUnit\AbstractRectorTestCase;
+use Rector\Injection\Rector\StaticCall\StaticCallToAnotherServiceConstructorInjectionRector;
+use Rector\Injection\ValueObject\StaticCallToMethodCall;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class StaticCallToAnotherServiceConstructorInjectionRectorTest extends AbstractRectorTestCase
+{
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(SmartFileInfo $fileInfo): void
+    {
+        $this->doTestFileInfo($fileInfo);
+    }
+
+    public function provideData(): Iterator
+    {
+        return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
+    }
+
+    protected function getRectorsWithConfiguration(): array
+    {
+        $configuration = [
+            new StaticCallToMethodCall(
+                'Nette\Utils\FileSystem',
+                'write',
+                'Symplify\SmartFileSystem\SmartFileSystem',
+                'dumpFile'
+            ),
+        ];
+
+        return [
+            StaticCallToAnotherServiceConstructorInjectionRector::class => [
+                '$staticCallsToMethodCalls' => $configuration,
+            ],
+        ];
+    }
+}
