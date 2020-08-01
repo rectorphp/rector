3171d58f6033481970229377c9fc55c5fc20e023
TomasVotruba <tomas.vot@gmail.com>
2020-08-01 23:30:01 +0200

    add failing test case + add support for previous assign type dim fetch

---
+++ stubs/Nette/ComponentModel/Component.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nette\ComponentModel;
+
+class Component extends Container implements \ArrayAccess
+{
+    public function offsetExists($offset)
+    {
+
+    }
+
+    public function offsetGet($offset)
+    {
+
+    }
+
+    public function offsetSet($offset, $value)
+    {
+
+    }
+
+    public function offsetUnset($offset)
+    {
+
+    }
+}
