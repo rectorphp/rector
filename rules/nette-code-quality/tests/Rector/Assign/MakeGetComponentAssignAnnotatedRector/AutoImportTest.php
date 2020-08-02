9dd4ef0a7e485d8c339eb525ea8a54029fe6c4d9
TomasVotruba <tomas.vot@gmail.com>
2020-08-02 11:57:15 +0200

    fix import slash

---
+++ rules/nette-code-quality/tests/Rector/Assign/MakeGetComponentAssignAnnotatedRector/AutoImportTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\NetteCodeQuality\Tests\Rector\Assign\MakeGetComponentAssignAnnotatedRector;
+
+use Iterator;
+use Rector\Core\Configuration\Option;
+use Rector\Core\Testing\PHPUnit\AbstractRectorTestCase;
+use Rector\NetteCodeQuality\Rector\Assign\MakeGetComponentAssignAnnotatedRector;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class AutoImportTest extends AbstractRectorTestCase
+{
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(SmartFileInfo $fileInfo): void
+    {
+        $this->setParameter(Option::AUTO_IMPORT_NAMES, true);
+        $this->setParameter(Option::IMPORT_DOC_BLOCKS, true);
+        $this->doTestFileInfo($fileInfo);
+    }
+
+    public function provideData(): Iterator
+    {
+        return $this->yieldFilesFromDirectory(__DIR__ . '/FixtureAutoImport');
+    }
+
+    protected function getRectorClass(): string
+    {
+        return MakeGetComponentAssignAnnotatedRector::class;
+    }
+}
