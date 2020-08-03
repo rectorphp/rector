1c4ac6a6e52a2efbdee95f05538859e2ef49eb8a
dobryy <igordobritskiy@gmail.com>
2020-08-03 23:45:02 +0200

    [Order] Enable rule in CI

--- rector-ci.php
+++ rector-ci.php
@@ -7,7 +7,6 @@ use Rector\CodingStyle\Rector\ClassMethod\ReturnArrayClassMethodToYieldRector;
 use Rector\CodingStyle\Rector\String_\SplitStringClassConstantToClassConstFetchRector;
 use Rector\Core\Configuration\Option;
 use Rector\DeadCode\Rector\ClassConst\RemoveUnusedClassConstantRector;
-use Rector\Order\Rector\Class_\OrderPrivateMethodsByUseRector;
 use Rector\Php55\Rector\String_\StringClassNameToClassConstantRector;
 use Rector\Set\ValueObject\SetList;
 use Rector\SymfonyPhpConfig\Rector\Closure\AddEmptyLineBetweenCallsInPhpConfigRector;
@@ -67,7 +66,5 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         SplitStringClassConstantToClassConstFetchRector::class,
         // false positives on constants used in rector-ci.php
         RemoveUnusedClassConstantRector::class,
-
-        OrderPrivateMethodsByUseRector::class,
     ]);
 };
