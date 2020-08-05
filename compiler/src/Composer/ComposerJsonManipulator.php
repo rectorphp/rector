c7731e63e3b157d61f01d9bac9fc2b1149b934c4
TomasVotruba <tomas.vot@gmail.com>
2020-08-05 16:33:37 +0200

    compiler: remove conflicting dev deps

--- compiler/src/Composer/ComposerJsonManipulator.php
+++ compiler/src/Composer/ComposerJsonManipulator.php
@@ -35,6 +35,11 @@ final class ComposerJsonManipulator
     /**
      * @var string
      */
+    private const REQUIRE_DEV = 'require-dev';
+
+    /**
+     * @var string
+     */
     private $originalComposerJsonFileContent;
 
     /**
@@ -103,6 +108,10 @@ final class ComposerJsonManipulator
         // use dev-master till this get's to tagged: https://github.com/phpstan/phpstan-src/commit/535c0e25429c1e3dd0dd05f61b43a34830da2a09
         $json[self::REQUIRE]['phpstan/phpstan-src'] = 'dev-master';
         unset($json[self::REQUIRE][self::PHPSTAN_PHPSTAN]);
+
+        // remove conflicting dev deps
+        unset($json[self::REQUIRE_DEV]['slam/phpstan-extensions']);
+        unset($json[self::REQUIRE_DEV]['slevomat/coding-standard']);
 
         $json['repositories'][] = [
             'type' => 'vcs',
