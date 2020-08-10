bb184e78af5924350a1de6e7d98c6f178abe3454
TomasVotruba <tomas.vot@gmail.com>
2020-08-11 00:14:29 +0200

    remove duplicated set

--- config/set/doctrine-repository-as-service.php
+++ config/set/doctrine-repository-as-service.php
@@ -8,15 +8,17 @@ use Rector\Architecture\Rector\MethodCall\ServiceLocatorToDIRector;
 use Rector\Doctrine\Rector\Class_\RemoveRepositoryFromEntityAnnotationRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
+/**
+ * @see https://tomasvotruba.com/blog/2017/10/16/how-to-use-repository-with-doctrine-as-service-in-symfony/
+ * @see https://tomasvotruba.com/blog/2018/04/02/rectify-turn-repositories-to-services-in-symfony/
+ */
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
 
     # order matters, this needs to be first to correctly detect parent repository
     $services->set(MoveRepositoryFromParentToConstructorRector::class);
-
     $services->set(ServiceLocatorToDIRector::class);
-
     $services->set(ReplaceParentRepositoryCallsByRepositoryPropertyRector::class);
-
     $services->set(RemoveRepositoryFromEntityAnnotationRector::class);
+    $services->set(ReplaceParentRepositoryCallsByRepositoryPropertyRector::class);
 };
