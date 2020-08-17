4a42b275696c996b108d43a2851b0c3c34fabf6e
dobryy <igordobritskiy@gmail.com>
2020-08-18 00:35:10 +0200

    Fix namespace issue in Symfony28Test

--- rules/generic/tests/Rector/ClassMethod/ArgumentDefaultValueReplacerRector/Symfony28Test.php
+++ rules/generic/tests/Rector/ClassMethod/ArgumentDefaultValueReplacerRector/Symfony28Test.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Rector\Generic\Tests\Rector\Argument\ArgumentDefaultValueReplacerRector;
+namespace Rector\Generic\Tests\Rector\ClassMethod\ArgumentDefaultValueReplacerRector;
 
 use Iterator;
 use Rector\Core\Testing\PHPUnit\AbstractRectorTestCase;
