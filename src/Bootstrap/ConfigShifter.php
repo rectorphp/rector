ac4e73b72f42273e019a91fb98b32b0c87ab08dc
Tomas Votruba <tomas.vot@gmail.com>
2020-08-20 15:37:13 +0200

    add ConfigShifter to make use of root config parameter override (#3994)

---
+++ src/Bootstrap/ConfigShifter.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Core\Bootstrap;
+
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class ConfigShifter
+{
+    /**
+     * Shift input config as last, so the parameters override previous rules loaded from sets
+     *
+     * @param SmartFileInfo[] $configFileInfos
+     * @noRector
+     */
+    public function shiftInputConfigAsLast(array $configFileInfos, ?SmartFileInfo $inputConfigFileInfo): array
+    {
+        if ($inputConfigFileInfo === null) {
+            return $configFileInfos;
+        }
+
+        $mainConfigShiftedAsLast = [];
+        foreach ($configFileInfos as $configFileInfo) {
+            if ($configFileInfo !== $inputConfigFileInfo) {
+                $mainConfigShiftedAsLast[] = $configFileInfo;
+            }
+        }
+
+        $mainConfigShiftedAsLast[] = $inputConfigFileInfo;
+
+        return $mainConfigShiftedAsLast;
+    }
+}
