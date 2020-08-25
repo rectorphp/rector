02087a5a0a3fbe92509df13b36e11713ce2bcafa
TomasVotruba <tomas.vot@gmail.com>
2020-08-26 01:09:26 +0200

    drop unamtained shopware sets

--- config/set/phpunit-yield-data-provider.php
+++ config/set/phpunit-yield-data-provider.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 use Rector\CodingStyle\Rector\ClassMethod\ReturnArrayClassMethodToYieldRector;
 use Rector\CodingStyle\ValueObject\MethodToYield;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use function Rector\SymfonyPhpConfig\inline_value_objects;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
@@ -13,8 +13,8 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->set(ReturnArrayClassMethodToYieldRector::class)
         ->call('configure', [[
             ReturnArrayClassMethodToYieldRector::METHODS_TO_YIELDS => inline_value_objects([
-                new MethodToYield('PHPUnit\Framework\TestCase','provide*'),
-                new MethodToYield('PHPUnit\Framework\TestCase','dataProvider*'),
+                new MethodToYield('PHPUnit\Framework\TestCase', 'provide*'),
+                new MethodToYield('PHPUnit\Framework\TestCase', 'dataProvider*'),
             ]),
         ]]);
 };
