02e703b9b9a3541d939c9e0a38b9b4ef61689c9d
Leonardo Losoviz <leo@getpop.org>
2020-09-02 00:12:01 +0800

    Configurable downgrade (#4099)

--- rules/downgrade/tests/Rector/Property/NoDocBlockDowngradeTypedPropertyRector/DowngradeTypedPropertyRectorTest.php
+++ rules/downgrade/tests/Rector/Property/NoDocBlockDowngradeTypedPropertyRector/DowngradeTypedPropertyRectorTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Rector\Downgrade\Tests\Rector\Property\DowngradeTypedPropertyRector;
+namespace Rector\Downgrade\Tests\Rector\Property\NoDocBlockDowngradeTypedPropertyRector;
 
 use Iterator;
 use Rector\Core\Testing\PHPUnit\AbstractRectorTestCase;
@@ -32,7 +32,7 @@ final class DowngradeTypedPropertyRectorTest extends AbstractRectorTestCase
     {
         return [
             DowngradeTypedPropertyRector::class => [
-                DowngradeTypedPropertyRector::ADD_DOC_BLOCK => true,
+                DowngradeTypedPropertyRector::ADD_DOC_BLOCK => false,
             ],
         ];
     }
