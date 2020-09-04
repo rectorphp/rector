a797801d16f9b6a2329daf50e678e7b6e03ac004
Markus Staab <maggus.staab@googlemail.com>
2020-09-04 16:12:15 +0200

    make EXCLUDE_PATHS work across different OSes

--- src/FileSystem/FilesFinder.php
+++ src/FileSystem/FilesFinder.php
@@ -157,9 +157,15 @@ final class FilesFinder
                 //dead symlink
                 return false;
             }
+            
+            // make the path work accross different OSes
+            $realPath = str_replace('\\', '/', $realPath);
 
             // return false to remove file
             foreach ($this->excludePaths as $excludePath) {
+                // make the path work accross different OSes
+                $excludePath = str_replace('\\', '/', $excludePath);
+                
                 if (Strings::match($realPath, '#' . preg_quote($excludePath, '#') . '#')) {
                     return false;
                 }
