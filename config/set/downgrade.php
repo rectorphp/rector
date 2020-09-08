712259f1b2d95cfa1a60795b3da6630a1bb3dac8
Leonardo Losoviz <leo@getpop.org>
2020-09-08 18:43:42 +0800

    Fixed cs

--- config/set/downgrade.php
+++ config/set/downgrade.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 
-use Rector\DowngradePhp74\Rector\ArrowFunction\ArrowFunctionToAnonymousFunctionRector;
-use Rector\DowngradePhp74\Rector\Coalesce\DowngradeNullCoalescingOperatorRector;
 use Rector\DowngradePhp72\Rector\FunctionLike\DowngradeParamObjectTypeDeclarationRector;
 use Rector\DowngradePhp72\Rector\FunctionLike\DowngradeReturnObjectTypeDeclarationRector;
+use Rector\DowngradePhp74\Rector\ArrowFunction\ArrowFunctionToAnonymousFunctionRector;
+use Rector\DowngradePhp74\Rector\Coalesce\DowngradeNullCoalescingOperatorRector;
 use Rector\DowngradePhp74\Rector\Property\DowngradeTypedPropertyRector;
 use Rector\DowngradePhp80\Rector\Property\DowngradeUnionTypeToDocBlockRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
