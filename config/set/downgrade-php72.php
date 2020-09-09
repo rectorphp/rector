1631abd63af6948929dd7741f54aeadff0611e25
Leonardo Losoviz <leo@getpop.org>
2020-09-09 09:40:01 +0800

    Split downgrade sets

---
+++ config/set/downgrade-php72.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\DowngradePhp72\Rector\FunctionLike\DowngradeParamObjectTypeDeclarationRector;
+use Rector\DowngradePhp72\Rector\FunctionLike\DowngradeReturnObjectTypeDeclarationRector;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+    $services->set(DowngradeParamObjectTypeDeclarationRector::class);
+    $services->set(DowngradeReturnObjectTypeDeclarationRector::class);
+};
--- config/set/downgrade-php80.php
+++ config/set/downgrade-php80.php
@@ -2,11 +2,6 @@
 
 declare(strict_types=1);
 
-use Rector\DowngradePhp72\Rector\FunctionLike\DowngradeParamObjectTypeDeclarationRector;
-use Rector\DowngradePhp72\Rector\FunctionLike\DowngradeReturnObjectTypeDeclarationRector;
-use Rector\DowngradePhp74\Rector\ArrowFunction\ArrowFunctionToAnonymousFunctionRector;
-use Rector\DowngradePhp74\Rector\Coalesce\DowngradeNullCoalescingOperatorRector;
-use Rector\DowngradePhp74\Rector\Property\DowngradeTypedPropertyRector;
 use Rector\DowngradePhp80\Rector\FunctionLike\DowngradeParamMixedTypeDeclarationRector;
 use Rector\DowngradePhp80\Rector\FunctionLike\DowngradeReturnMixedTypeDeclarationRector;
 use Rector\DowngradePhp80\Rector\FunctionLike\DowngradeReturnStaticTypeDeclarationRector;
@@ -17,23 +12,10 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
-    /**
-     * Execute in order, from higher to lower versions of PHP
-     * This is because `??=` in 7.4 downgrades to `??` from 7.3,
-     * and `??` downgrades to `isset` in 5.6.
-     */
-    // PHP 8.0
     $services->set(DowngradeUnionTypeTypedPropertyRector::class);
     $services->set(DowngradeUnionTypeReturnDeclarationRector::class);
     $services->set(DowngradeUnionTypeParamDeclarationRector::class);
     $services->set(DowngradeParamMixedTypeDeclarationRector::class);
     $services->set(DowngradeReturnMixedTypeDeclarationRector::class);
     $services->set(DowngradeReturnStaticTypeDeclarationRector::class);
-    // PHP 7.4
-    $services->set(DowngradeTypedPropertyRector::class);
-    $services->set(ArrowFunctionToAnonymousFunctionRector::class);
-    $services->set(DowngradeNullCoalescingOperatorRector::class);
-    // PHP 7.2
-    $services->set(DowngradeParamObjectTypeDeclarationRector::class);
-    $services->set(DowngradeReturnObjectTypeDeclarationRector::class);
 };
