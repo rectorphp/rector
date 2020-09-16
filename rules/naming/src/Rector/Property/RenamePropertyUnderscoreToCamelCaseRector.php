e8d4ba95be52f44dbcc6c7d1c7dc1c0632971513
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-09-16 15:23:31 +0700

    Fixes #4229 Add property camel case naming rule

---
+++ rules/naming/src/Rector/Property/RenamePropertyUnderscoreToCamelCaseRector.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Naming\Rector\Property;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\PropertyFetch;
+use PhpParser\Node\Stmt\PropertyProperty;
+use Rector\Core\Rector\AbstractRector;
+use Rector\Core\RectorDefinition\CodeSample;
+use Rector\Core\RectorDefinition\RectorDefinition;
+use Rector\Core\Util\StaticRectorStrings;
+
+/**
+ * @see \Rector\Naming\Tests\Rector\Property\RenamePropertyUnderscoreToCamelCaseRector\RenamePropertyUnderscoreToCamelCaseRectorTest
+ */
+final class RenamePropertyUnderscoreToCamelCaseRector extends AbstractRector
+{
+    public function getDefinition(): RectorDefinition
+    {
+        return new RectorDefinition('Renames property with underscore to camel case', [
+            new CodeSample(
+                <<<'CODE_SAMPLE'
+class SomeClass
+{
+    private $some_property;
+
+    public function run(): void
+    {
+        $this->some_property;
+    }
+}
+CODE_SAMPLE
+
+                ,
+                <<<'CODE_SAMPLE'
+class SomeClass
+{
+    private $someProperty;
+
+    public function run(): void
+    {
+        $this->someProperty;
+    }
+}
+CODE_SAMPLE
+
+            ),
+        ]);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [PropertyProperty::class, PropertyFetch::class];
+    }
+
+    /**
+     * @param PropertyProperty|PropertyFetch $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        $propertyName = $this->getName($node);
+        if (strpos($propertyName, '_') === false) {
+            return null;
+        }
+
+        $propertyName = StaticRectorStrings::underscoreToCamelCase($propertyName);
+        $node->name = $propertyName;
+
+        return $node;
+    }
+}
---
+++ rules/naming/tests/Rector/Property/RenamePropertyUnderscoreToCamelCaseRectorTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Naming\Tests\Rector\Property\RenamePropertyUnderscoreToCamelCaseRector;
+
+use Iterator;
+use Rector\Core\Testing\PHPUnit\AbstractRectorTestCase;
+use Rector\Naming\Rector\Property\RenamePropertyUnderscoreToCamelCaseRector;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class RenamePropertyUnderscoreToCamelCaseRectorTest extends AbstractRectorTestCase
+{
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(SmartFileInfo $fileInfo): void
+    {
+        $this->doTestFileInfo($fileInfo);
+    }
+
+    public function provideData(): Iterator
+    {
+        return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
+    }
+
+    protected function getRectorClass(): string
+    {
+        return RenamePropertyUnderscoreToCamelCaseRector::class;
+    }
+}
