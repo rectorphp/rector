e56e457715e17690c2721250612e8dd9559bbd83
TomasVotruba <tomas.vot@gmail.com>
2020-09-24 16:46:48 +0200

    monorepob-builder: add v prefix

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -8,6 +8,9 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 use Symplify\MonorepoBuilder\ValueObject\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::PACKAGE_ALIAS_FORMAT, 'v<major>.<minor>.x-dev');
+
     $services = $containerConfigurator->services();
 
     // release workers - in order to execute
