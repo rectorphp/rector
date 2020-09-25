b49333d628f73aedd41d134e2d378e306804b1a5
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-09-25 14:23:30 +0700

    Fixes #4290 wrong skip assignment not from parameter in UnderscoreToCamelCaseLocalVariableNameRector (#4293)
    Co-authored-by: rector-bot <tomas@getrector.org>

--- rules/coding-style/src/Rector/Variable/UnderscoreToCamelCaseLocalVariableNameRector.php
+++ rules/coding-style/src/Rector/Variable/UnderscoreToCamelCaseLocalVariableNameRector.php
@@ -7,8 +7,12 @@ namespace Rector\CodingStyle\Rector\Variable;
 use Nette\Utils\Strings;
 use PhpParser\Node;
 use PhpParser\Node\Arg;
+use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\Variable;
 use PhpParser\Node\Param;
+use PhpParser\Node\Stmt;
+use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\Node\Stmt\Function_;
 use Rector\Core\Php\ReservedKeywordAnalyzer;
 use Rector\Core\Rector\AbstractRector;
 use Rector\Core\RectorDefinition\CodeSample;
@@ -71,16 +75,6 @@ CODE_SAMPLE
      */
     public function refactor(Node $node): ?Node
     {
-        $parentNode = $node->getAttribute(AttributeKey::PARENT_NODE);
-        if ($parentNode instanceof Param || $parentNode instanceof Arg) {
-            return null;
-        }
-
-        $previousNode = $node->getAttribute(AttributeKey::PREVIOUS_NODE);
-        if ($previousNode instanceof Variable) {
-            return null;
-        }
-
         $nodeName = $this->getName($node);
         if ($nodeName === null) {
             return null;
@@ -99,8 +93,53 @@ CODE_SAMPLE
             return null;
         }
 
+        $parentNode = $node->getAttribute(AttributeKey::PARENT_NODE);
+        if ($parentNode instanceof Expr && $this->isFoundInParentNode($node)) {
+            return null;
+        }
+
+        if (($parentNode instanceof Arg || $parentNode instanceof Param || $parentNode instanceof Stmt)
+            && $this->isFoundInParentNode($node)) {
+            return null;
+        }
+
+        if ($this->isFoundInPreviousNode($node)) {
+            return null;
+        }
+
         $node->name = $camelCaseName;
 
         return $node;
+    }
+
+    private function isFoundInParentNode(Variable $variable): bool
+    {
+        $parentNode = $variable->getAttribute(AttributeKey::PARENT_NODE);
+        while ($parentNode) {
+            /** @var ClassMethod|Function_ $parentNode */
+            $parentNode = $parentNode->getAttribute(AttributeKey::PARENT_NODE);
+            if ($parentNode instanceof ClassMethod || $parentNode instanceof Function_) {
+                break;
+            }
+        }
+
+        if ($parentNode === null) {
+            return false;
+        }
+
+        $params = $parentNode->getParams();
+        foreach ($params as $param) {
+            if ($param->var->name === $variable->name) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function isFoundInPreviousNode(Variable $variable): bool
+    {
+        $previousNode = $variable->getAttribute(AttributeKey::PREVIOUS_NODE);
+        return $previousNode instanceof Expr && $this->isFoundInParentNode($variable);
     }
 }
