ae05ee976de76ed6062ab8b1bfe5a02ac6c83afc
Kerrial Newham <kerrialbeckettnewham@gmail.com>
2020-09-28 20:20:10 +0200

    add init command

---
+++ src/Console/Command/InitCommand.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Core\Console\Command;
+
+use Symfony\Component\Console\Exception\RuntimeException;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symplify\PackageBuilder\Console\Command\CommandNaming;
+use Symplify\PackageBuilder\Console\ShellCode;
+use Symplify\SmartFileSystem\SmartFileSystem;
+
+class InitCommand extends AbstractCommand
+{
+    /**
+     * @var SmartFileSystem
+     */
+    private $smartFileSystem;
+
+    /**
+     * @var SymfonyStyle
+     */
+    private $symfonyStyle;
+
+    /**
+     * InitCommand constructor.
+     */
+    public function __construct(SmartFileSystem $smartFileSystem, SymfonyStyle $symfonyStyle)
+    {
+        parent::__construct();
+
+        $this->smartFileSystem = $smartFileSystem;
+        $this->symfonyStyle = $symfonyStyle;
+    }
+
+    protected function configure(): void
+    {
+        $this->setName(CommandNaming::classToName(self::class));
+        $this->setDescription('Generate rector.php configuration file');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        try {
+            $rectorConfigFiles = $this->smartFileSystem->exists(getcwd() . '/rector.php');
+
+            if (! $rectorConfigFiles) {
+                $this->smartFileSystem->copy('rector.php.dist', getcwd() . '/rector.php');
+                $this->symfonyStyle->success('rector.php config file has been generated successfully');
+            } else {
+                $this->symfonyStyle->error('Config file not generated. A rector.php configuration file already exists');
+            }
+        } catch (RuntimeException $runtimeException) {
+            throw $runtimeException;
+        }
+
+        return ShellCode::SUCCESS;
+    }
+}
