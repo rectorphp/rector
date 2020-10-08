cec5b5de7e98b786fc15680ee983e3904ade1df2
TomasVotruba <tomas.vot@gmail.com>
2020-10-08 17:28:01 +0200

    [PSR-4] Make NormalizeNamespaceByPSR4ComposerAutoloadFileSystemRector also correct wrong namespace

---
+++ rules/psr4/tests/Rector/FileSystem/NormalizeNamespaceByPSR4ComposerAutoloadFileSystemRector/NormalizeNamespaceByPSR4ComposerAutoloadFileSystemRectorTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\PSR4\Tests\Rector\FileSystem\NormalizeNamespaceByPSR4ComposerAutoloadFileSystemRector;
+
+use Rector\Core\Testing\PHPUnit\AbstractFileSystemRectorTestCase;
+use Rector\PSR4\Rector\FileSystem\NormalizeNamespaceByPSR4ComposerAutoloadFileSystemRector;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class NormalizeNamespaceByPSR4ComposerAutoloadFileSystemRectorTest extends AbstractFileSystemRectorTestCase
+{
+    public function test(): void
+    {
+        $fixtureFileInfo = new SmartFileInfo(__DIR__ . '/Fixture/namespace_less_class.php.inc');
+        $this->doTestFileInfo($fixtureFileInfo);
+
+        $this->assertFileExists($this->getFixtureTempDirectory() . '/Fixture/namespace_less_class.php.inc');
+
+        $this->assertFileEquals(
+            __DIR__ . '/Source/ExpectedNamespaceLessClass.php',
+            $this->getFixtureTempDirectory() . '/Fixture/namespace_less_class.php.inc'
+        );
+    }
+
+    public function testNamespaceCorrection(): void
+    {
+        $fixtureFileInfo = new SmartFileInfo(__DIR__ . '/Fixture/wrong_namespace.php.inc');
+        $this->doTestFileInfo($fixtureFileInfo);
+
+        $this->assertFileExists($this->getFixtureTempDirectory() . '/Fixture/wrong_namespace.php.inc');
+
+        $this->assertFileEquals(
+            __DIR__ . '/Source/ExpectedWrongNamespace.php',
+            $this->getFixtureTempDirectory() . '/Fixture/wrong_namespace.php.inc'
+        );
+    }
+
+    protected function provideConfigFileInfo(): SmartFileInfo
+    {
+        return new SmartFileInfo(__DIR__ . '/config/normalize_namespace_without_namespace_config.php');
+    }
+
+    protected function getRectorClass(): string
+    {
+        return NormalizeNamespaceByPSR4ComposerAutoloadFileSystemRector::class;
+    }
+}
