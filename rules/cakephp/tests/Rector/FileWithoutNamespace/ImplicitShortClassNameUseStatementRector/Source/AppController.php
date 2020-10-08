161cd55cbf830bbacbf6d5bbaa6fdb023f9ea842
TomasVotruba <tomas.vot@gmail.com>
2020-10-08 17:47:43 +0200

    [PSR-4] Change NormalizeNamespaceByPSR4ComposerAutoloadFileSystemRector to FileWithoutNamespace node

--- rules/cakephp/tests/Rector/FileWithoutNamespace/ImplicitShortClassNameUseStatementRector/Source/AppController.php
+++ rules/cakephp/tests/Rector/FileWithoutNamespace/ImplicitShortClassNameUseStatementRector/Source/AppController.php
@@ -6,3 +6,17 @@ class AppController
 {
 
 }
+
+?>
+-----
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\PSR4\Tests\Rector\FileSystem\NormalizeNamespaceByPSR4ComposerAutoloadFileSystemRector\Fixture;
+
+class NamespaceLessClass
+{
+}
+
+?>
--- rules/psr4/tests/Rector/FileSystem/NormalizeNamespaceByPSR4ComposerAutoloadFileSystemRector/NormalizeNamespaceByPSR4ComposerAutoloadFileSystemRectorTest.php
+++ rules/psr4/tests/Rector/FileSystem/NormalizeNamespaceByPSR4ComposerAutoloadFileSystemRector/NormalizeNamespaceByPSR4ComposerAutoloadFileSystemRectorTest.php
@@ -4,36 +4,24 @@ declare(strict_types=1);
 
 namespace Rector\PSR4\Tests\Rector\FileSystem\NormalizeNamespaceByPSR4ComposerAutoloadFileSystemRector;
 
-use Rector\Core\Testing\PHPUnit\AbstractFileSystemRectorTestCase;
+use Iterator;
+use Rector\Core\Testing\PHPUnit\AbstractRectorTestCase;
 use Rector\PSR4\Rector\FileSystem\NormalizeNamespaceByPSR4ComposerAutoloadFileSystemRector;
 use Symplify\SmartFileSystem\SmartFileInfo;
 
-final class NormalizeNamespaceByPSR4ComposerAutoloadFileSystemRectorTest extends AbstractFileSystemRectorTestCase
+final class NormalizeNamespaceByPSR4ComposerAutoloadFileSystemRectorTest extends AbstractRectorTestCase
 {
-    public function test(): void
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(SmartFileInfo $smartFileInfo): void
     {
-        $fixtureFileInfo = new SmartFileInfo(__DIR__ . '/Fixture/namespace_less_class.php.inc');
-        $this->doTestFileInfo($fixtureFileInfo);
-
-        $this->assertFileExists($this->getFixtureTempDirectory() . '/Fixture/namespace_less_class.php.inc');
-
-        $this->assertFileEquals(
-            __DIR__ . '/Source/ExpectedNamespaceLessClass.php',
-            $this->getFixtureTempDirectory() . '/Fixture/namespace_less_class.php.inc'
-        );
+        $this->doTestFileInfo($smartFileInfo);
     }
 
-    public function testNamespaceCorrection(): void
+    public function provideData(): Iterator
     {
-        $fixtureFileInfo = new SmartFileInfo(__DIR__ . '/Fixture/wrong_namespace.php.inc');
-        $this->doTestFileInfo($fixtureFileInfo);
-
-        $this->assertFileExists($this->getFixtureTempDirectory() . '/Fixture/wrong_namespace.php.inc');
-
-        $this->assertFileEquals(
-            __DIR__ . '/Source/ExpectedWrongNamespace.php',
-            $this->getFixtureTempDirectory() . '/Fixture/wrong_namespace.php.inc'
-        );
+        return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
     }
 
     protected function provideConfigFileInfo(): SmartFileInfo
