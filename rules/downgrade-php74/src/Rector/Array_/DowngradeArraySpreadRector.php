f09b45e78ffd058836711ad9ef06009c5c32144d
Leonardo Losoviz <leo@getpop.org>
2020-10-09 10:51:48 +0800

    Implemented Rector

---
+++ rules/downgrade-php74/src/Rector/Array_/DowngradeArraySpreadRector.php
@@ -0,0 +1,126 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\DowngradePhp74\Rector\Array_;
+
+use PhpParser\Node;
+use PhpParser\Node\Arg;
+use PhpParser\Node\Expr\Array_;
+use PhpParser\Node\Expr\ArrayItem;
+use PhpParser\Node\Expr\FuncCall;
+use PhpParser\Node\Expr\Ternary;
+use PhpParser\Node\Name;
+use Rector\Core\Rector\AbstractRector;
+use Rector\Core\RectorDefinition\CodeSample;
+use Rector\Core\RectorDefinition\RectorDefinition;
+
+/**
+ * @see \Rector\DowngradePhp74\Tests\Rector\Array_\DowngradeArraySpreadRector\DowngradeArraySpreadRectorTest
+ */
+final class DowngradeArraySpreadRector extends AbstractRector
+{
+    public function getDefinition(): RectorDefinition
+    {
+        return new RectorDefinition('Replace array spread with array_merge function', [
+            new CodeSample(
+                <<<'CODE_SAMPLE'
+class SomeClass
+{
+    public function run()
+    {
+        $parts = ['apple', 'pear'];
+        $fruits = ['banana', 'orange', ...$parts, 'watermelon'];
+    }
+}
+CODE_SAMPLE
+                ,
+                <<<'CODE_SAMPLE'
+class SomeClass
+{
+    public function run()
+    {
+        $parts = ['apple', 'pear'];
+        $fruits = array_merge(['banana', 'orange'], $parts, ['watermelon']);
+    }
+}
+CODE_SAMPLE
+            ),
+        ]);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [Array_::class];
+    }
+
+    /**
+     * @param Array_ $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if (! $this->shouldRefactor($node)) {
+            return null;
+        }
+        // Iterate all array items:
+        // 1. If they are the spread, replace with the normal variable
+        // 2. If not, make them part of an array
+        $newItems = [];
+        $itemArray = [];
+        foreach ($node->items as $item) {
+            if ($item !== null && $item->unpack) {
+                // Spread operator found
+                // If previous items were in the new array, add them first
+                if ($itemArray) {
+                    $newItems[] = new ArrayItem(new Array_($itemArray));
+                    $itemArray = [];
+                }
+                // The item still has "unpack = true" here.
+                // It will be removed later on
+                $newItems[] = $item;
+            } else {
+                $itemArray[] = $item;
+            }
+        }
+        // Add the remaining items from the last run
+        if ($itemArray) {
+            $newItems[] = new ArrayItem(new Array_($itemArray));
+        }
+        // Replace this array node with an `array_merge`
+        return $this->createArrayMerge($newItems);
+    }
+
+    private function shouldRefactor(Array_ $node): bool
+    {
+        // Check that any item in the array is the spread
+        return count(array_filter($node->items, function (?ArrayItem $item) {
+            return $item !== null && $item->unpack;
+        })) > 0;
+    }
+
+    /**
+     * @see https://wiki.php.net/rfc/spread_operator_for_array
+     * @param (ArrayItem|null)[] $items
+     */
+    private function createArrayMerge(array $items): FuncCall
+    {
+        return new FuncCall(new Name('array_merge'), array_map(function (?ArrayItem $item) {
+            if ($item !== null && $item->unpack) {
+                // Do not unpack anymore
+                $item->unpack = false;
+                // array_merge only supports array, while spread operator also supports objects implementing Traversable.
+                return new Arg(
+                    new Ternary(
+                        new FuncCall(new Name('is_array'), [new Arg($item)]),
+                        $item,
+                        new FuncCall(new Name('iterator_to_array'), [new Arg($item)])
+                    )
+                );
+            }
+            return new Arg($item);
+        }, $items));
+    }
+}
