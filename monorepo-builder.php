2635f174c901afa2cda62013c116466434d4a047
TomasVotruba <tomas.vot@gmail.com>
2020-10-10 23:53:39 +0200

    bump symplify 8.3.33

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -17,5 +17,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::DIRECTORIES_TO_REPOSITORIES, [
         __DIR__ . '/packages/symfony-php-config' => 'git@github.com:rectorphp/symfony-php-config.git',
+//        __DIR__ . '/packages/symfony-php-config' => 'git@github.com:rectorphp/symfony-php-config.git',
     ]);
 };
