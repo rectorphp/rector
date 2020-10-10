8ca05b8571b1f81bb4cf5644f1d169a872e217dd
TomasVotruba <tomas.vot@gmail.com>
2020-10-11 01:15:12 +0200

    drop unused project dir

--- packages/file-system-rector/config/config.php
+++ packages/file-system-rector/config/config.php
@@ -5,15 +5,12 @@ declare(strict_types=1);
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $parameters = $containerConfigurator->parameters();
-
-    $parameters->set('project_directory', null);
-
     $services = $containerConfigurator->services();
 
     $services->defaults()
         ->public()
-        ->autowire();
+        ->autowire()
+        ->autoconfigure();
 
     $services->load('Rector\FileSystemRector\\', __DIR__ . '/../src')
         ->exclude([__DIR__ . '/../src/Rector']);
