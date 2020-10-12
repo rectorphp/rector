202631d1989d05f79cdc9b70d8c925646e71314f
TomasVotruba <tomas.vot@gmail.com>
2020-10-12 21:44:10 +0200

    add missing services

--- packages/simple-php-doc-parser/config/config.php
+++ packages/simple-php-doc-parser/config/config.php
@@ -3,7 +3,9 @@
 declare(strict_types=1);
 
 use PHPStan\PhpDocParser\Lexer\Lexer;
+use PHPStan\PhpDocParser\Parser\ConstExprParser;
 use PHPStan\PhpDocParser\Parser\PhpDocParser;
+use PHPStan\PhpDocParser\Parser\TypeParser;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
@@ -18,4 +20,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $services->set(PhpDocParser::class);
     $services->set(Lexer::class);
+    $services->set(TypeParser::class);
+    $services->set(ConstExprParser::class);
 };
