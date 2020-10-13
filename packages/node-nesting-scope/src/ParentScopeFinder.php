434586eb97ac708373caf454bdc74c8748d6586a
Anna Filina <afilina@gmail.com>
2020-10-13 17:22:11 -0400

    Add closure to possible parent scopes

--- packages/node-nesting-scope/src/ParentScopeFinder.php
+++ packages/node-nesting-scope/src/ParentScopeFinder.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Rector\NodeNestingScope;
 
 use PhpParser\Node;
+use PhpParser\Node\Expr\Closure;
 use PhpParser\Node\Stmt\Class_;
 use PhpParser\Node\Stmt\ClassMethod;
 use PhpParser\Node\Stmt\Function_;
@@ -14,11 +15,12 @@ use Rector\NodeTypeResolver\Node\AttributeKey;
 final class ParentScopeFinder
 {
     /**
-     * @return ClassMethod|Function_|Class_|Namespace_|null
+     * @return ClassMethod|Function_|Class_|Namespace_|Closure|null
      */
     public function find(Node $node): ?Node
     {
-        return $node->getAttribute(AttributeKey::METHOD_NODE)
+        return $node->getAttribute(AttributeKey::CLOSURE_NODE)
+            ?? $node->getAttribute(AttributeKey::METHOD_NODE)
             ?? $node->getAttribute(AttributeKey::FUNCTION_NODE)
             ?? $node->getAttribute(AttributeKey::CLASS_NODE)
             ?? $node->getAttribute(AttributeKey::NAMESPACE_NODE);
