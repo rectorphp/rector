2b95984eb6302ecac22b5f58da1813d8a397206d
Matthias Noback <matthiasnoback@gmail.com>
2020-10-14 16:46:33 +0200

    Add a tag value node for Doctrine Embedded tags

---
+++ packages/better-php-doc-parser/tests/PhpDocParser/TagValueNodeReprint/Fixture/DoctrineEmbedded/AnEntityWithAnEmbeddedAndAColumnPrefix.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Rector\BetterPhpDocParser\Tests\PhpDocParser\TagValueNodeReprint\Fixture\DoctrineEmbedded;
+
+use Doctrine\ORM\Mapping as ORM;
+use Rector\BetterPhpDocParser\Tests\PhpDocParser\TagValueNodeReprint\Source\Embeddable;
+
+final class AnEntityWithAnEmbeddedAndAColumnPrefix
+{
+    /**
+     * @ORM\Embedded(class="Embeddable", columnPrefix="prefix_")
+     */
+    private $embedded;
+}
