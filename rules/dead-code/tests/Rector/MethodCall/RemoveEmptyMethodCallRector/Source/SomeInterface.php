f64a6ff97e7817871d35518cdb1e5aa74bde3a87
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-10-17 19:17:40 +0700

    fixture skip interface

---
+++ rules/dead-code/tests/Rector/MethodCall/RemoveEmptyMethodCallRector/Source/SomeInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\DeadCode\Tests\Rector\MethodCall\RemoveEmptyMethodCallRector\Source;
+
+interface SomeInterface
+{
+    public function run();
+}
