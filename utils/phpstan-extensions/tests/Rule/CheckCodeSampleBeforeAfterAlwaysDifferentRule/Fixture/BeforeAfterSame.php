63615f41971bbbe795aa7a86070569df9e8ad4d8
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-10-18 19:14:55 +0700

    [PHPStan] Fixes #4433 Add CheckCodeSampleBeforeAfterAlwaysDifferentRule (#4439)

---
+++ utils/phpstan-extensions/tests/Rule/CheckCodeSampleBeforeAfterAlwaysDifferentRule/Fixture/BeforeAfterSame.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\PHPStanExtensions\Tests\Rule\CheckCodeSampleBeforeAfterAlwaysDifferentRule\Fixture;
+
+use Rector\Core\RectorDefinition\CodeSample;
+
+new CodeSample('A', 'A');
