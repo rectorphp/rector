16d19318c279cd064192035f80bd76dad339e401
TomasVotruba <tomas.vot@gmail.com>
2020-10-18 21:27:50 +0200

    make use of Types of doc types

--- rules/doctrine-code-quality/src/PhpDoc/CollectionTypeResolver.php
+++ rules/doctrine-code-quality/src/PhpDoc/CollectionTypeResolver.php
@@ -4,37 +4,52 @@ declare(strict_types=1);
 
 namespace Rector\DoctrineCodeQuality\PhpDoc;
 
+use PhpParser\Node;
 use PhpParser\Node\Stmt\Property;
 use PHPStan\PhpDocParser\Ast\Type\ArrayTypeNode;
 use PHPStan\PhpDocParser\Ast\Type\IdentifierTypeNode;
 use PHPStan\PhpDocParser\Ast\Type\TypeNode;
 use PHPStan\PhpDocParser\Ast\Type\UnionTypeNode;
-use Rector\AttributeAwarePhpDoc\Ast\Type\AttributeAwareIdentifierTypeNode;
 use Rector\BetterPhpDocParser\PhpDocInfo\PhpDocInfo;
 use Rector\BetterPhpDocParser\PhpDocNode\Doctrine\Property_\OneToManyTagValueNode;
 use Rector\Core\Exception\ShouldNotHappenException;
 use Rector\NodeTypeResolver\Node\AttributeKey;
+use Rector\PHPStan\Type\FullyQualifiedObjectType;
+use Rector\StaticTypeMapper\PHPStan\NameScopeFactory;
 
 final class CollectionTypeResolver
 {
-    public function resolveFromType(TypeNode $typeNode): ?IdentifierTypeNode
+    /**
+     * @var NameScopeFactory
+     */
+    private $nameScopeFactory;
+
+    public function __construct(NameScopeFactory $nameScopeFactory)
+    {
+        $this->nameScopeFactory = $nameScopeFactory;
+    }
+
+    public function resolveFromTypeNode(TypeNode $typeNode, Node $node): ?FullyQualifiedObjectType
     {
         if ($typeNode instanceof UnionTypeNode) {
             foreach ($typeNode->types as $unionedTypeNode) {
-                if ($this->resolveFromType($unionedTypeNode) !== null) {
-                    return $this->resolveFromType($unionedTypeNode);
+                $resolvedUnionedType = $this->resolveFromTypeNode($unionedTypeNode, $node);
+                if ($resolvedUnionedType !== null) {
+                    return $resolvedUnionedType;
                 }
             }
         }
 
         if ($typeNode instanceof ArrayTypeNode && $typeNode->type instanceof IdentifierTypeNode) {
-            return $typeNode->type;
+            $nameScope = $this->nameScopeFactory->createNameScopeFromNode($node);
+            $fullyQualifiedName = $nameScope->resolveStringName($typeNode->type->name);
+            return new FullyQualifiedObjectType($fullyQualifiedName);
         }
 
         return null;
     }
 
-    public function resolveFromOneToManyProperty(Property $property): ?IdentifierTypeNode
+    public function resolveFromOneToManyProperty(Property $property): ?FullyQualifiedObjectType
     {
         $phpDocInfo = $property->getAttribute(AttributeKey::PHP_DOC_INFO);
         if (! $phpDocInfo instanceof PhpDocInfo) {
@@ -51,6 +66,6 @@ final class CollectionTypeResolver
             throw new ShouldNotHappenException();
         }
 
-        return new AttributeAwareIdentifierTypeNode($fullyQualifiedTargetEntity);
+        return new FullyQualifiedObjectType($fullyQualifiedTargetEntity);
     }
 }
---
+++ rules/doctrine-code-quality/tests/Rector/Property/ImproveDoctrineCollectionDocTypeInEntityRector/Source/Training.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\DoctrineCodeQuality\Tests\Rector\Property\ImproveDoctrineCollectionDocTypeInEntityRector\Source;
+
+final class Training
+{
+
+}
