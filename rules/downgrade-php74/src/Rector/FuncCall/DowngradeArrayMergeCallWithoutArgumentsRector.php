382600f5382b041a45af84d8c8ca3b26c6228d57
Raffael Comi <RaffaelComi@web.de>
2020-10-18 13:59:12 +0200

    Downgrade PHP7.4 array_merge without arguments (#4377)

---
+++ rules/downgrade-php74/src/Rector/FuncCall/DowngradeArrayMergeCallWithoutArgumentsRector.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\DowngradePhp74\Rector\FuncCall;
+
+use PhpParser\Node;
+use PhpParser\Node\Arg;
+use PhpParser\Node\Expr\Array_;
+use PhpParser\Node\Expr\FuncCall;
+use Rector\Core\Rector\AbstractRector;
+use Rector\Core\RectorDefinition\CodeSample;
+use Rector\Core\RectorDefinition\RectorDefinition;
+
+/**
+ * @see \Rector\DowngradePhp74\Tests\Rector\FuncCall\DowngradeArrayMergeCallWithoutArgumentsRector\DowngradeArrayMergeCallWithoutArgumentsRectorTest
+ */
+final class DowngradeArrayMergeCallWithoutArgumentsRector extends AbstractRector
+{
+    public function getDefinition(): RectorDefinition
+    {
+        return new RectorDefinition('Add missing param to `array_merge` and `array_merge_recursive`', [
+            new CodeSample(
+                <<<'CODE_SAMPLE'
+class SomeClass
+{
+    public function run()
+    {
+        array_merge();
+        array_merge_recursive();
+    }
+}
+CODE_SAMPLE
+                ,
+                <<<'CODE_SAMPLE'
+class SomeClass
+{
+    public function run()
+    {
+        array_merge([]);
+        array_merge_recursive([]);
+    }
+}
+CODE_SAMPLE
+            ),
+        ]);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [FuncCall::class];
+    }
+
+    /**
+     * @param FuncCall $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if (! $this->shouldRefactor($node)) {
+            return null;
+        }
+
+        $node->args = [new Arg(new Array_())];
+
+        return $node;
+    }
+
+    /**
+     * @param FuncCall $node
+     */
+    private function shouldRefactor(Node $node): bool
+    {
+        if (! $this->isFuncCallName($node, 'array_merge') && ! $this->isFuncCallName($node, 'array_merge_recursive')) {
+            return false;
+        }
+
+        // If param is provided, do nothing
+        if ($node->args !== []) {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ rules/downgrade-php74/tests/Rector/FuncCall/DowngradeArrayMergeCallWithoutArgumentsRector/DowngradeArrayMergeCallWithoutArgumentsRectorTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\DowngradePhp74\Tests\Rector\FuncCall\DowngradeArrayMergeCallWithoutArgumentsRector;
+
+use Iterator;
+use Rector\Core\Testing\PHPUnit\AbstractRectorTestCase;
+use Rector\Core\ValueObject\PhpVersionFeature;
+use Rector\DowngradePhp74\Rector\FuncCall\DowngradeArrayMergeCallWithoutArgumentsRector;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class DowngradeArrayMergeCallWithoutArgumentsRectorTest extends AbstractRectorTestCase
+{
+    /**
+     * @requires PHP 7.4
+     * @dataProvider provideData()
+     */
+    public function test(SmartFileInfo $fileInfo): void
+    {
+        $this->doTestFileInfo($fileInfo);
+    }
+
+    public function provideData(): Iterator
+    {
+        return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
+    }
+
+    protected function getRectorClass(): string
+    {
+        return DowngradeArrayMergeCallWithoutArgumentsRector::class;
+    }
+
+    protected function getPhpVersion(): string
+    {
+        return PhpVersionFeature::BEFORE_STRIP_TAGS_WITH_ARRAY;
+    }
+}
