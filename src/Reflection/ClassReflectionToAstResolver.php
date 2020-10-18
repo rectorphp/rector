67bdc35fe508a9eae915d5620f5305e186b24b6c
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-10-18 15:31:37 +0700

    [core] Add ClassReflectionToAstResolver service (#4438)

---
+++ src/Reflection/ClassReflectionToAstResolver.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Core\Reflection;
+
+use PhpParser\Node;
+use PhpParser\Node\Stmt\Class_;
+use PhpParser\Parser;
+use PHPStan\Reflection\ClassReflection;
+use Rector\Core\PhpParser\Node\BetterNodeFinder;
+use Symplify\SmartFileSystem\SmartFileSystem;
+
+final class ClassReflectionToAstResolver
+{
+    /**
+     * @var Parser
+     */
+    private $parser;
+
+    /**
+     * @var SmartFileSystem
+     */
+    private $smartFileSystem;
+
+    /**
+     * @var BetterNodeFinder
+     */
+    private $betterNodeFinder;
+
+    public function __construct(Parser $parser, SmartFileSystem $smartFileSystem, BetterNodeFinder $betterNodeFinder)
+    {
+        $this->parser = $parser;
+        $this->smartFileSystem = $smartFileSystem;
+        $this->betterNodeFinder = $betterNodeFinder;
+    }
+
+    public function getClass(ClassReflection $classReflection, string $className): ?Class_
+    {
+        if ($classReflection->isBuiltin()) {
+            return null;
+        }
+
+        /** @var string $fileName */
+        $fileName = $classReflection->getFileName();
+
+        /** @var Node[] $contentNodes */
+        $contentNodes = $this->parser->parse($this->smartFileSystem->readFile($fileName));
+        $classes = $this->betterNodeFinder->findInstanceOf($contentNodes, Class_::class);
+
+        if ($classes === []) {
+            return null;
+        }
+
+        $reflectionClassName = $classReflection->getName();
+        foreach ($classes as $class) {
+            $shortClassName = $class->name;
+            if ($reflectionClassName === $className) {
+                return $class;
+            }
+        }
+
+        return null;
+    }
+}
