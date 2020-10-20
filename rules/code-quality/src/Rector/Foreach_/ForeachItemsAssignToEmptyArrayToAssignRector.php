8bdb879a559c1e8adbb0d3004c8152de5578000c
Tomas Votruba <tomas.vot@gmail.com>
2020-10-20 16:24:32 +0200

    [CodeQuality] Add nested foreach foreach that is required for assign (#4459)

--- rules/code-quality/src/Rector/Foreach_/ForeachItemsAssignToEmptyArrayToAssignRector.php
+++ rules/code-quality/src/Rector/Foreach_/ForeachItemsAssignToEmptyArrayToAssignRector.php
@@ -5,15 +5,15 @@ declare(strict_types=1);
 namespace Rector\CodeQuality\Rector\Foreach_;
 
 use PhpParser\Node;
-use PhpParser\Node\Expr;
-use PhpParser\Node\Expr\ArrayDimFetch;
 use PhpParser\Node\Expr\Assign;
-use PhpParser\Node\Stmt\Expression;
+use PhpParser\Node\Expr\Variable;
 use PhpParser\Node\Stmt\Foreach_;
+use PHPStan\Analyser\Scope;
+use Rector\CodeQuality\NodeAnalyzer\ForeachNodeAnalyzer;
+use Rector\Core\NodeFinder\NodeUsageFinder;
 use Rector\Core\Rector\AbstractRector;
 use Rector\Core\RectorDefinition\CodeSample;
 use Rector\Core\RectorDefinition\RectorDefinition;
-use Rector\NodeNestingScope\NodeFinder\ScopeAwareNodeFinder;
 use Rector\NodeTypeResolver\Node\AttributeKey;
 
 /**
@@ -22,13 +22,19 @@ use Rector\NodeTypeResolver\Node\AttributeKey;
 final class ForeachItemsAssignToEmptyArrayToAssignRector extends AbstractRector
 {
     /**
-     * @var ScopeAwareNodeFinder
+     * @var NodeUsageFinder
      */
-    private $scopeAwareNodeFinder;
+    private $nodeUsageFinder;
 
-    public function __construct(ScopeAwareNodeFinder $scopeAwareNodeFinder)
+    /**
+     * @var ForeachNodeAnalyzer
+     */
+    private $foreachNodeAnalyzer;
+
+    public function __construct(NodeUsageFinder $nodeUsageFinder, ForeachNodeAnalyzer $foreachNodeAnalyzer)
     {
-        $this->scopeAwareNodeFinder = $scopeAwareNodeFinder;
+        $this->nodeUsageFinder = $nodeUsageFinder;
+        $this->foreachNodeAnalyzer = $foreachNodeAnalyzer;
     }
 
     public function getDefinition(): RectorDefinition
@@ -40,9 +46,10 @@ class SomeClass
 {
     public function run($items)
     {
-        $items2 = [];
+        $collectedItems = [];
+
         foreach ($items as $item) {
-             $items2[] = $item;
+             $collectedItems[] = $item;
         }
     }
 }
@@ -53,8 +60,9 @@ class SomeClass
 {
     public function run($items)
     {
-        $items2 = [];
-        $items2 = $items;
+        $collectedItems = [];
+
+        $collectedItems = $items;
     }
 }
 CODE_SAMPLE
@@ -75,17 +83,18 @@ CODE_SAMPLE
      */
     public function refactor(Node $node): ?Node
     {
-        $assignVariable = $this->matchAssignItemsOnlyForeachArrayVariable($node);
+        $assignVariable = $this->foreachNodeAnalyzer->matchAssignItemsOnlyForeachArrayVariable($node);
         if ($assignVariable === null) {
             return null;
         }
 
-        $previousDeclaration = $this->findPreviousNodeUsage($node, $assignVariable);
+        /** @var Scope $scope */
+        $scope = $node->getAttribute(AttributeKey::SCOPE);
+        $previousDeclaration = $this->nodeUsageFinder->findPreviousForeachNodeUsage($node, $assignVariable);
         if ($previousDeclaration === null) {
             return null;
         }
 
-        // covers https://github.com/Symplify/Symplify/pull/1733/checks?check_run_id=379368887#step:5:64
         if ($this->shouldSkipAsPartOfNestedForeach($node)) {
             return null;
         }
@@ -104,74 +113,28 @@ CODE_SAMPLE
         return new Assign($assignVariable, $node->expr);
     }
 
-    private function matchAssignItemsOnlyForeachArrayVariable(Foreach_ $foreach): ?Expr
-    {
-        if (count($foreach->stmts) !== 1) {
-            return null;
-        }
-
-        $onlyStatement = $foreach->stmts[0];
-        if ($onlyStatement instanceof Expression) {
-            $onlyStatement = $onlyStatement->expr;
-        }
-
-        if (! $onlyStatement instanceof Assign) {
-            return null;
-        }
-
-        if (! $onlyStatement->var instanceof ArrayDimFetch) {
-            return null;
-        }
-
-        if ($onlyStatement->var->dim !== null) {
-            return null;
-        }
-
-        if (! $this->areNodesEqual($foreach->valueVar, $onlyStatement->expr)) {
-            return null;
-        }
-
-        return $onlyStatement->var->var;
-    }
-
-    private function findPreviousNodeUsage(Node $node, Expr $expr): ?Node
-    {
-        return $this->scopeAwareNodeFinder->findParent($node, function (Node $node) use ($expr): bool {
-            if ($node === $expr) {
-                return false;
-            }
-
-            return $this->areNodesEqual($node, $expr);
-        }, [Foreach_::class]);
-    }
-
     private function shouldSkipAsPartOfNestedForeach(Foreach_ $foreach): bool
     {
-        $previousForeachVariableUsage = $this->findPreviousNodeUsageInForeach($foreach, $foreach->expr);
+        /** @var Variable|null $foreachedVariable */
+        $foreachedVariable = $this->betterNodeFinder->findFirstInstanceOf($foreach->expr, Variable::class);
+        if ($foreachedVariable === null) {
+            return false;
+        }
+
+        $previousForeachVariableUsage = $this->nodeUsageFinder->findPreviousForeachNodeUsage(
+            $foreach,
+            $foreachedVariable
+        );
+
         if ($previousForeachVariableUsage === null) {
             return false;
         }
 
+        $parent = $previousForeachVariableUsage->getAttribute(AttributeKey::PARENT_NODE);
+
         /** @var Foreach_ $previousForeachVariableUsageParentNode */
-        $previousForeachVariableUsageParentNode = $previousForeachVariableUsage->getAttribute(
-            AttributeKey::PARENT_NODE
-        );
+        $previousForeachVariableUsageParentNode = $parent;
 
-        return $this->areNodesEqual($previousForeachVariableUsageParentNode->valueVar, $foreach->expr);
-    }
-
-    private function findPreviousNodeUsageInForeach(Node $node, Expr $expr): ?Node
-    {
-        return $this->betterNodeFinder->findFirstPrevious($node, function (Node $node) use ($expr): bool {
-            if ($node === $expr) {
-                return false;
-            }
-
-            if (! $this->areNodesEqual($node, $expr)) {
-                return false;
-            }
-
-            return $node->getAttribute(AttributeKey::PARENT_NODE) instanceof Foreach_;
-        });
+        return $this->areNodesEqual($previousForeachVariableUsageParentNode->valueVar, $foreachedVariable);
     }
 }
