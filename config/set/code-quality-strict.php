cb73da1ff330ece50c612ffec4daebe0d9279fee
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-10-29 05:48:37 +0700

    [CODE_QUALITY_STRICT] Add CountArrayToEmptyArrayComparisonRector to CodeQualityStrict

--- config/set/code-quality-strict.php
+++ config/set/code-quality-strict.php
@@ -3,9 +3,11 @@
 declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\If_\MoveOutMethodCallInsideIfConditionRector;
+use Rector\Performance\Rector\FuncCall\CountArrayToEmptyArrayComparisonRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
     $services->set(MoveOutMethodCallInsideIfConditionRector::class);
+    $services->set(CountArrayToEmptyArrayComparisonRector::class);
 };
