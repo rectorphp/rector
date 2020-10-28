d397ea57857b4e1fd7d7b652e6cd51093c7879f2
Raffael Comi <RaffaelComi@web.de>
2020-10-28 19:03:18 +0100

    Add test case

--- packages/node-type-resolver/tests/StaticTypeMapper/StaticTypeMapperTest.php
+++ packages/node-type-resolver/tests/StaticTypeMapper/StaticTypeMapperTest.php
@@ -54,6 +54,8 @@ final class StaticTypeMapperTest extends AbstractKernelTestCase
         ]);
 
         yield [$genericTypeNode, IterableType::class];
+
+        yield [new IdentifierTypeNode('mixed'), MixedType::class];
     }
 
     public function testMapPHPStanTypeToPHPStanPhpDocTypeNode(): void
