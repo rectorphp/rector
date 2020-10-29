f9bd271f782208c91ba897d7f9344fec557bdcf9
Leonardo Losoviz <leo@getpop.org>
2020-10-29 10:09:17 +0800

    Set unneeded transformations in false

--- config/set/downgrade-php73.php
+++ config/set/downgrade-php73.php
@@ -12,4 +12,10 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::PHP_VERSION_FEATURES, '7.2');
+
+    // skip root namespace classes, like \DateTime or \Exception [default: true]
+    $parameters->set(Option::IMPORT_SHORT_CLASSES, false);
+
+    // skip classes used in PHP DocBlocks, like in /** @var \Some\Class */ [default: true]
+    $parameters->set(Option::IMPORT_DOC_BLOCKS, false);
 };
