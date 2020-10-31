ddc2345fb751aa851b3b9d98a39f00f6f0d4a8c3
Tommy Quissens <tommy.quissens@storefront.be>
2020-10-31 12:39:51 +0100

    [Downgrade PHP 7.3] Flexible Heredoc syntax (#4272)

--- config/set/downgrade-php73.php
+++ config/set/downgrade-php73.php
@@ -2,10 +2,12 @@
 
 declare(strict_types=1);
 
+use Rector\DowngradePhp73\Rector\String_\DowngradeFlexibleHeredocSyntaxRector;
 use Rector\DowngradePhp73\Rector\List_\DowngradeListReferenceAssignmentRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
+    $services->set(DowngradeFlexibleHeredocSyntaxRector::class);
     $services->set(DowngradeListReferenceAssignmentRector::class);
 };
