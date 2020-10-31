36c3d5f404a1ea86f454cc4ef0c082eb21345750
Leonardo Losoviz <leo@getpop.org>
2020-10-31 20:39:44 +0800

    Added expression-language as dep for PROD

--- rector-downgrade-dependency.php
+++ rector-downgrade-dependency.php
@@ -14,12 +14,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         // they are not used by Rector, and they use classes
         // loaded with "require-dev" so it'd throw an error
         __DIR__ . '/vendor/symfony/cache/DoctrineProvider.php',
-        __DIR__ . '/vendor/symfony/dependency-injection/Compiler/AbstractRecursivePass.php',
-        __DIR__ . '/vendor/symfony/dependency-injection/Compiler/CheckTypeDeclarationsPass.php',
-        // __DIR__ . '/vendor/symfony/dependency-injection/ContainerBuilder.php',
-        __DIR__ . '/vendor/symfony/dependency-injection/Dumper/PhpDumper.php',
-        __DIR__ . '/vendor/symfony/dependency-injection/ExpressionLanguage.php',
-        __DIR__ . '/vendor/symfony/dependency-injection/ExpressionLanguageProvider.php',
         __DIR__ . '/vendor/symfony/http-kernel/HttpKernelBrowser.php',
         __DIR__ . '/vendor/symfony/string/Slugger/AsciiSlugger.php',
     ]);
