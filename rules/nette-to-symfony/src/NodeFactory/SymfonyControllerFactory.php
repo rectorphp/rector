cf48b2ea83f3432713f904fb2303aba4843535fb
Tomas Votruba <tomas.vot@gmail.com>
2020-11-01 17:03:30 +0100

    decouple (#4534)
    * decouple
    
    * [ci-review] Rector Rectify
    
    * fix static
    
    Co-authored-by: rector-bot <tomas@getrector.org>

---
+++ rules/nette-to-symfony/src/NodeFactory/SymfonyControllerFactory.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\NetteToSymfony\NodeFactory;
+
+use PhpParser\Node\Name;
+use PhpParser\Node\Name\FullyQualified;
+use PhpParser\Node\Stmt\Class_;
+use PhpParser\Node\Stmt\Namespace_;
+use Rector\NodeNameResolver\NodeNameResolver;
+use Rector\NodeTypeResolver\Node\AttributeKey;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class SymfonyControllerFactory
+{
+    /**
+     * @var NodeNameResolver
+     */
+    private $nodeNameResolver;
+
+    /**
+     * @var ActionWithFormProcessClassMethodFactory
+     */
+    private $actionWithFormProcessClassMethodFactory;
+
+    public function __construct(
+        NodeNameResolver $nodeNameResolver,
+        ActionWithFormProcessClassMethodFactory $actionWithFormProcessClassMethodFactory
+    ) {
+        $this->nodeNameResolver = $nodeNameResolver;
+        $this->actionWithFormProcessClassMethodFactory = $actionWithFormProcessClassMethodFactory;
+    }
+
+    public function createNamespace(Class_ $node, Class_ $formTypeClass): ?Namespace_
+    {
+        /** @var SmartFileInfo|null $fileInfo */
+        $fileInfo = $node->getAttribute(AttributeKey::FILE_INFO);
+        if ($fileInfo === null) {
+            return null;
+        }
+
+        /** @var string $namespaceName */
+        $namespaceName = $node->getAttribute(AttributeKey::NAMESPACE_NAME);
+
+        $formControllerClass = new Class_('SomeFormController');
+        $formControllerClass->extends = new FullyQualified(
+            'Symfony\Bundle\FrameworkBundle\Controller\AbstractController'
+        );
+
+        $formTypeClass = $namespaceName . '\\' . $this->nodeNameResolver->getName($formTypeClass);
+        $formControllerClass->stmts[] = $this->actionWithFormProcessClassMethodFactory->create($formTypeClass);
+
+        $namespace = new Namespace_(new Name($namespaceName));
+        $namespace->stmts[] = $formControllerClass;
+
+        return $namespace;
+    }
+}
