ac55464adc42f5343c7c43c37095a96b84c8785b
simivar <simivar@gmail.com>
2020-11-01 18:13:39 +0100

    Split rule into two separate: one for Property and one for Class_

--- config/set/doctrine-code-quality.php
+++ config/set/doctrine-code-quality.php
@@ -5,12 +5,13 @@ declare(strict_types=1);
 use Rector\Doctrine\Rector\Class_\ManagerRegistryGetManagerToEntityManagerRector;
 use Rector\DoctrineCodeQuality\Rector\Class_\InitializeDefaultEntityCollectionRector;
 use Rector\DoctrineCodeQuality\Rector\Class_\MoveCurrentDateTimeDefaultInEntityToConstructorRector;
+use Rector\DoctrineCodeQuality\Rector\Class_\RemoveRedundantDefaultClassAnnotationValuesRector;
 use Rector\DoctrineCodeQuality\Rector\ClassMethod\MakeEntityDateTimePropertyDateTimeInterfaceRector;
 use Rector\DoctrineCodeQuality\Rector\ClassMethod\MakeEntitySetterNullabilityInSyncWithPropertyRector;
 use Rector\DoctrineCodeQuality\Rector\Property\ChangeBigIntEntityPropertyToIntTypeRector;
 use Rector\DoctrineCodeQuality\Rector\Property\CorrectDefaultTypesOnEntityPropertyRector;
 use Rector\DoctrineCodeQuality\Rector\Property\ImproveDoctrineCollectionDocTypeInEntityRector;
-use Rector\DoctrineCodeQuality\Rector\Property\RemoveRedundantDefaultAnnotationValuesRector;
+use Rector\DoctrineCodeQuality\Rector\Property\RemoveRedundantDefaultPropertyAnnotationValuesRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
@@ -23,5 +24,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->set(CorrectDefaultTypesOnEntityPropertyRector::class);
     $services->set(ChangeBigIntEntityPropertyToIntTypeRector::class);
     $services->set(ImproveDoctrineCollectionDocTypeInEntityRector::class);
-    $services->set(RemoveRedundantDefaultAnnotationValuesRector::class);
+    $services->set(RemoveRedundantDefaultPropertyAnnotationValuesRector::class);
+    $services->set(RemoveRedundantDefaultClassAnnotationValuesRector::class);
 };
