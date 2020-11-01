cc441c2ead9ec81c1caa06a907de0ce9d8e1bf42
simivar <simivar@gmail.com>
2020-11-01 02:09:19 +0100

    Add RemoveRedundantDefaultAnnotationValuesRector rule with tests

---
+++ rules/doctrine-code-quality/tests/Rector/Class_/RemoveRedundantDefaultAnnotationValuesRector/RemoveRedundantDefaultAnnotationValuesRectorTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\DoctrineCodeQuality\Tests\Rector\Class_\RemoveRedundantDefaultAnnotationValuesRector;
+
+use Iterator;
+use Rector\DoctrineCodeQuality\Rector\Property\RemoveRedundantDefaultAnnotationValuesRector;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class RemoveRedundantDefaultAnnotationValuesRectorTest extends AbstractRectorTestCase
+{
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(SmartFileInfo $fileInfo): void
+    {
+        $this->doTestFileInfo($fileInfo);
+    }
+
+    public function provideData(): Iterator
+    {
+        return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
+    }
+
+    protected function getRectorClass(): string
+    {
+        return RemoveRedundantDefaultAnnotationValuesRector::class;
+    }
+}
