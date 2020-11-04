b9cf620e94fe143caf28bd59586ca28eac5aaf1e
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-11-04 21:16:02 +0700

    [FileSystem] Improve MovedFileWithNodesFactory : SKip if desiredGroupName already inside oldClassName

---
+++ rules/autodiscovery/tests/Rector/FileNode/MoveInterfacesToContractNamespaceDirectoryRector/Source/Contract/Foo/KeepThisSomeInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Autodiscovery\Tests\Rector\FileNode\MoveInterfacesToContractNamespaceDirectoryRector\Source\Contract\Foo;
+
+interface KeepThisSomeInterface
+{
+
+}
