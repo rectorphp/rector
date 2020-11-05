165ecc5342df1d2ca6f05c0b0c8386ceaf87d4bc
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-11-05 15:27:58 +0700

    [PHP 8.0] [WIP] Fixes #4527 Add NullsafeOperatorRector

---
+++ rules/php80/src/Rector/If_/NullsafeOperatorRector.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Php80\Rector\If_;
+
+use PhpParser\Node;
+use PhpParser\Node\Stmt\If_;
+use Rector\Core\Rector\AbstractRector;
+use Rector\Core\RectorDefinition\CodeSample;
+use Rector\Core\RectorDefinition\RectorDefinition;
+
+/**
+ * @see https://wiki.php.net/rfc/nullsafe_operator
+ * @see \Rector\Php80\Tests\Rector\If_\NullsafeOperatorRector\NullsafeOperatorRectorTest
+ */
+final class NullsafeOperatorRector extends AbstractRector
+{
+    public function getDefinition(): RectorDefinition
+    {
+        return new RectorDefinition('Change nullsafe operator ?-> with full short circuiting', [
+            new CodeSample(
+                <<<'CODE_SAMPLE'
+$country =  null;
+
+if ($session !== null) {
+    $user = $session->user;
+
+    if ($user !== null) {
+        $address = $user->getAddress();
+
+        if ($address !== null) {
+            $country = $address->country;
+        }
+    }
+}
+CODE_SAMPLE
+,
+                <<<'CODE_SAMPLE'
+$country = $session?->user?->getAddress()?->country;
+CODE_SAMPLE
+            ),
+        ]);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [If_::class];
+    }
+
+    /**
+     * @param If_ $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        return $node;
+    }
+}
