45e8b43b4c94f59b55c88b782a631f75fedccd8f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-11-06 11:00:12 +0700

    one side NullsafeMethodCall()

--- rules/php80/src/Rector/If_/NullsafeOperatorRector.php
+++ rules/php80/src/Rector/If_/NullsafeOperatorRector.php
@@ -5,10 +5,15 @@ declare(strict_types=1);
 namespace Rector\Php80\Rector\If_;
 
 use PhpParser\Node;
+use PhpParser\Node\Expr\Assign;
+use PhpParser\Node\Expr\NullsafeMethodCall;
 use PhpParser\Node\Stmt\If_;
+use PhpParser\Node\Stmt\Return_;
+use Rector\Core\PhpParser\Node\Manipulator\IfManipulator;
 use Rector\Core\Rector\AbstractRector;
 use Rector\Core\RectorDefinition\CodeSample;
 use Rector\Core\RectorDefinition\RectorDefinition;
+use Rector\NodeTypeResolver\Node\AttributeKey;
 
 /**
  * @see https://wiki.php.net/rfc/nullsafe_operator
@@ -16,30 +21,43 @@ use Rector\Core\RectorDefinition\RectorDefinition;
  */
 final class NullsafeOperatorRector extends AbstractRector
 {
+    /**
+     * @var IfManipulator
+     */
+    private $ifManipulator;
+
+    public function __construct(IfManipulator $ifManipulator)
+    {
+        $this->ifManipulator = $ifManipulator;
+    }
+
     public function getDefinition(): RectorDefinition
     {
         return new RectorDefinition('Change if null check with nullsafe operator ?-> with full short circuiting', [
             new CodeSample(
                 <<<'CODE_SAMPLE'
-$country =  null;
-
-if ($session !== null) {
-    $user = $session->user;
-
-    if ($user !== null) {
-        $address = $user->getAddress();
-
-        if ($address !== null) {
-            $country = $address->country;
+class SomeClass
+{
+    public function f($o)
+    {
+        $o2 = $o->mayFail1();
+        if ($o2 === null) {
+            return null;
         }
+
+        return $o2->mayFail2();
     }
 }
 CODE_SAMPLE
 ,
                 <<<'CODE_SAMPLE'
-$country =  null;
-
-$country = $session?->user?->getAddress()?->country;
+class SomeClass
+{
+    public function f($o)
+    {
+        return $o?->mayFail1()?->mayFail2();
+    }
+}
 CODE_SAMPLE
             ),
         ]);
@@ -58,6 +76,23 @@ CODE_SAMPLE
      */
     public function refactor(Node $node): ?Node
     {
-        return $node;
+        $comparedNode = $this->ifManipulator->matchIfValueReturnValue($node);
+        if ($comparedNode !== null) {
+            $prevNode = $node->getAttribute(AttributeKey::PREVIOUS_NODE);
+            while ($prevNode) {
+                $assign = $this->betterNodeFinder->findFirst($prevNode, function (Node $n) use ($node): bool {
+                    return $n instanceof Assign && $n->var->name === $node->cond->left->name;
+                });
+
+                if ($assign) {
+                    $this->removeNode($prevNode);
+                    return new NullsafeMethodCall($assign->expr->var, $assign->expr->name);
+                }
+
+                $prevNode = $prevNode->getAttribute(AttributeKey::PREVIOUS_NODE);
+            }
+        }
+
+        return null;
     }
 }
