35ea7e1b02dadecfb1c4e748bcdb1afdd556e7cb
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-11-12 10:59:01 +0700

    fixture fix

--- tests/Issues/Issue4581/DoNotChangeAnnotationColumn/config/some_config.php
+++ tests/Issues/Issue4581/DoNotChangeAnnotationColumn/config/some_config.php
@@ -2,12 +2,11 @@
 
 declare(strict_types=1);
 
-use Rector\Core\Configuration\Option;
-use Rector\Set\ValueObject\SetList;
+use Rector\Php74\Rector\Property\TypedPropertyRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $parameters = $containerConfigurator->parameters();
-
-    $parameters->set(Option::SETS, [SetList::DEAD_CODE]);
+    $services = $containerConfigurator->services();
+    // put anything here
+    $services->set(TypedPropertyRector::class);
 };
