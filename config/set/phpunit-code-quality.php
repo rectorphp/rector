eacfecda840e0b29206037a1510e6a0c83f13081
Tomas Votruba <tomas.vot@gmail.com>
2020-11-15 14:52:42 +0000

    [PHPUnit] Add AssertSameTrueFalseToAssertTrueFalseRector

--- config/set/phpunit-code-quality.php
+++ config/set/phpunit-code-quality.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Rector\CodingStyle\Rector\ClassMethod\ReturnArrayClassMethodToYieldRector;
 use Rector\PHPUnit\Rector\Class_\AddSeeTestAnnotationRector;
 use Rector\PHPUnit\Rector\Class_\ConstructClassMethodToSetUpTestCaseRector;
+use Rector\PHPUnit\Rector\MethodCall\AssertSameTrueFalseToAssertTrueFalseRector;
 use Rector\PHPUnit\Rector\MethodCall\RemoveExpectAnyFromMockRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
@@ -14,4 +15,5 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->set(AddSeeTestAnnotationRector::class);
     $services->set(ReturnArrayClassMethodToYieldRector::class);
     $services->set(ConstructClassMethodToSetUpTestCaseRector::class);
+    $services->set(AssertSameTrueFalseToAssertTrueFalseRector::class);
 };
