fdb0ef54eb7938c9424436b60251c9dda9d06fc1
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-11-17 20:26:43 +0700

    final touch

--- rules/strict-code-quality/src/Rector/ClassMethod/ParamTypeToAssertTypeRector.php
+++ rules/strict-code-quality/src/Rector/ClassMethod/ParamTypeToAssertTypeRector.php
@@ -90,71 +90,99 @@ CODE_SAMPLE
             return null;
         }
 
-        $processedTypes = [];
+        $toBeProcessedTypes = [];
         foreach ($paramTypes as $key => $paramType) {
             if (! $paramType instanceof FullyQualifiedObjectType && ! $paramType instanceof UnionType && ! $paramType instanceof ShortenedObjectType) {
                 continue;
             }
 
-            $types = ! $paramType instanceof UnionType
-                ? [$paramType]
-                : $paramType->getTypes();
-
-            foreach ($types as $type) {
-                if (! $type instanceof FullyQualifiedObjectType && ! $type instanceof ShortenedObjectType) {
-                    continue 2;
-                }
+            $types = $this->getTypes($paramType);
+            if ($this->isNotClassTypes($types)) {
+                continue;
             }
 
-            foreach ($params as $param) {
-                if (! $param->type instanceof FullyQualified) {
-                    continue;
-                }
-
-                $paramVarName = $this->getName($param->var);
-                if ($key !== '$' . $paramVarName) {
-                    continue;
-                }
-
-                foreach ($types as $type) {
-                    $className = $type instanceof ShortenedObjectType
-                        ? $type->getFullyQualifiedName()
-                        : $type->getClassName();
-
-                    if (! is_a($className, $param->type->toString(), true) || $className === $param->type->toString()) {
-                        continue 2;
-                    }
-
-                    $processedTypes[$paramVarName][] = '\\' . $className;
-                }
-            }
+            $toBeProcessedTypes = $this->getToBeProcessedTypes($params, $key, $types, $toBeProcessedTypes);
         }
 
-        return $this->processAddTypeAssert($node, $processedTypes);
+        return $this->processAddTypeAssert($node, $toBeProcessedTypes);
     }
 
     /**
-     * @param array<string, array<int, string>> $anyOfTypes
+     * @param Param[] $params
+     * @param Type[] $types
+     * @param array<string, array<int, string>> $toBeProcessedTypes
+     * @return array<string, array<int, string>>
      */
-    private function processAddTypeAssert(ClassMethod $classMethod, array $anyOfTypes): ClassMethod
+    private function getToBeProcessedTypes(array $params, string $key, array $types, array $toBeProcessedTypes): array
+    {
+        foreach ($params as $param) {
+            $paramVarName = $this->getName($param->var);
+            if (! $param->type instanceof FullyQualified || $key !== '$' . $paramVarName) {
+                continue;
+            }
+
+            foreach ($types as $type) {
+                $className = $type instanceof ShortenedObjectType
+                    ? $type->getFullyQualifiedName()
+                    : $type->getClassName();
+
+                if (! is_a($className, $param->type->toString(), true) || $className === $param->type->toString()) {
+                    continue 2;
+                }
+
+                $toBeProcessedTypes[$paramVarName][] = '\\' . $className;
+            }
+        }
+
+        return $toBeProcessedTypes;
+    }
+
+    /**
+     * @param Type[] $types
+     */
+    private function isNotClassTypes(array $types): bool
+    {
+        foreach ($types as $type) {
+            if (! $type instanceof FullyQualifiedObjectType && ! $type instanceof ShortenedObjectType) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @return Type[]
+     */
+    private function getTypes(Type $type): array
+    {
+        return ! $type instanceof UnionType
+            ? [$type]
+            : $type->getTypes();
+    }
+
+    /**
+     * @param array<string, array<int, string>> $toBeProcessedTypes
+     */
+    private function processAddTypeAssert(ClassMethod $classMethod, array $toBeProcessedTypes): ClassMethod
     {
         $assertStatements = [];
-        foreach ($anyOfTypes as $keyAnyOfType => $anyOfType) {
+        foreach ($toBeProcessedTypes as $key => $toBeProcessedType) {
             $types = [];
-            foreach ($anyOfType as $keyType => $type) {
-                $anyOfType[$keyType] = sprintf('%s::class', $type);
-                $types[] = new ArrayItem(new ConstFetch(new Name($anyOfType[$keyType])));
+            foreach ($toBeProcessedType as $keyType => $type) {
+                $toBeProcessedType[$keyType] = sprintf('%s::class', $type);
+                $types[] = new ArrayItem(new ConstFetch(new Name($toBeProcessedType[$keyType])));
             }
 
             if (count($types) > 1) {
                 $assertStatements[] = new Expression(new StaticCall(new Name('\Webmozart\Assert\Assert'), 'isAnyOf', [
-                    new Arg(new Variable($keyAnyOfType)),
+                    new Arg(new Variable($key)),
                     new Arg(new Array_($types)),
                 ]));
             } else {
                 $assertStatements[] = new Expression(new StaticCall(new Name('\Webmozart\Assert\Assert'), 'isAOf', [
-                    new Arg(new Variable($keyAnyOfType)),
-                    new Arg(new ConstFetch(new Name($anyOfType[0]))),
+                    new Arg(new Variable($key)),
+                    new Arg(new ConstFetch(new Name($toBeProcessedType[0]))),
                 ]));
             }
         }
