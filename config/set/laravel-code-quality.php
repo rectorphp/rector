791d2ab86c53df902f344ad6adcf4c94d77d59ae
Tomas Votruba <tomas.vot@gmail.com>
2020-11-18 01:00:26 +0000

    [Laravel] Add MakeTaggedPassedToParameterIterableTypeRector

--- config/set/laravel-code-quality.php
+++ config/set/laravel-code-quality.php
@@ -2,8 +2,11 @@
 
 declare(strict_types=1);
 
+use Rector\Laravel\Rector\Assign\CallOnAppArrayAccessToStandaloneAssignRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
+
+    $services->set(CallOnAppArrayAccessToStandaloneAssignRector::class);
 };
