3ef4cefa64d1e0b792a0844a30778a6ff029822e
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-11-18 23:09:22 +0700

    [BetterStandardPrinter] Fixes #3388 Skip Localization Annotation route (#4646)

--- src/PhpParser/Printer/ContentPatcher.php
+++ src/PhpParser/Printer/ContentPatcher.php
@@ -69,6 +69,18 @@ final class ContentPatcher
     public const INVALID_ANNOTATION_ROUTE_OPTION_REGEX = '#\*\s+@.*={([^"]*=[^"]*,?){1,}[^,]}.*\)#';
 
     /**
+     * @see https://regex101.com/r/Hm2idk/1
+     * @var string
+     */
+    public const VALID_ANNOTATION_ROUTE_LOCALIZATION_REGEX = '#^\s+\/\*\*\s+\s+\*\s+@.*\({(\s+\*\s{0,}".*":\s{0,}".*",?){1,}\s+\*\s{0,}[^,]}.*\)\s+\*\/#msU';
+
+    /**
+     * @see https://regex101.com/r/qVOGbC/2
+     * @var string
+     */
+    public const INVALID_ANNOTATION_ROUTE_LOCALIZATION_REGEX = '#^\s+\/\*\*\s+\s+\*\s+@.*(\s+\*\s{0,}[^"]*=\s{0,}[^"]*,?){1,}.*\)\s+\*\s+\*\/#msU';
+
+    /**
      * @see https://regex101.com/r/4mBd0y/2
      * @var string
      */
@@ -103,6 +115,12 @@ final class ContentPatcher
      * @var string
      */
     private const ROUTE_INVALID_REGEX = '#"\s?=\s?#';
+
+    /**
+     * @var string
+     * @see https://regex101.com/r/5DdLjE/1
+     */
+    private const ROUTE_LOCALIZATION_REPLACE_REGEX = '#[:=]#';
 
     /**
      * @var string[]
@@ -140,11 +158,13 @@ final class ContentPatcher
     }
 
     /**
+     * @see https://github.com/rectorphp/rector/issues/3388
      * @see https://github.com/rectorphp/rector/issues/3673
      * @see https://github.com/rectorphp/rector/issues/4274
      * @see https://github.com/rectorphp/rector/issues/4573
      * @see https://github.com/rectorphp/rector/issues/4581
      * @see https://github.com/rectorphp/rector/issues/4476
+     * @see https://github.com/rectorphp/rector/issues/4620
      */
     public function rollbackValidAnnotation(
         string $originalContent,
@@ -188,6 +208,11 @@ final class ContentPatcher
         if ($validAnnotationRegex !== self::VALID_ANNOTATION_ROUTE_REGEX) {
             $validAnnotation = Strings::replace($validAnnotation, self::STAR_QUOTE_REGEX, '');
             $invalidAnnotation = Strings::replace($invalidAnnotation, self::STAR_QUOTE_REGEX, '');
+
+            if ($validAnnotationRegex === self::VALID_ANNOTATION_ROUTE_LOCALIZATION_REGEX) {
+                $validAnnotation = Strings::replace($validAnnotation, self::ROUTE_LOCALIZATION_REPLACE_REGEX, '');
+                $invalidAnnotation = Strings::replace($invalidAnnotation, self::ROUTE_LOCALIZATION_REPLACE_REGEX, '');
+            }
 
             return $validAnnotation !== $invalidAnnotation;
         }
