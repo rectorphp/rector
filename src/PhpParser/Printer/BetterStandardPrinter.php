d2195d8c88168cd676f2b2134069ebb4f3ce234b
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-11-18 22:56:47 +0700

    [BetterStandardPrinter] Fixes #3388 Skip Localization Annotation route

--- src/PhpParser/Printer/BetterStandardPrinter.php
+++ src/PhpParser/Printer/BetterStandardPrinter.php
@@ -164,6 +164,12 @@ final class BetterStandardPrinter extends Standard
             ContentPatcher::VALID_ANNOTATION_ROUTE_OPTION_REGEX,
             ContentPatcher::INVALID_ANNOTATION_ROUTE_OPTION_REGEX
         );
+        $content = $this->contentPatcher->rollbackValidAnnotation(
+            $contentOriginal,
+            $content,
+            ContentPatcher::VALID_ANNOTATION_ROUTE_LOCALIZATION_REGEX,
+            ContentPatcher::INVALID_ANNOTATION_ROUTE_LOCALIZATION_REGEX
+        );
 
         // add new line in case of added stmts
         if (count($stmts) !== count($origStmts) && ! (bool) Strings::match($content, self::NEWLINE_END_REGEX)) {
