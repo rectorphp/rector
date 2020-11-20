56be65c90f56d9dafe8e9ef52396d46b1e91d0cc
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-11-20 18:43:00 +0700

    skip expr is array

--- rules/solid/src/Rector/Assign/MoveVariableDeclarationNearReferenceRector.php
+++ rules/solid/src/Rector/Assign/MoveVariableDeclarationNearReferenceRector.php
@@ -6,6 +6,7 @@ namespace Rector\SOLID\Rector\Assign;
 
 use PhpParser\Node;
 use PhpParser\Node\Expr;
+use PhpParser\Node\Expr\Array_;
 use PhpParser\Node\Expr\ArrayDimFetch;
 use PhpParser\Node\Expr\Assign;
 use PhpParser\Node\Expr\Closure;
@@ -208,7 +209,7 @@ CODE_SAMPLE
 
     private function isOutOfScopeExpr(Expr $expr): bool
     {
-        return $expr instanceof Methodcall || $expr instanceof StaticCall || $expr instanceof Closure;
+        return $expr instanceof Methodcall || $expr instanceof StaticCall || $expr instanceof Closure || $expr instanceof Array_;
     }
 
     private function isFoundInNext(Node $node, Expr $variable): bool
