5a4985ba41c918ed8ad56778e4459dfd40621420
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-11-24 16:28:07 +0700

    check numeric of string with .

--- src/Configuration/MinimalVersionChecker.php
+++ src/Configuration/MinimalVersionChecker.php
@@ -31,9 +31,16 @@ final class MinimalVersionChecker
     public function check(): void
     {
         $minimumPhpVersion = $this->composerJsonParser->getPhpVersion();
+        $installedPhpVersion = explode('.', $this->installedPhpVersion);
+
+        if (count($installedPhpVersion) > 1) {
+            $installedPhpVersion = $version[0] * 10000 + (int) $version[1] * 100 + (int) $version[2];
+        } else {
+            $installedPhpVersion = $this->installedPhpVersion;
+        }
 
         // Check minimum required PHP version
-        if ($this->installedPhpVersion <= preg_replace('#(\d).(\d)#', '${1}0${2}00', $minimumPhpVersion)) {
+        if ($installedPhpVersion <= preg_replace('#(\d).(\d)#', '${1}0${2}00', $minimumPhpVersion)) {
             throw new PhpVersionException(sprintf(
                 'PHP version %s or higher is required, but you currently have %s installed.',
                 $minimumPhpVersion,
