b65cd00ed5f0f85a031a3fd135315c25c7f63e96
Tomas Votruba <tomas.vot@gmail.com>
2020-11-27 16:05:19 +0000

    decouple DowngradeSet list with PHP downgrades

--- packages/set/src/ValueObject/SetList.php
+++ packages/set/src/ValueObject/SetList.php
@@ -181,30 +181,10 @@ final class SetList
      */
     public const DOCTRINE_SERVICES = __DIR__ . '/../../../../config/set/doctrine-services.php';
 
-    /**
-     * @var string
-     */
-    public const DOWNGRADE_PHP71 = __DIR__ . '/../../../../config/set/downgrade-php71.php';
 
-    /**
-     * @var string
-     */
-    public const DOWNGRADE_PHP72 = __DIR__ . '/../../../../config/set/downgrade-php72.php';
 
-    /**
-     * @var string
-     */
-    public const DOWNGRADE_PHP73 = __DIR__ . '/../../../../config/set/downgrade-php73.php';
 
-    /**
-     * @var string
-     */
-    public const DOWNGRADE_PHP74 = __DIR__ . '/../../../../config/set/downgrade-php74.php';
 
-    /**
-     * @var string
-     */
-    public const DOWNGRADE_PHP80 = __DIR__ . '/../../../../config/set/downgrade-php80.php';
 
     /**
      * @var string
