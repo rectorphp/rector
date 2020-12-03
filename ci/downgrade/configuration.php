449db7897ba55966244f38feb974d7664fc40d35
Leonardo Losoviz <leo@getpop.org>
2020-12-03 10:23:20 +0800

    Fixed exclude paths

--- ci/downgrade/configuration.php
+++ ci/downgrade/configuration.php
@@ -11,13 +11,13 @@ final class DowngradeRectorConfig
      * Exclude paths when downgrading a dependency
      */
     public const DEPENDENCY_EXCLUDE_PATHS = [
-        '/tests/',
+        '*/tests/*',
         // Individual classes that can be excluded because
         // they are not used by Rector, and they use classes
         // loaded with "require-dev" so it'd throw an error
-        __DIR__ . '/vendor/symfony/cache/DoctrineProvider.php',
-        __DIR__ . '/vendor/symfony/http-kernel/HttpKernelBrowser.php',
-        __DIR__ . '/vendor/symfony/string/Slugger/AsciiSlugger.php',
+        __DIR__ . '/../../vendor/symfony/cache/DoctrineProvider.php',
+        __DIR__ . '/../../vendor/symfony/http-kernel/HttpKernelBrowser.php',
+        __DIR__ . '/../../vendor/symfony/string/Slugger/AsciiSlugger.php',
     ];
     /**
      * Exclude paths when downgrading the Rector source code
@@ -27,13 +27,13 @@ final class DowngradeRectorConfig
         // '/*Source/',
         // '/Fixture/',
         // '/Expected/',
-        __DIR__ . '/packages/doctrine-annotation-generated/src/*',
-        __DIR__ . '/packages/rector-generator/templates/*',
-        __DIR__ . '/vendor/*',
-        __DIR__ . '/ci/*',
-        __DIR__ . '/compiler/*',
-        __DIR__ . '/stubs/*',
-        '/tests/',
+        __DIR__ . '/../../packages/doctrine-annotation-generated/src/*',
+        __DIR__ . '/../../packages/rector-generator/templates/*',
+        __DIR__ . '/../../vendor/*',
+        __DIR__ . '/../../ci/*',
+        __DIR__ . '/../../compiler/*',
+        __DIR__ . '/../../stubs/*',
+        '*/tests/*',
         // '*.php.inc',
     ];
 }
