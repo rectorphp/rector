24dda87f60daa6991a1c271f7f0b7039af75acff
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-12-04 21:07:54 +0700

    [Downgrade PHP 7.1] Fixes #4196 Add DowngradePipeToMultiCatchExceptionRector (#4776)
    Co-authored-by: Tomas Votruba <tomas.vot@gmail.com>

---
+++ rules/downgrade-php71/src/Rector/TryCatch/DowngradePipeToMultiCatchExceptionRector.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\DowngradePhp71\Rector\TryCatch;
+
+use PhpParser\Node;
+use PhpParser\Node\Stmt\Catch_;
+use PhpParser\Node\Stmt\TryCatch;
+use Rector\Core\ValueObject\PhpVersionFeature;
+use Rector\Core\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \Rector\DowngradePhp71\Tests\Rector\TryCatch\DowngradePipeToMultiCatchExceptionRector\DowngradePipeToMultiCatchExceptionRectorTest
+ */
+final class DowngradePipeToMultiCatchExceptionRector extends AbstractRector
+{
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'Downgrade single one | separated to multi catch exception',
+            [
+                new CodeSample(
+                    <<<'CODE_SAMPLE'
+try {
+    // Some code...
+ } catch (ExceptionType1 | ExceptionType2 $exception) {
+    $sameCode;
+ }
+CODE_SAMPLE
+                    ,
+                    <<<'CODE_SAMPLE'
+try {
+    // Some code...
+} catch (ExceptionType1 $exception) {
+    $sameCode;
+} catch (ExceptionType2 $exception) {
+    $sameCode;
+}
+CODE_SAMPLE
+                    ,
+                ),
+            ]
+        );
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [TryCatch::class];
+    }
+
+    /**
+     * @param TryCatch $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if (! $this->isAtLeastPhpVersion(PhpVersionFeature::MULTI_EXCEPTION_CATCH)) {
+            return null;
+        }
+
+        $originalCatches = $node->catches;
+        foreach ($node->catches as $key => $catch) {
+            if (count($catch->types) === 1) {
+                continue;
+            }
+
+            $types = $catch->types;
+            $node->catches[$key]->types = [$catch->types[0]];
+            foreach ($types as $keyCatchType => $catchType) {
+                if ($keyCatchType === 0) {
+                    continue;
+                }
+
+                $this->addNodeAfterNode(new Catch_([$catchType], $catch->var, $catch->stmts), $node->catches[$key]);
+            }
+        }
+
+        if ($this->areNodesEqual($originalCatches, $node->catches)) {
+            return null;
+        }
+
+        return $node;
+    }
+}
