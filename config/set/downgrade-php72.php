b1d01b7e26703ecd6adfea58a68ded0ed5f45e0a
Leonardo Losoviz <leo@getpop.org>
2020-12-04 11:47:17 +0800

    Bug fixed => uncommented using rector

--- config/set/downgrade-php72.php
+++ config/set/downgrade-php72.php
@@ -22,9 +22,8 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     // skip classes used in PHP DocBlocks, like in /** @var \Some\Class */ [default: true]
     $parameters->set(Option::IMPORT_DOC_BLOCKS, false);
-    // This rule does not work, so commented out
-    // $services->set(ChangePhpVersionInPlatformCheckRector::class)
-    //     ->call('configure', [[
-    //         ChangePhpVersionInPlatformCheckRector::TARGET_PHP_VERSION => 70200,
-    //     ]]);
+    $services->set(ChangePhpVersionInPlatformCheckRector::class)
+        ->call('configure', [[
+            ChangePhpVersionInPlatformCheckRector::TARGET_PHP_VERSION => 70200,
+        ]]);
 };
