8582801c83db80cdcf969738ff16019f99dd1c1e
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-12-04 21:49:44 +0700

    class based @requires PHP < 8.0

--- rules/php72/tests/Rector/FuncCall/StringsAssertNakedRector/StringsAssertNakedRectorTest.php
+++ rules/php72/tests/Rector/FuncCall/StringsAssertNakedRector/StringsAssertNakedRectorTest.php
@@ -9,11 +9,13 @@ use Rector\Php72\Rector\FuncCall\StringsAssertNakedRector;
 use Rector\Testing\PHPUnit\AbstractRectorTestCase;
 use Symplify\SmartFileSystem\SmartFileInfo;
 
+/**
+ * @requires PHP < 8.0
+ */
 final class StringsAssertNakedRectorTest extends AbstractRectorTestCase
 {
     /**
      * @dataProvider provideData()
-     * @requires PHP < 8.0
      */
     public function test(SmartFileInfo $fileInfo): void
     {
