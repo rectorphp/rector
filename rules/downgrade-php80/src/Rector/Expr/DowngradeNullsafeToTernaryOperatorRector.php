89ba505b7cc7e64e11f7d80e27c7af1bbadde3c9
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-12-06 13:27:02 +0700

    [Downgrade PHP 8.0] [WIP] Fixes #4154 Add DowngradeNullsafeToTernaryOperatorRector

---
+++ rules/downgrade-php80/src/Rector/Expr/DowngradeNullsafeToTernaryOperatorRector.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\DowngradePhp80\Rector\Expr;
+
+use PhpParser\Node\Expr\NullsafeMethodCall;
+use PhpParser\Node\Expr\NullsafePropertyFetch;
+use Rector\Core\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \Rector\DowngradePhp80\Tests\Rector\Expr\DowngradeNullsafeToTernaryOperatorRector\DowngradeNullsafeToTernaryOperatorRectorTest
+ */
+final class DowngradeNullsafeToTernaryOperatorRector extends AbstractRector
+{
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition('Change nullsafe operator to ternary operator rector', [
+            new CodeSample(
+                <<<'CODE_SAMPLE'
+$dateAsString = $booking->getStartDate()?->asDateTimeString();
+$dateAsString = $booking->startDate?->dateTimeString;
+CODE_SAMPLE
+,
+                <<<'CODE_SAMPLE'
+$dateAsString = $booking->getStartDate() ? $booking->getStartDate()->asDateTimeString() : null;
+$dateAsString = $booking->startDate ? $booking->startDate->dateTimeString : null;
+CODE_SAMPLE
+            ),
+        ]);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [NullsafeMethodCall::class, NullsafePropertyFetch::class];
+    }
+
+    /**
+     * @param NullsafeMethodCall|NullsafePropertyFetch $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        return $node;
+    }
+}
