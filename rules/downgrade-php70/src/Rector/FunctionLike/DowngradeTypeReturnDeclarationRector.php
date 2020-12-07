3a40760e1854d40e31a3f975608a5ee2cc8fc52b
Leonardo Losoviz <leo@getpop.org>
2020-12-07 21:22:56 +0800

    Removed unused import

--- rules/downgrade-php70/src/Rector/FunctionLike/DowngradeTypeReturnDeclarationRector.php
+++ rules/downgrade-php70/src/Rector/FunctionLike/DowngradeTypeReturnDeclarationRector.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Rector\DowngradePhp70\Rector\FunctionLike;
 
 use PhpParser\Node\FunctionLike;
-use PhpParser\Node\NullableType;
 use PhpParser\Node\Stmt\ClassMethod;
 use PhpParser\Node\Stmt\Function_;
 use Rector\DowngradePhp70\Rector\FunctionLike\AbstractDowngradeReturnDeclarationRector;
