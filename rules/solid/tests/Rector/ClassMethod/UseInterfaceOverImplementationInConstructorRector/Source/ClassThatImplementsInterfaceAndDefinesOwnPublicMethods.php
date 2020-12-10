d29b817d3ea5e4bca2d8da949d66a2efefe96640
Grzegorz Korba <grzegorz.korba@codito.pl>
2020-12-10 17:44:58 +0100

    Test cases for #4844

---
+++ rules/solid/tests/Rector/ClassMethod/UseInterfaceOverImplementationInConstructorRector/Source/ClassThatImplementsInterfaceAndDefinesOwnPublicMethods.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\SOLID\Tests\Rector\ClassMethod\UseInterfaceOverImplementationInConstructorRector\Source;
+
+final class ClassThatImplementsInterfaceAndDefinesOwnPublicMethods implements InterfaceFour
+{
+    public function foo(): string
+    {
+        return 'bar';
+    }
+}
