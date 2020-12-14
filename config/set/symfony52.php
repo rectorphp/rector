6aecbd02610c244ea87a769d97e780e95e55126f
Krystian Marcisz <simivar@gmail.com>
2020-12-14 12:35:15 +0100

    [Symfony 5.2] Add ReflectionExtractorEnableMagicCallExtractorRector  (#4876)
    * [Symfony5] Add ReflectionExtractorEnableMagicCallExtractorRector
    
    * [Symfony5] Add to Symfony 5.2 set
    
    * [Symfony5] CS Fix

--- config/set/symfony52.php
+++ config/set/symfony52.php
@@ -6,6 +6,7 @@ use Rector\Renaming\Rector\ClassConstFetch\RenameClassConstantRector;
 use Rector\Renaming\Rector\MethodCall\RenameMethodRector;
 use Rector\Renaming\ValueObject\MethodCallRename;
 use Rector\Renaming\ValueObject\RenameClassConstant;
+use Rector\Symfony5\Rector\MethodCall\ReflectionExtractorEnableMagicCallExtractorRector;
 use Rector\Symfony5\Rector\New_\PropertyAccessorCreationBooleanToFlagsRector;
 use Rector\Symfony5\Rector\StaticCall\BinaryFileResponseCreateToNewInstanceRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -31,6 +32,9 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     # https://github.com/symfony/symfony/blob/5.x/UPGRADE-5.2.md#propertyaccess
     $services->set(PropertyAccessorCreationBooleanToFlagsRector::class);
+
+    # https://github.com/symfony/symfony/blob/5.x/UPGRADE-5.2.md#propertyinfo
+    $services->set(ReflectionExtractorEnableMagicCallExtractorRector::class);
 
     # https://github.com/symfony/symfony/blob/5.x/UPGRADE-5.2.md#security
     $services->set(RenameClassConstantRector::class)
