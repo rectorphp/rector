bf4cd089479960b7a4f2f249e239a23e507532d3
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-12-16 01:51:03 +0700

    [Utils] Run validate-fixture-namespace and validate-fixture-classname  for /tests/ directory (#4901)
    Co-authored-by: rector-bot <tomas@getrector.org>

--- utils/project-validator/src/Command/ValidateFixtureNamespaceCommand.php
+++ utils/project-validator/src/Command/ValidateFixtureNamespaceCommand.php
@@ -163,9 +163,7 @@ final class ValidateFixtureNamespaceCommand extends Command
             ->notName('#_\.php\.inc$#')
             ->notPath('#/ParamTypeDeclarationRector/#')
             ->notPath('#/ReturnTypeDeclarationRector/#')
-            ->in(__DIR__ . '/../../../../tests')
-            ->in(__DIR__ . '/../../../../packages/*/tests')
-            ->in(__DIR__ . '/../../../../rules/*/tests');
+            ->in(__DIR__ . '/../../../../tests');
 
         return $this->finderSanitizer->sanitize($finder);
     }
