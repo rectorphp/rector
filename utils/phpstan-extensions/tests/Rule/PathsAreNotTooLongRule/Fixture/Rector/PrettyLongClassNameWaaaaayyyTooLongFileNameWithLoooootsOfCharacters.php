2bdc0fde512ce6f11a93319e4aa5a95553d65675
Markus Staab <m.staab@complex-it.de>
2020-12-15 14:22:39 +0100

    loong testfile

--- utils/phpstan-extensions/tests/Rule/PathsAreNotTooLongRule/Fixture/Rector/PrettyLongClassNameWaaaaayyyTooLongFileNameWithLoooootsOfCharacters.php
+++ utils/phpstan-extensions/tests/Rule/PathsAreNotTooLongRule/Fixture/Rector/PrettyLongClassNameWaaaaayyyTooLongFileNameWithLoooootsOfCharacters.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Rector\PHPStanExtensions\Tests\Rule\KeepRectorNamespaceForRectorRule\Source\Rector;
 
-final class WrongClass
+final class PrettyLongClassNameWaaaaayyyTooLongFileNameWithLoooootsOfCharacters
 {
 
 }
