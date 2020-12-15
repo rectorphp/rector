579a11ea1ddfa9b7a3503eaab13db2cfd90c7f69
Markus Staab <m.staab@complex-it.de>
2020-12-15 14:19:00 +0100

    copy tests

---
+++ utils/phpstan-extensions/tests/Rule/PathsAreNotTooLongRule/PathsAreNotTooLongRuleTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\PHPStanExtensions\Tests\Rule\KeepRectorNamespaceForRectorRule;
+
+use Iterator;
+use PHPStan\Rules\Rule;
+use Rector\PHPStanExtensions\Rule\KeepRectorNamespaceForRectorRule;
+use Symplify\PHPStanExtensions\Testing\AbstractServiceAwareRuleTestCase;
+
+final class PathsAreNotTooLongRuleTest extends AbstractServiceAwareRuleTestCase
+{
+    /**
+     * @dataProvider provideData()
+     * @param array<string|string[]|int[]> $expectedErrorsWithLines
+     */
+    public function testRule(string $filePath, array $expectedErrorsWithLines): void
+    {
+        $this->analyse([$filePath], $expectedErrorsWithLines);
+    }
+
+    public function provideData(): Iterator
+    {
+        yield [__DIR__ . '/Fixture/Rector/ClassInCorrectNamespaceRector.php', []];
+
+        $errorMessage = sprintf(KeepRectorNamespaceForRectorRule::ERROR_MESSAGE, 'WrongClass');
+        yield [__DIR__ . '/Fixture/Rector/WrongClass.php', [[$errorMessage, 7]]];
+    }
+
+    protected function getRule(): Rule
+    {
+        return $this->getRuleFromConfig(
+            PathsAreNotTooLongRule::class,
+            __DIR__ . '/config/configured_rule.neon'
+        );
+    }
+}
