144c0f4974146cd1efa6dca83e8f9ae34ec805c0
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-12-17 00:05:25 +0700

    [EarlyReturn] [WIP] Fixes #4869 Add ReturnBinaryAndToEarlyReturnRector

---
+++ rules/early-return/src/Rector/Return_/ReturnBinaryAndToEarlyReturnRector.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\EarlyReturn\Rector\Return_;
+
+use PhpParser\Node;
+use PhpParser\Node\Stmt\Return_;
+use Rector\Core\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \Rector\EarlyReturn\Tests\Rector\Return_\ReturnBinaryAndToEarlyReturnRector\ReturnBinaryAndToEarlyReturnRector
+ */
+final class ReturnBinaryAndToEarlyReturnRector extends AbstractRector
+{
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition('Changes Single return of && && to early returns', [
+            new CodeSample(
+                <<<'CODE_SAMPLE'
+class SomeClass
+{
+    public function accept($something, $somethingelse)
+    {
+        return $something && $somethingelse;
+    }
+}
+CODE_SAMPLE
+
+                ,
+                <<<'CODE_SAMPLE'
+class SomeClass
+{
+    public function accept($something, $somethingelse)
+    {
+        if (! $something) {
+            return false;
+        }
+
+        return (bool) $somethingelse;
+    }
+}
+CODE_SAMPLE
+            ),
+        ]);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [Return_::class];
+    }
+
+    /**
+     * @param Return_ $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        return $node;
+    }
+}
