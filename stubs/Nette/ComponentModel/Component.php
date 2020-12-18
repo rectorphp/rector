4efe56a3549cc2746ac7efde32b7fa9d37170f2a
Leonardo Losoviz <leo@getpop.org>
2020-12-18 18:10:05 +0800

    Don't load if original class exists

--- stubs/Nette/ComponentModel/Component.php
+++ stubs/Nette/ComponentModel/Component.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Nette\ComponentModel;
 
+if (class_exists('Nette\ComponentModel\Component')) {
+    return;
+}
+
 class Component extends Container implements \ArrayAccess
 {
     public function offsetExists($offset)
