fea3a13f392e3bffc4b3ce75b6cc03c8e47d0a96
Markus Staab <m.staab@complex-it.de>
2020-12-18 16:17:53 +0100

    test

--- utils/phpstan-extensions/tests/Rule/PathsAreNotTooLongRule/PathsAreNotTooLongRuleTest.php
+++ utils/phpstan-extensions/tests/Rule/PathsAreNotTooLongRule/PathsAreNotTooLongRuleTest.php
@@ -6,6 +6,7 @@ namespace Rector\PHPStanExtensions\Tests\Rule\PathsAreNotTooLongRule;
 
 use Iterator;
 use PHPStan\Rules\Rule;
+use Rector\PHPStanExtensions\Rule\KeepRectorNamespaceForRectorRule;
 use Rector\PHPStanExtensions\Rule\PathsAreNotTooLongRule;
 use Symplify\PHPStanExtensions\Testing\AbstractServiceAwareRuleTestCase;
 use PhpParser\Node\Stmt\ClassLike;
@@ -14,14 +15,21 @@ use Symplify\SmartFileSystem\SmartFileInfo;
 final class PathsAreNotTooLongRuleTest extends AbstractServiceAwareRuleTestCase
 {
     /**
-     * @param array<string|string[]> $expectedError
+     * @dataProvider provideData()
+     * @param array<string|string[]|int[]> $expectedErrorsWithLines
      */
-    public function testRule(string $filePath, array $expectedError): void
+    public function testRule(string $filePath, array $expectedErrorsWithLines): void
     {
-        // $classLike = new ClassLike();
-        // $classLike->setAttribute(SmartFileInfo::class);
+        $this->analyse([$filePath], $expectedErrorsWithLines);
+    }
 
-        // $this->assertSame("a", "a");
+    public function provideData(): Iterator
+    {
+        yield [__DIR__ . '/Fixture/Rector/ShortClassName.php.php', [[]]];
+
+        $longFile = __DIR__ . '/Fixture/Rector/PrettyLongClassNameWaaaaayyyTooLongFileNameWithLoooootsOfCharacters.php.php';
+        $errorMessage = sprintf(PathsAreNotTooLongRule::ERROR_MESSAGE, $longFile, '%s', '%s');
+        yield [$longFile, [$errorMessage]];
     }
 
     protected function getRule(): Rule
