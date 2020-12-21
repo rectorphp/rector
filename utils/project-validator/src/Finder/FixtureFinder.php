15fce75d82ee3ca63e30177a559104c1a57785dd
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-12-21 23:57:39 +0700

    apply to phpunit

--- utils/project-validator/src/Finder/FixtureFinder.php
+++ utils/project-validator/src/Finder/FixtureFinder.php
@@ -56,7 +56,8 @@ final class FixtureFinder
             ->in(__DIR__ . '/../../../../rules/downgrade-php73')
             ->in(__DIR__ . '/../../../../rules/downgrade-php74')
             ->in(__DIR__ . '/../../../../rules/downgrade-php80')
-            ->in(__DIR__ . '/../../../../rules/phpstan');
+            ->in(__DIR__ . '/../../../../rules/phpstan')
+            ->in(__DIR__ . '/../../../../rules/phpunit');
 
         return $this->finderSanitizer->sanitize($finder);
     }
