8c19599c1ab431b8f8fd78eca250d0a38844068f
TomasVotruba <tomas.vot@gmail.com>
2020-12-22 10:52:22 +0100

    [Downgrade] Drop ChangePhpVersionInPlatformCheckRector as not useful, paltform check false is good enough

--- config/set/downgrade-php71.php
+++ config/set/downgrade-php71.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use Rector\Downgrade\Rector\LNumber\ChangePhpVersionInPlatformCheckRector;
 use Rector\DowngradePhp71\Rector\Array_\SymmetricArrayDestructuringToListRector;
 use Rector\DowngradePhp71\Rector\ClassConst\DowngradeClassConstantVisibilityRector;
 use Rector\DowngradePhp71\Rector\FunctionLike\DowngradeNullableTypeParamDeclarationRector;
@@ -18,10 +17,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->set(DowngradeNullableTypeReturnDeclarationRector::class);
     $services->set(DowngradeVoidTypeReturnDeclarationRector::class);
     $services->set(DowngradeClassConstantVisibilityRector::class);
-    $services->set(ChangePhpVersionInPlatformCheckRector::class)
-        ->call('configure', [[
-            ChangePhpVersionInPlatformCheckRector::TARGET_PHP_VERSION => 70000,
-        ]]);
     $services->set(DowngradePipeToMultiCatchExceptionRector::class);
     $services->set(SymmetricArrayDestructuringToListRector::class);
     $services->set(DowngradeNegativeStringOffsetToStrlenRector::class);
