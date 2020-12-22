73755b7fc38ae338ba4c5c6f91a51dc8b82a94c9
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-12-22 16:47:11 +0700

    apply to php52

--- utils/project-validator/src/Finder/FixtureFinder.php
+++ utils/project-validator/src/Finder/FixtureFinder.php
@@ -89,6 +89,7 @@ final class FixtureFinder
             ->in(__DIR__ . '/../../../../rules/php-deglobalize')
             ->in(__DIR__ . '/../../../../rules/php-office')
             ->in(__DIR__ . '/../../../../rules/php-spec-to-phpunit')
+            ->in(__DIR__ . '/../../../../rules/php52')
             ->in(__DIR__ . '/../../../../rules/phpstan')
             ->in(__DIR__ . '/../../../../rules/phpunit')
             ->in(__DIR__ . '/../../../../rules/phpunit-symfony')
