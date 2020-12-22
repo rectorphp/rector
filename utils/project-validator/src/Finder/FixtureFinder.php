6798f90c8ebddaa51af79716ca5779c46910d3ee
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-12-22 16:23:29 +0700

    apply to naming

--- utils/project-validator/src/Finder/FixtureFinder.php
+++ utils/project-validator/src/Finder/FixtureFinder.php
@@ -75,6 +75,7 @@ final class FixtureFinder
             ->in(__DIR__ . '/../../../../rules/mockery-to-prophecy')
             ->in(__DIR__ . '/../../../../rules/mockista-to-mockery')
             ->in(__DIR__ . '/../../../../rules/mysql-to-mysqli')
+            ->in(__DIR__ . '/../../../../rules/naming')
             ->in(__DIR__ . '/../../../../rules/phpstan')
             ->in(__DIR__ . '/../../../../rules/phpunit')
             ->in(__DIR__ . '/../../../../rules/phpunit-symfony')
