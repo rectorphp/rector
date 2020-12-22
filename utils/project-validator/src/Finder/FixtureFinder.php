4bc5ababa3158cb4a57b62652b2be9edb38245bd
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-12-22 15:37:39 +0700

    apply to symfony5

--- utils/project-validator/src/Finder/FixtureFinder.php
+++ utils/project-validator/src/Finder/FixtureFinder.php
@@ -79,7 +79,8 @@ final class FixtureFinder
             ->in(__DIR__ . '/../../../../rules/symfony-php-config')
             ->in(__DIR__ . '/../../../../rules/symfony2')
             ->in(__DIR__ . '/../../../../rules/symfony3')
-            ->in(__DIR__ . '/../../../../rules/symfony4');
+            ->in(__DIR__ . '/../../../../rules/symfony4')
+            ->in(__DIR__ . '/../../../../rules/symfony5');
 
         return $this->finderSanitizer->sanitize($finder);
     }
