afc841c12ae419c565cc247ed1979869ba7a3af6
TomasVotruba <tomas.vot@gmail.com>
2020-12-23 22:41:32 +0100

    [PHP 8.0] Skip exception in match, must be expr

---
+++ rules/php80/src/NodeResolver/SwitchExprsResolver.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Php80\NodeResolver;
+
+use PhpParser\Node\Expr;
+use PhpParser\Node\Expr\Assign;
+use PhpParser\Node\Stmt\Expression;
+use PhpParser\Node\Stmt\Return_;
+use PhpParser\Node\Stmt\Switch_;
+use Rector\Php80\ValueObject\CondAndExpr;
+
+final class SwitchExprsResolver
+{
+    /**
+     * @return CondAndExpr[]
+     */
+    public function resolve(Switch_ $switch): array
+    {
+        $condAndExpr = [];
+
+        foreach ($switch->cases as $case) {
+            if (! isset($case->stmts[0])) {
+                return [];
+            }
+
+            $expr = $case->stmts[0];
+            if ($expr instanceof Expression) {
+                $expr = $expr->expr;
+            }
+
+            if ($expr instanceof Return_) {
+                $returnedExpr = $expr->expr;
+                if ($returnedExpr === null) {
+                    return [];
+                }
+                $condAndExpr[] = new CondAndExpr($case->cond, $returnedExpr, CondAndExpr::TYPE_RETURN);
+            } elseif ($expr instanceof Assign) {
+                $condAndExpr[] = new CondAndExpr($case->cond, $expr, CondAndExpr::TYPE_ASSIGN);
+            } elseif ($expr instanceof Expr) {
+                $condAndExpr[] = new CondAndExpr($case->cond, $expr, CondAndExpr::TYPE_NORMAL);
+            } else {
+                return [];
+            }
+        }
+
+        return $condAndExpr;
+    }
+}
---
+++ rules/php80/src/ValueObject/CondAndExpr.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Php80\ValueObject;
+
+use PhpParser\Node\Expr;
+
+final class CondAndExpr
+{
+    /**
+     * @var string
+     */
+    public const TYPE_NORMAL = 'normal';
+    /**
+     * @var string
+     */
+    public const TYPE_ASSIGN = 'assign';
+    /**
+     * @var string
+     */
+    public const TYPE_RETURN = 'return';
+
+    /**
+     * @var Expr|null
+     */
+    private $condExpr;
+
+    /**
+     * @var Expr
+     */
+    private $expr;
+    /**
+     * @var string
+     */
+    private $kind;
+
+    public function __construct(?Expr $condExpr, Expr $expr, string $kind)
+    {
+        $this->condExpr = $condExpr;
+        $this->expr = $expr;
+        $this->kind = $kind;
+    }
+
+    public function getExpr(): Expr
+    {
+        return $this->expr;
+    }
+
+    public function getCondExpr(): ?Expr
+    {
+        return $this->condExpr;
+    }
+
+    public function getKind(): string
+    {
+        return $this->kind;
+    }
+}
