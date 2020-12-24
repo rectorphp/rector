3ba00cc2317c6bfdc7a579515ff7f3ff169889fc
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-12-25 00:05:06 +0700

    [Util] Add StaticInstanceOf

---
+++ src/Util/StaticInstanceOf.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Core\Util;
+
+/**
+ * @see \Rector\Core\Tests\Util\StaticInstanceOfTest
+ */
+final class StaticInstanceOf
+{
+    /**
+     * @param class-string[] $array
+     */
+    public static function isOneOf(object $object, array $array): bool
+    {
+        foreach ($array as $classLike) {
+            if (is_a($object, $classLike, true)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ tests/Util/StaticInstanceOfTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Core\Tests\Util;
+
+use DateTime;
+use Iterator;
+use PHPUnit\Framework\TestCase;
+use Rector\Core\Util\StaticInstanceOf;
+use stdClass;
+
+final class StaticInstanceOfTest extends TestCase
+{
+    /**
+     * @dataProvider provideIsOneOf()
+     */
+    public function testIsOneOf(object $object, array $array, $expected): void
+    {
+        $this->assertSame($expected, StaticInstanceOf::isOneOf($object, $array));
+    }
+
+    public function provideIsOneOf(): Iterator
+    {
+        yield [new DateTime('now'), [DateTime::class, stdClass::class], true];
+        yield [new stdClass(), [DateTime::class, Iterator::class], false];
+    }
+}
