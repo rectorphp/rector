374c061e7f7890c504f006908afd593d4be994d0
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-12-24 21:42:56 +0700

    fix

--- rules/dead-code/src/Rector/Assign/RemoveDoubleAssignRector.php
+++ rules/dead-code/src/Rector/Assign/RemoveDoubleAssignRector.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Rector\DeadCode\Rector\Assign;
 
 use PhpParser\Node;
+use PhpParser\Node\Expr;
 use PhpParser\Node\Expr\Assign;
 use PhpParser\Node\Expr\FuncCall;
 use PhpParser\Node\Expr\MethodCall;
