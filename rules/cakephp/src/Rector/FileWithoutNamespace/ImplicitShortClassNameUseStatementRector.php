0c33c2df76dc662fc787e35b2f3382c45d715a6a
TomasVotruba <tomas.vot@gmail.com>
2020-12-25 12:00:36 +0100

    remove duplicated

--- rules/cakephp/src/Rector/FileWithoutNamespace/ImplicitShortClassNameUseStatementRector.php
+++ rules/cakephp/src/Rector/FileWithoutNamespace/ImplicitShortClassNameUseStatementRector.php
@@ -68,9 +68,7 @@ CODE_SAMPLE
      */
     public function refactor(Node $node): ?Node
     {
-        /** @var Name[] $names */
         $names = $this->findNames($node);
-
         if ($names === []) {
             return null;
         }
