9e72348b91b1b3f1c7a3ffe1208a5c7a5face595
TomasVotruba <tomas.vot@gmail.com>
2020-12-25 18:24:25 +0100

    change rector-ci to rector

--- ecs.php
+++ ecs.php
@@ -56,7 +56,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         __DIR__ . '/config',
         __DIR__ . '/ecs.php',
         __DIR__ . '/rector.php',
-        __DIR__ . '/rector-ci.php',
         __DIR__ . '/config/set',
     ]);
 
