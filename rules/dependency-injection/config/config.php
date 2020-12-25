c63ffaf46d3b2b3d25896081cd88409c8aef4a0f
TomasVotruba <tomas.vot@gmail.com>
2020-12-25 17:43:03 +0100

    [SOLID] Drop UseInterfaceOverImplementationInConstructorRector, very subjective

--- rules/dependency-injection/config/config.php
+++ rules/dependency-injection/config/config.php
@@ -12,6 +12,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ->autowire()
         ->autoconfigure();
 
-    $services->load('Rector\SOLID\\', __DIR__ . '/../src')
-        ->exclude([__DIR__ . '/../src/Rector', __DIR__ . '/../src/ValueObject']);
+    $services->load('Rector\DependencyInjection\\', __DIR__ . '/../src')
+        ->exclude([__DIR__ . '/../src/Rector']);
 };
