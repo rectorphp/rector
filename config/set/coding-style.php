96ba8bc96b7887bc01d92918f40708958e4f78d2
TomasVotruba <tomas.vot@gmail.com>
2020-12-25 14:07:25 +0100

    move UseMessageVariableForSprintfInSymfonyStyleRector from solid set to coding-style

--- config/set/coding-style.php
+++ config/set/coding-style.php
@@ -22,6 +22,7 @@ use Rector\CodingStyle\Rector\FuncCall\VersionCompareFuncCallToConstantRector;
 use Rector\CodingStyle\Rector\Function_\CamelCaseFunctionNamingToUnderscoreRector;
 use Rector\CodingStyle\Rector\If_\NullableCompareToNullRector;
 use Rector\CodingStyle\Rector\Include_\FollowRequireByDirRector;
+use Rector\CodingStyle\Rector\MethodCall\UseMessageVariableForSprintfInSymfonyStyleRector;
 use Rector\CodingStyle\Rector\Plus\UseIncrementAssignRector;
 use Rector\CodingStyle\Rector\PostInc\PostIncDecToPreIncDecRector;
 use Rector\CodingStyle\Rector\String_\SplitStringClassConstantToClassConstFetchRector;
@@ -60,6 +61,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->set(MakeInheritedMethodVisibilitySameAsParentRector::class);
     $services->set(CallUserFuncCallToVariadicRector::class);
     $services->set(VersionCompareFuncCallToConstantRector::class);
+    $services->set(UseMessageVariableForSprintfInSymfonyStyleRector::class);
 
     $services->set(FunctionCallToConstantRector::class)
         ->call('configure', [[
