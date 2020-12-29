630c3d324ee6798050b710b1a2994095c7aeb0c7
Tomas Votruba <tomas.vot@gmail.com>
2020-12-29 01:32:48 +0100

    [PhpVersionResolver] Add project composer.json as source + decouple to composer json fatcory (#5031)
    * [PhpVersionResolver] Add project composer.json as source + decouple to composer json fatcory
    
    * [ci-review] Rector Rectify
    
    Co-authored-by: rector-bot <tomas@getrector.org>

---
+++ src/Php/PhpVersionResolver/ProjectComposerJsonPhpVersionResolver.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Core\Php\PhpVersionResolver;
+
+use Composer\Semver\VersionParser;
+use Rector\Core\Util\PhpVersionFactory;
+use Symplify\ComposerJsonManipulator\ComposerJsonFactory;
+
+/**
+ * @see \Rector\Core\Tests\Php\PhpVersionResolver\ProjectComposerJsonPhpVersionResolver\ProjectComposerJsonPhpVersionResolverTest
+ */
+final class ProjectComposerJsonPhpVersionResolver
+{
+    /**
+     * @var ComposerJsonFactory
+     */
+    private $composerJsonFactory;
+
+    /**
+     * @var VersionParser
+     */
+    private $versionParser;
+
+    /**
+     * @var PhpVersionFactory
+     */
+    private $phpVersionFactory;
+
+    public function __construct(
+        ComposerJsonFactory $composerJsonFactory,
+        VersionParser $versionParser,
+        PhpVersionFactory $phpVersionFactory
+    ) {
+        $this->composerJsonFactory = $composerJsonFactory;
+        $this->versionParser = $versionParser;
+        $this->phpVersionFactory = $phpVersionFactory;
+    }
+
+    public function resolve(string $composerJson): ?int
+    {
+        $projectComposerJson = $this->composerJsonFactory->createFromFilePath($composerJson);
+
+        // see https://getcomposer.org/doc/06-config.md#platform
+        $platformPhp = $projectComposerJson->getConfig()['platform']['php'] ?? null;
+        if ($platformPhp !== null) {
+            return $this->phpVersionFactory->createIntVersion($platformPhp);
+        }
+
+        $requirePhpVersion = $projectComposerJson->getRequirePhpVersion();
+        if ($requirePhpVersion === null) {
+            return null;
+        }
+
+        return $this->createIntVersionFromComposerVersion($requirePhpVersion);
+    }
+
+    private function createIntVersionFromComposerVersion(string $projectPhpVersion): int
+    {
+        $constraint = $this->versionParser->parseConstraints($projectPhpVersion);
+
+        $lowerBound = $constraint->getLowerBound();
+        $lowerBoundVersion = $lowerBound->getVersion();
+
+        return $this->phpVersionFactory->createIntVersion($lowerBoundVersion);
+    }
+}
