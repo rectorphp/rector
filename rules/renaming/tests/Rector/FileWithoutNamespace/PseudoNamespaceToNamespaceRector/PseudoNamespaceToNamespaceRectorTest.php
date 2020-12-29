9ab5b2c8a8ba006fc1c599a702b7f767bbc2526f
Tom Klingenberg <tklingenberg@lastflood.net>
2020-12-29 03:02:14 +0100

    Regression test class-name validity
    In PseudoNamespaceToNamespaceRectorTest
    
    For class_exists() which triggers the auto-loader, extend the test-case
    to keep track of all auto-loaded classes and assert that all class-names
    used in auto-loading are valid class-names.
    
    NOTE: The class-names in the PseudoNamespaceToNamespaceRector are not
          meant to be actually existing class-names, but just some that
          might be and there-fore always valid ones.
    
    The problem with invalid class-names is specifically, that they never
    exist and trigger the auto-loader always. If they contain two consecutive
    backslashes (name-space-separator) then the mapping to the file-system
    might merge them to an already included file which will make rector exit
    with a fatal error as a class is getting re-defined by the auto-load
    include:
    
    > Fatal error: Cannot declare class <class-name>, because the name is
    > already in use in ...
    
    Therefore the PseudoNamespaceToNamespaceRector must not trigger
    auto-loading on invalid class-names.
    
    Fixtures known to fail:
    
    - fixture4.php.inc
    - fixture5.php.inc
    - fixture6.php.inc
    - var_doc.php.inc
    
    Notes on Phpstan supressions (others should be self-explanatory):
    
    - getAutoloadClassNameAssertion() has no return-type hint by intention
      as static code-analyzers (Phpstorm) won't be able to infer the concrete
      type of the anonymous class returned.
    
    - bogus error for anonymous classes:
    
            '#Class "AnonymousClassb3d71a09d1e123ac8b2eb26b93f1b476" should have suffix "TestCase" by parent class/interface#'

--- rules/renaming/tests/Rector/FileWithoutNamespace/PseudoNamespaceToNamespaceRector/PseudoNamespaceToNamespaceRectorTest.php
+++ rules/renaming/tests/Rector/FileWithoutNamespace/PseudoNamespaceToNamespaceRector/PseudoNamespaceToNamespaceRectorTest.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Rector\Renaming\Tests\Rector\FileWithoutNamespace\PseudoNamespaceToNamespaceRector;
 
 use Iterator;
+use PHPUnit\Framework\ExpectationFailedException;
+use PHPUnit\Framework\TestCase;
 use Rector\Generic\ValueObject\PseudoNamespaceToNamespace;
 use Rector\Renaming\Rector\FileWithoutNamespace\PseudoNamespaceToNamespaceRector;
 use Rector\Testing\PHPUnit\AbstractRectorTestCase;
@@ -17,7 +19,11 @@ final class PseudoNamespaceToNamespaceRectorTest extends AbstractRectorTestCase
      */
     public function test(SmartFileInfo $fileInfo): void
     {
+        $autoloadClassNameAssertion = $this->getAutoloadClassNameAssertion();
+
         $this->doTestFileInfo($fileInfo);
+
+        $autoloadClassNameAssertion->assert($fileInfo);
     }
 
     public function provideData(): Iterator
@@ -41,5 +47,66 @@ final class PseudoNamespaceToNamespaceRectorTest extends AbstractRectorTestCase
                 ],
             ],
         ];
+    }
+
+    /** @noinspection PhpMissingReturnTypeInspection */
+    // @phpstan-ignore-next-line
+    private function getAutoloadClassNameAssertion()
+    {
+        return new class ($this) { // @phpstan-ignore-line
+            // ref: https://www.php.net/manual/en/language.oop5.basic.php
+            private const CLASS_NAME = '/^
+                # valid PHP class-name (incl. optional leading \\ name-space separator)
+                \\\\?
+                [a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*
+                (?: \\\\ [a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]* )*
+                $/Dx';
+
+            /**
+             * @var TestCase
+             */
+            private $testCase;
+
+            /**
+             * @var string[]
+             */
+            private $sequence;
+
+            /**
+             * @param TestCase $testCase
+             * @noinspection InterfacesAsConstructorDependenciesInspection
+             */
+            public function __construct(TestCase $testCase)
+            {
+                $this->testCase = $testCase;
+                \spl_autoload_register($this, true ,true);
+            }
+
+            public function __invoke(string $className): void
+            {
+                $this->sequence[] = $className;
+            }
+
+            /**
+             * @param SmartFileInfo $fileInfo
+             * @throws ExpectationFailedException
+             */
+            public function assert(SmartFileInfo $fileInfo): void
+            {
+                foreach ($this->sequence as $className) {
+                    $this->testCase::assertMatchesRegularExpression(
+                        self::CLASS_NAME,
+                        $className,
+                        $fileInfo->getBasename() . ' class-name validity'
+                    );
+                }
+                $this->sequence = [];
+            }
+
+            public function __destruct()
+            {
+                \spl_autoload_unregister($this);
+            }
+        };
     }
 }
