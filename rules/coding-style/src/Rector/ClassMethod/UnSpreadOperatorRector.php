1c265985185a5a9fa79b8f4e0e75c81169b2c6d1
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-12-31 20:28:58 +0700

    [CodingStyle] Register UnSpreadOperatorRector to coding-style config set (#5050)

--- rules/coding-style/src/Rector/ClassMethod/UnSpreadOperatorRector.php
+++ rules/coding-style/src/Rector/ClassMethod/UnSpreadOperatorRector.php
@@ -4,13 +4,19 @@ declare(strict_types=1);
 
 namespace Rector\CodingStyle\Rector\ClassMethod;
 
+use Nette\Utils\Strings;
 use PhpParser\Node;
 use PhpParser\Node\Arg;
 use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Identifier;
 use PhpParser\Node\Param;
+use PhpParser\Node\Stmt\Class_;
 use PhpParser\Node\Stmt\ClassMethod;
+use PHPStan\Type\ThisType;
+use PHPStan\Type\ObjectType;
 use Rector\Core\Rector\AbstractRector;
+use Rector\NodeTypeResolver\Node\AttributeKey;
+use ReflectionClass;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
@@ -19,6 +25,12 @@ use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
  */
 final class UnSpreadOperatorRector extends AbstractRector
 {
+    /**
+     * @see https://regex101.com/r/ChpDsj/1
+     * @var string
+     */
+    private const ANONYMOUS_CLASS_REGEX = '#^AnonymousClass[\w+]#';
+
     public function getRuleDefinition(): RuleDefinition
     {
         return new RuleDefinition('Remove spread operator', [
@@ -74,8 +86,58 @@ CODE_SAMPLE
         return $this->processUnspreadOperatorMethodCallArgs($node);
     }
 
+    private function getClassFileNameByClassMethod(ClassMethod $classMethod): ?string
+    {
+        $parent = $classMethod->getAttribute(AttributeKey::PARENT_NODE);
+        if (! $parent instanceof Class_) {
+            return null;
+        }
+
+        $shortClassName = (string) $parent->name;
+        if (Strings::match($shortClassName, self::ANONYMOUS_CLASS_REGEX)) {
+            return null;
+        }
+
+        $reflectionClass = new ReflectionClass((string) $parent->namespacedName);
+        return (string) $reflectionClass->getFileName();
+    }
+
+    private function getClassFileNameByMethodCall(MethodCall $methodCall): ?string
+    {
+        $scope = $methodCall->getAttribute(AttributeKey::SCOPE);
+        if ($scope === null) {
+            return null;
+        }
+
+        $type = $scope->getType($methodCall->var);
+        if ($type instanceof ObjectType) {
+            $classReflection = $type->getClassReflection();
+            if ($classReflection === null) {
+                return null;
+            }
+
+            return (string) $classReflection->getFileName();
+        }
+
+        if ($type instanceof ThisType) {
+            $staticObjectType = $type->getStaticObjectType();
+            $classReflection = $staticObjectType->getClassReflection();
+            if ($classReflection === null) {
+                return null;
+            }
+
+            return (string) $classReflection->getFileName();
+        }
+
+        return null;
+    }
+
     private function processUnspreadOperatorClassMethodParams(ClassMethod $classMethod): ?ClassMethod
     {
+        if ($this->isInVendor($classMethod)) {
+            return null;
+        }
+
         $params = $classMethod->params;
         if ($params === []) {
             return null;
@@ -94,8 +156,29 @@ CODE_SAMPLE
         return $classMethod;
     }
 
+    /**
+     * @param ClassMethod|MethodCall $node
+     */
+    private function isInVendor(Node $node): bool
+    {
+        $scope = $node->getAttribute(AttributeKey::SCOPE);
+        $fileName = $node instanceof ClassMethod
+            ? $this->getClassFileNameByClassMethod($node)
+            : $this->getClassFileNameByMethodCall($node);
+
+        if ($fileName === null) {
+            return false;
+        }
+
+        return Strings::contains($fileName, 'vendor');
+    }
+
     private function processUnspreadOperatorMethodCallArgs(MethodCall $methodCall): ?MethodCall
     {
+        if ($this->isInVendor($methodCall)) {
+            return null;
+        }
+
         $args = $methodCall->args;
         if ($args === []) {
             return null;
