567f9d23a094cbe4b7ebe93e598481e089d7c9e6
Floris Luiten <github@florisluiten.nl>
2021-01-05 20:51:45 +0100

    Changed "lenght" instead of "length", fixing bug in class_exists using incorrect classname

--- stubs/Symfony/Component/Validator/Constraints/Length.php
+++ stubs/Symfony/Component/Validator/Constraints/Length.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Symfony\Component\Validator\Constraints;
 
-if (class_exists('Symfony\Component\Validator\Constraints\Lenght')) {
+if (class_exists('Symfony\Component\Validator\Constraints\Length')) {
     return;
 }
 
