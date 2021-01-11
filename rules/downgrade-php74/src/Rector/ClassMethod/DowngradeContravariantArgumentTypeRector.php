a57d55cd0aaf73756e9bd1802ffb91bc164cb7f3
Leonardo Losoviz <leo@getpop.org>
2021-01-12 07:03:29 +0800

    [Downgrade PHP 7.4] Skip downgrading contravariant argument for __construct (#5148)
    * Added (failing) test
    
    * Implemented feature

--- rules/downgrade-php74/src/Rector/ClassMethod/DowngradeContravariantArgumentTypeRector.php
+++ rules/downgrade-php74/src/Rector/ClassMethod/DowngradeContravariantArgumentTypeRector.php
@@ -85,6 +85,11 @@ CODE_SAMPLE
             return false;
         }
 
+        // Contravariant arguments are supported for __construct
+        if ($this->getName($functionLike) === '__construct') {
+            return false;
+        }
+
         // Check if the type is different from the one declared in some ancestor
         return $this->getDifferentParamTypeFromAncestorClass($param, $functionLike) !== null;
     }
