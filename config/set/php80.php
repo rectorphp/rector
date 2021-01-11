fc3c06ddc7e7c2ab4067fc39e689fe9bff1384d5
TomasVotruba <tomas.vot@gmail.com>
2021-01-11 12:54:04 +0100

    [PHP 8.0] Add new pgsql function names

--- config/set/php80.php
+++ config/set/php80.php
@@ -19,6 +19,7 @@ use Rector\Php80\Rector\Identical\StrStartsWithRector;
 use Rector\Php80\Rector\NotIdentical\StrContainsRector;
 use Rector\Php80\Rector\Switch_\ChangeSwitchToMatchRector;
 use Rector\Php80\Rector\Ternary\GetDebugTypeRector;
+use Rector\Renaming\Rector\FuncCall\RenameFunctionRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\SymfonyPhpConfig\ValueObjectInliner;
 
@@ -60,4 +61,35 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->set(RemoveParentCallWithoutParentRector::class);
     $services->set(SetStateToStaticRector::class);
     $services->set(FinalPrivateToPrivateVisibilityRector::class);
+
+    // @see https://php.watch/versions/8.0/pgsql-aliases-deprecated
+    $services->set(RenameFunctionRector::class)
+        ->call('configure', [[
+            RenameFunctionRector::OLD_FUNCTION_TO_NEW_FUNCTION => [
+                'pg_clientencoding' => 'pg_client_encoding',
+                'pg_cmdtuples' => 'pg_affected_rows',
+                'pg_errormessage' => 'pg_last_error',
+                'pg_fieldisnull' => 'pg_field_is_null',
+                'pg_fieldname' => 'pg_field_name',
+                'pg_fieldnum' => 'pg_field_num',
+                'pg_fieldprtlen' => 'pg_field_prtlen',
+                'pg_fieldsize' => 'pg_field_size',
+                'pg_fieldtype' => 'pg_field_type',
+                'pg_freeresult' => 'pg_free_result',
+                'pg_getlastoid' => 'pg_last_oid',
+                'pg_loclose' => 'pg_lo_close',
+                'pg_locreate' => 'pg_lo_create',
+                'pg_loexport' => 'pg_lo_export',
+                'pg_loimport' => 'pg_lo_import',
+                'pg_loopen' => 'pg_lo_open',
+                'pg_loread' => 'pg_lo_read',
+                'pg_loreadall' => 'pg_lo_read_all',
+                'pg_lounlink' => 'pg_lo_unlink',
+                'pg_lowrite' => 'pg_lo_write',
+                'pg_numfields' => 'pg_num_fields',
+                'pg_numrows' => 'pg_num_rows',
+                'pg_result' => 'pg_fetch_result',
+                'pg_setclientencoding' => 'pg_set_client_encoding',
+            ]
+        ]]);
 };
