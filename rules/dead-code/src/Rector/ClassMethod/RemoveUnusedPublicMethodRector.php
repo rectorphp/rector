a9f238c4aa554914e11324bb88cef1553fe8f5a2
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-01-12 13:45:21 +0700

    fix

--- rules/dead-code/src/Rector/ClassMethod/RemoveUnusedPublicMethodRector.php
+++ rules/dead-code/src/Rector/ClassMethod/RemoveUnusedPublicMethodRector.php
@@ -6,10 +6,12 @@ namespace Rector\DeadCode\Rector\ClassMethod;
 
 use PhpParser\Node;
 use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Expr\StaticCall;
 use PhpParser\Node\Param;
 use PhpParser\Node\Stmt\ClassMethod;
 use Rector\Caching\Contract\Rector\ZeroCacheRectorInterface;
 use Rector\Core\Rector\AbstractRector;
+use Rector\NodeCollector\ValueObject\ArrayCallable;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
@@ -19,7 +21,7 @@ use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 final class RemoveUnusedPublicMethodRector extends AbstractRector implements ZeroCacheRectorInterface
 {
     /**
-     * @var MethodCall[]
+     * @var MethodCall[]|StaticCall[]|ArrayCallable[]
      */
     private $calls = [];
 
@@ -89,23 +91,6 @@ CODE_SAMPLE
 
         $this->calls = array_merge($this->calls, $this->nodeRepository->findCallsByClassMethod($node));
         if ($this->calls !== []) {
-            return null;
-        }
-
-        $isFoundCall = (bool) $this->betterNodeFinder->findFirst(
-            $node->stmts,
-            function (Node $node): bool {
-                foreach ($this->calls as $call) {
-                    if ($this->areNodesEqual($call, $node)) {
-                        return true;
-                    }
-                }
-
-                return false;
-            }
-        );
-
-        if ($isFoundCall) {
             return null;
         }
 
