3d55b3a82ba33dba0e784460b1c4bb65d466926a
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-01-12 20:53:28 +0700

    add open source type test

---
+++ rules/dead-code/tests/Rector/ClassMethod/RemoveUnusedPublicMethodRector/OpenSourceRectorTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\DeadCode\Tests\Rector\ClassMethod\RemoveUnusedPublicMethodRector;
+
+use Iterator;
+use Rector\Core\Configuration\Option;
+use Rector\Core\ValueObject\ProjectType;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPublicMethodRector;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class OpenSourceRectorTest extends AbstractRectorTestCase
+{
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(SmartFileInfo $fileInfo): void
+    {
+        $this->setParameter(Option::PROJECT_TYPE, ProjectType::OPEN_SOURCE);
+        $this->doTestFileInfo($fileInfo);
+    }
+
+    public function provideData(): Iterator
+    {
+        return $this->yieldFilesFromDirectory(__DIR__ . '/FixtureOpenSource');
+    }
+
+    protected function getRectorClass(): string
+    {
+        return RemoveUnusedPublicMethodRector::class;
+    }
+}
