5feddd0dc734ab29b302cc15cd532af36b1d6641
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-01-17 18:28:14 +0700

    phpstan

--- rules/type-declaration/src/Rector/Identical/FlipTypeControlToUseExclusiveTypeRector.php
+++ rules/type-declaration/src/Rector/Identical/FlipTypeControlToUseExclusiveTypeRector.php
@@ -16,9 +16,11 @@ use Rector\Core\Rector\AbstractRector;
 use Rector\NodeTypeResolver\Node\AttributeKey;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+use PHPStan\PhpDocParser\Ast\PhpDoc\VarTagValueNode;
+use Rector\AttributeAwarePhpDoc\Ast\Type\AttributeAwareIdentifierTypeNode;
 
 /**
- * @see \Rector\TypeDeclaration\Tests\Rector\Assign\FlipTypeControlToUseExclusiveTypeRector\FlipTypeControlToUseExclusiveTypeRectorTest
+ * @see \Rector\TypeDeclaration\Tests\Rector\Identical\FlipTypeControlToUseExclusiveTypeRector\FlipTypeControlToUseExclusiveTypeRectorTest
  */
 final class FlipTypeControlToUseExclusiveTypeRector extends AbstractRector
 {
@@ -81,6 +83,7 @@ CODE_SAMPLE
             ? $node->right
             : $node->left;
 
+        /** @var Assign|null $assign */
         $assign = $this->betterNodeFinder->findFirstPrevious($node, function (Node $node) use ($variable): bool {
             if (! $node instanceof Assign) {
                 return false;
@@ -88,6 +91,10 @@ CODE_SAMPLE
 
             return $this->areNodesEqual($node->var, $variable);
         });
+
+        if (! $assign instanceof Assign) {
+            return null;
+        }
 
         $expression = $assign->getAttribute(Attributekey::PARENT_NODE);
         if (! $expression instanceof Expression) {
@@ -99,7 +106,12 @@ CODE_SAMPLE
             return null;
         }
 
+        /** @var VarTagValueNode|null $tagValueNode */
         $tagValueNode = $phpDocInfo->getVarTagValueNode();
+        if (! $tagValueNode instanceof VarTagValueNode) {
+            return null;
+        }
+
         if (! $tagValueNode->type instanceof AttributeAwareUnionTypeNode) {
             return null;
         }
@@ -108,17 +120,25 @@ CODE_SAMPLE
             return null;
         }
 
-        if ($tagValueNode->type->types[0]->name === $tagValueNode->type->types[1]->name) {
+        /** @var AttributeAwareIdentifierTypeNode[] $types */
+        $types = $tagValueNode->type->types;
+        foreach ($tagValueNode->type->types as $type) {
+            if (! $type instanceof AttributeAwareIdentifierTypeNode) {
+                return null;
+            }
+        }
+
+        if ($types[0]->name === $types[1]->name) {
             return null;
         }
 
-        if ($tagValueNode->type->types[0]->name !== 'null' && $tagValueNode->type->types[1]->name !== 'null') {
+        if ($types[0]->name !== 'null' && $types[1]->name !== 'null') {
             return null;
         }
 
-        $type = $tagValueNode->type->types[0]->name === 'null'
-            ? $tagValueNode->type->types[1]->name
-            : $tagValueNode->type->types[0]->name;
+        $type = $types[0]->name === 'null'
+            ? $types[1]->name
+            : $types[0]->name;
 
         if (class_exists($type) || interface_exists($type)) {
             return new Instanceof_($variable, new FullyQualified($type));
