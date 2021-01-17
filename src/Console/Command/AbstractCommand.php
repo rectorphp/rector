91a19e549bd2c25073c4b20289169db3fee41e20
TomasVotruba <tomas.vot@gmail.com>
2021-01-18 00:25:39 +0100

    remove unneeded command layer for showing not enough args

--- src/Console/Command/AbstractCommand.php
+++ src/Console/Command/AbstractCommand.php
@@ -4,15 +4,11 @@ declare(strict_types=1);
 
 namespace Rector\Core\Console\Command;
 
-use Nette\Utils\Strings;
 use Rector\Caching\Detector\ChangedFilesDetector;
 use Rector\Core\Configuration\Option;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Descriptor\TextDescriptor;
-use Symfony\Component\Console\Exception\RuntimeException;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symplify\PackageBuilder\Console\ShellCode;
 
 abstract class AbstractCommand extends Command
 {
@@ -22,45 +18,11 @@ abstract class AbstractCommand extends Command
     protected $changedFilesDetector;
 
     /**
-     * @var TextDescriptor
-     */
-    private $textDescriptor;
-
-    /**
      * @required
      */
-    public function autowireAbstractCommand(
-        TextDescriptor $textDescriptor,
-        ChangedFilesDetector $changedFilesDetector
-    ): void {
-        $this->textDescriptor = $textDescriptor;
-        $this->changedFilesDetector = $changedFilesDetector;
-    }
-
-    public function run(InputInterface $input, OutputInterface $output): int
+    public function autowireAbstractCommand(ChangedFilesDetector $changedFilesDetector): void
     {
-        // show help on arguments fail
-        try {
-            return parent::run($input, $output);
-        } catch (RuntimeException $runtimeException) {
-            if (Strings::contains($runtimeException->getMessage(), 'Not enough arguments')) {
-                // sometimes there is "command" argument, not really needed on fail of chosen command and missing argument
-                $inputDefinition = $this->getDefinition();
-                $arguments = $inputDefinition->getArguments();
-
-                if (isset($arguments['command'])) {
-                    unset($arguments['command']);
-
-                    $inputDefinition->setArguments($arguments);
-                }
-
-                $this->textDescriptor->describe($output, $this);
-
-                return ShellCode::SUCCESS;
-            }
-
-            throw $runtimeException;
-        }
+        $this->changedFilesDetector = $changedFilesDetector;
     }
 
     protected function initialize(InputInterface $input, OutputInterface $output): void
