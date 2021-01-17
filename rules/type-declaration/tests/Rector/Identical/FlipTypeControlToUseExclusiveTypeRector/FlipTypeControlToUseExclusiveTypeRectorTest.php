24ef77c08d3db9a92ac3e63dd0e8ca85346359f4
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-01-17 19:22:49 +0700

    cs fix

--- rules/type-declaration/tests/Rector/Identical/FlipTypeControlToUseExclusiveTypeRector/FlipTypeControlToUseExclusiveTypeRectorTest.php
+++ rules/type-declaration/tests/Rector/Identical/FlipTypeControlToUseExclusiveTypeRector/FlipTypeControlToUseExclusiveTypeRectorTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Rector\TypeDeclaration\Tests\Rector\Identical\FlipTypeControlToUseExclusiveTypeRector;
 
 use Iterator;
-use Rector\Core\ValueObject\PhpVersionFeature;
 use Rector\Testing\PHPUnit\AbstractRectorTestCase;
 use Rector\TypeDeclaration\Rector\Identical\FlipTypeControlToUseExclusiveTypeRector;
 use Symplify\SmartFileSystem\SmartFileInfo;
