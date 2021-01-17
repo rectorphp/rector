d6837cbeea1c3424fe839d2ed37045e3723040fb
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-01-17 17:24:16 +0700

    adding test

---
+++ rules/type-declaration/tests/Rector/Assign/FlipTypeControlToUseExclusiveTypeRector/FlipTypeControlToUseExclusiveTypeRectorTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\TypeDeclaration\Tests\Rector\Assign\FlipTypeControlToUseExclusiveTypeRector;
+
+use Iterator;
+use Rector\Core\ValueObject\PhpVersionFeature;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+use Rector\TypeDeclaration\Rector\Assign\FlipTypeControlToUseExclusiveTypeRector;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class FlipTypeControlToUseExclusiveTypeRectorTest extends AbstractRectorTestCase
+{
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(SmartFileInfo $fileInfo): void
+    {
+        $this->doTestFileInfo($fileInfo);
+    }
+
+    public function provideData(): Iterator
+    {
+        return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
+    }
+
+    protected function getRectorClass(): string
+    {
+        return FlipTypeControlToUseExclusiveTypeRector::class;
+    }
+}
