efbed691b5da84df22b7750abfe79cbe77e6ac0d
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-01-25 21:22:48 +0700

    fixture

---
+++ rules/type-declaration/tests/Rector/FunctionLike/ReturnTypeDeclarationRector/Source/vendor/RespectVendorReturnType.php
@@ -0,0 +1,13 @@
+<?php
+namespace Rector\TypeDeclaration\Tests\Rector\FunctionLike\ReturnTypeDeclarationRector\Source\vendor;
+
+class RespectVendorReturnType
+{
+    /**
+     * @return bool
+     */
+    public function run()
+    {
+        return false;
+    }
+}
