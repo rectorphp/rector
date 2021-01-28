3c092e6659b4b7febf146b56a947e95416f47d93
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-01-28 08:57:40 +0700

    [Symfony 5.2][WIP] Add BuilderSetDataMapperRector

---
+++ rules/symfony5/src/Rector/MethodCall/BuilderSetDataMapperRector.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Symfony5\Rector\MethodCall;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\MethodCall;
+use Rector\Core\Rector\AbstractRector;
+use Symfony\Component\Form\FormConfigBuilderInterface;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see https://github.com/symfony/symfony/commit/878effaf47cfb23f73c984d806eb8e9d9206cb5c
+ * @see \Rector\Symfony5\Tests\Rector\MethodCall\BuilderSetDataMapperRector\BuilderSetDataMapperRectorTest
+ */
+final class BuilderSetDataMapperRector extends AbstractRector
+{
+    /**
+     * @var class-string[]
+     */
+    private const REQUIRED_TYPE = [FormConfigBuilderInterface::class];
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'Migrates from deprecated Form Builder->setDataMapper(new PropertyPathMapper()) to Builder->setDataMapper(new DataMapper(new PropertyPathAccessor()))',
+            [
+                new CodeSample(
+                    <<<'CODE_SAMPLE'
+use Symfony\Component\Form\Extension\Core\DataMapper\PropertyPathMapper;
+
+class SomeClass
+{
+    public function run(FormConfigBuilderInterface $builder)
+    {
+        $builder->setDataMapper(new PropertyPathMapper());
+    }
+}
+CODE_SAMPLE
+                    ,
+                    <<<'CODE_SAMPLE'
+use Symfony\Component\Form\Extension\Core\DataAccessor\PropertyPathAccessor;
+use Symfony\Component\Form\Extension\Core\DataMapper\DataMapper;
+
+class SomeClass
+{
+    public function run(FormConfigBuilderInterface $builder)
+    {
+        $builder->setDataMapper(new DataMapper(new PropertyPathAccessor()));
+    }
+}
+CODE_SAMPLE
+                ),
+            ]);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [MethodCall::class];
+    }
+
+    /**
+     * @param MethodCall $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if (! $this->isObjectType($node->var, self::REQUIRED_TYPE)) {
+            return null;
+        }
+
+        if (! $this->isName($node->name, 'setDataMapper')) {
+            return null;
+        }
+
+        return $node;
+    }
+}
