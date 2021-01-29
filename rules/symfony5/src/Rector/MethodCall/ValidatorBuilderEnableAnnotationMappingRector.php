fbecdbf84ce9e1405ad28378068f281848329f58
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-01-29 10:46:30 +0700

    [Symfony 5.2] [WIP] Add ValidatorBuilderEnableAnnotationMappingRector

---
+++ rules/symfony5/src/Rector/MethodCall/ValidatorBuilderEnableAnnotationMappingRector.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Symfony5\Rector\MethodCall;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\MethodCall;
+use Rector\Core\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see https://github.com/symfony/symfony/blob/5.x/UPGRADE-5.2.md#validator
+ * @see \Rector\Symfony5\Tests\Rector\MethodCall\ValidatorBuilderEnableAnnotationMappingRector\ValidatorBuilderEnableAnnotationMappingRectorTest
+ */
+final class ValidatorBuilderEnableAnnotationMappingRector extends AbstractRector
+{
+    /**
+     * @var string
+     */
+    private const REQUIRED_TYPE = 'Symfony\Component\Validator\ValidatorBuilder';
+
+    /**
+     * @var string
+     */
+    private const ARG_OLD_TYPE = 'use Doctrine\Common\Annotations\Reader';
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'Migrates from deprecated ValidatorBuilder->enableAnnotationMapping($reader) to ValidatorBuilder->enableAnnotationMapping(true)->setDoctrineAnnotationReader($reader)',
+            [
+                new CodeSample(
+                    <<<'CODE_SAMPLE'
+use Doctrine\Common\Annotations\Reader;
+use Symfony\Component\Validator\ValidatorBuilder;
+
+class SomeClass
+{
+    public function run(ValidatorBuilder $builder, Reader $reader)
+    {
+        $builder->enableAnnotationMapping($reader);
+    }
+}
+CODE_SAMPLE
+                    ,
+                    <<<'CODE_SAMPLE'
+use Doctrine\Common\Annotations\Reader;
+use Symfony\Component\Validator\ValidatorBuilder;
+
+class SomeClass
+{
+    public function run(ValidatorBuilder $builder, Reader $reader)
+    {
+        $builder->enableAnnotationMapping(true)->setDoctrineAnnotationReader($reader);
+    }
+}
+CODE_SAMPLE
+                ),
+            ]);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [MethodCall::class];
+    }
+
+    /**
+     * @param MethodCall $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if (! $this->isObjectType($node->var, self::REQUIRED_TYPE)) {
+            return null;
+        }
+
+        if (! $this->isName($node->name, 'enableAnnotationMapping')) {
+            return null;
+        }
+
+        return $node;
+    }
+}
