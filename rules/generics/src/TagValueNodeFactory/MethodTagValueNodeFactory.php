227c244a857c889ca1e13a6fe1a1c6b7b94fa358
TomasVotruba <tomas.vot@gmail.com>
2021-01-29 20:26:57 +0100

    add array-like

--- rules/generics/src/TagValueNodeFactory/MethodTagValueNodeFactory.php
+++ rules/generics/src/TagValueNodeFactory/MethodTagValueNodeFactory.php
@@ -53,6 +53,10 @@ final class MethodTagValueNodeFactory
         if ($returnTagValueNode->type instanceof UnionTypeNode) {
             $resolvedTypes = [];
             foreach ($returnTagValueNode->type->types as $unionedTypeNode) {
+                if (! $unionedTypeNode instanceof IdentifierTypeNode) {
+                    continue;
+                }
+
                 $resolvedTypes[] = $this->resolveIdentifierTypeNode(
                     $unionedTypeNode,
                     $templateTypeMap,
