6dae35bb65ee480de9406ac14d483b6a75c9c362
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-02-05 15:49:51 +0700

    more interface register

--- config/set/laminas-servicemanager-40.php
+++ config/set/laminas-servicemanager-40.php
@@ -11,8 +11,20 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->set(RenameClassRector::class)
         ->call('configure', [[
             RenameClassRector::OLD_TO_NEW_CLASSES => [
+                // interop deprecated interop to psr
                 'Interop\Container\ContainerInterface' => 'Psr\Container\ContainerInterface',
+
+                // update deprecated root factory to new factory under new namespace
+                'Laminas\ServiceManager\AbstractFactoryInterface' => 'Laminas\ServiceManager\Factory\AbstractFactoryInterface',
                 'Laminas\ServiceManager\FactoryInterface' => 'Laminas\ServiceManager\Factory\FactoryInterface',
+                'Laminas\ServiceManager\DelegatorFactoryInterface' => 'Laminas\ServiceManager\Factory\DelegatorFactoryInterface',
+                'Laminas\ServiceManager\InitializerInterface' => 'Laminas\ServiceManager\Initializer\InitializerInterface',
+
+                // update deprecated root factory to new factory under new namespace for laminas/laminas-zendframework-bridge
+                'Zend\ServiceManager\AbstractFactoryInterface' => 'Laminas\ServiceManager\Factory\AbstractFactoryInterface',
+                'Zend\ServiceManager\FactoryInterface' => 'Laminas\ServiceManager\Factory\FactoryInterface',
+                'Zend\ServiceManager\DelegatorFactoryInterface' => 'Laminas\ServiceManager\Factory\DelegatorFactoryInterface',
+                'Zend\ServiceManager\InitializerInterface' => 'Laminas\ServiceManager\Initializer\InitializerInterface',
             ],
         ]]);
 };
