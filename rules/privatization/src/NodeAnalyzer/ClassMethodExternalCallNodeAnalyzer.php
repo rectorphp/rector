93aaa53b4f652f9a2586a4f959b8a56913f80eb0
kaizen-ci <info@kaizen-ci.org>
2021-02-06 05:27:11 +0000

    [ci-review] Rector Rectify

--- rules/privatization/src/NodeAnalyzer/ClassMethodExternalCallNodeAnalyzer.php
+++ rules/privatization/src/NodeAnalyzer/ClassMethodExternalCallNodeAnalyzer.php
@@ -73,11 +73,9 @@ final class ClassMethodExternalCallNodeAnalyzer
      */
     public function getExternalCalls(ClassMethod $classMethod, array $methodCalls = []): array
     {
-        /** @var MethodCall[]|StaticCall[]|ArrayCallable[] $methodCalls */
+        /** @var MethodCall[] $methodCalls */
         $methodCalls = $methodCalls ?: $this->nodeRepository->findCallsByClassMethod($classMethod);
 
-        // remove static calls and [$this, 'call']
-        /** @var MethodCall[] $methodCalls */
         $methodCalls = array_filter($methodCalls, function (object $node): bool {
             return $node instanceof MethodCall;
         });
