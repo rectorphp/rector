359247a6bbdc5ec9204b385f34b3a8b1b9ae6599
kaizen-ci <info@kaizen-ci.org>
2021-02-09 02:15:40 +0000

    [ci-review] Rector Rectify

--- rules/dead-code/tests/Rector/ClassMethod/RemoveUnusedConstructorParamRector/RemoveUnusedConstructorParamRectorTest.php
+++ rules/dead-code/tests/Rector/ClassMethod/RemoveUnusedConstructorParamRector/RemoveUnusedConstructorParamRectorTest.php
@@ -4,20 +4,22 @@ declare(strict_types=1);
 
 namespace Rector\DeadCode\Tests\Rector\ClassMethod\RemoveUnusedConstructorParamRector;
 
+use Iterator;
 use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedConstructorParamRector;
 use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+use Symplify\SmartFileSystem\SmartFileInfo;
 
 final class RemoveUnusedConstructorParamRectorTest extends AbstractRectorTestCase
 {
     /**
      * @dataProvider provideData()
      */
-    public function test(\Symplify\SmartFileSystem\SmartFileInfo $fileInfo): void
+    public function test(SmartFileInfo $fileInfo): void
     {
         $this->doTestFileInfo($fileInfo);
     }
 
-    public function provideData(): \Iterator
+    public function provideData(): Iterator
     {
         return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
     }
