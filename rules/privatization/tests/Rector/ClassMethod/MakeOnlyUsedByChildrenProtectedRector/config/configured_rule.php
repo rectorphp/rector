2b9de66e10c158332244542e1dbcf4804d8070db
TomasVotruba <tomas.vot@gmail.com>
2021-02-12 01:49:48 +0100

    [Privatizatoin] Skip on open-source abstract

---
+++ rules/privatization/tests/Rector/ClassMethod/MakeOnlyUsedByChildrenProtectedRector/config/configured_rule.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Core\Configuration\Option;
+use Rector\Core\ValueObject\ProjectType;
+use Rector\Privatization\Rector\ClassMethod\MakeOnlyUsedByChildrenProtectedRector;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::PROJECT_TYPE, ProjectType::OPEN_SOURCE);
+
+    $services = $containerConfigurator->services();
+    $services->set(MakeOnlyUsedByChildrenProtectedRector::class);
+};
