8f9fa77a84a7dec9a49bee016d5c734d64cee212
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-02-13 20:24:38 +0700

    [EarlyReturn] [WIP] Fixes #5218 Add ReturnAfterToEarlyBreakRector

---
+++ rules/early-return/src/Rector/Foreach_/ReturnAfterToEarlyOnBreakRector.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\EarlyReturn\Rector\Foreach_;
+
+use PhpParser\Node;
+use PhpParser\Node\Stmt\Foreach_;
+use Rector\Core\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \Rector\EarlyReturn\Tests\Rector\Foreach_\ReturnAfterToEarlyOnBreakRector\ReturnAfterToEarlyOnBreakRectorTest
+ */
+final class ReturnAfterToEarlyOnBreakRector extends AbstractRector
+{
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition('Change after foreach to early return in foreach on break', [
+            new CodeSample(
+                <<<'CODE_SAMPLE'
+class SomeClass
+{
+    public function run(array $pathConstants, string $allowedPath)
+    {
+        $pathOK = false;
+
+        foreach ($pathConstants as $allowedPath) {
+            if ($dirPath == $allowedPath) {
+                $pathOK = true;
+            }
+        }
+
+        return $pathOK;
+    }
+}
+CODE_SAMPLE
+,
+                <<<'CODE_SAMPLE'
+class SomeClass
+{
+    public function run(array $pathConstants, string $allowedPath)
+    {
+        $pathOK = false;
+
+        foreach ($pathConstants as $allowedPath) {
+            if ($dirPath == $allowedPath) {
+                $pathOK = true;
+            }
+        }
+
+        return $pathOK;
+    }
+}
+CODE_SAMPLE
+            ),
+        ]);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [Foreach_::class];
+    }
+
+    /**
+     * @param Foreach_ $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        return $node;
+    }
+}
