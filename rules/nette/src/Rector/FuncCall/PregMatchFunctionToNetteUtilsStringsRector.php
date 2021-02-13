0c0d9a70720ff3379306ba038b9800574b051820
TomasVotruba <tomas.vot@gmail.com>
2021-02-13 21:01:46 +0100

    drop unused AddPropertyByParentRector

--- rules/nette/src/Rector/FuncCall/PregMatchFunctionToNetteUtilsStringsRector.php
+++ rules/nette/src/Rector/FuncCall/PregMatchFunctionToNetteUtilsStringsRector.php
@@ -21,7 +21,7 @@ use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
 /**
- * @see https://www.tomasvotruba.cz/blog/2019/02/07/what-i-learned-by-using-thecodingmachine-safe/#is-there-a-better-way
+ * @see https://tomasvotruba.com/blog/2019/02/07/what-i-learned-by-using-thecodingmachine-safe/#is-there-a-better-way
  *
  * @see \Rector\Nette\Tests\Rector\FuncCall\PregMatchFunctionToNetteUtilsStringsRector\PregMatchFunctionToNetteUtilsStringsRectorTest
  */
