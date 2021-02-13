1a45becf74b352c6135504cdd37d5230c128a5d9
Tomas Votruba <tomas.vot@gmail.com>
2021-02-13 23:26:00 +0100

    [Nette] Add false return check to template check (#5535)
    Co-authored-by: kaizen-ci <info@kaizen-ci.org>

---
+++ rules/nette/src/NodeAnalyzer/ReturnAnalyzer.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Nette\NodeAnalyzer;
+
+use PhpParser\Node\Expr\Assign;
+use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\Node\Stmt\Return_;
+use Rector\Core\PhpParser\Node\BetterNodeFinder;
+use Rector\NodeNestingScope\ScopeNestingComparator;
+
+final class ReturnAnalyzer
+{
+    /**
+     * @var BetterNodeFinder
+     */
+    private $betterNodeFinder;
+
+    /**
+     * @var ScopeNestingComparator
+     */
+    private $scopeNestingComparator;
+
+    public function __construct(BetterNodeFinder $betterNodeFinder, ScopeNestingComparator $scopeNestingComparator)
+    {
+        $this->betterNodeFinder = $betterNodeFinder;
+        $this->scopeNestingComparator = $scopeNestingComparator;
+    }
+
+    public function findLastClassMethodReturn(ClassMethod $classMethod): ?Return_
+    {
+        /** @var Return_[] $returns */
+        $returns = $this->betterNodeFinder->findInstanceOf($classMethod, Return_::class);
+
+        // put the latest first
+        $returns = array_reverse($returns);
+
+        foreach ($returns as $return) {
+            if ($this->scopeNestingComparator->areReturnScopeNested($return, $classMethod)) {
+                return $return;
+            }
+        }
+
+        return null;
+    }
+
+    public function isBeforeLastReturn(Assign $assign, ?Return_ $lastReturn): bool
+    {
+        if (! $lastReturn instanceof Return_) {
+            return true;
+        }
+
+        return $lastReturn->getStartTokenPos() < $assign->getStartTokenPos();
+    }
+}
