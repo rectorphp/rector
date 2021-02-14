fbd6c61b14754788a9661dea44928192976ba6f1
TomasVotruba <tomas.vot@gmail.com>
2021-02-14 01:02:06 +0100

    [DoctrineCodeQuality] Add tests

---
+++ rules/doctrine-code-quality/tests/Set/DoctrineRepositoryAsService/DoctrineRepositoryAsServiceTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\DoctrineCodeQuality\Tests\Set\DoctrineRepositoryAsService;
+
+use Iterator;
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class DoctrineRepositoryAsServiceTest extends AbstractRectorTestCase
+{
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(SmartFileInfo $fileInfo): void
+    {
+        $this->doTestFileInfo($fileInfo);
+    }
+
+    public function provideData(): Iterator
+    {
+        return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
+    }
+
+    protected function provideConfigFilePath(): string
+    {
+        return __DIR__ . '/config/configured_set.php';
+    }
+}
