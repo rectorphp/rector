ba7799c19dd517cb5fbf5974c4c65a851a005175
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-02-15 23:53:11 +0700

    use nodeRepository to get children class nodes

--- rules/dead-code/src/Rector/Class_/RemoveEmptyAbstractClassRector.php
+++ rules/dead-code/src/Rector/Class_/RemoveEmptyAbstractClassRector.php
@@ -10,8 +10,6 @@ use PhpParser\Node\Name\FullyQualified;
 use Rector\Core\Rector\AbstractRector;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
-use Rector\FamilyTree\Reflection\FamilyRelationsAnalyzer;
-use ReflectionClass;
 
 /**
  * @see \Rector\DeadCode\Tests\Rector\Class_\RemoveEmptyAbstractClassRector\RemoveEmptyAbstractClassRectorTest
@@ -19,19 +17,9 @@ use ReflectionClass;
 final class RemoveEmptyAbstractClassRector extends AbstractRector
 {
     /**
-     * @var FamilyRelationsAnalyzer
-     */
-    private $familyRelationsAnalyzer;
-
-    /**
      * @var string[]
      */
     private $removedAbstractClasses = [];
-
-    public function __construct(FamilyRelationsAnalyzer $familyRelationsAnalyzer)
-    {
-        $this->familyRelationsAnalyzer = $familyRelationsAnalyzer;
-    }
 
     /**
      * @return string[]
@@ -57,11 +45,11 @@ final class RemoveEmptyAbstractClassRector extends AbstractRector
     private function shouldSkip(Class_ $class): bool
     {
         if (! $class->isAbstract()) {
-            return false;
+            return true;
         }
 
         if ($class->implements !== []) {
-            return false;
+            return true;
         }
 
         $stmts = $class->stmts;
@@ -75,7 +63,7 @@ final class RemoveEmptyAbstractClassRector extends AbstractRector
         }
 
         $extends  = $class->extends;
-        $children = $this->familyRelationsAnalyzer->getChildrenOfClass($this->getName($class->namespacedName));
+        $children = $this->nodeRepository->findChildrenOfClass($this->getName($class->namespacedName));
 
         foreach ($children as $child) {
             if (in_array($child->extends->toString(), $this->removedAbstractClasses, true)) {
