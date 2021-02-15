1a003e071ab4e28fee7ead89ec9d56e8a0a6de27
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-02-16 06:48:20 +0700

    using FileNode

--- rules/dead-code/src/Rector/Class_/RemoveEmptyAbstractClassRector.php
+++ rules/dead-code/src/Rector/Class_/RemoveEmptyAbstractClassRector.php
@@ -7,7 +7,9 @@ namespace Rector\DeadCode\Rector\Class_;
 use PhpParser\Node;
 use PhpParser\Node\Name\FullyQualified;
 use PhpParser\Node\Stmt\Class_;
+use Rector\Core\PhpParser\Node\CustomNode\FileNode;
 use Rector\Core\Rector\AbstractRector;
+use Rector\NodeTypeResolver\Node\AttributeKey;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
@@ -26,21 +28,25 @@ final class RemoveEmptyAbstractClassRector extends AbstractRector
      */
     public function getNodeTypes(): array
     {
-        return [Class_::class];
+        return [FileNode::class, Class_::class];
     }
 
     /**
-     * @param Class_ $node
+     * @param FileNode|Class_ $node
      */
     public function refactor(Node $node): ?Node
     {
-        if (! $node->isAbstract()) {
-            $class = $node;
-            $this->fullyQualifieds[] = $this->betterNodeFinder->findFirst($node->stmts, function (Node $node) use ($class): bool {
-                return $this->areNodesEqual($node, $class);
-            });
+        if ($node instanceof FileNode) {
+            $fullyQualifieds = $this->betterNodeFinder->findInstanceOf($node, FullyQualified::class);
+            foreach ($fullyQualifieds as $fullyQualified) {
+                $parent = $fullyQualified->getAttribute(AttributeKey::PARENT_NODE);
+                if ($parent instanceof Class_) {
+                    continue;
+                }
 
-            return null;
+                $this->fullyQualifieds[] = $fullyQualified;
+            }
+            return $node;
         }
 
         if ($this->shouldSkip($node)) {
@@ -94,6 +100,10 @@ CODE_SAMPLE
 
     private function shouldSkip(Class_ $class): bool
     {
+        if (! $class->isAbstract()) {
+            return true;
+        }
+
         if ($class->implements !== []) {
             return true;
         }
@@ -108,14 +118,15 @@ CODE_SAMPLE
             return null;
         }
 
-        $extends = $class->extends;
+        $className = $class->namespacedName->toString();
         foreach ($this->fullyQualifieds as $fullyQualified) {
-            if ($this->areNodesEqual($extends, $fullyQualified)) {
+            if ($className === $fullyQualified->toString()) {
                 return null;
             }
         }
 
         $children = $this->nodeRepository->findChildrenOfClass($this->getName($class->namespacedName));
+        $extends = $class->extends;
         foreach ($children as $child) {
             $child->extends = $extends;
         }
