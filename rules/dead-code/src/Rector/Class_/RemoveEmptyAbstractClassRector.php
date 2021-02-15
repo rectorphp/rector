b154ae6a88652e53624b0752550d89aecb274f1e
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-02-15 22:54:35 +0700

    [DeadCode][WIP] Fixes #3282 Add RemoveEmptyAbstractClassRector

---
+++ rules/dead-code/src/Rector/Class_/RemoveEmptyAbstractClassRector.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\DeadCode\Rector\Class_;
+
+use PhpParser\Node;
+use PhpParser\Node\Stmt\Class_;
+use Rector\Core\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \Rector\DeadCode\Tests\Rector\Class_\RemoveEmptyAbstractClassRector\RemoveEmptyAbstractClassRectorTest
+ */
+final class RemoveEmptyAbstractClassRector extends AbstractRector
+{
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [Class_::class];
+    }
+
+    /**
+     * @param Class_ $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        return $node;
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'Empty abstract class that does nothing',
+            [
+                new CodeSample(
+<<<'CODE_SAMPLE'
+class SomeClass extends SomeAbstractClass
+{
+}
+
+abstract class SomeAbstractClass extends AnotherAbstractClass
+{
+}
+
+abstracst clas AnotherAbstractClass
+{
+     public function getName()
+     {
+          return 'cowo';
+     }
+}
+CODE_SAMPLE
+,
+<<<'CODE_SAMPLE'
+class SomeClass extends AnotherAbstractClass
+{
+}
+
+abstracst clas AnotherAbstractClass
+{
+     public function getName()
+     {
+          return 'cowo';
+     }
+}
+CODE_SAMPLE
+                ),
+
+            ]);
+    }
+}
