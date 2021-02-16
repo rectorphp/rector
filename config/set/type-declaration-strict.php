5924cf2ddda08d34ec6807ba7b49e62f960b868d
TomasVotruba <tomas.vot@gmail.com>
2021-02-16 10:54:55 +0100

    apply cs on sets

--- config/set/type-declaration-strict.php
+++ config/set/type-declaration-strict.php
@@ -2,12 +2,16 @@
 
 declare(strict_types=1);
 
-return static function (
-    \Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator $containerConfigurator
-): void {
+use Rector\TypeDeclaration\Rector\ClassMethod\ParamTypeFromStrictTypedPropertyRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictTypedPropertyRector;
+use Rector\TypeDeclaration\Rector\Closure\AddClosureReturnTypeRector;
+use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
-    $services->set(\Rector\TypeDeclaration\Rector\Closure\AddClosureReturnTypeRector::class);
-    $services->set(\Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictTypedPropertyRector::class);
-    $services->set(\Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector::class);
-    $services->set(\Rector\TypeDeclaration\Rector\ClassMethod\ParamTypeFromStrictTypedPropertyRector::class);
+    $services->set(AddClosureReturnTypeRector::class);
+    $services->set(ReturnTypeFromStrictTypedPropertyRector::class);
+    $services->set(TypedPropertyFromStrictConstructorRector::class);
+    $services->set(ParamTypeFromStrictTypedPropertyRector::class);
 };
