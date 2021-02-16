9086bd8b944f91830b5712999d014127c64d1e43
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-02-17 03:47:11 +0700

    add names property

--- packages/node-collector/src/NodeCollector/NodeRepository.php
+++ packages/node-collector/src/NodeCollector/NodeRepository.php
@@ -116,6 +116,11 @@ final class NodeRepository
      */
     private $attributes = [];
 
+    /**
+     * @var array<string, Name[]>
+     */
+    private $names = [];
+
     public function __construct(
         ArrayCallableMethodReferenceAnalyzer $arrayCallableMethodReferenceAnalyzer,
         ParsedPropertyFetchNodeCollector $parsedPropertyFetchNodeCollector,
@@ -175,7 +180,7 @@ final class NodeRepository
 
         if ($node instanceof Name) {
             $name = $this->nodeNameResolver->getName($node);
-            $this->attributes[$name][] = $node;
+            $this->names[$name][] = $node;
         }
     }
 
