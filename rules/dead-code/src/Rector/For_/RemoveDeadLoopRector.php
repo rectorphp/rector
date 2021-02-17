b5b8b8caafe9ef910a16d8606d3f0aebfdb160af
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-02-17 22:28:42 +0700

    [DeadCode] Fixes #5513 Add RemoveDeadLoopRector

---
+++ rules/dead-code/src/Rector/For_/RemoveDeadLoopRector.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\DeadCode\Rector\For_;
+
+use PhpParser\Node;
+use PhpParser\Node\Stmt\Do_;
+use PhpParser\Node\Stmt\For_;
+use PhpParser\Node\Stmt\Foreach_;
+use PhpParser\Node\Stmt\While_;
+use Rector\Core\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \Rector\DeadCode\Tests\Rector\For_\RemoveDeadLoopRector\RemoveDeadLoopRectorTest
+ */
+final class RemoveDeadLoopRector extends AbstractRector
+{
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'Remove loop with no body',
+            [
+                new CodeSample(
+                    <<<'CODE_SAMPLE'
+class SomeClass
+{
+    public function run($values)
+    {
+        for ($i=1; $i<count($values); ++$i) {
+        }
+    }
+}
+CODE_SAMPLE
+                    ,
+                    <<<'CODE_SAMPLE'
+class SomeClass
+{
+    public function run($values)
+    {
+        for ($i=1; $i<count($values); ++$i) {
+        }
+    }
+}
+CODE_SAMPLE
+                ),
+
+            ]);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getNodeTypes(): array
+    {
+        return [Do_::class, For_::class, Foreach_::class, While_::class];
+    }
+
+    /**
+     * @param Do_|For_|Foreach_|While_ $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if ((array) $node->stmts !== []) {
+            return null;
+        }
+
+        $this->removeNode($node);
+        return $node;
+    }
+}
