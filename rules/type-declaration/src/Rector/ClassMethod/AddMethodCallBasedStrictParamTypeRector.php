348b59164a4277adc65fc901e85704484ad9381f
TomasVotruba <tomas.vot@gmail.com>
2021-02-17 16:59:13 +0100

    use ->getNativeType() to get native types

--- rules/type-declaration/src/Rector/ClassMethod/AddMethodCallBasedStrictParamTypeRector.php
+++ rules/type-declaration/src/Rector/ClassMethod/AddMethodCallBasedStrictParamTypeRector.php
@@ -15,7 +15,6 @@ use PHPStan\Type\Type;
 use Rector\Core\Rector\AbstractRector;
 use Rector\NodeCollector\ValueObject\ArrayCallable;
 use Rector\NodeTypeResolver\PHPStan\Type\TypeFactory;
-use Rector\TypeDeclaration\NodeTypeAnalyzer\ParamTypeCompatibilityChecker;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
@@ -31,17 +30,9 @@ final class AddMethodCallBasedStrictParamTypeRector extends AbstractRector
      */
     private $typeFactory;
 
-    /**
-     * @var ParamTypeCompatibilityChecker
-     */
-    private $paramTypeCompatibilityChecker;
-
-    public function __construct(
-        TypeFactory $typeFactory,
-        ParamTypeCompatibilityChecker $paramTypeCompatibilityChecker
-    ) {
+    public function __construct(TypeFactory $typeFactory)
+    {
         $this->typeFactory = $typeFactory;
-        $this->paramTypeCompatibilityChecker = $paramTypeCompatibilityChecker;
     }
 
     public function getRuleDefinition(): RuleDefinition
@@ -135,16 +126,14 @@ CODE_SAMPLE
     private function getCallTypesByPosition(array $calls): array
     {
         $staticTypesByArgumentPosition = [];
+
         foreach ($calls as $call) {
             if (! $call instanceof StaticCall && ! $call instanceof MethodCall) {
                 continue;
             }
 
             foreach ($call->args as $position => $arg) {
-                $argValueType = $this->getStaticType($arg->value);
-                if (! $this->paramTypeCompatibilityChecker->isCompatibleWithParamStrictTyped($arg, $argValueType)) {
-                    continue;
-                }
+                $argValueType = $this->nodeTypeResolver->getNativeType($arg->value);
 
                 // "self" in another object is not correct, this make it independent
                 $argValueType = $this->correctSelfType($argValueType);
