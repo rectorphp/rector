2205766dbc8839f7d8548a427d689045f86a316a
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-02-21 16:33:21 +0700

    [CodeQuality] Mirror comment on InlineIfToExplicitIfRector (#5643)

--- rules/code-quality/src/Rector/Expression/InlineIfToExplicitIfRector.php
+++ rules/code-quality/src/Rector/Expression/InlineIfToExplicitIfRector.php
@@ -111,6 +111,7 @@ CODE_SAMPLE
         $if = new If_($expr);
         $if->stmts[] = new Expression($booleanExpr->right);
 
+        $this->mirrorComments($if, $expression);
         return $if;
     }
 }
