30dac31758dde054796e8460923546dad3e83153
TomasVotruba <tomas.vot@gmail.com>
2021-02-23 16:58:12 +0100

    ecs: remove already set present rules

--- ecs.php
+++ ecs.php
@@ -11,24 +11,17 @@ use PhpCsFixer\Fixer\PhpUnit\PhpUnitStrictFixer;
 use PhpCsFixer\Fixer\ReturnNotation\ReturnAssignmentFixer;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symplify\CodingStandard\Fixer\ArrayNotation\ArrayListItemNewlineFixer;
-use Symplify\CodingStandard\Fixer\ArrayNotation\ArrayOpenerAndCloserNewlineFixer;
-use Symplify\CodingStandard\Fixer\ArrayNotation\StandaloneLineInMultilineArrayFixer;
 use Symplify\CodingStandard\Fixer\Commenting\RemoveCommentedCodeFixer;
-use Symplify\CodingStandard\Fixer\LineLength\LineLengthFixer;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
-    $services->set(StandaloneLineInMultilineArrayFixer::class);
-    $services->set(ArrayOpenerAndCloserNewlineFixer::class);
 
     $services->set(GeneralPhpdocAnnotationRemoveFixer::class)
         ->call('configure', [[
             'annotations' => ['throws', 'author', 'package', 'group'],
         ]]);
-
-    $services->set(LineLengthFixer::class);
 
     $services->set(NoSuperfluousPhpdocTagsFixer::class)
         ->call('configure', [[
@@ -56,13 +49,15 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         '*/Source/*',
         '*/Fixture/*',
         '*/Expected/*',
-        # generated from /vendor
+
+        // generated from /vendor
         __DIR__ . '/packages/doctrine-annotation-generated/src/ConstantPreservingDocParser.php',
         __DIR__ . '/packages/doctrine-annotation-generated/src/ConstantPreservingAnnotationReader.php',
         // template files
         __DIR__ . '/packages/rector-generator/templates',
 
         UnaryOperatorSpacesFixer::class,
+
         // buggy with specific markdown snippet file in docs/rules_overview.md
         ArrayListItemNewlineFixer::class,
         BlankLineAfterOpeningTagFixer::class,
@@ -73,7 +68,9 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         // breaks on-purpose annotated variables
         ReturnAssignmentFixer::class,
 
+        // buggy on "Float" class
         PhpdocTypesFixer::class => [__DIR__ . '/rules/php74/src/Rector/Double/RealToFloatTypeCastRector.php'],
+
         PhpUnitStrictFixer::class => [
             __DIR__ . '/packages/better-php-doc-parser/tests/PhpDocInfo/PhpDocInfo/PhpDocInfoTest.php',
             __DIR__ . '/tests/PhpParser/Node/NodeFactoryTest.php',
