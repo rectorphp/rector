3093fabacb1178f9021dc9b238596d137045b2ad
TomasVotruba <tomas.vot@gmail.com>
2021-02-23 12:14:37 +0100

    update monorepo-builder syntax

--- changelog-linker.php
+++ changelog-linker.php
@@ -3,9 +3,12 @@
 declare(strict_types=1);
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\ChangelogLinker\ValueObject\ChangelogFormat;
 use Symplify\ChangelogLinker\ValueObject\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
+
     $parameters->set(Option::AUTHORS_TO_IGNORE, ['TomasVotruba']);
+    $parameters->set(Option::CHANGELOG_FORMAT, ChangelogFormat::CATEGORIES_ONLY);
 };
