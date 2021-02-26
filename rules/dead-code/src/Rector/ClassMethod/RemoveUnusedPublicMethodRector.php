10cb730c809a2903e9c60c9790a0ff28fa69f805
TomasVotruba <tomas.vot@gmail.com>
2021-02-26 11:31:18 +0100

    [DeadCode] Skip RemoveUnusedPublicMethodRector for test

--- rules/dead-code/src/Rector/ClassMethod/RemoveUnusedPublicMethodRector.php
+++ rules/dead-code/src/Rector/ClassMethod/RemoveUnusedPublicMethodRector.php
@@ -83,15 +83,7 @@ CODE_SAMPLE
      */
     public function refactor(Node $node): ?Node
     {
-        if ($this->isOpenSourceProjectType()) {
-            return null;
-        }
-
-        if (! $node->isPublic()) {
-            return null;
-        }
-
-        if ($this->isNames($node, ObjectMagicMethods::METHOD_NAMES)) {
+        if ($this->shouldSkip($node)) {
             return null;
         }
 
@@ -113,5 +105,26 @@ CODE_SAMPLE
 
         $this->removeNode($node);
         return $node;
+    }
+
+    private function shouldSkip(ClassMethod $classMethod): bool
+    {
+        if ($this->isOpenSourceProjectType()) {
+            return true;
+        }
+
+        if (! $classMethod->isPublic()) {
+            return true;
+        }
+
+        if ($this->isNames($classMethod, ObjectMagicMethods::METHOD_NAMES)) {
+            return true;
+        }
+
+        if ($this->isNames($classMethod, ['test'])) {
+            return true;
+        }
+
+        return false;
     }
 }
