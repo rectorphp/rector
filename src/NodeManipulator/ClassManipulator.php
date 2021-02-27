05188ec14d5ef5fa3ff6aa2c562bca40b6ca527c
kaizen-ci <info@kaizen-ci.org>
2021-02-27 00:47:00 +0000

    [ci-review] Rector Rectify

--- src/NodeManipulator/ClassManipulator.php
+++ src/NodeManipulator/ClassManipulator.php
@@ -12,9 +12,7 @@ use PhpParser\Node\Stmt\ClassMethod;
 use PhpParser\Node\Stmt\Interface_;
 use PhpParser\Node\Stmt\Property;
 use PhpParser\Node\Stmt\Trait_;
-use Rector\NodeCollector\NodeCollector\NodeRepository;
 use Rector\NodeNameResolver\NodeNameResolver;
-use Rector\NodeTypeResolver\NodeTypeResolver;
 use Rector\PostRector\Collector\NodesToRemoveCollector;
 
 final class ClassManipulator
@@ -25,30 +23,16 @@ final class ClassManipulator
     private $nodeNameResolver;
 
     /**
-     * @var NodeTypeResolver
-     */
-    private $nodeTypeResolver;
-
-    /**
      * @var NodesToRemoveCollector
      */
     private $nodesToRemoveCollector;
 
-    /**
-     * @var NodeRepository
-     */
-    private $nodeRepository;
-
     public function __construct(
         NodeNameResolver $nodeNameResolver,
-        NodeTypeResolver $nodeTypeResolver,
-        NodesToRemoveCollector $nodesToRemoveCollector,
-        NodeRepository $nodeRepository
+        NodesToRemoveCollector $nodesToRemoveCollector
     ) {
         $this->nodeNameResolver = $nodeNameResolver;
-        $this->nodeTypeResolver = $nodeTypeResolver;
         $this->nodesToRemoveCollector = $nodesToRemoveCollector;
-        $this->nodeRepository = $nodeRepository;
     }
 
     /**
