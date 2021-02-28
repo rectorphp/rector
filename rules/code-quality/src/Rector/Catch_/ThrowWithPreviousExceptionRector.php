7215d103f0b7b64a6e81b6abfe3ed66a7bf57edb
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-02-28 10:25:35 +0700

    ensure named "previous"

--- rules/code-quality/src/Rector/Catch_/ThrowWithPreviousExceptionRector.php
+++ rules/code-quality/src/Rector/Catch_/ThrowWithPreviousExceptionRector.php
@@ -10,6 +10,7 @@ use PhpParser\Node\Arg;
 use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Expr\New_;
 use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Identifier;
 use PhpParser\Node\Name;
 use PhpParser\Node\Stmt\Catch_;
 use PhpParser\Node\Stmt\Throw_;
@@ -142,8 +143,8 @@ CODE_SAMPLE
             $new->args[1] = new Arg(new MethodCall($catchedThrowableVariable, 'getCode'));
         }
 
-        if (isset($new->args[1]->name)) {
-            $arg1 = $new->args[1];
+        $arg1 = $new->args[1];
+        if (isset($arg1->name) && $arg1->name instanceof Identifier && $arg1->name->toString() === 'previous') {
             $new->args[1] = new Arg(new MethodCall($catchedThrowableVariable, 'getCode'));
             $new->args[$exceptionArgumentPosition] = $arg1;
         } else {
