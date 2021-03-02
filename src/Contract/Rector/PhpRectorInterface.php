f9143bbf8572bf74f8ba29be4e1d0b5b257b163d
Matthias Noback <matthiasnoback@gmail.com>
2021-03-02 11:56:55 +0100

    Provide the correct URL for the nodes overview

--- src/Contract/Rector/PhpRectorInterface.php
+++ src/Contract/Rector/PhpRectorInterface.php
@@ -11,7 +11,7 @@ interface PhpRectorInterface extends NodeVisitor, RectorInterface
 {
     /**
      * List of nodes this class checks, classes that implements \PhpParser\Node
-     * See beautiful map of all nodes https://github.com/rectorphp/rector/blob/master/docs/nodes_overview.md
+     * See beautiful map of all nodes https://github.com/rectorphp/php-parser-nodes-docs
      *
      * @return array<class-string<Node>>
      */
