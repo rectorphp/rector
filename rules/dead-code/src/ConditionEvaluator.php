1647b19c1574ac6c2cf75e572eef17c8bb3e001e
TomasVotruba <tomas.vot@gmail.com>
2021-03-02 13:55:57 +0100

    bump deps

--- rules/dead-code/src/ConditionEvaluator.php
+++ rules/dead-code/src/ConditionEvaluator.php
@@ -37,10 +37,17 @@ final class ConditionEvaluator
     private function evaluateVersionCompareCondition(VersionCompareCondition $versionCompareCondition)
     {
         $compareSign = $versionCompareCondition->getCompareSign();
+        if ($compareSign !== null) {
+            return version_compare(
+                (string) $versionCompareCondition->getFirstVersion(),
+                (string) $versionCompareCondition->getSecondVersion(),
+                $compareSign
+            );
+        }
+
         return version_compare(
             (string) $versionCompareCondition->getFirstVersion(),
-            (string) $versionCompareCondition->getSecondVersion(),
-            $compareSign
+            (string) $versionCompareCondition->getSecondVersion()
         );
     }
 
