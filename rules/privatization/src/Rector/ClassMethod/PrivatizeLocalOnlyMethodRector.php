7ab94b00506e50667e0839cdb893e848cd301c19
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-03-03 14:07:46 +0700

    Revert "try use Strings::endsWith($name, Action)"
    This reverts commit 5cc41f7fe7e937c87a68f5abd50cfad3150f99b1.

--- rules/privatization/src/Rector/ClassMethod/PrivatizeLocalOnlyMethodRector.php
+++ rules/privatization/src/Rector/ClassMethod/PrivatizeLocalOnlyMethodRector.php
@@ -6,6 +6,7 @@ namespace Rector\Privatization\Rector\ClassMethod;
 
 use Nette\Utils\Strings;
 use PhpParser\Node;
+use PhpParser\Node\Name;
 use PhpParser\Node\Stmt\Class_;
 use PhpParser\Node\Stmt\ClassMethod;
 use PHPStan\Type\ObjectType;
@@ -13,12 +14,14 @@ use Rector\AttributeAwarePhpDoc\Ast\PhpDoc\SymfonyRequiredTagNode;
 use Rector\BetterPhpDocParser\PhpDocInfo\PhpDocInfo;
 use Rector\BetterPhpDocParser\ValueObject\PhpDocNode\ApiPhpDocTagNode;
 use Rector\BetterPhpDocParser\ValueObject\PhpDocNode\Nette\NetteInjectTagNode;
+use Rector\BetterPhpDocParser\ValueObject\PhpDocNode\Symfony\SymfonyRouteTagValueNode;
 use Rector\Caching\Contract\Rector\ZeroCacheRectorInterface;
 use Rector\Core\Rector\AbstractRector;
 use Rector\Doctrine\PhpDocParser\DoctrineDocBlockResolver;
 use Rector\NodeTypeResolver\Node\AttributeKey;
 use Rector\Privatization\NodeAnalyzer\ClassMethodExternalCallNodeAnalyzer;
 use Rector\VendorLocker\NodeVendorLocker\ClassMethodVisibilityVendorLockResolver;
+use Symfony\Component\Routing\Annotation\Route;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
@@ -235,7 +238,23 @@ CODE_SAMPLE
             return true;
         }
 
-        $name = $this->getName($classMethod);
-        return Strings::endsWith($name, 'Action');
+        if ($this->hasSymfonyRouteAttrGroup($classMethod)) {
+            return true;
+        }
+
+        return $phpDocInfo->hasByType(SymfonyRouteTagValueNode::class);
+    }
+
+    private function hasSymfonyRouteAttrGroup(ClassMethod $classMethod): bool
+    {
+        foreach ($classMethod->attrGroups as $attrGroup) {
+            foreach ($attrGroup->attrs as $attr) {
+                if ($attr->name->toString() === Route::class) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
