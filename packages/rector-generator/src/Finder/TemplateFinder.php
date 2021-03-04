68c258592bb7b31f58716e8fb20a3ce018da6767
TomasVotruba <tomas.vot@gmail.com>
2021-03-04 01:23:23 +0100

    update paths

--- packages/rector-generator/src/Finder/TemplateFinder.php
+++ packages/rector-generator/src/Finder/TemplateFinder.php
@@ -66,9 +66,9 @@ final class TemplateFinder
             $filePaths[] = __DIR__ . '/../../templates/rules/__package__/src/Rector/__Category__/__Configured__Name__.php';
 
             if ($rectorRecipe->getExtraFileContent()) {
-                $filePaths[] = __DIR__ . '/../../templates/rules/__package__/tests/Rector/__Category__/__Name__/__Configured__Extra__Name__Test.php.inc';
+                $filePaths[] = __DIR__ . '/../../templates/rules/__package__/tests/Rector/__Category__/__Name__/__Extra__Name__Test.php.inc';
             } else {
-                $filePaths[] = __DIR__ . '/../../templates/rules/__package__/tests/Rector/__Category__/__Name__/__Configured__Name__Test.php.inc';
+                $filePaths[] = __DIR__ . '/../../templates/rules/__package__/tests/Rector/__Category__/__Name__/__Name__Test.php.inc';
             }
 
             return $filePaths;
