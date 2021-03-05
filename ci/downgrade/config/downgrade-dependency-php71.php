33eb27a12f2f824bf5ff056964803d714b9650ea
TomasVotruba <tomas.vot@gmail.com>
2021-03-05 13:15:35 +0100

    [downgrade] simplify to 2 configs

---
+++ ci/downgrade/config/downgrade-dependency-php71.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Core\Configuration\Option;
+use Rector\Set\ValueObject\DowngradeSetList;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+
+    $parameters->set(Option::SKIP, DowngradeRectorConfig::DEPENDENCY_EXCLUDE_PATHS);
+
+    $parameters->set(Option::SETS, [
+        DowngradeSetList::PHP_80,
+        DowngradeSetList::PHP_74,
+        DowngradeSetList::PHP_73,
+        DowngradeSetList::PHP_72,
+    ]);
+};
