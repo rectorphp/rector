4cd387ee718e42928cf8d616147c13c0eef8da1b
TomasVotruba <tomas.vot@gmail.com>
2021-03-13 21:48:07 +0100

    use config

--- tests/PhpParser/Printer/CommentPreserving/CommentPreservingTest.php
+++ tests/PhpParser/Printer/CommentPreserving/CommentPreservingTest.php
@@ -27,8 +27,8 @@ final class CommentPreservingTest extends AbstractRectorTestCase
         return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
     }
 
-    protected function getRectorClass(): string
+    public function provideConfigFilePath(): string
     {
-        return TypedPropertyRector::class;
+        return __DIR__ . '/config/configured_rule.php';
     }
 }
