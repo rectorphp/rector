66925a996a83d6b2faf0eaa2ebc4956478d53840
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-03-15 02:43:52 +0700

    try back to check PrettyVersions only

--- scoper.php
+++ scoper.php
@@ -32,11 +32,10 @@ return [
         // fixes https://github.com/rectorphp/rector-prefixed/runs/2103759172
         // and https://github.com/rectorphp/rector-prefixed/blob/0cc433e746b645df5f905fa038573c3a1a9634f0/vendor/jean85/pretty-package-versions/src/PrettyVersions.php#L6
         function (string $filePath, string $prefix, string $content): string {
-            if (getenv('IMAGES') !== 'docker') {
+            if (! Strings::endsWith($filePath, 'vendor/jean85/pretty-package-versions/src/PrettyVersions.php')) {
                 return $content;
             }
 
-            // return Composer\InstalledVersions;
             // see https://regex101.com/r/v8zRMm/1
             return Strings::replace($content, '#' . $prefix . '\\\\Composer\\\\InstalledVersions#', 'Composer\InstalledVersions');
         },
