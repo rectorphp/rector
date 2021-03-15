fa5ab63f572d9c1a5146f7407451adfcbc8b895b
TomasVotruba <tomas.vot@gmail.com>
2021-03-15 23:43:45 +0100

    remove dead-code-strict [skip ci]

--- packages/Set/ValueObject/SetList.php
+++ packages/Set/ValueObject/SetList.php
@@ -101,11 +101,6 @@ final class SetList implements SetListInterface
     /**
      * @var string
      */
-    public const DEAD_CODE_STRICT = __DIR__ . '/../../../config/set/dead-code-strict.php';
-
-    /**
-     * @var string
-     */
     public const DOCTRINE_25 = __DIR__ . '/../../../config/set/doctrine25.php';
 
     /**
