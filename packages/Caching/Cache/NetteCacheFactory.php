7ec9adb79c41b0290baf92b2f480ad53f44cbe9f
TomasVotruba <tomas.vot@gmail.com>
2021-03-18 02:07:29 +0100

    make sure cache directory exists

--- packages/Caching/Cache/NetteCacheFactory.php
+++ packages/Caching/Cache/NetteCacheFactory.php
@@ -9,6 +9,7 @@ use Nette\Caching\Storages\FileStorage;
 use Nette\Utils\Strings;
 use Rector\Core\Configuration\Option;
 use Symplify\PackageBuilder\Parameter\ParameterProvider;
+use Symplify\SmartFileSystem\SmartFileSystem;
 
 final class NetteCacheFactory
 {
@@ -17,14 +18,26 @@ final class NetteCacheFactory
      */
     private $parameterProvider;
 
-    public function __construct(ParameterProvider $parameterProvider)
+    /**
+     * @var SmartFileSystem
+     */
+    private $smartFileSystem;
+
+    public function __construct(ParameterProvider $parameterProvider, SmartFileSystem $smartFileSystem)
     {
         $this->parameterProvider = $parameterProvider;
+        $this->smartFileSystem = $smartFileSystem;
     }
 
     public function create(): Cache
     {
         $cacheDirectory = $this->parameterProvider->provideStringParameter(Option::CACHE_DIR);
+
+        // ensure cache directory exists
+        if (! $this->smartFileSystem->exists($cacheDirectory)) {
+            $this->smartFileSystem->mkdir($cacheDirectory);
+        }
+
         $fileStorage = new FileStorage($cacheDirectory);
 
         // namespace is unique per project
