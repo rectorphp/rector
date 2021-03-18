2500cbf8d1c763840e7a0b6fe02a2a8ebc3717f4
TomasVotruba <tomas.vot@gmail.com>
2021-03-19 00:15:52 +0100

    add vendor deps locatoins

--- config/config.php
+++ config/config.php
@@ -10,7 +10,10 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/services-packages.php');
     $containerConfigurator->import(__DIR__ . '/parameters.php');
 
-    $containerConfigurator->import(__DIR__ . '/../vendor/rector/rector-symfony/config/config.php');
+    // rector root
+    $containerConfigurator->import(__DIR__ . '/../vendor/rector/rector-symfony/config/config.php', null, 'not_found');
+    // rector sub-package
+    $containerConfigurator->import(__DIR__ . '/../../rector-symfony/config/config.php', null, 'not_found');
 
     // require only in dev
     $containerConfigurator->import(__DIR__ . '/../utils/compiler/config/config.php', null, 'not_found');
