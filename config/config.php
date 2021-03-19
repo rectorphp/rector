056ec7e62a83f7088a9deda0d2d05593b8ba5e85
TomasVotruba <tomas.vot@gmail.com>
2021-03-19 22:34:04 +0100

    add dev configs

--- config/config.php
+++ config/config.php
@@ -17,16 +17,24 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/../vendor/rector/rector-nette/config/config.php', null, 'not_found');
     $containerConfigurator->import(__DIR__ . '/../vendor/rector/rector-laravel/config/config.php', null, 'not_found');
     $containerConfigurator->import(__DIR__ . '/../vendor/rector/rector-phpunit/config/config.php', null, 'not_found');
+
     // rector sub-package
     $containerConfigurator->import(__DIR__ . '/../../rector-symfony/config/config.php', null, 'not_found');
     $containerConfigurator->import(__DIR__ . '/../../rector-nette/config/config.php', null, 'not_found');
     $containerConfigurator->import(__DIR__ . '/../../rector-laravel/config/config.php', null, 'not_found');
     $containerConfigurator->import(__DIR__ . '/../../rector-phpunit/config/config.php', null, 'not_found');
 
-    $containerConfigurator->import(__DIR__ . '/../vendor/symplify/console-color-diff/config/config.php');
-    $containerConfigurator->import(__DIR__ . '/../vendor/symplify/composer-json-manipulator/config/config.php');
-    $containerConfigurator->import(__DIR__ . '/../vendor/symplify/skipper/config/config.php');
-    $containerConfigurator->import(__DIR__ . '/../vendor/symplify/simple-php-doc-parser/config/config.php');
+    // rector root
+    $containerConfigurator->import(__DIR__ . '/../vendor/symplify/console-color-diff/config/config.php', null, 'not_found');
+    $containerConfigurator->import(__DIR__ . '/../vendor/symplify/composer-json-manipulator/config/config.php', null, 'not_found');
+    $containerConfigurator->import(__DIR__ . '/../vendor/symplify/skipper/config/config.php', null, 'not_found');
+    $containerConfigurator->import(__DIR__ . '/../vendor/symplify/simple-php-doc-parser/config/config.php', null, 'not_found');
+
+    // rector sub-package
+    $containerConfigurator->import(__DIR__ . '/../../../symplify/console-color-diff/config/config.php', null, 'not_found');
+    $containerConfigurator->import(__DIR__ . '/../../../symplify/composer-json-manipulator/config/config.php');
+    $containerConfigurator->import(__DIR__ . '/../../../symplify/skipper/config/config.php');
+    $containerConfigurator->import(__DIR__ . '/../../../symplify/simple-php-doc-parser/config/config.php');
 
     // only for dev
     if (class_exists(RectorGeneratorBundle::class)) {
