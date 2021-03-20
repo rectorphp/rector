ed44dd9580f74d2a94eb69a6ec0d93a16cbb7609
TomasVotruba <tomas.vot@gmail.com>
2021-03-20 16:39:37 +0100

    remove commented code

--- rules-tests/Renaming/Rector/Name/RenameClassRector/config/auto_import_names.php
+++ rules-tests/Renaming/Rector/Name/RenameClassRector/config/auto_import_names.php
@@ -17,14 +17,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::AUTO_IMPORT_NAMES, true);
 
-//    $parameters->set(Option::SKIP, [
-//        NameImportingPostRector::class,
-//        UseAddingPostRector::class,
-//        NodeAddingPostRector::class,
-//    ]);
-
     $services = $containerConfigurator->services();
-
     $services->set(RenameClassRector::class)
         ->call('configure', [[
             RenameClassRector::OLD_TO_NEW_CLASSES => [
