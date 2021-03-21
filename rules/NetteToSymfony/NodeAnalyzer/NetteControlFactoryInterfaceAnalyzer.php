747104df6b81939b41cca9ee8edb985f1a2ecaa0
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-03-21 19:12:35 +0700

    Refactor is_a (#5907)
    * Change is_a to UnionType()->isSuperTypeOf()->yes in NetteControlFactoryInterfaceAnalyzer
    
    * fix with check ShortenedObjectType
    
    * change is_a in ThrowWithPreviousExceptionRector

--- rules/NetteToSymfony/NodeAnalyzer/NetteControlFactoryInterfaceAnalyzer.php
+++ rules/NetteToSymfony/NodeAnalyzer/NetteControlFactoryInterfaceAnalyzer.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace Rector\NetteToSymfony\NodeAnalyzer;
 
 use PhpParser\Node\Stmt\Interface_;
+use PHPStan\Type\ObjectType;
 use PHPStan\Type\TypeWithClassName;
-use Rector\NodeTypeResolver\NodeTypeResolver;
+use PHPStan\Type\UnionType;
+use Rector\StaticTypeMapper\ValueObject\Type\ShortenedObjectType;
 use Rector\TypeDeclaration\TypeInferer\ReturnTypeInferer;
 
 final class NetteControlFactoryInterfaceAnalyzer
@@ -16,15 +18,9 @@ final class NetteControlFactoryInterfaceAnalyzer
      */
     private $returnTypeInferer;
 
-    /**
-     * @var NodeTypeResolver
-     */
-    private $nodeTypeResolver;
-
-    public function __construct(ReturnTypeInferer $returnTypeInferer, NodeTypeResolver $nodeTypeResolver)
+    public function __construct(ReturnTypeInferer $returnTypeInferer)
     {
         $this->returnTypeInferer = $returnTypeInferer;
-        $this->nodeTypeResolver = $nodeTypeResolver;
     }
 
     /**
@@ -38,12 +34,16 @@ final class NetteControlFactoryInterfaceAnalyzer
                 return false;
             }
 
-            $className = $this->nodeTypeResolver->getFullyQualifiedClassName($returnType);
-            if (is_a($className, 'Nette\Application\UI\Control', true)) {
-                return true;
+            $controlOrForm = new UnionType([
+                new ObjectType('Nette\Application\UI\Control'),
+                new ObjectType('Nette\Application\UI\Form'),
+            ]);
+
+            if ($returnType instanceof ShortenedObjectType) {
+                $returnType = new ObjectType($returnType->getFullyQualifiedName());
             }
 
-            if (is_a($className, 'Nette\Application\UI\Form', true)) {
+            if ($controlOrForm->isSuperTypeOf($returnType)->yes()) {
                 return true;
             }
         }
