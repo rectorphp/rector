3b502b8a7b1d5bb83a8cb45e7b6d335bfa78c9f4
Michal Lulco <lulco@efabrica.sk>
2021-03-24 01:26:55 +0100

    Added league event to set list

--- packages/Set/ValueObject/SetList.php
+++ packages/Set/ValueObject/SetList.php
@@ -323,6 +323,11 @@ final class SetList implements SetListInterface
     /**
      * @var string
      */
+    public const LEAGUE_EVENT_30 = __DIR__ . '/../../../config/set/league-event-30.php';
+
+    /**
+     * @var string
+     */
     public const MONOLOG_20 = __DIR__ . '/../../../config/set/monolog20.php';
 
     /**
