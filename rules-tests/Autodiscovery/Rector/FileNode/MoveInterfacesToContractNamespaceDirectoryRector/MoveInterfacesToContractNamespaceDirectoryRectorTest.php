8613732966c7286b909f139d79f8683318dabae6
kaizen-ci <info@kaizen-ci.org>
2021-04-04 12:12:30 +0000

    [ci-review] Rector Rectify

--- rules-tests/Autodiscovery/Rector/FileNode/MoveInterfacesToContractNamespaceDirectoryRector/MoveInterfacesToContractNamespaceDirectoryRectorTest.php
+++ rules-tests/Autodiscovery/Rector/FileNode/MoveInterfacesToContractNamespaceDirectoryRector/MoveInterfacesToContractNamespaceDirectoryRectorTest.php
@@ -14,6 +14,7 @@ final class MoveInterfacesToContractNamespaceDirectoryRectorTest extends Abstrac
 {
     /**
      * @requires PHP 7.4
+     *
      * @dataProvider provideData()
      * @param SmartFileInfo[] $extraFileInfos
      */
