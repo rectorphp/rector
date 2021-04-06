7bd5ac80cf8bcbcec207b115295531c010d61995
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-04-06 12:01:04 +0700

    [CodeQuality] [WIP] Fixes #5712 Add SimplifyIfNullableReturnRector

---
+++ rules/CodeQuality/Rector/If_/SimplifyIfNullableReturnRector.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\CodeQuality\Rector\If_;
+
+use PhpParser\Node;
+use PhpParser\Node\Stmt\If_;
+use Rector\Core\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \Rector\Tests\CodeQuality\Rector\If_\SimplifyIfNullableReturnRector\SimplifyIfNullableReturnRectorTest
+ */
+final class SimplifyIfNullableReturnRector extends AbstractRector
+{
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition('Direct return on if nullable check before return', [
+            new CodeSample(
+                <<<'CODE_SAMPLE'
+class SomeClass
+{
+    public function run()
+    {
+        $property = $this->underscoreCamelCasePropertyRenamer->rename($propertyRename);
+        if (! $property instanceof Property) {
+            return null;
+        }
+
+        return $property;
+    }
+}
+CODE_SAMPLE
+                ,
+                <<<'CODE_SAMPLE'
+class SomeClass
+{
+    public function run()
+    {
+        return $this->underscoreCamelCasePropertyRenamer->rename($propertyRename);
+    }
+}
+CODE_SAMPLE
+            ),
+        ]);
+    }
+
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [If_::class];
+    }
+
+    /**
+     * @param If_ $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        return $node;
+    }
+}
