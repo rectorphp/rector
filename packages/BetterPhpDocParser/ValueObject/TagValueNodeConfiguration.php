126da19c860836318fe05a3a8675b56efe14764d
TomasVotruba <tomas.vot@gmail.com>
2021-04-07 14:03:41 +0200

    deprecated tag value node configuration

--- packages/BetterPhpDocParser/ValueObject/TagValueNodeConfiguration.php
+++ packages/BetterPhpDocParser/ValueObject/TagValueNodeConfiguration.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Rector\BetterPhpDocParser\ValueObject;
 
+/**
+ * @deprecated This should be removed soon
+ */
 final class TagValueNodeConfiguration
 {
     /**
