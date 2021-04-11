6a167fd76aff957930f9baf07ac7a8326a9af2f4
Michal Lulco <lulco@efabrica.sk>
2021-04-11 14:09:33 +0200

    Removed todo

--- src/NonPhpFile/NeonMethodCallRenamer.php
+++ src/NonPhpFile/NeonMethodCallRenamer.php
@@ -8,9 +8,6 @@ use Rector\Core\ValueObject\NonPhpFile\NonPhpFileChange;
 use Rector\Renaming\Configuration\MethodCallRenameCollector;
 use Symplify\SmartFileSystem\SmartFileInfo;
 
-/**
- * @todo maybe it should be in rector-nette package, this is just prototype
- */
 class NeonMethodCallRenamer implements FileProcessorInterface
 {
     /**
@@ -30,11 +27,9 @@ class NeonMethodCallRenamer implements FileProcessorInterface
         foreach ($this->methodCallRenameCollector->getMethodCallRenames() as $methodCallRename) {
             $oldObjectType = $methodCallRename->getOldObjectType();
 
-            // TODO potrebujeme vsetkych childov tejto classy - v rename method rectorovi moze byt interface, ale v config.neon uz nejaka implementacia
             $className = str_replace('\\', '\\\\', $oldObjectType->getClassName());
             $oldMethodName = $methodCallRename->getOldMethod();
 
-            // @see https://regex101.com/r/9xA96W/1 TODO service moze byt bez ale aj so zatvorkami, mozno aj metoda bez parametrov moze byt bez zatvoriek, treba zistit
             $pattern = '/\n(.*?)(class|factory): ' . $className . '\n\1setup:(.*?)- ' . $oldMethodName . '\(/s';
             while (preg_match($pattern, $newContent, $matches)) {
                 $replacedMatch = str_replace($methodCallRename->getOldMethod(), $methodCallRename->getNewMethod(), $matches[0]);
