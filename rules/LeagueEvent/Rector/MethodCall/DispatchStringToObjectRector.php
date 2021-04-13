22d35689bf98f80726acad5e2e391f782db1dea7
Michal Lulco <lulco@efabrica.sk>
2021-04-13 19:14:34 +0200

    [League event] Dispatch only strings to anonymous class (#6120)
    * [League event] Fix: dispatch only strings to anonymous class
    
    * [League event] Dispatch only strings to anonymous class
    
    * Fixed phpstan

--- rules/LeagueEvent/Rector/MethodCall/DispatchStringToObjectRector.php
+++ rules/LeagueEvent/Rector/MethodCall/DispatchStringToObjectRector.php
@@ -5,14 +5,21 @@ namespace Rector\LeagueEvent\Rector\MethodCall;
 use PhpParser\Node;
 use PhpParser\Node\Arg;
 use PhpParser\Node\Expr;
+use PhpParser\Node\Expr\Assign;
 use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Expr\New_;
+use PhpParser\Node\Expr\Variable;
 use PhpParser\Node\Name\FullyQualified;
+use PhpParser\Node\Param;
 use PhpParser\Node\Stmt;
 use PhpParser\Node\Stmt\Class_;
 use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\Node\Stmt\Expression;
+use PhpParser\Node\Stmt\Property;
+use PhpParser\Node\Stmt\PropertyProperty;
 use PhpParser\Node\Stmt\Return_;
 use PHPStan\Type\ObjectType;
+use PHPStan\Type\StringType;
 use Rector\Core\Rector\AbstractRector;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
@@ -90,10 +97,14 @@ CODE_SAMPLE
             return true;
         }
 
-        return ! $this->nodeTypeResolver->isObjectTypes($methodCall->var, [
+        if (! $this->nodeTypeResolver->isObjectTypes($methodCall->var, [
             new ObjectType('League\Event\EventDispatcher'),
             new ObjectType('League\Event\Emitter'),
-        ]);
+        ])) {
+            return true;
+        }
+
+        return ! $this->getStaticType($methodCall->args[0]->value) instanceof StringType;
     }
 
     private function updateNode(MethodCall $methodCall): MethodCall
@@ -108,21 +119,42 @@ CODE_SAMPLE
 
         return new New_(new Class_(null, [
             'implements' => $implements,
-            'stmts' => $this->createAnonymousEventClassBody($expr),
-        ]));
+            'stmts' => $this->createAnonymousEventClassBody(),
+        ]), [new Arg($expr)]);
     }
 
     /**
      * @return Stmt[]
      */
-    private function createAnonymousEventClassBody(Expr $expr): array
+    private function createAnonymousEventClassBody(): array
     {
         return [
+            new Property(Class_::MODIFIER_PRIVATE, [new PropertyProperty('name')]),
+            new ClassMethod('__construct', [
+                'flags' => Class_::MODIFIER_PUBLIC,
+                'params' => $this->createConstructParams(),
+                'stmts' => [new Expression($this->createConstructAssign())],
+            ]),
             new ClassMethod('eventName', [
                 'flags' => Class_::MODIFIER_PUBLIC,
                 'returnType' => 'string',
-                'stmts' => [new Return_($expr)],
+                'stmts' => [new Return_(new Variable('this->name'))],
             ]),
         ];
+    }
+
+    /**
+     * @return Param[]
+     */
+    private function createConstructParams(): array
+    {
+        return [
+            new Param(new Variable('name'), null, 'string')
+        ];
+    }
+
+    private function createConstructAssign(): Assign
+    {
+        return new Assign(new Variable('this->name'), new Variable('name'));
     }
 }
