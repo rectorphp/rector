43acf4ee32a88c137940d90519fc413ee612c169
TomasVotruba <tomas.vot@gmail.com>
2021-04-14 23:01:12 +0200

    [Downgrade 72] Add DowngradeStreamIsattyRector

---
+++ rules-tests/Downgrade72/Rector/FuncCall/DowngradeStreamIsattyRector/DowngradeStreamIsattyRectorTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Tests\Downgrade72\Rector\FuncCall\DowngradeStreamIsattyRector;
+
+use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class DowngradeStreamIsattyRectorTest extends AbstractRectorTestCase
+{
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(SmartFileInfo $fileInfo): void
+    {
+        $this->doTestFileInfo($fileInfo);
+    }
+
+    public function provideData(): \Iterator
+    {
+        return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
+    }
+
+    public function provideConfigFilePath(): string
+    {
+        return __DIR__ . '/config/configured_rule.php';
+    }
+}
