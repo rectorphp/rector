bd2c7aee9f7e2224296429de32ff6d57e1fb8ddb
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-04-15 00:55:20 +0700

    [DowngradePhp70] Add DowngradeAnonymousClassRector (#6122)
    * [DowngradePhp70] [WIP] Add DowngradeAnonymousClassRector
    
    * typo
    
    * add test
    
    * check is anonymous
    
    * adding new Anonymous class before class
    
    * anonymous class must have New_ parent
    
    * getting args
    
    * without param
    
    * with args fixture
    
    * adding with args
    
    * debug
    
    * with args
    
    * getting args from next attribute
    
    * try move to New_
    
    * skip not anonymous
    
    * try using New_
    
    * [ci-review] Rector Rectify
    
    * it works with New_ node
    
    * with addNodeBeforeNode()
    
    * class exists fixture
    
    * adding increment class if exists
    
    * implemented increment class name for inside class
    
    * handle no namespace
    
    * in function
    
    * in function no namespace
    
    * works
    
    * register to downgrade php70 set
    
    * [ci-review] Rector Rectify
    
    * typo fix
    
    * phpstan
    
    * [ci-review] Rector Rectify
    
    * add handle in closure
    
    Co-authored-by: kaizen-ci <info@kaizen-ci.org>

--- rules-tests/DowngradePhp70/Rector/New_/DowngradeAnonymousClassRector/config/php_70.php
+++ rules-tests/DowngradePhp70/Rector/New_/DowngradeAnonymousClassRector/config/php_70.php
@@ -3,18 +3,14 @@
 declare(strict_types=1);
 
 use Rector\Core\Configuration\Option;
-use Rector\Core\ValueObject\PhpVersion;
-use Rector\DowngradePhp70\Rector\Declare_\DowngradeStrictTypeDeclarationRector;
-use Rector\DowngradePhp70\Rector\FunctionLike\DowngradeTypeDeclarationRector;
+use Rector\Core\ValueObject\PhpVersionFeature;
 use Rector\DowngradePhp70\Rector\New_\DowngradeAnonymousClassRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_56);
+    $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersionFeature::SCALAR_TYPES - 1);
 
     $services = $containerConfigurator->services();
-    $services->set(DowngradeTypeDeclarationRector::class);
-    $services->set(DowngradeStrictTypeDeclarationRector::class);
     $services->set(DowngradeAnonymousClassRector::class);
 };
