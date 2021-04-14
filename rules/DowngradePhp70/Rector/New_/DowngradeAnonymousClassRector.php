057fc337ef9b92d0366408dc92f09a2a05a17256
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-04-14 22:54:04 +0700

    in function

--- rules/DowngradePhp70/Rector/New_/DowngradeAnonymousClassRector.php
+++ rules/DowngradePhp70/Rector/New_/DowngradeAnonymousClassRector.php
@@ -8,6 +8,7 @@ use PhpParser\Node;
 use PhpParser\Node\Expr\New_;
 use PhpParser\Node\Name;
 use PhpParser\Node\Stmt\Class_;
+use PhpParser\Node\Stmt\Function_;
 use Rector\Core\Rector\AbstractRector;
 use Rector\NodeTypeResolver\Node\AttributeKey;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
@@ -21,7 +22,7 @@ final class DowngradeAnonymousClassRector extends AbstractRector
     /**
      * @var string
      */
-    private const CLASS_NAME = 'Anonymous';
+    private const CLASS_NAME = 'AnonymousFor_';
 
     /**
      * @return array<class-string<Node>>
@@ -82,36 +83,65 @@ CODE_SAMPLE
 
         $classNode = $this->betterNodeFinder->findParentType($node, Class_::class);
         if ($classNode instanceof Class_) {
-            return $this->procesMoveAnonymousClass($node, $classNode);
+            return $this->procesMoveAnonymousClassInClass($node, $classNode);
+        }
+
+        $functionNode = $this->betterNodeFinder->findParentType($node, Function_::class);
+        if ($functionNode instanceof Function_) {
+            return $this->procesMoveAnonymousClassInFunction($node, $functionNode);
         }
 
         return $node;
     }
 
-    private function getNamespacedClassName(?string $namespace, string $className): string
+    private function getNamespacedClassName(string $namespace, string $className): string
     {
-        return $namespace === null
+        return $namespace === ''
             ? $className
             : $namespace . '\\' . $className;;
     }
 
-    private function procesMoveAnonymousClass(New_ $new, Class_ $class): New_
+    private function getClassName(string $namespace, string $shortName): string
     {
-        $namespacedClassName = $this->getName($class->namespacedName);
-        $shortClassName      = $this->getName($class->name);
-        $namespace           = $namespacedClassName === $shortClassName
-            ? null
-            : substr($namespacedClassName, 0, - strlen($shortClassName) - 1);
-        $className           = self::CLASS_NAME;
+        $className           = self::CLASS_NAME . $shortName;
         $namespacedClassName = $this->getNamespacedClassName($namespace, $className);
 
         $count = 0;
-        while ($this->nodeRepository->findClass($namespacedClassName)) {
+        while ($this->nodeRepository->findClass($namespacedClassName) || class_exists($namespacedClassName)) {
             $className           = $className . ++$count;
             $namespacedClassName = $this->getNamespacedClassName($namespace, $className);
         }
 
-        $newClass = new Class_(
+        return $className;
+    }
+
+    private function procesMoveAnonymousClassInClass(New_ $new, Class_ $class): New_
+    {
+        $namespacedClassName = $this->getName($class->namespacedName);
+        $shortClassName      = $this->getName($class->name);
+        $namespace           = $namespacedClassName === $shortClassName
+            ? ''
+            : substr($namespacedClassName, 0, - strlen($shortClassName) - 1);
+        $className           = $this->getClassName($namespace, $shortClassName);
+
+        return $this->processMove($new, $className, $class);
+    }
+
+    private function procesMoveAnonymousClassInFunction(New_ $new, Function_ $function): New_
+    {
+        $namespacedFunctionName = $this->getName($function);
+        $shortFunctionName      = $this->getName($function->name);
+        $namespace              = $namespacedFunctionName === $shortFunctionName
+            ? null
+            : substr($namespacedFunctionName, 0, - strlen($shortFunctionName) - 1);
+        $className           = $this->getClassName($namespace, $shortFunctionName);
+
+        return $this->processMove($new, $className, $function);
+    }
+
+    private function processMove(New_ $new, string $className, Node $node): New_
+    {
+        $class = new Class_(
             new Name($className),
             [
                 'flags' => $new->class->flags,
@@ -121,7 +151,7 @@ CODE_SAMPLE
                 'attrGroups' => $new->class->attrGroups,
             ]
         );
-        $this->addNodeBeforeNode($newClass, $class);
+        $this->addNodeBeforeNode($class, $node);
 
         return new New_(new Name($className), $new->args);
     }
