5d70d1ed43a6e8ead75d6521c1e7bd36c3a2763e
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-04-20 01:50:57 +0700

    [DowngradePhp71] [WIP] Add DowngradeKeysInListRector

---
+++ rules/DowngradePhp71/Rector/FunctionLike/DowngradeKeysInListRector.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\DowngradePhp71\Rector\FunctionLike;
+
+use PhpParser\Node;
+use PhpParser\Node\Stmt\Function_;
+use Rector\Core\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \Rector\Tests\DowngradePhp71\Rector\FunctionLike\DowngradeKeysInListRector\DowngradeKeysInListRectorTest
+ */
+final class DowngradeKeysInListRector extends AbstractRector
+{
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [Function_::class];
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'Extract keys in list with its own variable assignment',
+            [
+                new CodeSample(
+                    <<<'CODE_SAMPLE'
+class SomeClass
+{
+    public function run(): void
+    {
+        $data = [
+            ["id" => 1, "name" => 'Tom'],
+            ["id" => 2, "name" => 'Fred'],
+        ];
+
+        list("id" => $id1, "name" => $name1) = $data[0];
+    }
+}
+CODE_SAMPLE
+                    ,
+                    <<<'CODE_SAMPLE'
+class SomeClass
+{
+    public function run(): void
+    {
+        $data = [
+            ["id" => 1, "name" => 'Tom'],
+            ["id" => 2, "name" => 'Fred'],
+        ];
+
+        $id1 = $data[0]['id'];
+        $name1 = $data[0]['name'];
+    }
+}
+CODE_SAMPLE
+                ),
+            ]
+        );
+    }
+
+    /**
+     * @param Function_ $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        return $node;
+    }
+}
