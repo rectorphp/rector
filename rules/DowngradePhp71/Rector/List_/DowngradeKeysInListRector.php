26f6532cf81508adb67c9a55d308f4c31823179f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-04-20 02:58:46 +0700

    functional

--- rules/DowngradePhp71/Rector/List_/DowngradeKeysInListRector.php
+++ rules/DowngradePhp71/Rector/List_/DowngradeKeysInListRector.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Rector\DowngradePhp71\Rector\List_;
 
 use PhpParser\Node;
+use PhpParser\Node\Expr\ArrayDimFetch;
 use PhpParser\Node\Expr\Assign;
 use PhpParser\Node\Stmt\Expression;
 use PhpParser\Node\Expr\List_;
@@ -13,6 +14,9 @@ use Rector\Core\Rector\AbstractRector;
 use Rector\NodeTypeResolver\Node\AttributeKey;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+use PhpParser\Node\Expr\ArrayItem;
+use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Scalar\String_;
 
 /**
  * @see \Rector\Tests\DowngradePhp71\Rector\List_\DowngradeKeysInListRector\DowngradeKeysInListRectorTest
@@ -75,25 +79,41 @@ CODE_SAMPLE
     {
         $this->verify($node);
 
+        $assign = $node->getAttribute(AttributeKey::PARENT_NODE);
         $items = $node->items;
+
+        $countToBeChanged = 0;
         foreach ($items as $item) {
-            if ($item->key !== null) {
-                return $this->processExtractToItsOwnVariable($node);
+            if ($item->key instanceof String_) {
+                $this->processExtractToItsOwnVariable($node, $item, $assign);
+                ++$countToBeChanged;
             }
+        }
+
+        $parentExpression = $assign->getAttribute(AttributeKey::PARENT_NODE);
+
+        if ($countToBeChanged === count($items)) {
+            $this->removeNode($parentExpression);
         }
 
         return null;
     }
 
-    private function processExtractToItsOwnVariable(List_ $list): ?List_
+    private function processExtractToItsOwnVariable(List_ $list, ArrayItem $arrayItem, Assign $assign): void
     {
-        return $list;
+        $assignExpression = new Expression(
+            new Assign(
+                $arrayItem->value,
+                new ArrayDimFetch($assign->expr, $arrayItem->key)
+            )
+        );
+        $this->addNodeBeforeNode($assignExpression, $list);
     }
 
-    private function verify(List_ $function): void
+    private function verify(List_ $list): void
     {
-        $parent = $function->getAttribute(AttributeKey::PARENT_NODE);
-        if (! $parent instanceof Assign || $parent->var !== $function) {
+        $parent = $list->getAttribute(AttributeKey::PARENT_NODE);
+        if (! $parent instanceof Assign || $parent->var !== $list) {
             throw new ShouldNotHappenException();
         }
 
