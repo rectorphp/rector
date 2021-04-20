4b25b481652b77bb4c0edd76780c391a68d089fa
kaizen-ci <info@kaizen-ci.org>
2021-04-20 04:00:36 +0000

    [ci-review] Rector Rectify

--- rules/DowngradePhp71/Rector/List_/DowngradeKeysInListRector.php
+++ rules/DowngradePhp71/Rector/List_/DowngradeKeysInListRector.php
@@ -188,7 +188,7 @@ CODE_SAMPLE
         $count = 0;
         if ($this->foreachAnalyzer->isValueVarUsed($foreach, $newValueVar)) {
             $newValueVar .= ++$count;
-            return $this->getNewValueVar($foreach, (string) $newValueVar);
+            return $this->getNewValueVar($foreach, $newValueVar);
         }
 
         return $newValueVar;
