02248a4e7210556ee94ac274c9ee101f71d19412
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-04-20 22:20:17 +0700

    roll

--- build/downgrade-rector.sh
+++ build/downgrade-rector.sh
@@ -30,7 +30,7 @@ for directory in $directories; do
     echo "[NOTE] Downgrading '$directory' directory\n"
 
     # --working-dir is needed, so "SKIP" parameter is applied in absolute path of nested directory
-    #php -d memory_limit=-1 bin/rector process $directory --config build/config/config-downgrade.php --working-dir $BUILD_DIRECTORY --ansi
+    php -d memory_limit=-1 bin/rector process $directory --config build/config/config-downgrade.php --working-dir $BUILD_DIRECTORY --ansi
 done
 
 # 5. downgrade rector.php config as well
