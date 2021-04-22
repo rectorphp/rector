60195c951a1629d4df0ed94a2035c46fe48e6df5
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-04-22 20:22:16 +0700

    [DowngradePhp71] Add DowngradeIsIterableRector (#6202)
    * [DowngradePhp71] Add DowngradeIsIterableRector
    
    * add test
    
    * return new BooleanOr
    
    * [ci-review] Rector Rectify
    
    * fixed with traverseNodesWithCallable
    
    * skip not is iterable
    
    * fix
    
    * example
    
    * register to downgrade-php71 config set
    
    Co-authored-by: kaizen-ci <info@kaizen-ci.org>

---
+++ rules/DowngradePhp71/Rector/FuncCall/DowngradeIsIterableRector.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\DowngradePhp71\Rector\FuncCall;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\BinaryOp\BooleanOr;
+use PhpParser\Node\Expr\FuncCall;
+use PhpParser\Node\Expr\Instanceof_;
+use PhpParser\Node\Name\FullyQualified;
+use Rector\Core\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \Rector\Tests\DowngradePhp71\Rector\FuncCall\DowngradeIsIterableRector\DowngradeIsIterableRectorTest
+ */
+final class DowngradeIsIterableRector extends AbstractRector
+{
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [FuncCall::class];
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'Change is_iterable with array and Traversable object type check',
+            [
+                new CodeSample(
+                    <<<'CODE_SAMPLE'
+class SomeClass
+{
+    public function run($obj)
+    {
+        is_iterable($obj);
+    }
+}
+CODE_SAMPLE
+                    ,
+                    <<<'CODE_SAMPLE'
+class SomeClass
+{
+    public function run($obj)
+    {
+        is_array($obj) || $obj instanceof \Traversable;
+    }
+}
+CODE_SAMPLE
+                ),
+            ]
+        );
+    }
+
+    /**
+     * @param FuncCall $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if (! $this->isName($node, 'is_iterable')) {
+            return null;
+        }
+
+        /** @var mixed $arg */
+        $arg = $node->args[0]->value;
+        $funcCall = $this->nodeFactory->createFuncCall('is_array', [$arg]);
+        $instanceOf = new Instanceof_($arg, new FullyQualified('Traversable'));
+
+        return new BooleanOr($funcCall, $instanceOf);
+    }
+}
