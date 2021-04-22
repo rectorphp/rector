eea2e2997f371661cfc6637634944332114aef7a
TomasVotruba <tomas.vot@gmail.com>
2021-04-22 14:28:23 +0200

    downgrade nullable type

--- config/set/downgrade-php71.php
+++ config/set/downgrade-php71.php
@@ -19,6 +19,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_70);
 
     $services = $containerConfigurator->services();
+
     $services->set(DowngradeNullableTypeDeclarationRector::class);
     $services->set(DowngradeVoidTypeDeclarationRector::class);
     $services->set(DowngradeClassConstantVisibilityRector::class);
