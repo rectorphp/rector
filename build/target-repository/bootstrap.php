9b1558e70186b2d8b5b6274980ce60a5b9490120
TomasVotruba <tomas.vot@gmail.com>
2021-04-24 13:21:42 +0200

    [prefixed] add bootstrap.php to autoload Rector-prefixed in tests + add simple rule test

---
+++ build/target-repository/bootstrap.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types = 1);
+
+// inspired by https://github.com/phpstan/phpstan/blob/master/bootstrap.php
+
+spl_autoload_register(function (string $class): void {
+    static $composerAutoloader;
+
+    // already loaded in bin/rector.php
+    if (defined('__RECTOR_RUNNING__')) {
+        return;
+    }
+
+    // load prefixed or native class, e.g. for running tests
+    if (strpos($class, 'RectorPrefix') === 0 || strpos($class, 'Rector\\') === 0) {
+        if ($composerAutoloader === null) {
+            // prefixed version autoload
+            $composerAutoloader = require __DIR__ . '/vendor/autoload.php';
+        }
+        $composerAutoloader->loadClass($class);
+    }
+});
