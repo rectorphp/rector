557a283c8a6d70621c4265bdeb6d27d57cead927
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-04-30 14:37:26 +0700

    check $phpDocInfo->hasChanged()

--- rules/CodeQuality/Rector/ClassMethod/NarrowUnionTypeDocRector.php
+++ rules/CodeQuality/Rector/ClassMethod/NarrowUnionTypeDocRector.php
@@ -95,7 +95,11 @@ CODE_SAMPLE
             }
         }
 
-        return $node;
+        if ($phpDocInfo->hasChanged()) {
+            return $node;
+        }
+
+        return null;
     }
 
     private function changeDocObjectWithoutClassType(
