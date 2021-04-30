98b59de095d845de0fb8ae15d87d02ab1b78c1e7
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-04-30 20:20:18 +0700

    skip inside method or inside function

--- rules/DowngradePhp70/Rector/FuncCall/DowngradeDefineArrayConstantRector.php
+++ rules/DowngradePhp70/Rector/FuncCall/DowngradeDefineArrayConstantRector.php
@@ -6,15 +6,15 @@ namespace Rector\DowngradePhp70\Rector\FuncCall;
 
 use PhpParser\Node;
 use PhpParser\Node\Const_;
+use PhpParser\Node\Expr\Array_;
 use PhpParser\Node\Expr\FuncCall;
+use PhpParser\Node\Name;
+use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\Node\Stmt\Function_;
 use Rector\Core\Rector\AbstractRector;
+use Rector\NodeTypeResolver\Node\AttributeKey;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
-use PhpParser\Node\Expr\Array_;
-use PhpParser\Node\Name;
-use PhpParser\Node\Stmt\Const_ as StmtConst_;
-use PhpParser\Node\Stmt\Expression;
-use Rector\NodeTypeResolver\Node\AttributeKey;
 
 /**
  * @see \Rector\Tests\DowngradePhp70\Rector\Declare_\DowngradeDefineArrayConstantRector\DowngradeDefineArrayConstantRectorTest
@@ -64,12 +64,7 @@ CODE_SAMPLE
             return null;
         }
 
-        return new Const_(
-            new Name(
-                'const ' . $node->args[0]->value->value
-            ),
-            $node->args[1]->value
-        );
+        return new Const_(new Name('const ' . $node->args[0]->value->value), $node->args[1]->value);
     }
 
     private function shouldSkip(FuncCall $funcCall): bool
@@ -79,6 +74,15 @@ CODE_SAMPLE
         }
 
         $args = $funcCall->args;
-        return ! $args[1]->value instanceof Array_;
+        if (! $args[1]->value instanceof Array_) {
+            return true;
+        }
+
+        $methodNode = $funcCall->getAttribute(AttributeKey::METHOD_NODE);
+        if ($methodNode instanceof ClassMethod) {
+            return true;
+        }
+
+        return (bool) $this->betterNodeFinder->findParentType($funcCall, Function_::class);
     }
 }
