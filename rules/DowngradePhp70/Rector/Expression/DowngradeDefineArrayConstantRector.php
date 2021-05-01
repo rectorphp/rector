9e91a8d401791ecbb829f5d4decfdc955db8f91d
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-05-01 14:43:20 +0700

    [DowngradePhp70] Add DowngradeDefineArrayConstantRector (#6270)
    * [DowngradePhp70] [WIP] Add DowngradeDefineArrayConstantRector
    
    * add fixture
    
    * create const
    
    * it works
    
    * skip inside method or inside function
    
    * register DowngradeDefineArrayConstantRector to downgrade-php70 config set
    
    * skip not in expression
    
    * phpstan
    
    * final touch
    
    * ensure expression expr is a define func call
    
    * final touch
    
    * [ci-review] Rector Rectify
    
    * working prototype
    
    * update to Expression
    
    * config
    
    * phpstan
    
    * final touch
    
    Co-authored-by: kaizen-ci <info@kaizen-ci.org>
    Co-authored-by: TomasVotruba <tomas.vot@gmail.com>

---
+++ rules/DowngradePhp70/Rector/Expression/DowngradeDefineArrayConstantRector.php
@@ -0,0 +1,100 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\DowngradePhp70\Rector\Expression;
+
+use PhpParser\Node;
+use PhpParser\Node\Const_;
+use PhpParser\Node\Expr\Array_;
+use PhpParser\Node\Expr\FuncCall;
+use PhpParser\Node\Scalar\String_;
+use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\Node\Stmt\Expression;
+use PhpParser\Node\Stmt\Function_;
+use Rector\Core\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \Rector\Tests\DowngradePhp70\Rector\Expression\DowngradeDefineArrayConstantRector\DowngradeDefineArrayConstantRectorTest
+ */
+final class DowngradeDefineArrayConstantRector extends AbstractRector
+{
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [Expression::class];
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'Change array contant definition via define to const',
+            [
+                new CodeSample(
+                    <<<'CODE_SAMPLE'
+define('ANIMALS', [
+    'dog',
+    'cat',
+    'bird'
+]);
+CODE_SAMPLE
+                    ,
+                    <<<'CODE_SAMPLE'
+const ANIMALS = [
+    'dog',
+    'cat',
+    'bird'
+];
+CODE_SAMPLE
+                ),
+            ]
+        );
+    }
+
+    /**
+     * @param Expression $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if (! $node->expr instanceof FuncCall) {
+            return null;
+        }
+
+        $funcCall = $node->expr;
+
+        if ($this->shouldSkip($funcCall)) {
+            return null;
+        }
+
+        /** @var String_ $arg0 */
+        $arg0 = $funcCall->args[0]->value;
+        $arg0Value = $arg0->value;
+
+        /** @var Array_ $arg1Value */
+        $arg1Value = $funcCall->args[1]->value;
+
+        return new Node\Stmt\Const_([new Const_($arg0Value, $arg1Value)]);
+    }
+
+    private function shouldSkip(FuncCall $funcCall): bool
+    {
+        if (! $this->isName($funcCall, 'define')) {
+            return true;
+        }
+
+        $args = $funcCall->args;
+        if (! $args[0]->value instanceof String_) {
+            return true;
+        }
+
+        if (! $args[1]->value instanceof Array_) {
+            return true;
+        }
+
+        return (bool) $this->betterNodeFinder->findParentTypes($funcCall, [ClassMethod::class, Function_::class]);
+    }
+}
