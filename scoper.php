f3b15d444b23629241437aac85614e1b15855cea
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-05-03 00:24:17 +0700

    cover Composer\InstalledVersions

--- scoper.php
+++ scoper.php
@@ -57,19 +57,6 @@ return [
         },
 
         function (string $filePath, string $prefix, string $content): string {
-            if (! Strings::endsWith($filePath, 'vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php')) {
-                return $content;
-            }
-
-            // see https://regex101.com/r/v8zRMm/1
-            return Strings::replace(
-                $content, '
-                #' . $prefix . '\\\\Composer\\\\InstalledVersions#',
-                'Composer\InstalledVersions'
-            );
-        },
-
-        function (string $filePath, string $prefix, string $content): string {
             if (! Strings::contains($content, $prefix . '\Composer\\')) {
                 return $content;
             }
