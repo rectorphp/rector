57cbe50fc8c9e7393065f7ae4c239276e474bf39
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-05-03 21:14:50 +0700

    [Scoper] Patch ParameterSkipper class via scoper-php70 config

--- utils/compiler/src/Command/DowngradePathsCommand.php
+++ utils/compiler/src/Command/DowngradePathsCommand.php
@@ -59,7 +59,7 @@ final class DowngradePathsCommand extends Command
 
         $downgradePaths = array_merge([
             // must be separated to cover container get() trait + psr container contract get()
-            'vendor/symfony vendor/psr vendor/symplify/autowire-array-parameter',
+            'vendor/symfony vendor/psr',
             'vendor/symplify vendor/nikic bin src packages rector.php',
         ], $downgradePaths);
 
