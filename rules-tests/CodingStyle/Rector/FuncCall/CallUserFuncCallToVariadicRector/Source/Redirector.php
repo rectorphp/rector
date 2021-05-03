2e6d188522d6ff8f9661b4307b9857074c50a638
Tomas Votruba <tomas.vot@gmail.com>
2021-05-03 21:03:16 +0200

    [CodingStyle] Add variadic method call unpack to CallUserFuncCallToVariadicRector (#6316)

---
+++ rules-tests/CodingStyle/Rector/FuncCall/CallUserFuncCallToVariadicRector/Source/Redirector.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Tests\CodingStyle\Rector\FuncCall\CallUserFuncCallToVariadicRector\Source;
+
+final class Redirector
+{
+    public function redirect($first, $second)
+    {
+    }
+}
