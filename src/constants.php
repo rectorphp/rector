f5bb6cd17e4f871fbace388efe4e951529ad3a75
Tomas Votruba <tomas.vot@gmail.com>
2021-05-04 16:30:35 +0200

    composer: use phpstan with php-parser 4.10.5 (#6327)
    * composer: use phpstan with php-parser 4.10.5
    
    * bump to php-cs-fixer 3.0
    
    * bump to xdebug handler 2.0

--- src/constants.php
+++ src/constants.php
@@ -3,11 +3,11 @@
 declare(strict_types=1);
 
 // mimic missing T_ENUM constant on PHP 8.0-
-if (defined('T_ENUM') === false) {
+if (! defined('T_ENUM')) {
     define('T_ENUM', 5000);
 }
 
 // mimic missing T_NAME_RELATIVE constant on PHP 8.0-
-if (defined('T_NAME_RELATIVE') === false) {
+if (! defined('T_NAME_RELATIVE')) {
     define('T_NAME_RELATIVE', 5001);
 }
