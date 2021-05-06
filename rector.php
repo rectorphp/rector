d225be28d8867cc6225576e22fbaf470ba8373b1
Tomas Votruba <tomas.vot@gmail.com>
2021-05-06 23:43:45 +0200

    [scoped on PHP 7.0] bin/rector run with extracted phpstan (#6371)

--- rector.php
+++ rector.php
@@ -23,9 +23,6 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 use Symplify\SymfonyPhpConfig\ValueObjectInliner;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::SETS, [SetList::NAMING]);
-
     // include sets
     $containerConfigurator->import(SetList::CODING_STYLE);
     $containerConfigurator->import(SetList::CODE_QUALITY);
