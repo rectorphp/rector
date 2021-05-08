7a33e856fd1a3d7cf672311a6815d003468c659a
Tomas Votruba <tomas.vot@gmail.com>
2021-05-08 12:55:30 +0200

    [Downgrade PHP 7.0] Add string retype on object (#6390)

--- rules/DowngradePhp70/Rector/FunctionLike/DowngradeScalarTypeDeclarationRector.php
+++ rules/DowngradePhp70/Rector/FunctionLike/DowngradeScalarTypeDeclarationRector.php
@@ -5,8 +5,13 @@ declare(strict_types=1);
 namespace Rector\DowngradePhp70\Rector\FunctionLike;
 
 use PhpParser\Node;
+use PhpParser\Node\Expr\Assign;
+use PhpParser\Node\Expr\Cast\String_;
+use PhpParser\Node\Expr\Variable;
 use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\Node\Stmt\Expression;
 use PhpParser\Node\Stmt\Function_;
+use PhpParser\Node\Stmt\If_;
 use PHPStan\Type\BooleanType;
 use PHPStan\Type\FloatType;
 use PHPStan\Type\IntegerType;
@@ -84,6 +89,20 @@ CODE_SAMPLE
                 $node,
                 [StringType::class, IntegerType::class, BooleanType::class, FloatType::class]
             );
+
+            $paramType = $this->getStaticType($param);
+            if ($paramType instanceof StringType) {
+                // add possible object with __toString() re-type to keep original behavior
+                // @see https://twitter.com/VotrubaT/status/1390974218108538887
+
+                /** @var string $variableName */
+                $variableName = $this->getName($param->var);
+
+                $if = $this->createObjetVariableStringCast($variableName);
+                $node->stmts = array_merge([$if], (array) $node->stmts);
+
+                return $node;
+            }
         }
 
         if (! $this->phpDocFromTypeDeclarationDecorator->decorateReturn($node)) {
@@ -91,5 +110,16 @@ CODE_SAMPLE
         }
 
         return $node;
+    }
+
+    private function createObjetVariableStringCast(string $variableName): If_
+    {
+        $variable = new Variable($variableName);
+        $isObjectFuncCall = $this->nodeFactory->createFuncCall('is_object', [$variable]);
+
+        $if = new If_($isObjectFuncCall);
+        $assign = new Assign($variable, new String_($variable));
+        $if->stmts[] = new Expression($assign);
+        return $if;
     }
 }
