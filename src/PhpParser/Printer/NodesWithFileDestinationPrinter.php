8e7ba00e50bf732dd47ce68d932b0262037e4cfc
Tomas Votruba <tomas.vot@gmail.com>
2021-05-11 13:25:51 +0200

    remove last empty token on new printed content; go for newline by default as best practise conventions (#13)

--- src/PhpParser/Printer/NodesWithFileDestinationPrinter.php
+++ src/PhpParser/Printer/NodesWithFileDestinationPrinter.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Rector\Core\PhpParser\Printer;
 
-use Nette\Utils\Strings;
-use PhpParser\Lexer;
 use Rector\FileSystemRector\Contract\FileWithNodesInterface;
 use Rector\PostRector\Application\PostFileProcessor;
 
@@ -13,7 +11,6 @@ final class NodesWithFileDestinationPrinter
 {
     public function __construct(
         private BetterStandardPrinter $betterStandardPrinter,
-        private Lexer $lexer,
         private PostFileProcessor $postFileProcessor
     ) {
     }
@@ -21,23 +18,6 @@ final class NodesWithFileDestinationPrinter
     public function printNodesWithFileDestination(FileWithNodesInterface $fileWithNodes): string
     {
         $nodes = $this->postFileProcessor->traverse($fileWithNodes->getNodes());
-        $prettyPrintContent = $this->betterStandardPrinter->prettyPrintFile($nodes);
-
-        return $this->resolveLastEmptyLine($prettyPrintContent);
-    }
-
-    /**
-     * Add empty line in the end, if it is in the original tokens
-     */
-    private function resolveLastEmptyLine(string $prettyPrintContent): string
-    {
-        $tokens = $this->lexer->getTokens();
-        $lastToken = array_pop($tokens);
-
-        if ($lastToken && isset($lastToken[1]) && Strings::contains($lastToken[1], "\n")) {
-            $prettyPrintContent = trim($prettyPrintContent) . PHP_EOL;
-        }
-
-        return $prettyPrintContent;
+        return $this->betterStandardPrinter->prettyPrintFile($nodes);
     }
 }
