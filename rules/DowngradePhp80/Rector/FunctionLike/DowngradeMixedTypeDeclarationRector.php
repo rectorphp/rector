ed8487954dc7d76ca5a728605f55ebafe47caa67
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-05-26 17:07:49 +0700

    [DowngradePhp80] Handle DowngradeMixedTypeDeclarationRector on Closure (#106)
    Co-authored-by: kaizen-ci <info@kaizen-ci.org>

--- rules/DowngradePhp80/Rector/FunctionLike/DowngradeMixedTypeDeclarationRector.php
+++ rules/DowngradePhp80/Rector/FunctionLike/DowngradeMixedTypeDeclarationRector.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Rector\DowngradePhp80\Rector\FunctionLike;
 
 use PhpParser\Node;
+use PhpParser\Node\Expr\Closure;
 use PhpParser\Node\Stmt\ClassMethod;
 use PhpParser\Node\Stmt\Function_;
 use PHPStan\Type\MixedType;
@@ -28,7 +29,7 @@ final class DowngradeMixedTypeDeclarationRector extends AbstractRector
      */
     public function getNodeTypes(): array
     {
-        return [Function_::class, ClassMethod::class];
+        return [Function_::class, ClassMethod::class, Closure::class];
     }
 
     public function getRuleDefinition(): RuleDefinition
