af678a73b025590fb069f65fba20a77fddb9052d
Michal Lulco <lulco@efabrica.sk>
2021-06-06 11:52:36 +0200

    [Fix] use League\Flysystem\FilesystemInterface for RenameMethodRector (#153)

--- config/set/flysystem-20.php
+++ config/set/flysystem-20.php
@@ -13,23 +13,23 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ->call('configure', [[
             RenameMethodRector::METHOD_CALL_RENAMES => ValueObjectInliner::inline([
                 // Rename is now move, specific for files.
-                new MethodCallRename('League\Flysystem\Filesystem', 'rename', 'move'),
+                new MethodCallRename('League\Flysystem\FilesystemInterface', 'rename', 'move'),
 
                 // No arbitrary abbreviations
-                new MethodCallRename('League\Flysystem\Filesystem', 'createDir', 'createDirectory'),
+                new MethodCallRename('League\Flysystem\FilesystemInterface', 'createDir', 'createDirectory'),
 
                 // Writes are now deterministic
-                new MethodCallRename('League\Flysystem\Filesystem', 'update', 'write'),
-                new MethodCallRename('League\Flysystem\Filesystem', 'updateStream', 'writeStream'),
-                new MethodCallRename('League\Flysystem\Filesystem', 'put', 'write'),
-                new MethodCallRename('League\Flysystem\Filesystem', 'putStream', 'writeStream'),
+                new MethodCallRename('League\Flysystem\FilesystemInterface', 'update', 'write'),
+                new MethodCallRename('League\Flysystem\FilesystemInterface', 'updateStream', 'writeStream'),
+                new MethodCallRename('League\Flysystem\FilesystemInterface', 'put', 'write'),
+                new MethodCallRename('League\Flysystem\FilesystemInterface', 'putStream', 'writeStream'),
 
                 // Metadata getters are renamed
-                new MethodCallRename('League\Flysystem\Filesystem', 'getTimestamp', 'lastModified'),
-                new MethodCallRename('League\Flysystem\Filesystem', 'has', 'fileExists'),
-                new MethodCallRename('League\Flysystem\Filesystem', 'getMimetype', 'mimeType'),
-                new MethodCallRename('League\Flysystem\Filesystem', 'getSize', 'fileSize'),
-                new MethodCallRename('League\Flysystem\Filesystem', 'getVisibility', 'visibility'),
+                new MethodCallRename('League\Flysystem\FilesystemInterface', 'getTimestamp', 'lastModified'),
+                new MethodCallRename('League\Flysystem\FilesystemInterface', 'has', 'fileExists'),
+                new MethodCallRename('League\Flysystem\FilesystemInterface', 'getMimetype', 'mimeType'),
+                new MethodCallRename('League\Flysystem\FilesystemInterface', 'getSize', 'fileSize'),
+                new MethodCallRename('League\Flysystem\FilesystemInterface', 'getVisibility', 'visibility'),
             ]),
         ]]);
 };
