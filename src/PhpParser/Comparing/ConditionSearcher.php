4b41e97a6dd2beb3043d2ae205ea41a0a033613f
Robin Richtsfeld <robin.richtsfeld@gmail.com>
2021-06-11 19:39:10 +0200

    [DeadCode] Skip RemoveUnusedVariableAssignRector if variable used before or in assign expr (#206)
    Co-authored-by: Abdul Malik Ikhsan <samsonasik@gmail.com>
    Co-authored-by: GitHub Action <action@github.com>

--- src/PhpParser/Comparing/ConditionSearcher.php
+++ src/PhpParser/Comparing/ConditionSearcher.php
@@ -4,72 +4,106 @@ declare(strict_types=1);
 
 namespace Rector\Core\PhpParser\Comparing;
 
+use PhpParser\Node;
 use PhpParser\Node\Expr\Assign;
 use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Stmt;
 use PhpParser\Node\Stmt\Else_;
 use PhpParser\Node\Stmt\Expression;
 use PhpParser\Node\Stmt\If_;
+use Rector\Core\PhpParser\Node\BetterNodeFinder;
 
 final class ConditionSearcher
 {
+    public function __construct(
+        private BetterNodeFinder $betterNodeFinder,
+        private NodeComparator $nodeComparator
+    ) {
+    }
+
     public function searchIfAndElseForVariableRedeclaration(Assign $assign, If_ $if): bool
     {
+        $elseNode = $if->else;
+
+        if (! $elseNode instanceof Else_) {
+            return false;
+        }
+
         /** @var Variable $varNode */
         $varNode = $assign->var;
 
-        // search if for redeclaration of variable
-        foreach ($if->stmts as $statementIf) {
-            if (! $statementIf instanceof Expression) {
-                continue;
+        if (! $this->searchForVariableRedeclaration($varNode, $if->stmts)) {
+            return false;
+        }
+
+        foreach ($if->elseifs as $elseifNode) {
+            if (! $this->searchForVariableRedeclaration($varNode, $elseifNode->stmts)) {
+                return false;
+            }
+        }
+
+        if (! $this->searchForVariableRedeclaration($varNode, $elseNode->stmts)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @param Stmt[] $stmts
+     */
+    private function searchForVariableRedeclaration(Variable $varNode, array $stmts): bool
+    {
+        foreach ($stmts as $stmt) {
+            if ($this->checkIfVariableUsedInExpression($varNode, $stmt)) {
+                return false;
             }
 
-            if (! $statementIf->expr instanceof Assign) {
-                continue;
+            if ($this->checkForVariableRedeclaration($varNode, $stmt)) {
+                return true;
             }
-
-            $assignVar = $statementIf->expr->var;
-            if (! $assignVar instanceof Variable) {
-                continue;
-            }
-
-            if ($varNode->name !== $assignVar->name) {
-                continue;
-            }
-
-            $elseNode = $if->else;
-            if (! $elseNode instanceof Else_) {
-                continue;
-            }
-
-            // search else for redeclaration of variable
-            return $this->searchElseForVariableRedeclaration($assign, $elseNode);
         }
 
         return false;
     }
 
-    private function searchElseForVariableRedeclaration(Assign $assign, Else_ $else): bool
+    private function checkIfVariableUsedInExpression(Variable $varNode, Stmt $stmt): bool
     {
-        foreach ($else->stmts as $statementElse) {
-            if (! $statementElse instanceof Expression) {
-                continue;
+        if ($stmt instanceof Expression) {
+            if ($stmt->expr instanceof Assign) {
+                $node = $stmt->expr->expr;
+            } else {
+                $node = $stmt->expr;
             }
-
-            if (! $statementElse->expr instanceof Assign) {
-                continue;
-            }
-
-            /** @var Variable $varElse */
-            $varElse = $statementElse->expr->var;
-            /** @var Variable $varNode */
-            $varNode = $assign->var;
-            if ($varNode->name !== $varElse->name) {
-                continue;
-            }
-
-            return true;
+        } else {
+            $node = $stmt;
         }
 
-        return false;
+        return (bool) $this->betterNodeFinder->findFirst(
+            $node,
+            fn (Node $subNode): bool => $this->nodeComparator->areNodesEqual($varNode, $subNode)
+        );
+    }
+
+    private function checkForVariableRedeclaration(Variable $varNode, Stmt $stmt): bool
+    {
+        if (! $stmt instanceof Expression) {
+            return false;
+        }
+
+        if (! $stmt->expr instanceof Assign) {
+            return false;
+        }
+
+        $assignVar = $stmt->expr->var;
+        if (! $assignVar instanceof Variable) {
+            return false;
+        }
+
+        if ($varNode->name !== $assignVar->name) {
+            return false;
+        }
+
+        return true;
     }
 }
