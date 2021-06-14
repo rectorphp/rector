fc17cc90a9dbee71396866d1baf7c3fd3fef419b
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-06-14 09:03:56 +0700

    [Php80] Test for misplaced } on multiline in AnnotationToAttributeRector (#218)
    * Add failing test fixture for AnnotationToAttributeRector
    
    # Failing Test for AnnotationToAttributeRector
    
    Based on https://getrector.org/demo/a8f88218-ee7e-4fdb-a68a-5f871fd0ab5c
    
    * Update rules-tests/Php80/Rector/Class_/AnnotationToAttributeRector/Fixture/foo.php.inc
    
    * fixture fix
    
    * test fix
    
    * rename fixture
    
    Co-authored-by: Oskar Stark <oskarstark@googlemail.com>

--- rules-tests/Php80/Rector/Class_/AnnotationToAttributeRector/config/configured_rule.php
+++ rules-tests/Php80/Rector/Class_/AnnotationToAttributeRector/config/configured_rule.php
@@ -18,6 +18,8 @@ return static function (ContainerConfigurator $containerConfigurator): void {
                 new AnnotationToAttribute('Symfony\Component\Routing\Annotation\Route'),
                 new AnnotationToAttribute('Symfony\Component\Validator\Constraints\Choice'),
 
+                new AnnotationToAttribute('Doctrine\ORM\Mapping\Entity'),
+                new AnnotationToAttribute('Doctrine\ORM\Mapping\Table'),
                 new AnnotationToAttribute('Doctrine\ORM\Mapping\Id'),
                 new AnnotationToAttribute('Doctrine\ORM\Mapping\Column'),
                 new AnnotationToAttribute('ApiPlatform\Core\Annotation\ApiResource'),
