c251806319f0f9e140a8ea0c9c3d7ba7c6529327
Tomas Votruba <tomas.vot@gmail.com>
2021-06-18 20:01:14 +0200

    [Order] Cleanup set from rules that are nicely handled by php-cs-fixer (#245)

--- config/set/order.php
+++ config/set/order.php
@@ -2,18 +2,10 @@
 
 declare(strict_types=1);
 
-use Rector\Order\Rector\Class_\OrderClassConstantsByIntegerValueRector;
-use Rector\Order\Rector\Class_\OrderConstantsByVisibilityRector;
-use Rector\Order\Rector\Class_\OrderMethodsByVisibilityRector;
 use Rector\Order\Rector\Class_\OrderPrivateMethodsByUseRector;
-use Rector\Order\Rector\Class_\OrderPropertiesByVisibilityRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
     $services->set(OrderPrivateMethodsByUseRector::class);
-    $services->set(OrderClassConstantsByIntegerValueRector::class);
-    $services->set(OrderMethodsByVisibilityRector::class);
-    $services->set(OrderPropertiesByVisibilityRector::class);
-    $services->set(OrderConstantsByVisibilityRector::class);
 };
