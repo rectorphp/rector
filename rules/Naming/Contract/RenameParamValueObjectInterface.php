6238705e938a643cb84e0b4275cb682d26307975
Tomas Votruba <tomas.vot@gmail.com>
2021-07-02 01:54:06 +0200

    mics

--- rules/Naming/Contract/RenameParamValueObjectInterface.php
+++ rules/Naming/Contract/RenameParamValueObjectInterface.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 namespace Rector\Naming\Contract;
 
-use PhpParser\Node\FunctionLike;
+use PhpParser\Node\Expr\Closure;
 use PhpParser\Node\Param;
+use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\Node\Stmt\Function_;
 
 interface RenameParamValueObjectInterface extends RenameValueObjectInterface
 {
-    public function getFunctionLike(): FunctionLike;
+    public function getFunctionLike(): ClassMethod | Function_ | Closure;
 
     public function getParam(): Param;
 }
