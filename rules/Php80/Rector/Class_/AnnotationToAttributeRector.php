d08a87f692fcea45633084a12889324a19da49d8
Karel Sommer <karel.sommer@gmail.com>
2021-07-12 10:20:19 +0200

    added promoted property test for annotation to attribute rule (#425)

--- rules/Php80/Rector/Class_/AnnotationToAttributeRector.php
+++ rules/Php80/Rector/Class_/AnnotationToAttributeRector.php
@@ -8,6 +8,7 @@ use PhpParser\Node;
 use PhpParser\Node\AttributeGroup;
 use PhpParser\Node\Expr\ArrowFunction;
 use PhpParser\Node\Expr\Closure;
+use PhpParser\Node\Param;
 use PhpParser\Node\Stmt\Class_;
 use PhpParser\Node\Stmt\ClassMethod;
 use PhpParser\Node\Stmt\Function_;
@@ -113,6 +114,7 @@ CODE_SAMPLE
         return [
             Class_::class,
             Property::class,
+            Param::class,
             ClassMethod::class,
             Function_::class,
             Closure::class,
@@ -121,7 +123,7 @@ CODE_SAMPLE
     }
 
     /**
-     * @param Class_|Property|ClassMethod|Function_|Closure|ArrowFunction $node
+     * @param Class_|Property|Param|ClassMethod|Function_|Closure|ArrowFunction $node
      */
     public function refactor(Node $node): ?Node
     {
