039c173279b4250caf67a044444f08a9d8269697
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-07-22 23:53:08 +0700

    [Core] Avoid "Backtrack limit was exhausted" error on BetterStandardPrinter (#487)
    * [Core] Avoid "Backtrack limit was exhausted" error on BetterStandardPrinter
    
    * update
    
    * using mU
    
    * allow static modifier
    
    * allow direct static function
    
    * function can be final too
    
    * fix
    
    * fix

--- src/PhpParser/Printer/BetterStandardPrinter.php
+++ src/PhpParser/Printer/BetterStandardPrinter.php
@@ -59,10 +59,10 @@ final class BetterStandardPrinter extends Standard
     private const EXTRA_SPACE_BEFORE_NOP_REGEX = '#^[ \t]+$#m';
 
     /**
-     * @see https://regex101.com/r/qZiqGo/4
+     * @see https://regex101.com/r/qZiqGo/13
      * @var string
      */
-    private const REPLACE_COLON_WITH_SPACE_REGEX = '#(function .*?\(.*?\)) : #';
+    private const REPLACE_COLON_WITH_SPACE_REGEX = '#(^.*function .*\(.*\)) : #';
 
     /**
      * Use space by default
