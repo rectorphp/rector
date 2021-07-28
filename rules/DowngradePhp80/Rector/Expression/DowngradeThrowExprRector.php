a467f764ec34703a3dab0bd861775f16656b8ac8
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-07-28 23:42:54 +0700

    [DowngradePhp80] Add DowngradeThrowExprRector (#533)
    * [DowngradePhp80] Add DowngradeThrowExprRector
    
    * add test
    
    * move to use Expression node to check Throw inside Expression
    
    * skip direct throw
    
    * skip no throw in assign
    
    * handle coalesce
    
    * implemented for starter
    
    * ensure left coalese is Variable, PropertyFetch, or StaticPropertyFetch, no other expr
    
    * eol

---
+++ rules/DowngradePhp80/Rector/Expression/DowngradeThrowExprRector.php
@@ -0,0 +1,133 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\DowngradePhp80\Rector\Expression;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\Assign;
+use PhpParser\Node\Expr\BinaryOp\Coalesce;
+use PhpParser\Node\Expr\BooleanNot;
+use PhpParser\Node\Expr\Isset_;
+use PhpParser\Node\Expr\PropertyFetch;
+use PhpParser\Node\Expr\StaticPropertyFetch;
+use PhpParser\Node\Expr\Throw_;
+use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Stmt\Expression;
+use PhpParser\Node\Stmt\If_;
+use Rector\Core\NodeManipulator\IfManipulator;
+use Rector\Core\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @changelog https://wiki.php.net/rfc/throw_expression
+ *
+ * @see \Rector\Tests\DowngradePhp80\Rector\Expression\DowngradeThrowExprRector\DowngradeThrowExprRectorTest
+ */
+final class DowngradeThrowExprRector extends AbstractRector
+{
+    public function __construct(
+        private IfManipulator $ifManipulator
+    ) {
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition('Downgrade throw as expr', [
+            new CodeSample(
+                <<<'CODE_SAMPLE'
+class SomeClass
+{
+    public function run()
+    {
+        $id = $somethingNonexistent ?? throw new RuntimeException();
+    }
+}
+CODE_SAMPLE
+
+                ,
+                <<<'CODE_SAMPLE'
+class SomeClass
+{
+    public function run()
+    {
+        if (!isset($somethingNonexistent)) {
+            throw new RuntimeException();
+        }
+        $id = $somethingNonexistent;
+    }
+}
+CODE_SAMPLE
+            ),
+        ]);
+    }
+
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [Expression::class];
+    }
+
+    /**
+     * @param Expression $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if ($node->expr instanceof Throw_) {
+            return null;
+        }
+
+        if ($node->expr instanceof Assign) {
+            return $this->processAssign($node, $node->expr);
+        }
+
+        return $node;
+    }
+
+    private function processAssign(Expression $expression, Assign $assign): If_ | Expression | null
+    {
+        if (! $this->hasThrowInAssignExpr($assign)) {
+            return null;
+        }
+
+        if ($assign->expr instanceof Coalesce) {
+            return $this->processCoalesce($assign, $assign->expr);
+        }
+
+        if ($assign->expr instanceof Throw_) {
+            return new Expression(($assign->expr));
+        }
+
+        return $expression;
+    }
+
+    private function processCoalesce(Assign $assign, Coalesce $coalesce): ?If_
+    {
+        if (! $coalesce->right instanceof Throw_) {
+            return null;
+        }
+
+        if (! $coalesce->left instanceof Variable && ! $coalesce->left instanceof PropertyFetch && ! $coalesce->left instanceof StaticPropertyFetch) {
+            return null;
+        }
+
+        $booleanNot = new BooleanNot(new Isset_([$coalesce->left]));
+        $assign->expr = $coalesce->left;
+
+        $if = $this->ifManipulator->createIfExpr($booleanNot, new Expression($coalesce->right));
+
+        $this->addNodeAfterNode(new Expression($assign), $if);
+        return $if;
+    }
+
+    private function hasThrowInAssignExpr(Assign $assign): bool
+    {
+        return (bool) $this->betterNodeFinder->findFirst(
+            $assign->expr,
+            fn (Node $node): bool => $node instanceof Throw_
+        );
+    }
+}
