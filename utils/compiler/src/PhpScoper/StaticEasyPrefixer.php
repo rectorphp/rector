aff0c5d90c327386f6d253fbf05f5f3e1ac9e0c7
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-08-04 00:10:53 +0700

    [Scoper] Remove Doctrine\Inflector\Inflector from exclude class (#582)

--- utils/compiler/src/PhpScoper/StaticEasyPrefixer.php
+++ utils/compiler/src/PhpScoper/StaticEasyPrefixer.php
@@ -12,8 +12,6 @@ final class StaticEasyPrefixer
     public const EXCLUDED_CLASSES = [
         // part of public interface of configs.php
         'Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator',
-        // this is not prefixed on few places by php-scoper by default, probably some bug
-        'Doctrine\Inflector\Inflector',
         // for SmartFileInfo
         'Symplify\SmartFileSystem\SmartFileInfo',
         // for ComposerJson because it is part of the public API. I.e. ComposerRectorInterface
