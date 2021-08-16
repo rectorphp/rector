2dbf877472a3a98cdf4febe1abcd2c96b3bdd2c6
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-08-16 15:44:56 +0700

    [DeadCode] Skip RemoveUselessReturnTagRector on BracketsAwareUnionTypeNode (#694)
    * Add failing test fixture for RemoveUselessReturnTagRector
    
    # Failing Test for RemoveUselessReturnTagRector
    
    Based on https://getrector.org/demo/1ebfe6a8-4b08-6440-8cb6-8bfed0a3a869
    
    * Closes #692
    
    * handle array typed
    
    * phpstan
    
    * case by case to avoid unwanted behavior
    
    Co-authored-by: Jáchym Toušek <enumag@gmail.com>

--- rules/DeadCode/PhpDoc/DeadReturnTagValueNodeAnalyzer.php
+++ rules/DeadCode/PhpDoc/DeadReturnTagValueNodeAnalyzer.php
@@ -7,6 +7,8 @@ namespace Rector\DeadCode\PhpDoc;
 use PhpParser\Node\FunctionLike;
 use PHPStan\PhpDocParser\Ast\PhpDoc\ReturnTagValueNode;
 use PHPStan\PhpDocParser\Ast\Type\GenericTypeNode;
+use PHPStan\PhpDocParser\Ast\Type\IdentifierTypeNode;
+use Rector\BetterPhpDocParser\ValueObject\Type\BracketsAwareUnionTypeNode;
 use Rector\BetterPhpDocParser\ValueObject\Type\SpacingAwareCallableTypeNode;
 use Rector\NodeTypeResolver\TypeComparator\TypeComparator;
 
@@ -32,14 +34,38 @@ final class DeadReturnTagValueNodeAnalyzer
             return false;
         }
 
-        if ($returnTagValueNode->type instanceof GenericTypeNode) {
+        if (in_array($returnTagValueNode->type::class, [
+            GenericTypeNode::class,
+            SpacingAwareCallableTypeNode::class,
+        ], true)) {
             return false;
         }
 
-        if ($returnTagValueNode->type instanceof SpacingAwareCallableTypeNode) {
-            return false;
+        if (! $returnTagValueNode->type instanceof BracketsAwareUnionTypeNode) {
+            return $returnTagValueNode->description === '';
         }
 
-        return $returnTagValueNode->description === '';
+        if (! $this->hasGenericType($returnTagValueNode->type)) {
+            return $returnTagValueNode->description === '';
+        }
+
+        return false;
+    }
+
+    private function hasGenericType(BracketsAwareUnionTypeNode $bracketsAwareUnionTypeNode): bool
+    {
+        $types = $bracketsAwareUnionTypeNode->types;
+
+        foreach ($types as $type) {
+            if ($type instanceof GenericTypeNode) {
+                if ($type->type instanceof IdentifierTypeNode && $type->type->name === 'array') {
+                    continue;
+                }
+
+                return true;
+            }
+        }
+
+        return false;
     }
 }
