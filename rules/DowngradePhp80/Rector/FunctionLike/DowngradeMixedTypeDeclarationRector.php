29a6d9420275f0ea7e6b1508ce77503d11d19281
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-08-21 00:45:09 +0700

    [DowngradePhp80] Skip child return parent on DowngradeMixedTypeDeclarationRector (#727)
    * [DowngradePhp80] Skip child return parent on DowngradeMixedTypeDeclarationRector
    
    * fixed

--- rules/DowngradePhp80/Rector/FunctionLike/DowngradeMixedTypeDeclarationRector.php
+++ rules/DowngradePhp80/Rector/FunctionLike/DowngradeMixedTypeDeclarationRector.php
@@ -6,6 +6,7 @@ namespace Rector\DowngradePhp80\Rector\FunctionLike;
 
 use PhpParser\Node;
 use PhpParser\Node\Expr\Closure;
+use PhpParser\Node\Name;
 use PhpParser\Node\Stmt\ClassMethod;
 use PhpParser\Node\Stmt\Function_;
 use PHPStan\Type\MixedType;
@@ -69,6 +70,10 @@ CODE_SAMPLE
      */
     public function refactor(Node $node): ?Node
     {
+        if ($node->returnType instanceof Name && $this->nodeNameResolver->isName($node->returnType, 'parent')) {
+            return null;
+        }
+
         $mixedType = new MixedType();
 
         foreach ($node->getParams() as $param) {
