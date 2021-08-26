e6751a18c7ec5e74a4cfeb54bec40bc301f21efb
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-08-26 21:02:50 +0700

    [ECS] Import SetList ECS after Option Skip (#775)
    * [ECS] Add Comment in ECS config on import Control Structur SetList after Skip Option
    
    * [ECS] Import SetList ECS after Option Skip

--- ecs.php
+++ ecs.php
@@ -52,11 +52,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         __DIR__ . '/scoper.php',
     ]);
 
-    $containerConfigurator->import(SetList::PSR_12);
-    $containerConfigurator->import(SetList::SYMPLIFY);
-    $containerConfigurator->import(SetList::COMMON);
-    $containerConfigurator->import(SetList::CLEAN_CODE);
-
     $parameters->set(Option::SKIP, [
         '*/Source/*',
         '*/Fixture/*',
@@ -97,6 +92,11 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ],
     ]);
 
-    $containerConfigurator->import(SetList::CONTROL_STRUCTURES);
+    // import SetList here on purpose to avoid overridden by existing Skip Option in current config
+    $containerConfigurator->import(SetList::PSR_12);
+    $containerConfigurator->import(SetList::SYMPLIFY);
+    $containerConfigurator->import(SetList::COMMON);
+    $containerConfigurator->import(SetList::CLEAN_CODE);
+
     $parameters->set(Option::LINE_ENDING, "\n");
 };
