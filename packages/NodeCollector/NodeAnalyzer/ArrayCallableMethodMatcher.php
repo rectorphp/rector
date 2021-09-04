3c534be5876d9f5b3dc1ccb0e1a95ec7d664a789
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-09-05 03:57:10 +0700

    [CodeQuality] Skip associative array on CallableThisArrayToAnonymousFunctionRector (#829)
    * [CodeQuality] Skip associative array on CallableThisArrayToAnonymousFunctionRector
    
    * Fixed :tada:
    
    * phpstan
    
    * fix
    
    * final touch: move check to ArrayCallableMethodMatcher
    
    * final touch: cs fix
    
    * final touch: phpstan
    
    * [ci-review] Rector Rectify
    
    * clean up
    
    * reduce complexity when combined with #830
    
    Co-authored-by: GitHub Action <action@github.com>

--- packages/NodeCollector/NodeAnalyzer/ArrayCallableMethodMatcher.php
+++ packages/NodeCollector/NodeAnalyzer/ArrayCallableMethodMatcher.php
@@ -6,6 +6,7 @@ namespace Rector\NodeCollector\NodeAnalyzer;
 
 use PhpParser\Node\Arg;
 use PhpParser\Node\Expr\Array_;
+use PhpParser\Node\Expr\ArrayItem;
 use PhpParser\Node\Expr\ClassConstFetch;
 use PhpParser\Node\Expr\FuncCall;
 use PhpParser\Node\Scalar\String_;
@@ -41,22 +42,24 @@ final class ArrayCallableMethodMatcher
             return null;
         }
 
-        if ($array->items[0] === null) {
+        if ($this->shouldSkipNullItems($array)) {
             return null;
         }
 
-        if ($array->items[1] === null) {
-            return null;
-        }
-
-        $secondItemValue = $array->items[1]->value;
+        /** @var ArrayItem[] $items */
+        $items = $array->items;
+        $secondItemValue = $items[1]->value;
 
         if (! $secondItemValue instanceof String_) {
             return null;
         }
 
+        if ($this->shouldSkipAssociativeArray($array)) {
+            return null;
+        }
+
         // $this, self, static, FQN
-        $firstItemValue = $array->items[0]->value;
+        $firstItemValue = $items[0]->value;
 
         // static ::class reference?
         if ($firstItemValue instanceof ClassConstFetch) {
@@ -81,6 +84,23 @@ final class ArrayCallableMethodMatcher
         }
 
         return new ArrayCallable($firstItemValue, $className, $methodName);
+    }
+
+    private function shouldSkipNullItems(Array_ $array): bool
+    {
+        if ($array->items[0] === null) {
+            return true;
+        }
+
+        return $array->items[1] === null;
+    }
+
+    private function shouldSkipAssociativeArray(Array_ $array): bool
+    {
+        $values = $this->valueResolver->getValue($array);
+        $keys = array_keys($values);
+
+        return $keys !== [0, 1] && $keys !== [1];
     }
 
     /**
