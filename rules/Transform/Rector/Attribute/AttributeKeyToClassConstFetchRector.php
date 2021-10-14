01cb29e5d431df6e3ec5674fdd09b0dc77c17c88
Tomas Votruba <tomas.vot@gmail.com>
2021-10-14 17:20:40 +0200

    [Transform] Add AttributeKeyToClassConstFetchRector (#1002)

---
+++ rules/Transform/Rector/Attribute/AttributeKeyToClassConstFetchRector.php
@@ -0,0 +1,126 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Transform\Rector\Attribute;
+
+use PhpParser\Node;
+use PhpParser\Node\Attribute;
+use PhpParser\Node\Identifier;
+use Rector\Core\Contract\Rector\ConfigurableRectorInterface;
+use Rector\Core\Rector\AbstractRector;
+use Rector\Transform\ValueObject\AttributeKeyToClassConstFetch;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\ConfiguredCodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+use Webmozart\Assert\Assert;
+
+/**
+ * @changelog https://github.com/doctrine/dbal/blob/3.1.x/src/Types/Types.php
+ *
+ * @see \Rector\Tests\Transform\Rector\Attribute\AttributeKeyToClassConstFetchRector\AttributeKeyToClassConstFetchRectorTest
+ */
+final class AttributeKeyToClassConstFetchRector extends AbstractRector implements ConfigurableRectorInterface
+{
+    /**
+     * @var string
+     */
+    public const ATTRIBUTE_KEYS_TO_CLASS_CONST_FETCHES = 'attribute_keys_to_class_const_fetches';
+
+    /**
+     * @var AttributeKeyToClassConstFetch[]
+     */
+    private array $attributeKeysToClassConstFetches = [];
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition('Replace key value on specific attribute to class constant', [
+            new ConfiguredCodeSample(
+                <<<'CODE_SAMPLE'
+use Doctrine\ORM\Mapping\Column;
+
+class SomeClass
+{
+    #[Column(type: "string")]
+    public $name;
+}
+CODE_SAMPLE
+,
+                <<<'CODE_SAMPLE'
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\DBAL\Types\Types;
+
+class SomeClass
+{
+    #[Column(type: Types::STRING)]
+    public $name;
+}
+CODE_SAMPLE
+,
+                [
+                    self::ATTRIBUTE_KEYS_TO_CLASS_CONST_FETCHES => [
+                        new AttributeKeyToClassConstFetch('Doctrine\ORM\Mapping\Column', 'type', 'Doctrine\DBAL\Types\Types', [
+                            'string' => 'STRING',
+                        ]),
+                    ],
+                ]
+            ),
+        ]);
+    }
+
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [Attribute::class];
+    }
+
+    /**
+     * @param Attribute $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        foreach ($this->attributeKeysToClassConstFetches as $attributeKeyToClassConstFetch) {
+            if (! $this->isName($node->name, $attributeKeyToClassConstFetch->getAttributeClass())) {
+                continue;
+            }
+
+            foreach ($node->args as $arg) {
+                $argName = $arg->name;
+                if (! $argName instanceof Identifier) {
+                    continue;
+                }
+
+                if (! $this->isName($argName, $attributeKeyToClassConstFetch->getAttributeKey())) {
+                    continue;
+                }
+
+                $value = $this->valueResolver->getValue($arg->value);
+
+                $constName = $attributeKeyToClassConstFetch->getValuesToConstantsMap()[$value] ?? null;
+                if ($constName === null) {
+                    continue;
+                }
+
+                $arg->value = $this->nodeFactory->createClassConstFetch(
+                    $attributeKeyToClassConstFetch->getConstantClass(),
+                    $constName
+                );
+
+                return $node;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * @param array<string, AttributeKeyToClassConstFetch[]> $configuration
+     */
+    public function configure(array $configuration): void
+    {
+        $attributeKeysToClassConstFetches = $configuration[self::ATTRIBUTE_KEYS_TO_CLASS_CONST_FETCHES] ?? [];
+        Assert::allIsAOf($attributeKeysToClassConstFetches, AttributeKeyToClassConstFetch::class);
+        $this->attributeKeysToClassConstFetches = $attributeKeysToClassConstFetches;
+    }
+}
---
+++ rules/Transform/ValueObject/AttributeKeyToClassConstFetch.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Transform\ValueObject;
+
+final class AttributeKeyToClassConstFetch
+{
+    /**
+     * @param array<mixed, string> $valuesToConstantsMap
+     */
+    public function __construct(
+        private string $attributeClass,
+        private string $attributeKey,
+        private string $constantClass,
+        private array $valuesToConstantsMap
+    ) {
+    }
+
+    public function getAttributeClass(): string
+    {
+        return $this->attributeClass;
+    }
+
+    public function getAttributeKey(): string
+    {
+        return $this->attributeKey;
+    }
+
+    public function getConstantClass(): string
+    {
+        return $this->constantClass;
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    public function getValuesToConstantsMap(): array
+    {
+        return $this->valuesToConstantsMap;
+    }
+}
