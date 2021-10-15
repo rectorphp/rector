2d6a25bc49178fd8977a0e707b6519e3523fd703
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-10-15 23:24:50 +0700

    [Php73] Skip double escape dash on next with quantifier outside bracket on RegexDashEscapeRector (#1006)
    * [Php73] Skip escape dash outside bracket on RegexDashEscapeRector
    
    * rename fixture
    
    * rename
    
    * fix more quantifiers
    
    * final touch
    
    * add more fixture

--- rules/Php73/Rector/FuncCall/RegexDashEscapeRector.php
+++ rules/Php73/Rector/FuncCall/RegexDashEscapeRector.php
@@ -39,9 +39,9 @@ final class RegexDashEscapeRector extends AbstractRector implements MinPhpVersio
 
     /**
      * @var string
-     * @see https://regex101.com/r/TBVme9/3
+     * @see https://regex101.com/r/TBVme9/8
      */
-    private const RIGHT_HAND_UNESCAPED_DASH_REGEX = '#(?<!\[)-(\\\\(w|s|d)[^\?]*?)\]#i';
+    private const RIGHT_HAND_UNESCAPED_DASH_REGEX = '#(?<!\[)-(\\\\(w|s|d)[.*]?)\]#i';
 
     public function __construct(
         private RegexPatternArgumentManipulator $regexPatternArgumentManipulator
