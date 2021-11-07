d5f92968c042fdcc3a5acc6d1d95f117289fec3b
Tomas Votruba <tomas.vot@gmail.com>
2021-11-07 16:32:16 +0100

    Check namespaced name on non-anonymous class (#1180)

--- rules/CodingStyle/ClassNameImport/UsedImportsResolver.php
+++ rules/CodingStyle/ClassNameImport/UsedImportsResolver.php
@@ -26,11 +26,10 @@ final class UsedImportsResolver
      */
     public function resolveForNode(Node $node): array
     {
-        if ($node instanceof Namespace_) {
-            $namespace = $node;
-        } else {
-            $namespace = $this->betterNodeFinder->findParentType($node, Namespace_::class);
-        }
+        $namespace = $node instanceof Namespace_ ? $node : $this->betterNodeFinder->findParentType(
+            $node,
+            Namespace_::class
+        );
 
         if ($namespace instanceof Namespace_) {
             return $this->resolveForNamespace($namespace);
@@ -51,7 +50,8 @@ final class UsedImportsResolver
         $class = $this->betterNodeFinder->findFirstInstanceOf($stmts, Class_::class);
 
         // add class itself
-        if ($class !== null) {
+        // is not anonymous class
+        if ($class !== null && property_exists($class, 'namespacedName')) {
             $className = $class->namespacedName->toString();
             $usedImports[] = new FullyQualifiedObjectType($className);
         }
