02def089cc9974bc58cbeb083acd387c742cf742
Fabien Villepinte <fabien.villepinte@gmail.com>
2021-11-09 23:01:30 +0100

    [Downgrade PHP 5.4] Add ShortArrayToLongArrayRector (#1198)

--- rules-tests/DowngradePhp54/Rector/Array_/ShortArrayToLongArrayRector/config/configured_rule.php
+++ rules-tests/DowngradePhp54/Rector/Array_/ShortArrayToLongArrayRector/config/configured_rule.php
@@ -2,17 +2,11 @@
 
 declare(strict_types=1);
 
-use Rector\Core\Configuration\Option;
-use Rector\Core\ValueObject\PhpVersion;
 use Rector\DowngradePhp54\Rector\Array_\ShortArrayToLongArrayRector;
-use Rector\DowngradePhp54\Rector\Closure\DowngradeStaticClosureRector;
+
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_53);
-
     $services = $containerConfigurator->services();
     $services->set(ShortArrayToLongArrayRector::class);
-    $services->set(DowngradeStaticClosureRector::class);
 };
