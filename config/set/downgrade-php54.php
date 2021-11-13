82de3667411f8e6e9461d0a7f48974badab957c0
Fabien Villepinte <fabien.villepinte@gmail.com>
2021-11-13 20:47:32 +0100

    [Downgrade PHP 5.4] Add DowngradeInstanceMethodCallRector (#1226)

--- config/set/downgrade-php54.php
+++ config/set/downgrade-php54.php
@@ -8,6 +8,7 @@ use Rector\DowngradePhp54\Rector\Array_\ShortArrayToLongArrayRector;
 use Rector\DowngradePhp54\Rector\Closure\DowngradeStaticClosureRector;
 use Rector\DowngradePhp54\Rector\FunctionLike\DowngradeCallableTypeDeclarationRector;
 use Rector\DowngradePhp54\Rector\LNumber\DowngradeBinaryNotationRector;
+use Rector\DowngradePhp54\Rector\MethodCall\DowngradeInstanceMethodCallRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
@@ -19,4 +20,5 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->set(DowngradeStaticClosureRector::class);
     $services->set(DowngradeCallableTypeDeclarationRector::class);
     $services->set(DowngradeBinaryNotationRector::class);
+    $services->set(DowngradeInstanceMethodCallRector::class);
 };
