8bcd5577db48647750b6b191cf504db73319c4dd
Tomas Votruba <tomas.vot@gmail.com>
2021-11-15 11:48:42 +0300

    Revert "[StaticTypeMapper] Avoid error Rector\StaticTypeMapper\PhpDocParser\IdentifierTypeMapper::mapStatic() must implement interface PHPStan\Analyser\Scope, null given" (#1241)

--- packages/StaticTypeMapper/PhpDocParser/IdentifierTypeMapper.php
+++ packages/StaticTypeMapper/PhpDocParser/IdentifierTypeMapper.php
@@ -86,6 +86,8 @@ final class IdentifierTypeMapper implements PhpDocTypeMapperInterface
         }
 
         $objectType = new ObjectType($typeNode->name);
+
+        $scope = $node->getAttribute(AttributeKey::SCOPE);
         return $this->objectTypeSpecifier->narrowToFullyQualifiedOrAliasedObjectType($node, $objectType, $scope);
     }
 
@@ -106,12 +108,8 @@ final class IdentifierTypeMapper implements PhpDocTypeMapperInterface
         return new SelfObjectType($className);
     }
 
-    private function mapParent(?Scope $scope): ParentStaticType | MixedType
+    private function mapParent(Scope $scope): ParentStaticType | MixedType
     {
-        if (! $scope instanceof Scope) {
-            return new MixedType();
-        }
-
         $parentClassReflection = $this->parentClassScopeResolver->resolveParentClassReflection($scope);
         if (! $parentClassReflection instanceof ClassReflection) {
             return new MixedType();
@@ -120,12 +118,8 @@ final class IdentifierTypeMapper implements PhpDocTypeMapperInterface
         return new ParentStaticType($parentClassReflection);
     }
 
-    private function mapStatic(?Scope $scope): MixedType | StaticType
+    private function mapStatic(Scope $scope): MixedType | StaticType
     {
-        if (! $scope instanceof Scope) {
-            return new MixedType();
-        }
-
         $classReflection = $scope->getClassReflection();
         if (! $classReflection instanceof ClassReflection) {
             return new MixedType();
