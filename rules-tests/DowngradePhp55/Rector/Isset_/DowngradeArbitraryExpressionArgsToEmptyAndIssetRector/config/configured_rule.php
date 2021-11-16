5e39ff5889b08dc391a3114dd4746536501ecf75
Fabien Villepinte <fabien.villepinte@gmail.com>
2021-11-16 16:40:49 +0100

    [Downgrade PHP 5.5] Add DowngradeArbitraryExpressionArgsToEmptyAndIssetRector (#1250)
    * [Downgrade PHP 5.5] Add DowngradeArbitraryExpressionArgsToEmptyAndIssetRector
    
    * Fix support for ArrayDimFetch

--- rules-tests/DowngradePhp55/Rector/Isset_/DowngradeArbitraryExpressionArgsToEmptyAndIssetRector/config/configured_rule.php
+++ rules-tests/DowngradePhp55/Rector/Isset_/DowngradeArbitraryExpressionArgsToEmptyAndIssetRector/config/configured_rule.php
@@ -2,17 +2,11 @@
 
 declare(strict_types=1);
 
-use Rector\Core\Configuration\Option;
-use Rector\Core\ValueObject\PhpVersion;
-use Rector\DowngradePhp55\Rector\ClassConstFetch\DowngradeClassConstantToStringRector;
 use Rector\DowngradePhp55\Rector\Isset_\DowngradeArbitraryExpressionArgsToEmptyAndIssetRector;
+
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_54);
-
     $services = $containerConfigurator->services();
-    $services->set(DowngradeClassConstantToStringRector::class);
     $services->set(DowngradeArbitraryExpressionArgsToEmptyAndIssetRector::class);
 };
