a86cd63bd39b333e92ef22634253195461b10a59
Fabien Villepinte <fabien.villepinte@gmail.com>
2021-11-17 12:20:49 +0100

    [Downgrade PHP 5.5] Add DowngradeForeachListRector (#1256)

--- config/set/downgrade-php55.php
+++ config/set/downgrade-php55.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Rector\Core\Configuration\Option;
 use Rector\Core\ValueObject\PhpVersion;
 use Rector\DowngradePhp55\Rector\ClassConstFetch\DowngradeClassConstantToStringRector;
+use Rector\DowngradePhp55\Rector\Foreach_\DowngradeForeachListRector;
 use Rector\DowngradePhp55\Rector\Isset_\DowngradeArbitraryExpressionArgsToEmptyAndIssetRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
@@ -14,5 +15,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $services = $containerConfigurator->services();
     $services->set(DowngradeClassConstantToStringRector::class);
+    $services->set(DowngradeForeachListRector::class);
     $services->set(DowngradeArbitraryExpressionArgsToEmptyAndIssetRector::class);
 };
---
+++ rules/DowngradePhp55/Rector/Foreach_/DowngradeForeachListRector.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\DowngradePhp55\Rector\Foreach_;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr;
+use PhpParser\Node\Expr\Assign;
+use PhpParser\Node\Expr\List_;
+use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Stmt\Expression;
+use PhpParser\Node\Stmt\Foreach_;
+use Rector\Core\Rector\AbstractRector;
+use Rector\Naming\Naming\VariableNaming;
+use Rector\NodeTypeResolver\Node\AttributeKey;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+use Webmozart\Assert\Assert;
+
+/**
+ * @changelog https://wiki.php.net/rfc/foreachlist
+ *
+ * @see Rector\Tests\DowngradePhp55\Rector\Foreach_\DowngradeForeachListRector\DowngradeForeachListRectorTest
+ */
+final class DowngradeForeachListRector extends AbstractRector
+{
+    public function __construct(
+        private VariableNaming $variableNaming
+    ) {
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'Downgrade list() support in foreach constructs',
+            [
+                new CodeSample(
+                    <<<'CODE_SAMPLE'
+foreach ($array as $key => list($item1, $item2)) {
+    var_dump($item1, $item2);
+}
+CODE_SAMPLE
+                    ,
+                    <<<'CODE_SAMPLE'
+foreach ($array as $key => arrayItem) {
+    list($item1, $item2) = $arrayItem;
+    var_dump($item1, $item2);
+}
+CODE_SAMPLE
+                ),
+            ]
+        );
+    }
+
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [Foreach_::class];
+    }
+
+    /**
+     * @param Foreach_ $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if (! $node->valueVar instanceof List_) {
+            return null;
+        }
+
+        $variable = $this->createVariable($node);
+        $exprAssign = new Expression(new Assign($node->valueVar, $variable));
+        $node->valueVar = $variable;
+        $node->stmts = array_merge([$exprAssign], $node->stmts);
+
+        return $node;
+    }
+
+    private function createVariable(Foreach_ $foreach): Variable
+    {
+        $currentStmt = $foreach->getAttribute(AttributeKey::CURRENT_STATEMENT);
+        $scope = $currentStmt->getAttribute(AttributeKey::SCOPE);
+
+        return new Variable($this->variableNaming->createCountedValueName('arrayItem', $scope));
+    }
+}
