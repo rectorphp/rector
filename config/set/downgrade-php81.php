59809f30caa68689bb707d99e4f051f2eabd9c01
Fabien Villepinte <fabien.villepinte@gmail.com>
2021-11-18 19:35:12 +0100

    [Downgrade PHP 8.1] Add DowngradeFirstClassCallableSyntaxRector (#1267)

--- config/set/downgrade-php81.php
+++ config/set/downgrade-php81.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Rector\Core\Configuration\Option;
 use Rector\Core\ValueObject\PhpVersion;
 use Rector\DowngradePhp81\Rector\ClassConst\DowngradeFinalizePublicClassConstantRector;
+use Rector\DowngradePhp81\Rector\FuncCall\DowngradeFirstClassCallableSyntaxRector;
 use Rector\DowngradePhp81\Rector\FunctionLike\DowngradeNeverTypeDeclarationRector;
 use Rector\DowngradePhp81\Rector\Instanceof_\DowngradePhp81ResourceReturnToObjectRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -15,6 +16,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $services = $containerConfigurator->services();
     $services->set(DowngradeFinalizePublicClassConstantRector::class);
+    $services->set(DowngradeFirstClassCallableSyntaxRector::class);
     $services->set(DowngradeNeverTypeDeclarationRector::class);
     $services->set(DowngradePhp81ResourceReturnToObjectRector::class);
 };
