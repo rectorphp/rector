4ad4d16ac63bd20127e23389c40e5cf7296c41ea
Tomas Votruba <tomas.vot@gmail.com>
2021-11-29 02:53:11 +0300

    Fix child method check - both on native (#1336)

--- packages/FamilyTree/NodeAnalyzer/ClassChildAnalyzer.php
+++ packages/FamilyTree/NodeAnalyzer/ClassChildAnalyzer.php
@@ -20,7 +20,7 @@ final class ClassChildAnalyzer
         $childrenClassReflections = $this->familyRelationsAnalyzer->getChildrenOfClassReflection($classReflection);
 
         foreach ($childrenClassReflections as $childClassReflection) {
-            if (! $childClassReflection->hasMethod($methodName)) {
+            if (! $childClassReflection->hasNativeMethod($methodName)) {
                 continue;
             }
 
