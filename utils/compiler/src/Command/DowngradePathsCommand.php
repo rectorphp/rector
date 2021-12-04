b7ef6368f3f45670019f1236ea567e634830b379
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-12-04 23:19:44 +0700

    [Scoped] Add missing utils to downgrade paths (#1389)

--- utils/compiler/src/Command/DowngradePathsCommand.php
+++ utils/compiler/src/Command/DowngradePathsCommand.php
@@ -47,6 +47,7 @@ final class DowngradePathsCommand extends Command
             'vendor/symplify vendor/nikic src packages',
             'vendor/ssch',
             'rules',
+            'utils',
             'bin rector.php',
         ], $downgradePaths);
 
