dda87fc6d2b8632967f4907bab94918c18822807
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-12-06 13:39:12 +0700

    [VendorLocker] Allow both in vendor for child and parent on ParentClassMethodTypeOverrideGuard (#1399)
    * [VendorLocker] Allow both in vendor for child and parent on ParentClassMethodTypeOverrideGuard
    
    * fix check
    
    * add test from anonymous class to check that file name of current class method will always found

--- packages/VendorLocker/ParentClassMethodTypeOverrideGuard.php
+++ packages/VendorLocker/ParentClassMethodTypeOverrideGuard.php
@@ -51,8 +51,32 @@ final class ParentClassMethodTypeOverrideGuard
             return false;
         }
 
+        /*
+         * Below verify that both current file name and parent file name is not in the /vendor/, if yes, then allowed.
+         * This can happen when rector run into /vendor/ directory while child and parent both are there.
+         *
+         *  @see https://3v4l.org/Rc0RF#v8.0.13
+         *
+         *     - both in /vendor/ -> allowed
+         *     - one of them in /vendor/ -> not allowed
+         *     - both not in /vendor/ -> allowed
+         */
+        /** @var Scope $scope */
+        $scope = $classMethod->getAttribute(AttributeKey::SCOPE);
+        /** @var ClassReflection $currentClassReflection */
+        $currentClassReflection = $scope->getClassReflection();
+        /** @var string $currentFileName */
+        $currentFileName = $currentClassReflection->getFileName();
+
+        // child (current)
+        $normalizedCurrentFileName = $this->pathNormalizer->normalizePath($currentFileName);
+        $isCurrentInVendor = str_contains($normalizedCurrentFileName, '/vendor/');
+
+        // parent
         $normalizedFileName = $this->pathNormalizer->normalizePath($fileName);
-        return ! str_contains($normalizedFileName, '/vendor/');
+        $isParentInVendor = str_contains($normalizedFileName, '/vendor/');
+
+        return ($isCurrentInVendor && $isParentInVendor) || (! $isCurrentInVendor && ! $isParentInVendor);
     }
 
     public function hasParentClassMethod(ClassMethod $classMethod): bool
---
+++ rules-tests/TypeDeclaration/Rector/ClassMethod/ReturnNeverTypeRector/Source/MixedReturn.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Tests\TypeDeclaration\Rector\ClassMethod\ReturnNeverTypeRector\Source;
+
+interface MixedReturn
+{
+    public function run();
+}
