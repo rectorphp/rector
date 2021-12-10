eb76f4562c573ac067c46ab32d4429696b3ab66a
Dan <self@danpock.me>
2021-12-09 19:56:36 -0500

    [8.2] Add a RemoveAllowDynamicPropertyAttribute rule (#1392)
    Co-authored-by: Abdul Malik Ikhsan <samsonasik@gmail.com>

---
+++ rules/Transform/Rector/Class_/RemoveAllowDynamicPropertiesAttributeRector.php
@@ -0,0 +1,131 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Transform\Rector\Class_;
+
+use PhpParser\Node;
+use PhpParser\Node\Stmt\Class_;
+use Rector\Core\Contract\Rector\AllowEmptyConfigurableRectorInterface;
+use Rector\Core\Rector\AbstractRector;
+use Rector\Php80\NodeAnalyzer\PhpAttributeAnalyzer;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\ConfiguredCodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+use Webmozart\Assert\Assert;
+
+/**
+ * @changelog https://wiki.php.net/rfc/deprecate_dynamic_properties
+ *
+ * @see \Rector\Tests\Transform\Rector\Class_\RemoveAllowDynamicPropertiesAttributeRector\RemoveAllowDynamicPropertiesAttributeRectorTest
+ */
+final class RemoveAllowDynamicPropertiesAttributeRector extends AbstractRector implements AllowEmptyConfigurableRectorInterface
+{
+    /**
+     * @var string
+     */
+    private const ATTRIBUTE = 'AllowDynamicProperties';
+
+    public const TRANSFORM_ON_NAMESPACES = 'transform_on_namespaces';
+
+    /**
+     * @var array<array-key, string>
+     */
+    private array $transformOnNamespaces = [];
+
+    public function __construct(
+        private PhpAttributeAnalyzer $phpAttributeAnalyzer,
+    ) {
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition('Remove the `AllowDynamicProperties` attribute from all classes', [
+            new ConfiguredCodeSample(
+                <<<'CODE_SAMPLE'
+namespace Example\Domain;
+
+#[AllowDynamicProperties]
+class SomeObject {
+    public string $someProperty = 'hello world';
+}
+CODE_SAMPLE,
+
+                <<<'CODE_SAMPLE'
+namespace Example\Domain;
+
+class SomeObject {
+    public string $someProperty = 'hello world';
+}
+CODE_SAMPLE,
+                [
+                    RemoveAllowDynamicPropertiesAttributeRector::TRANSFORM_ON_NAMESPACES => [
+                        'Example\*'
+                    ]
+                ]
+            ),
+        ]);
+    }
+
+    /**
+     * @return array<class-string<Node>>
+     */
+    public function getNodeTypes(): array
+    {
+        return [Class_::class];
+    }
+
+    public function configure(array $configuration): void
+    {
+        $transformOnNamespaces = $configuration[self::TRANSFORM_ON_NAMESPACES] ?? $configuration;
+
+        Assert::isArray($transformOnNamespaces);
+        Assert::allString($transformOnNamespaces);
+
+        $this->transformOnNamespaces = $transformOnNamespaces;
+    }
+
+    /**
+     * @param Class_ $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if ($this->shouldRemove($node)) {
+            return $this->removeAllowDynamicPropertiesAttribute($node);
+        }
+
+        return null;
+    }
+
+    private function removeAllowDynamicPropertiesAttribute(Class_ $class): Class_
+    {
+        $newAttrGroups = [];
+        foreach ($class->attrGroups as $attrGroup) {
+            $newAttrs = [];
+            foreach ($attrGroup->attrs as $attribute) {
+                if (! $this->nodeNameResolver->isName($attribute, self::ATTRIBUTE)) {
+                    $newAttrs[] = $attribute;
+                }
+            }
+            $attrGroup->attrs = $newAttrs;
+            if (count($attrGroup->attrs) !== 0) {
+                $newAttrGroups[] = $attrGroup;
+            }
+        }
+        $class->attrGroups = $newAttrGroups;
+        return $class;
+    }
+
+    private function shouldRemove(Class_ $class): bool
+    {
+        if (count($this->transformOnNamespaces) !== 0) {
+            $className = (string) $this->nodeNameResolver->getName($class);
+            foreach ($this->transformOnNamespaces as $transformOnNamespace) {
+                if (! $this->nodeNameResolver->isStringName($className, $transformOnNamespace)) {
+                    return false;
+                }
+            }
+        }
+
+        return $this->phpAttributeAnalyzer->hasPhpAttribute($class, self::ATTRIBUTE);
+    }
+}
