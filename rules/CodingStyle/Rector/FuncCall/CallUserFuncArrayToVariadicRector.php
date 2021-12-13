363908f47774ef1f9d003d0946ea5f3439c8b44b
Tomas Votruba <tomas.vot@gmail.com>
2021-12-13 23:14:27 +0100

    Fix phpstan configured parser to treat project with its code version (#1480)

--- rules/CodingStyle/Rector/FuncCall/CallUserFuncArrayToVariadicRector.php
+++ rules/CodingStyle/Rector/FuncCall/CallUserFuncArrayToVariadicRector.php
@@ -82,10 +82,11 @@ CODE_SAMPLE
         }
 
         /** @var Arg $firstArg */
-        $firstArg = $node->args[0];
+        $firstArg = $node->getArgs()[0];
         $firstArgValue = $firstArg->value;
+
         /** @var Arg $secondArg */
-        $secondArg = $node->args[1];
+        $secondArg = $node->getArgs()[1];
         $secondArgValue = $secondArg->value;
 
         if ($firstArgValue instanceof String_) {
