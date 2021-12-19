dbd10cf9f02bb4b31579adfe738bcd8d4df794ac
Tomas Votruba <tomas.vot@gmail.com>
2021-12-19 02:40:40 +0100

    [Doctrine] Extend list of repeated nested attributes (#1523)

--- packages/PhpAttribute/UnwrapableAnnotationAnalyzer.php
+++ packages/PhpAttribute/UnwrapableAnnotationAnalyzer.php
@@ -6,20 +6,33 @@ namespace Rector\PhpAttribute;
 
 use Rector\BetterPhpDocParser\PhpDoc\DoctrineAnnotationTagValueNode;
 use Rector\BetterPhpDocParser\ValueObject\PhpDocAttributeKey;
+use Rector\Core\Php\PhpVersionProvider;
+use Rector\Core\ValueObject\PhpVersionFeature;
 use Rector\Php80\ValueObject\AnnotationToAttribute;
 
 final class UnwrapableAnnotationAnalyzer
 {
     /**
      * List of annotation classes that can be un-wrapped
+     * @see https://github.com/doctrine/orm/commit/b6b3c974361d7042e4b7d868fb34daca76bb2a48 - repeatable attributes
      * @var string[]
      */
-    private const UNWRAPEABLE_ANNOTATION_CLASSES = ['Doctrine\ORM\Mapping\UniqueConstraint'];
+    private const UNWRAPEABLE_ANNOTATION_CLASSES = [
+        'Doctrine\ORM\Mapping\UniqueConstraint',
+        'Doctrine\ORM\Mapping\JoinColumn',
+        'Doctrine\ORM\Mapping\Index',
+        'Doctrine\ORM\Mapping\AttributeOverride',
+    ];
 
     /**
      * @var AnnotationToAttribute[]
      */
     private array $annotationsToAttributes = [];
+
+    public function __construct(
+        private readonly PhpVersionProvider $phpVersionProvider
+    ) {
+    }
 
     /**
      * @param AnnotationToAttribute[] $annotationsToAttributes
@@ -34,6 +47,11 @@ final class UnwrapableAnnotationAnalyzer
      */
     public function areUnwrappable(array $doctrineAnnotationTagValueNodes): bool
     {
+        // the new in initilazers is handled directly
+        if ($this->phpVersionProvider->isAtLeastPhpVersion(PhpVersionFeature::NEW_INITIALIZERS)) {
+            return false;
+        }
+
         foreach ($doctrineAnnotationTagValueNodes as $doctrineAnnotationTagValueNode) {
             $annotationClassName = $doctrineAnnotationTagValueNode->identifierTypeNode->getAttribute(
                 PhpDocAttributeKey::RESOLVED_CLASS
