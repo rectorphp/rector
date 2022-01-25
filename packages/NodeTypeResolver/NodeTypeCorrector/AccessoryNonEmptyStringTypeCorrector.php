1c651e2d02058a7370f65cf3ac3ce75c8b265535
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-01-25 21:19:09 +0700

    [TypeDeclaration] Skip Prevent object in intersection type on ReturnTypeDeclarationRector (#1727)
    Co-authored-by: Dominik Peters <kuhlesdominik@gmx.de>

--- packages/NodeTypeResolver/NodeTypeCorrector/AccessoryNonEmptyStringTypeCorrector.php
+++ packages/NodeTypeResolver/NodeTypeCorrector/AccessoryNonEmptyStringTypeCorrector.php
@@ -6,23 +6,28 @@ namespace Rector\NodeTypeResolver\NodeTypeCorrector;
 
 use PHPStan\Type\Accessory\AccessoryNonEmptyStringType;
 use PHPStan\Type\IntersectionType;
+use PHPStan\Type\ObjectWithoutClassType;
 use PHPStan\Type\Type;
 
 final class AccessoryNonEmptyStringTypeCorrector
 {
+    /**
+     * @var array<class-string<Type>>
+     */
+    private const INTERSECTION_DISALLOWED_TYPES = [
+        AccessoryNonEmptyStringType::class,
+        ObjectWithoutClassType::class,
+    ];
+
     public function correct(Type $mainType): Type | IntersectionType
     {
         if (! $mainType instanceof IntersectionType) {
             return $mainType;
         }
 
-        if (! $mainType->isSubTypeOf(new AccessoryNonEmptyStringType())->yes()) {
-            return $mainType;
-        }
-
         $clearIntersectionedTypes = [];
         foreach ($mainType->getTypes() as $intersectionedType) {
-            if ($intersectionedType instanceof AccessoryNonEmptyStringType) {
+            if (in_array($intersectionedType::class, self::INTERSECTION_DISALLOWED_TYPES, true)) {
                 continue;
             }
 
@@ -31,6 +36,12 @@ final class AccessoryNonEmptyStringTypeCorrector
 
         if (count($clearIntersectionedTypes) === 1) {
             return $clearIntersectionedTypes[0];
+        }
+
+        $countIntersectionTypes = count($mainType->getTypes());
+        $countClearIntersectionedTypes = count($clearIntersectionedTypes);
+        if ($countIntersectionTypes === $countClearIntersectionedTypes) {
+            return $mainType;
         }
 
         return new IntersectionType($clearIntersectionedTypes);
