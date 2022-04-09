211a3b6bd05abdd4d9e407c9b928d1c19f2b033b
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-04-09 14:29:46 +0700

    [Php74] Skip by ref used by inner Closure use on ClosureToArrowFunctionRector (#2031)
    * [Php74] Skip by ref used by inner Closure use on ClosureToArrowFunctionRector
    
    * Fixed :tada:
    
    * phpstan
    
    * final touch: variable
    
    * clean up

--- rules/Php74/NodeAnalyzer/ClosureArrowFunctionAnalyzer.php
+++ rules/Php74/NodeAnalyzer/ClosureArrowFunctionAnalyzer.php
@@ -52,7 +52,7 @@ final class ClosureArrowFunctionAnalyzer
             return false;
         }
 
-        return (bool) $this->betterNodeFinder->findFirstInFunctionLikeScoped($closure, function (Node $node) use (
+        $isFoundInStmt = (bool) $this->betterNodeFinder->findFirstInFunctionLikeScoped($closure, function (Node $node) use (
             $referencedValues
         ): bool {
             foreach ($referencedValues as $referencedValue) {
@@ -63,6 +63,43 @@ final class ClosureArrowFunctionAnalyzer
 
             return false;
         });
+
+        if ($isFoundInStmt) {
+            return true;
+        }
+
+        return $this->isFoundInInnerUses($closure, $referencedValues);
+    }
+
+    /**
+     * @param Variable[] $referencedValues
+     */
+    private function isFoundInInnerUses(Closure $node, array $referencedValues): bool
+    {
+        foreach ($referencedValues as $referencedValue) {
+            $isFoundInInnerUses = (bool) $this->betterNodeFinder->findFirstInFunctionLikeScoped(
+                $node,
+                function (Node $subNode) use ($referencedValue): bool {
+                    if (! $subNode instanceof Closure) {
+                        return false;
+                    }
+
+                    return (bool) array_filter(
+                        $subNode->uses,
+                        fn (ClosureUse $closureUse): bool => $closureUse->byRef && $this->nodeComparator->areNodesEqual(
+                            $closureUse->var,
+                            $referencedValue
+                        )
+                    );
+                }
+            );
+
+            if ($isFoundInInnerUses) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     /**
