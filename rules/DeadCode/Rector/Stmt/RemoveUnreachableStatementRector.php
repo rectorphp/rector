469f0eed632bd7db26bcfe63a9545f9c5396e66c
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-04-24 01:51:57 +0700

    [DeadCode] Register more Stmt Nodes on RemoveUnreachableStatementRector (#2140)
    * [DeadCode] Register more Stmt Nodes on RemoveUnreachableStatementRector
    
    * add try
    
    * final touch: exclude ArrowFunction FunctionLike as only 1 stmt
    
    * [ci-review] Rector Rectify
    
    Co-authored-by: GitHub Action <action@github.com>

--- rules/DeadCode/Rector/Stmt/RemoveUnreachableStatementRector.php
+++ rules/DeadCode/Rector/Stmt/RemoveUnreachableStatementRector.php
@@ -5,18 +5,26 @@ declare(strict_types=1);
 namespace Rector\DeadCode\Rector\Stmt;
 
 use PhpParser\Node;
+use PhpParser\Node\Expr\Closure;
 use PhpParser\Node\Expr\Exit_;
-use PhpParser\Node\FunctionLike;
 use PhpParser\Node\Stmt;
+use PhpParser\Node\Stmt\Case_;
+use PhpParser\Node\Stmt\Catch_;
+use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\Node\Stmt\Do_;
 use PhpParser\Node\Stmt\Else_;
+use PhpParser\Node\Stmt\ElseIf_;
 use PhpParser\Node\Stmt\Expression;
 use PhpParser\Node\Stmt\Finally_;
+use PhpParser\Node\Stmt\For_;
 use PhpParser\Node\Stmt\Foreach_;
+use PhpParser\Node\Stmt\Function_;
 use PhpParser\Node\Stmt\If_;
 use PhpParser\Node\Stmt\Nop;
 use PhpParser\Node\Stmt\Return_;
 use PhpParser\Node\Stmt\Throw_;
 use PhpParser\Node\Stmt\TryCatch;
+use PhpParser\Node\Stmt\While_;
 use Rector\Core\Rector\AbstractRector;
 use Rector\NodeTypeResolver\Node\AttributeKey;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
@@ -63,11 +71,26 @@ CODE_SAMPLE
      */
     public function getNodeTypes(): array
     {
-        return [Foreach_::class, FunctionLike::class, Else_::class, If_::class];
+        return [
+            For_::class,
+            Foreach_::class,
+            Do_::class,
+            While_::class,
+            ClassMethod::class,
+            Function_::class,
+            Closure::class,
+            If_::class,
+            ElseIf_::class,
+            Else_::class,
+            Case_::class,
+            TryCatch::class,
+            Catch_::class,
+            Finally_::class,
+        ];
     }
 
     /**
-     * @param Foreach_|FunctionLike|Else_|If_ $node
+     * @param For_|Foreach_|Do_|While_|ClassMethod|Function_|Closure|If_|ElseIf_|Else_|Case_|TryCatch|Catch_|Finally_ $node
      */
     public function refactor(Node $node): ?Node
     {
