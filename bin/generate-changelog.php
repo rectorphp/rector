aa89fde52b66924e3b52ab7a106396c2b2aeb850
Tomas Votruba <tomas.vot@gmail.com>
2022-04-28 00:32:10 +0200

    link the token

--- bin/generate-changelog.php
+++ bin/generate-changelog.php
@@ -20,6 +20,8 @@ require __DIR__ . '/../vendor/autoload.php';
  * Usage:
  * GITHUB_TOKEN=<github_token> php bin/generate-changelog.php <from-commit> <to-commit> >> <file_to_dump.md>
  * GITHUB_TOKEN=ghp_... php bin/generate-changelog.php 07736c1 cb74bb6 >> CHANGELOG_dumped.md
+ *
+ * Generate the Composer token here: https://github.com/settings/tokens/new
  */
 final class GenerateChangelogCommand extends Command
 {
