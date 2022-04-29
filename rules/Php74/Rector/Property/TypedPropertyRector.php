dbc2638e4267c557e55bfe2372a8c91c72cec736
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-04-29 21:44:44 +0700

    [Php74][TypeDeclaration] Document for INLINE_PUBLIC constant set to false by default allow change protected on final with extends as far as property and/or its usage only in current class (#2198)

--- rules/Php74/Rector/Property/TypedPropertyRector.php
+++ rules/Php74/Rector/Property/TypedPropertyRector.php
@@ -50,7 +50,7 @@ final class TypedPropertyRector extends AbstractRector implements AllowEmptyConf
      * Default to false, which only apply changes:
      *
      *  – private modifier property
-     *  - protected modifier property on final class without extends
+     *  - protected modifier property on final class without extends or has extends but property and/or its usage only in current class
      *
      * Set to true will allow change other modifiers as well as far as not forbidden, eg: callable type, null type, etc.
      */
