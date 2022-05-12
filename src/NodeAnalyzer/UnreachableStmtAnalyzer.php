e88903f64e4075478882f15443cdffa7c16f3003
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-05-12 17:20:20 +0700

    [Core] Improve UnreachableStmtAnalyzer: verify itself by recursive call (#2300)

--- src/NodeAnalyzer/UnreachableStmtAnalyzer.php
+++ src/NodeAnalyzer/UnreachableStmtAnalyzer.php
@@ -4,30 +4,23 @@ declare(strict_types=1);
 
 namespace Rector\Core\NodeAnalyzer;
 
-use PhpParser\Node;
 use PhpParser\Node\Stmt;
 use Rector\NodeTypeResolver\Node\AttributeKey;
 
 final class UnreachableStmtAnalyzer
 {
-    public function isStmtPHPStanUnreachable(Stmt $stmt): bool
+    public function isStmtPHPStanUnreachable(?Stmt $stmt): bool
     {
+        if (! $stmt instanceof Stmt) {
+            return false;
+        }
+
         if ($stmt->getAttribute(AttributeKey::IS_UNREACHABLE) === true) {
             // here the scope is never available for next stmt so we have nothing to refresh
             return true;
         }
 
-        $previousStmt = $stmt;
-        while ($previousStmt = $previousStmt->getAttribute(AttributeKey::PREVIOUS_NODE)) {
-            if (! $previousStmt instanceof Node) {
-                break;
-            }
-
-            if ($previousStmt->getAttribute(AttributeKey::IS_UNREACHABLE) === true) {
-                return true;
-            }
-        }
-
-        return false;
+        $previousStmt = $stmt->getAttribute(AttributeKey::PREVIOUS_NODE);
+        return $this->isStmtPHPStanUnreachable($previousStmt);
     }
 }
