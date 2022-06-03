e8f058a6e473078290b3235f0b4472b02008f0ba
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-06-04 03:41:31 +0700

    [e2e] Add e2e for parallel process with current directory contains space  (#2421)
    * [e2e] Add e2e for parallel process with current directory contains space
    
    * [e2e] Add e2e for parallel process with current directory contains space
    
    * dir name
    
    * Fixed :tada:
    
    * phpstan
    
    * final touch: eol
    
    * clean up
    
    * clean up
    
    * final touch: use escapeshellarg()
    
    * Add note for why escapeshellarg() is needed in parallel

---
+++ e2e/parallel with space/rector.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__ . '/src',
+    ]);
+    $rectorConfig->parallel();
+};
--- packages/Parallel/Command/WorkerCommandLineFactory.php
+++ packages/Parallel/Command/WorkerCommandLineFactory.php
@@ -93,7 +93,24 @@ final class WorkerCommandLineFactory
 
         if ($input->hasOption(Option::CONFIG)) {
             $workerCommandArray[] = '--config';
-            $workerCommandArray[] = $input->getOption(Option::CONFIG);
+            /**
+             * On parallel, the command is generated with `--config` addition
+             * Using escapeshellarg() to ensure the --config path escaped, even when it has a space.
+             *
+             * eg:
+             *    --config /path/e2e/parallel with space/rector.php
+             *
+             * that can cause error:
+             *
+             *    File /rector-src/e2e/parallel\" was not found
+             *
+             * the escaped result is:
+             *
+             *    --config '/path/e2e/parallel with space/rector.php'
+             *
+             * tested in macOS and Ubuntu (github action)
+             */
+            $workerCommandArray[] = escapeshellarg($input->getOption(Option::CONFIG));
         }
 
         return implode(' ', $workerCommandArray);
