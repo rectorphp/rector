183493eab8bae7d784f673019e9fe5947493c3a7
Tomas Votruba <tomas.vot@gmail.com>
2022-06-04 11:33:15 +0200

    [DeadCode] Fix RemoveUnusedPrivatePropertyRector with left part of array dim fetch (#2428)
    * add fixture with private array dim fetch property
    
    * fix RemoveUnusedPrivatePropertyRector with left part of array dim fetch
    
    * [ci-review] Rector Rectify
    
    Co-authored-by: GitHub Action <action@github.com>

---
+++ packages/ReadWrite/ParentNodeReadAnalyzer/ArrayDimFetchParentNodeReadAnalyzer.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\ReadWrite\ParentNodeReadAnalyzer;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr;
+use PhpParser\Node\Expr\ArrayDimFetch;
+use PhpParser\Node\Expr\Assign;
+use Rector\Core\PhpParser\Node\BetterNodeFinder;
+use Rector\ReadWrite\Contract\ParentNodeReadAnalyzerInterface;
+
+final class ArrayDimFetchParentNodeReadAnalyzer implements ParentNodeReadAnalyzerInterface
+{
+    public function __construct(
+        private readonly BetterNodeFinder $betterNodeFinder,
+    ) {
+    }
+
+    public function isRead(Expr $expr, Node $parentNode): bool
+    {
+        if (! $parentNode instanceof ArrayDimFetch) {
+            return false;
+        }
+
+        if ($parentNode->dim !== $expr) {
+            return false;
+        }
+
+        // is left part of assign
+        return $this->isLeftPartOfAssign($expr);
+    }
+
+    private function isLeftPartOfAssign(Expr $expr): bool
+    {
+        $parentAssign = $this->betterNodeFinder->findParentType($expr, Assign::class);
+        if (! $parentAssign instanceof Assign) {
+            return true;
+        }
+
+        return ! (bool) $this->betterNodeFinder->findFirst(
+            $parentAssign->var,
+            fn (Node $node): bool => $node === $expr
+        );
+    }
+}
