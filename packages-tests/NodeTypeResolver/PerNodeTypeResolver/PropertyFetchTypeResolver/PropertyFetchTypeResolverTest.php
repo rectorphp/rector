dd4064e431f6876e706c7615bee5e01e11a71044
Tomas Votruba <tomas.vot@gmail.com>
2022-06-21 09:59:38 +0200

    cleanup php-version bound tests, now under one version no need to separate (#2546)

--- packages-tests/NodeTypeResolver/PerNodeTypeResolver/PropertyFetchTypeResolver/PropertyFetchTypeResolverTest.php
+++ packages-tests/NodeTypeResolver/PerNodeTypeResolver/PropertyFetchTypeResolver/PropertyFetchTypeResolverTest.php
@@ -5,10 +5,15 @@ declare(strict_types=1);
 namespace Rector\Tests\NodeTypeResolver\PerNodeTypeResolver\PropertyFetchTypeResolver;
 
 use Iterator;
+use PhpParser\Node\Expr\PropertyFetch;
+use PHPStan\Type\Type;
+use PHPStan\Type\VerbosityLevel;
+use Rector\Tests\NodeTypeResolver\PerNodeTypeResolver\AbstractNodeTypeResolverTest;
 use Symplify\EasyTesting\DataProvider\StaticFixtureFinder;
+use Symplify\EasyTesting\StaticFixtureSplitter;
 use Symplify\SmartFileSystem\SmartFileInfo;
 
-final class PropertyFetchTypeResolverTest extends AbstractPropertyFetchTypeResolverTest
+final class PropertyFetchTypeResolverTest extends AbstractNodeTypeResolverTest
 {
     /**
      * @dataProvider provideData()
@@ -24,5 +29,29 @@ final class PropertyFetchTypeResolverTest extends AbstractPropertyFetchTypeResol
     public function provideData(): Iterator
     {
         return StaticFixtureFinder::yieldDirectoryExclusively(__DIR__ . '/Fixture');
+    }
+
+    private function doTestFileInfo(SmartFileInfo $smartFileInfo): void
+    {
+        $inputFileInfoAndExpectedFileInfo = StaticFixtureSplitter::splitFileInfoToLocalInputAndExpectedFileInfos(
+            $smartFileInfo
+        );
+        $inputFileInfo = $inputFileInfoAndExpectedFileInfo->getInputFileInfo();
+        $expectedFileInfo = $inputFileInfoAndExpectedFileInfo->getExpectedFileInfo();
+
+        $propertyFetchNodes = $this->getNodesForFileOfType($inputFileInfo->getRealPath(), PropertyFetch::class);
+        $resolvedType = $this->nodeTypeResolver->getType($propertyFetchNodes[0]);
+
+        $expectedType = include $expectedFileInfo->getRealPath();
+
+        $expectedTypeAsString = $this->getStringFromType($expectedType);
+        $resolvedTypeAsString = $this->getStringFromType($resolvedType);
+
+        $this->assertSame($expectedTypeAsString, $resolvedTypeAsString);
+    }
+
+    private function getStringFromType(Type $type): string
+    {
+        return $type->describe(VerbosityLevel::precise());
     }
 }
--- rules-tests/TypeDeclaration/Rector/ClassMethod/ReturnNeverTypeRector/config/configured_rule.php
+++ rules-tests/TypeDeclaration/Rector/ClassMethod/ReturnNeverTypeRector/config/configured_rule.php
@@ -3,10 +3,8 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Core\ValueObject\PhpVersion;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnNeverTypeRector;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->phpVersion(PhpVersion::PHP_81);
     $rectorConfig->rule(ReturnNeverTypeRector::class);
 };
