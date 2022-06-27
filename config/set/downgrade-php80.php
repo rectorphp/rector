e81691f1c68a976c1589830642cd45873389d962
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-06-27 19:26:13 +0700

    [DowngradePhp80] Add DowngradeMixedTypeTypedPropertyRector (#2579)
    * [DowngradePhp80] Add DowngradeMixedTypeTypedPropertyRector
    
    * [ci-review] Rector Rectify
    
    Co-authored-by: GitHub Action <action@github.com>

--- config/set/downgrade-php80.php
+++ config/set/downgrade-php80.php
@@ -30,6 +30,7 @@ use Rector\DowngradePhp80\Rector\MethodCall\DowngradeReflectionGetAttributesRect
 use Rector\DowngradePhp80\Rector\MethodCall\DowngradeReflectionPropertyGetDefaultValueRector;
 use Rector\DowngradePhp80\Rector\New_\DowngradeArbitraryExpressionsSupportRector;
 use Rector\DowngradePhp80\Rector\NullsafeMethodCall\DowngradeNullsafeToTernaryOperatorRector;
+use Rector\DowngradePhp80\Rector\Property\DowngradeMixedTypeTypedPropertyRector;
 use Rector\DowngradePhp80\Rector\Property\DowngradeUnionTypeTypedPropertyRector;
 use Rector\DowngradePhp80\Rector\StaticCall\DowngradePhpTokenRector;
 use Rector\DowngradePhp80\ValueObject\DowngradeAttributeToAnnotation;
@@ -79,4 +80,5 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(DowngradeArrayFilterNullableCallbackRector::class);
     $rectorConfig->rule(DowngradeNumberFormatNoFourthArgRector::class);
     $rectorConfig->rule(DowngradeStringReturnTypeOnToStringRector::class);
+    $rectorConfig->rule(DowngradeMixedTypeTypedPropertyRector::class);
 };
