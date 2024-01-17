7b5aa03f24788dde58f29dcac71cabb5b5c98e31
Tomas Votruba <tomas.vot@gmail.com>
2024-01-17 11:26:39 +0000

    Updated Rector to commit fb6241fb6725b19f348fd05bc3430828c07196eb
    https://github.com/rectorphp/rector-src/commit/fb6241fb6725b19f348fd05bc3430828c07196eb Revert "[Autoload] Add false value for autoload on class_alias() on core_namespace_aliases.php" (#5475)

--- src/Application/VersionResolver.php
+++ src/Application/VersionResolver.php
@@ -19,12 +19,12 @@ final class VersionResolver
      * @api
      * @var string
      */
-    public const PACKAGE_VERSION = '6a1b2746dfe2116c1878396d4acbb92dd4b70526';
+    public const PACKAGE_VERSION = 'fb6241fb6725b19f348fd05bc3430828c07196eb';
     /**
      * @api
      * @var string
      */
-    public const RELEASE_DATE = '2024-01-17 18:12:36';
+    public const RELEASE_DATE = '2024-01-17 11:24:27';
     /**
      * @var int
      */
