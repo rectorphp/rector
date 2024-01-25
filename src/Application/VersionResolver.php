f28faf4aeb4d5101d8520d998444ebcdbc03f13e
Tomas Votruba <tomas.vot@gmail.com>
2024-01-25 17:26:13 +0000

    Updated Rector to commit 65dbbdd002c9ef71187752d92d1f15a969e7d2ad
    https://github.com/rectorphp/rector-src/commit/65dbbdd002c9ef71187752d92d1f15a969e7d2ad cs

--- src/Application/VersionResolver.php
+++ src/Application/VersionResolver.php
@@ -19,12 +19,12 @@ final class VersionResolver
      * @api
      * @var string
      */
-    public const PACKAGE_VERSION = '5b23ffe923e57d49b92156c15725ac63cb52a9b5';
+    public const PACKAGE_VERSION = '65dbbdd002c9ef71187752d92d1f15a969e7d2ad';
     /**
      * @api
      * @var string
      */
-    public const RELEASE_DATE = '2024-01-25 18:15:33';
+    public const RELEASE_DATE = '2024-01-25 18:24:25';
     /**
      * @var int
      */
