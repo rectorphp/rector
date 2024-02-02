5c93328f48c03b8271b99dd37c45b812576e81d4
Tomas Votruba <tomas.vot@gmail.com>
2024-02-02 06:45:48 +0000

    Updated Rector to commit cdc57233696fd680c8ebe3040b02c54d1da9c779
    https://github.com/rectorphp/rector-src/commit/cdc57233696fd680c8ebe3040b02c54d1da9c779 [TypeDeclaration] Change bool to false or true docblock when union has false/true standalone type on ReturnUnionTypeRector (#5543)

--- rules/TypeDeclaration/Rector/ClassMethod/ReturnUnionTypeRector.php
+++ rules/TypeDeclaration/Rector/ClassMethod/ReturnUnionTypeRector.php
@@ -8,7 +8,13 @@ use PhpParser\Node\Expr\Closure;
 use PhpParser\Node\Stmt\ClassMethod;
 use PhpParser\Node\Stmt\Function_;
 use PHPStan\Analyser\Scope;
+use PHPStan\PhpDocParser\Ast\PhpDoc\ReturnTagValueNode;
+use PHPStan\PhpDocParser\Ast\Type\IdentifierTypeNode;
+use PHPStan\Type\Constant\ConstantBooleanType;
 use PHPStan\Type\UnionType;
+use Rector\BetterPhpDocParser\PhpDocInfo\PhpDocInfoFactory;
+use Rector\BetterPhpDocParser\ValueObject\Type\BracketsAwareUnionTypeNode;
+use Rector\Comments\NodeDocBlock\DocBlockUpdater;
 use Rector\PHPStanStaticTypeMapper\Enum\TypeKind;
 use Rector\PHPStanStaticTypeMapper\TypeMapper\UnionTypeMapper;
 use Rector\Rector\AbstractScopeAwareRector;
@@ -38,11 +44,23 @@ final class ReturnUnionTypeRector extends AbstractScopeAwareRector implements Mi
      * @var \Rector\PHPStanStaticTypeMapper\TypeMapper\UnionTypeMapper
      */
     private $unionTypeMapper;
-    public function __construct(ReturnTypeInferer $returnTypeInferer, ClassMethodReturnTypeOverrideGuard $classMethodReturnTypeOverrideGuard, UnionTypeMapper $unionTypeMapper)
+    /**
+     * @readonly
+     * @var \Rector\BetterPhpDocParser\PhpDocInfo\PhpDocInfoFactory
+     */
+    private $phpDocInfoFactory;
+    /**
+     * @readonly
+     * @var \Rector\Comments\NodeDocBlock\DocBlockUpdater
+     */
+    private $docBlockUpdater;
+    public function __construct(ReturnTypeInferer $returnTypeInferer, ClassMethodReturnTypeOverrideGuard $classMethodReturnTypeOverrideGuard, UnionTypeMapper $unionTypeMapper, PhpDocInfoFactory $phpDocInfoFactory, DocBlockUpdater $docBlockUpdater)
     {
         $this->returnTypeInferer = $returnTypeInferer;
         $this->classMethodReturnTypeOverrideGuard = $classMethodReturnTypeOverrideGuard;
         $this->unionTypeMapper = $unionTypeMapper;
+        $this->phpDocInfoFactory = $phpDocInfoFactory;
+        $this->docBlockUpdater = $docBlockUpdater;
     }
     public function getRuleDefinition() : RuleDefinition
     {
@@ -115,7 +133,39 @@ CODE_SAMPLE
         if (!$returnType instanceof Node) {
             return null;
         }
+        $this->mapStandaloneSubType($node, $inferReturnType);
         $node->returnType = $returnType;
         return $node;
+    }
+    /**
+     * @param \PhpParser\Node\Stmt\ClassMethod|\PhpParser\Node\Stmt\Function_|\PhpParser\Node\Expr\Closure $node
+     */
+    private function mapStandaloneSubType($node, UnionType $unionType) : void
+    {
+        $value = null;
+        foreach ($unionType->getTypes() as $type) {
+            if ($type instanceof ConstantBooleanType) {
+                $value = $type->getValue() ? 'true' : 'false';
+                break;
+            }
+        }
+        if ($value === null) {
+            return;
+        }
+        $phpDocInfo = $this->phpDocInfoFactory->createFromNodeOrEmpty($node);
+        $returnType = $phpDocInfo->getReturnTagValue();
+        if (!$returnType instanceof ReturnTagValueNode) {
+            return;
+        }
+        if (!$returnType->type instanceof BracketsAwareUnionTypeNode) {
+            return;
+        }
+        foreach ($returnType->type->types as $key => $type) {
+            if ($type instanceof IdentifierTypeNode && $type->__toString() === 'bool') {
+                $returnType->type->types[$key] = new IdentifierTypeNode($value);
+                $this->docBlockUpdater->updateRefactoredNodeWithPhpDocInfo($node);
+                break;
+            }
+        }
     }
 }
--- src/Application/VersionResolver.php
+++ src/Application/VersionResolver.php
@@ -19,12 +19,12 @@ final class VersionResolver
      * @api
      * @var string
      */
-    public const PACKAGE_VERSION = 'fa75fcf0d1445f90cff9199875aba352671795f9';
+    public const PACKAGE_VERSION = 'cdc57233696fd680c8ebe3040b02c54d1da9c779';
     /**
      * @api
      * @var string
      */
-    public const RELEASE_DATE = '2024-02-02 06:52:25';
+    public const RELEASE_DATE = '2024-02-02 13:43:27';
     /**
      * @var int
      */
