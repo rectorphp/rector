18992ab52d1b4c26183eee65c96fbc58557b6684
Tomas Votruba <tomas.vot@gmail.com>
2024-02-12 09:34:34 +0000

    Updated Rector to commit 5edb023974a55b0efee74dd9fe92b314dd521f2c
    https://github.com/rectorphp/rector-src/commit/5edb023974a55b0efee74dd9fe92b314dd521f2c fix config for configured rule

--- vendor/rector/rector-doctrine/rules/CodeQuality/AnnotationTransformer/PropertyAnnotationTransformer/JoinColumnsTransformer.php
+++ vendor/rector/rector-doctrine/rules/CodeQuality/AnnotationTransformer/PropertyAnnotationTransformer/JoinColumnsTransformer.php
@@ -3,9 +3,11 @@
 declare (strict_types=1);
 namespace Rector\Doctrine\CodeQuality\AnnotationTransformer\PropertyAnnotationTransformer;
 
+use Rector\BetterPhpDocParser\PhpDoc\SpacelessPhpDocTagNode;
 use PhpParser\Node\Stmt\Property;
 use Rector\BetterPhpDocParser\PhpDoc\ArrayItemNode;
 use Rector\BetterPhpDocParser\PhpDocInfo\PhpDocInfo;
+use Rector\BetterPhpDocParser\ValueObject\PhpDoc\DoctrineAnnotation\CurlyListNode;
 use Rector\Doctrine\CodeQuality\Contract\PropertyAnnotationTransformerInterface;
 use Rector\Doctrine\CodeQuality\DocTagNodeFactory;
 use Rector\Doctrine\CodeQuality\NodeFactory\ArrayItemNodeFactory;
@@ -33,16 +35,28 @@ final class JoinColumnsTransformer implements PropertyAnnotationTransformerInter
         }
         $joinColumnArrayItemNodes = [];
         foreach ($joinColumns as $columnName => $joinColumn) {
-            $joinColumn = \array_merge(['name' => $columnName], $joinColumn);
-            $arrayItemNodes = $this->arrayItemNodeFactory->create($joinColumn, ['name', 'referencedColumnName']);
-            $joinColumnSpacelessPhpDocTagNode = DocTagNodeFactory::createSpacelessPhpDocTagNode($arrayItemNodes, 'Doctrine\\ORM\\Mapping\\JoinColumn');
+            $joinColumnSpacelessPhpDocTagNode = $this->createJoinColumnSpacelessTagValueNode($columnName, $joinColumn);
             $joinColumnArrayItemNodes[] = new ArrayItemNode($joinColumnSpacelessPhpDocTagNode);
         }
-        $spacelessPhpDocTagNode = DocTagNodeFactory::createSpacelessPhpDocTagNode($joinColumnArrayItemNodes, $this->getClassName());
+        if (\count($joinColumnArrayItemNodes) === 1) {
+            $spacelessPhpDocTagNode = $joinColumnArrayItemNodes[0]->value;
+        } else {
+            $spacelessPhpDocTagNode = DocTagNodeFactory::createSpacelessPhpDocTagNode([new CurlyListNode($joinColumnArrayItemNodes)], $this->getClassName());
+        }
         $propertyPhpDocInfo->addPhpDocTagNode($spacelessPhpDocTagNode);
     }
     public function getClassName() : string
     {
         return 'Doctrine\\ORM\\Mapping\\JoinColumns';
+    }
+    /**
+     * @param int|string $columnName
+     * @param mixed $joinColumn
+     */
+    private function createJoinColumnSpacelessTagValueNode($columnName, $joinColumn) : SpacelessPhpDocTagNode
+    {
+        $joinColumn = \array_merge(['name' => $columnName], $joinColumn);
+        $arrayItemNodes = $this->arrayItemNodeFactory->create($joinColumn, ['name', 'referencedColumnName']);
+        return DocTagNodeFactory::createSpacelessPhpDocTagNode($arrayItemNodes, 'Doctrine\\ORM\\Mapping\\JoinColumn');
     }
 }
