d1ec79d324e55f8354ce306510cfcb239a24572c
Tomas Votruba <tomas.vot@gmail.com>
2024-03-07 14:24:18 +0000

    Updated Rector to commit 4a1880bd2976e3cda6db8b1ca27c44c2c933b9e4
    https://github.com/rectorphp/rector-src/commit/4a1880bd2976e3cda6db8b1ca27c44c2c933b9e4 [CodeQuality] Add Equal and NotEqual support on SimplifyIfReturnBoolRector (#5697)

--- vendor/rector/rector-doctrine/config/sets/doctrine-collection-22.php
+++ vendor/rector/rector-doctrine/config/sets/doctrine-collection-22.php
@@ -7,5 +7,5 @@ use Rector\Config\RectorConfig;
 use Rector\Renaming\Rector\ClassConstFetch\RenameClassConstFetchRector;
 use Rector\Renaming\ValueObject\RenameClassAndConstFetch;
 return static function (RectorConfig $rectorConfig) : void {
-    $rectorConfig->ruleWithConfiguration(RenameClassConstFetchRector::class, [new RenameClassAndConstFetch('Doctrine\\Common\\Collections\\Criteria', 'ASC', 'Doctrine\\Common\\Collections\\Order', 'ASC'), new RenameClassAndConstFetch('Doctrine\\Common\\Collections\\Criteria', 'DESC', 'Doctrine\\Common\\Collections\\Order', 'DESC')]);
+    $rectorConfig->ruleWithConfiguration(RenameClassConstFetchRector::class, [new RenameClassAndConstFetch('Doctrine\\Common\\Collections\\Criteria', 'ASC', 'Doctrine\\Common\\Collections\\Order', 'Ascending'), new RenameClassAndConstFetch('Doctrine\\Common\\Collections\\Criteria', 'DESC', 'Doctrine\\Common\\Collections\\Order', 'Descending')]);
 };
