2c9529a8812536d4bce1238d17ea01d16e4bac3f
Tomas Votruba <tomas.vot@gmail.com>
2024-03-07 14:03:51 +0000

    Updated Rector to commit 4a1880bd2976e3cda6db8b1ca27c44c2c933b9e4
    https://github.com/rectorphp/rector-src/commit/4a1880bd2976e3cda6db8b1ca27c44c2c933b9e4 [CodeQuality] Add Equal and NotEqual support on SimplifyIfReturnBoolRector (#5697)

---
+++ vendor/rector/rector-doctrine/config/sets/doctrine-collection-22.php
@@ -0,0 +1,11 @@
+<?php
+
+declare (strict_types=1);
+namespace RectorPrefix202403;
+
+use Rector\Config\RectorConfig;
+use Rector\Renaming\Rector\ClassConstFetch\RenameClassConstFetchRector;
+use Rector\Renaming\ValueObject\RenameClassAndConstFetch;
+return static function (RectorConfig $rectorConfig) : void {
+    $rectorConfig->ruleWithConfiguration(RenameClassConstFetchRector::class, [new RenameClassAndConstFetch('Doctrine\\Common\\Collections\\Criteria', 'ASC', 'Doctrine\\Common\\Collections\\Order', 'ASC'), new RenameClassAndConstFetch('Doctrine\\Common\\Collections\\Criteria', 'DESC', 'Doctrine\\Common\\Collections\\Order', 'DESC')]);
+};
--- vendor/rector/rector-doctrine/src/Set/DoctrineSetList.php
+++ vendor/rector/rector-doctrine/src/Set/DoctrineSetList.php
@@ -24,6 +24,10 @@ final class DoctrineSetList implements SetListInterface
     /**
      * @var string
      */
+    public const DOCTRINE_COLLECTION_22 = __DIR__ . '/../../config/sets/doctrine-collection-22.php';
+    /**
+     * @var string
+     */
     public const DOCTRINE_DBAL_211 = __DIR__ . '/../../config/sets/doctrine-dbal-211.php';
     /**
      * @var string
