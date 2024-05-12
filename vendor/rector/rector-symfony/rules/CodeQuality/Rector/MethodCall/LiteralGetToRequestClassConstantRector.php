af81b5db1b460d8ab049e8abe1dc89a952f98b8f
Tomas Votruba <tomas.vot@gmail.com>
2024-05-12 07:34:26 +0000

    Updated Rector to commit b18da7d458c2d240dd50682b31670e134f2b1ed3
    https://github.com/rectorphp/rector-src/commit/b18da7d458c2d240dd50682b31670e134f2b1ed3 [CodeQuality] Skip on property and class const on CallableThisArrayToAnonymousFunctionRector (#5869)

--- vendor/rector/rector-symfony/rules/CodeQuality/Rector/MethodCall/LiteralGetToRequestClassConstantRector.php
+++ vendor/rector/rector-symfony/rules/CodeQuality/Rector/MethodCall/LiteralGetToRequestClassConstantRector.php
@@ -76,7 +76,7 @@ CODE_SAMPLE
         }
         // for client, the transitional dependency to browser-kit might be missing and cause fatal error on PHPStan reflection
         // in most cases that should be skipped, @changelog https://github.com/rectorphp/rector/issues/7135
-        if ($this->reflectionProvider->hasClass('Symfony\\Component\\BrowserKit\\AbstractBrowser') && $this->isObjectType($node->var, new ObjectType('Symfony\\Component\\HttpKernel\\Client'))) {
+        if ($this->reflectionProvider->hasClass('Symfony\\Component\\BrowserKit\\AbstractBrowser') && ($this->isObjectType($node->var, new ObjectType('Symfony\\Component\\HttpKernel\\Client')) || $this->isObjectType($node->var, new ObjectType('Symfony\\Bundle\\FrameworkBundle\\KernelBrowser')))) {
             return $this->refactorClientMethodCall($node);
         }
         if (!$this->isName($node->name, 'setMethod')) {
