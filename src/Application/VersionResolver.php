7a1293fda78fca1187a85cd3b1f05e0c3169a615
Tomas Votruba <tomas.vot@gmail.com>
2024-05-20 02:37:37 +0000

    Updated Rector to commit 09d2a599613ac2e174e9d0b825c9da1b8c93b0e4
    https://github.com/rectorphp/rector-src/commit/09d2a599613ac2e174e9d0b825c9da1b8c93b0e4 few static fixes (#5902)

--- src/Application/VersionResolver.php
+++ src/Application/VersionResolver.php
@@ -19,12 +19,12 @@ final class VersionResolver
      * @api
      * @var string
      */
-    public const PACKAGE_VERSION = 'f30405751176cf6504a86a8f9e6661c55806b38e';
+    public const PACKAGE_VERSION = '09d2a599613ac2e174e9d0b825c9da1b8c93b0e4';
     /**
      * @api
      * @var string
      */
-    public const RELEASE_DATE = '2024-05-20 02:12:01';
+    public const RELEASE_DATE = '2024-05-20 02:34:26';
     /**
      * @var int
      */
