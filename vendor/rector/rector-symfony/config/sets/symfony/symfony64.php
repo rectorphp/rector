881c2651964eda594adad0e019d97de14c14b4d5
Tomas Votruba <tomas.vot@gmail.com>
2024-07-08 19:59:30 +0000

    Updated Rector to commit 18ac56ba991202762fae3deb9e7ab190b2d369e4
    https://github.com/rectorphp/rector-src/commit/18ac56ba991202762fae3deb9e7ab190b2d369e4 refactor: Move function first-class-callable to a standalone rule (#6131)

--- vendor/rector/rector-symfony/config/sets/symfony/symfony64.php
+++ vendor/rector/rector-symfony/config/sets/symfony/symfony64.php
@@ -3,10 +3,7 @@
 declare (strict_types=1);
 namespace RectorPrefix202407;
 
-use PHPStan\Type\NullType;
-use PHPStan\Type\ObjectWithoutClassType;
-use PHPStan\Type\StringType;
-use PHPStan\Type\UnionType;
+use PHPStan\Type\MixedType;
 use Rector\Config\RectorConfig;
 use Rector\Renaming\Rector\Class_\RenameAttributeRector;
 use Rector\Renaming\Rector\Name\RenameClassRector;
@@ -17,5 +14,5 @@ use Rector\TypeDeclaration\ValueObject\AddReturnTypeDeclaration;
 return static function (RectorConfig $rectorConfig) : void {
     $rectorConfig->ruleWithConfiguration(RenameClassRector::class, ['Symfony\\Component\\HttpKernel\\UriSigner' => 'Symfony\\Component\\HttpFoundation\\UriSigner', 'Symfony\\Component\\HttpKernel\\Debug\\FileLinkFormatter' => 'Symfony\\Component\\ErrorHandler\\ErrorRenderer\\FileLinkFormatter']);
     $rectorConfig->ruleWithConfiguration(RenameAttributeRector::class, [new RenameAttribute('Symfony\\Component\\Routing\\Annotation\\Route', 'Symfony\\Component\\Routing\\Attribute\\Route')]);
-    $rectorConfig->ruleWithConfiguration(AddReturnTypeDeclarationRector::class, [new AddReturnTypeDeclaration('Symfony\\Component\\Form\\DataTransformerInterface', 'transform', new UnionType([new StringType(), new NullType()])), new AddReturnTypeDeclaration('Symfony\\Component\\Form\\DataTransformerInterface', 'reverseTransform', new UnionType([new StringType(), new ObjectWithoutClassType()]))]);
+    $rectorConfig->ruleWithConfiguration(AddReturnTypeDeclarationRector::class, [new AddReturnTypeDeclaration('Symfony\\Component\\Form\\DataTransformerInterface', 'transform', new MixedType()), new AddReturnTypeDeclaration('Symfony\\Component\\Form\\DataTransformerInterface', 'reverseTransform', new MixedType())]);
 };
