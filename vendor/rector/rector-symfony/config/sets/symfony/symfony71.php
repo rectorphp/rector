8a2b111d6a96f940d2c70d73a1f4cc1bdd4600b4
Tomas Votruba <tomas.vot@gmail.com>
2024-10-19 10:01:38 +0000

    Updated Rector to commit 3bd920dc75b8b25cace7af734d127a2bf275881d
    https://github.com/rectorphp/rector-src/commit/3bd920dc75b8b25cace7af734d127a2bf275881d [CodeQuality] Handle If cond as assign on ExplicitBoolCompareRector (#6389)

--- vendor/rector/rector-symfony/config/sets/symfony/symfony71.php
+++ vendor/rector/rector-symfony/config/sets/symfony/symfony71.php
@@ -11,5 +11,5 @@ use Rector\Renaming\ValueObject\RenameAttribute;
 // @see https://github.com/symfony/symfony/blob/7.1/UPGRADE-7.1.md
 return RectorConfig::configure()->withConfiguredRule(RenameAttributeRector::class, [new RenameAttribute('Symfony\\Component\\DependencyInjection\\Attribute\\TaggedIterator', 'Symfony\\Component\\DependencyInjection\\Attribute\\AutowireIterator'), new RenameAttribute('Symfony\\Component\\DependencyInjection\\Attribute\\TaggedLocator', 'Symfony\\Component\\DependencyInjection\\Attribute\\AutowireLocator')])->withConfiguredRule(RenameMethodRector::class, [
     // typo fix
-    new MethodCallRename('̈́Symfony\\Component\\Serializer\\Context\\Normalizer\\AbstractNormalizerContextBuilder', 'withDefaultContructorArguments', 'withDefaultConstructorArguments'),
+    new MethodCallRename('Symfony\\Component\\Serializer\\Context\\Normalizer\\AbstractNormalizerContextBuilder', 'withDefaultContructorArguments', 'withDefaultConstructorArguments'),
 ]);
