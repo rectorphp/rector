ff5ae8bdfc3335c3edfcc7c0c308e55fc2f6908a
Tomas Votruba <tomas.vot@gmail.com>
2024-11-05 15:39:59 +0000

    Updated Rector to commit afe7c46c0387e27e1eae37cf76f8a3a4119a1d39
    https://github.com/rectorphp/rector-src/commit/afe7c46c0387e27e1eae37cf76f8a3a4119a1d39 [DeadCode] Skip nullable @ template on RemoveUselessReturnTagRector (#6409)

--- vendor/rector/rector-phpunit/rules/CodeQuality/Rector/Class_/NarrowUnusedSetUpDefinedPropertyRector.php
+++ vendor/rector/rector-phpunit/rules/CodeQuality/Rector/Class_/NarrowUnusedSetUpDefinedPropertyRector.php
@@ -11,8 +11,11 @@ use PhpParser\Node\Stmt\Class_;
 use PhpParser\Node\Stmt\ClassMethod;
 use PhpParser\Node\Stmt\Property;
 use PhpParser\NodeFinder;
+use PHPStan\Reflection\ClassReflection;
+use Rector\NodeManipulator\PropertyManipulator;
 use Rector\PHPUnit\NodeAnalyzer\TestsNodeAnalyzer;
 use Rector\Rector\AbstractRector;
+use Rector\Reflection\ReflectionResolver;
 use Rector\ValueObject\MethodName;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
@@ -28,12 +31,24 @@ final class NarrowUnusedSetUpDefinedPropertyRector extends AbstractRector
     private $testsNodeAnalyzer;
     /**
      * @readonly
+     * @var \Rector\Reflection\ReflectionResolver
+     */
+    private $reflectionResolver;
+    /**
+     * @readonly
+     * @var \Rector\NodeManipulator\PropertyManipulator
+     */
+    private $propertyManipulator;
+    /**
+     * @readonly
      * @var \PhpParser\NodeFinder
      */
     private $nodeFinder;
-    public function __construct(TestsNodeAnalyzer $testsNodeAnalyzer)
+    public function __construct(TestsNodeAnalyzer $testsNodeAnalyzer, ReflectionResolver $reflectionResolver, PropertyManipulator $propertyManipulator)
     {
         $this->testsNodeAnalyzer = $testsNodeAnalyzer;
+        $this->reflectionResolver = $reflectionResolver;
+        $this->propertyManipulator = $propertyManipulator;
         $this->nodeFinder = new NodeFinder();
     }
     public function getRuleDefinition() : RuleDefinition
@@ -85,12 +100,20 @@ CODE_SAMPLE
         }
         $hasChanged = \false;
         $isFinalClass = $node->isFinal();
+        $classReflection = $this->reflectionResolver->resolveClassReflection($node);
+        if (!$classReflection instanceof ClassReflection) {
+            return null;
+        }
         foreach ($node->stmts as $key => $classStmt) {
             if (!$classStmt instanceof Property) {
                 continue;
             }
             $property = $classStmt;
-            if ($this->shouldSkipProperty($isFinalClass, $property)) {
+            if (\count($property->props) !== 1) {
+                continue;
+            }
+            $propertyName = $property->props[0]->name->toString();
+            if ($this->shouldSkipProperty($isFinalClass, $property, $classReflection, $propertyName)) {
                 continue;
             }
             if ($this->isPropertyUsedOutsideSetUpClassMethod($node, $setUpClassMethod, $property)) {
@@ -98,7 +121,6 @@ CODE_SAMPLE
             }
             $hasChanged = \true;
             unset($node->stmts[$key]);
-            $propertyName = $property->props[0]->name->toString();
             // change property to variable in setUp() method
             $this->traverseNodesWithCallable($setUpClassMethod, function (Node $node) use($propertyName) : ?Variable {
                 if (!$node instanceof PropertyFetch) {
@@ -143,7 +165,7 @@ CODE_SAMPLE
         }
         return $isPropertyUsed;
     }
-    private function shouldSkipProperty(bool $isFinalClass, Property $property) : bool
+    private function shouldSkipProperty(bool $isFinalClass, Property $property, ClassReflection $classReflection, string $propertyName) : bool
     {
         // possibly used by child
         if (!$isFinalClass && !$property->isPrivate()) {
@@ -153,6 +175,9 @@ CODE_SAMPLE
         if ($property->isStatic()) {
             return \true;
         }
-        return $property->props[0]->default instanceof Expr;
+        if ($property->props[0]->default instanceof Expr) {
+            return \true;
+        }
+        return $this->propertyManipulator->isUsedByTrait($classReflection, $propertyName);
     }
 }
