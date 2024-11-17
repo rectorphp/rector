98901dc89510d951a6d3cc535c380dafb5b44cb3
Tomas Votruba <tomas.vot@gmail.com>
2024-11-17 13:57:35 +0000

    Updated Rector to commit ecf8aadee272778347ab5738c73a300493daf199
    https://github.com/rectorphp/rector-src/commit/ecf8aadee272778347ab5738c73a300493daf199 remove config without fixture (#6448)

--- src/Application/VersionResolver.php
+++ src/Application/VersionResolver.php
@@ -19,12 +19,12 @@ final class VersionResolver
      * @api
      * @var string
      */
-    public const PACKAGE_VERSION = '26b4d97db9db66662f752f0631036509536c41d7';
+    public const PACKAGE_VERSION = 'ecf8aadee272778347ab5738c73a300493daf199';
     /**
      * @api
      * @var string
      */
-    public const RELEASE_DATE = '2024-11-17 00:38:21';
+    public const RELEASE_DATE = '2024-11-17 20:55:20';
     /**
      * @var int
      */
