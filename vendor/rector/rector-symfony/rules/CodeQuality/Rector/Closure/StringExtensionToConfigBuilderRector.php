eae014ab8aa6bee2ffd30c522ab27457c3c79d47
Tomas Votruba <tomas.vot@gmail.com>
2025-03-11 10:22:09 +0000

    Updated Rector to commit a2b010d3152c065aeb91bdbcd5437b8645a3f7c7
    https://github.com/rectorphp/rector-src/commit/a2b010d3152c065aeb91bdbcd5437b8645a3f7c7 [Carbon] Skip first class callable on DateFuncCallToCarbonRector (#6776)

--- vendor/rector/rector-symfony/rules/CodeQuality/Rector/Closure/StringExtensionToConfigBuilderRector.php
+++ vendor/rector/rector-symfony/rules/CodeQuality/Rector/Closure/StringExtensionToConfigBuilderRector.php
@@ -64,7 +64,22 @@ final class StringExtensionToConfigBuilderRector extends AbstractRector
     /**
      * @var array<string, string>
      */
-    private const EXTENSION_KEY_TO_CLASS_MAP = ['security' => 'Symfony\\Config\\SecurityConfig', 'framework' => 'Symfony\\Config\\FrameworkConfig', 'monolog' => 'Symfony\\Config\\MonologConfig', 'twig' => 'Symfony\\Config\\TwigConfig', 'doctrine' => 'Symfony\\Config\\DoctrineConfig', 'doctrine_migrations' => 'Symfony\\Config\\DoctrineMigrationsConfig', 'sentry' => 'Symfony\\Config\\SentryConfig', 'web_profiler' => 'Symfony\\Config\\WebProfilerConfig', 'debug' => 'Symfony\\Config\\DebugConfig', 'maker' => 'Symfony\\Config\\MakerConfig', 'nelmio_cors' => 'Symfony\\Config\\NelmioCorsConfig', 'api_platform' => 'Symfony\\Config\\ApiPlatformConfig'];
+    private const EXTENSION_KEY_TO_CLASS_MAP = [
+        'security' => 'Symfony\\Config\\SecurityConfig',
+        'framework' => 'Symfony\\Config\\FrameworkConfig',
+        'monolog' => 'Symfony\\Config\\MonologConfig',
+        'twig' => 'Symfony\\Config\\TwigConfig',
+        'doctrine' => 'Symfony\\Config\\DoctrineConfig',
+        'doctrine_migrations' => 'Symfony\\Config\\DoctrineMigrationsConfig',
+        'sentry' => 'Symfony\\Config\\SentryConfig',
+        'web_profiler' => 'Symfony\\Config\\WebProfilerConfig',
+        'debug' => 'Symfony\\Config\\DebugConfig',
+        'maker' => 'Symfony\\Config\\MakerConfig',
+        'nelmio_cors' => 'Symfony\\Config\\NelmioCorsConfig',
+        'api_platform' => 'Symfony\\Config\\ApiPlatformConfig',
+        // @see https://github.com/thephpleague/flysystem-bundle/blob/3.x/src/DependencyInjection/Configuration.php
+        'flysystem' => 'Symfony\\Config\\FlysystemConfig',
+    ];
     public function __construct(SymfonyPhpClosureDetector $symfonyPhpClosureDetector, SymfonyClosureExtensionMatcher $symfonyClosureExtensionMatcher, PropertyNaming $propertyNaming, ValueResolver $valueResolver, NestedConfigCallsFactory $nestedConfigCallsFactory, SecurityAccessDecisionManagerConfigArrayHandler $securityAccessDecisionManagerConfigArrayHandler, SymfonyClosureFactory $symfonyClosureFactory)
     {
         $this->symfonyPhpClosureDetector = $symfonyPhpClosureDetector;
