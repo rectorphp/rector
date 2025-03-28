059b827cc648929711606e9824337e41e2f9ed92
Tomas Votruba <tomas.vot@gmail.com>
2025-03-28 10:25:17 +0000

    Rector 2.0.11

--- src/Application/VersionResolver.php
+++ src/Application/VersionResolver.php
@@ -19,7 +19,7 @@ final class VersionResolver
      * @api
      * @var string
      */
-    public const PACKAGE_VERSION = 'e7b063f554393e1465565a54f33be58e93ef0c54';
+    public const PACKAGE_VERSION = '2.0.11';
     /**
      * @api
      * @var string
