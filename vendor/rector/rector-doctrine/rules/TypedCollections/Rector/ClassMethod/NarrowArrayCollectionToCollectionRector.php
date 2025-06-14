56f58264049d493188d1c9eba8e7e2cca0674b10
Tomas Votruba <tomas.vot@gmail.com>
2025-06-14 17:05:14 +0000

    Updated Rector to commit bacfbff0b11ba6144d37912d4554c9628d74b677
    https://github.com/rectorphp/rector-src/commit/bacfbff0b11ba6144d37912d4554c9628d74b677 Typo fix (#6981)

--- vendor/rector/rector-doctrine/rules/TypedCollections/Rector/ClassMethod/NarrowArrayCollectionToCollectionRector.php
+++ vendor/rector/rector-doctrine/rules/TypedCollections/Rector/ClassMethod/NarrowArrayCollectionToCollectionRector.php
@@ -105,14 +105,17 @@ CODE_SAMPLE
         if ($tagValueNode->type instanceof UnionTypeNode) {
             $unionTypeNode = $tagValueNode->type;
             foreach ($unionTypeNode->types as $key => $unionedType) {
-                if (!$unionedType instanceof IdentifierTypeNode) {
-                    continue;
+                if ($unionedType instanceof IdentifierTypeNode) {
+                    if (!\in_array($unionedType->name, ['ArrayCollection', DoctrineClass::ARRAY_COLLECTION], \true)) {
+                        continue;
+                    }
+                    $unionTypeNode->types[$key] = new IdentifierTypeNode('\\' . DoctrineClass::COLLECTION);
+                    return \true;
                 }
-                if (!\in_array($unionedType->name, ['ArrayCollection', DoctrineClass::ARRAY_COLLECTION], \true)) {
-                    continue;
+                if ($unionedType instanceof GenericTypeNode && $unionedType->type->name === 'ArrayCollection') {
+                    $unionedType->type = new IdentifierTypeNode('\\' . DoctrineClass::COLLECTION);
+                    return \true;
                 }
-                $unionTypeNode->types[$key] = new IdentifierTypeNode('\\' . DoctrineClass::COLLECTION);
-                return \true;
             }
         }
         // 3. handle single type
