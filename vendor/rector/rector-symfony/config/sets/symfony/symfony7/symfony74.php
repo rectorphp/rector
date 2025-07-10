f9e3d1bbe08efb5c8e3ac6246abbe00edc58140d
Tomas Votruba <tomas.vot@gmail.com>
2025-07-10 13:54:54 +0000

    Updated Rector to commit b3c4bdf2fec8a32eb301812f734c82b52179e146
    https://github.com/rectorphp/rector-src/commit/b3c4bdf2fec8a32eb301812f734c82b52179e146 [Php82] Handle array dim fetch variable on VariableInStringInterpolationFixerRector (#7060)

--- vendor/rector/rector-symfony/config/sets/symfony/symfony7/symfony74.php
+++ vendor/rector/rector-symfony/config/sets/symfony/symfony7/symfony74.php
@@ -7,4 +7,5 @@ use Rector\Config\RectorConfig;
 // @see https://github.com/symfony/symfony/blob/7.4/UPGRADE-7.4.md
 return static function (RectorConfig $rectorConfig) : void {
     $rectorConfig->import(__DIR__ . '/symfony74/symfony74-console.php');
+    $rectorConfig->import(__DIR__ . '/symfony74/symfony74-framework-bundle.php');
 };
