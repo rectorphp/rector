a85fc7d07af387b9ab69d61adf94a5abe4e36430
Tomas Votruba <tomas.vot@gmail.com>
2025-07-11 08:02:10 +0000

    Updated Rector to commit 50798810910bbfa2971373b86946df9522d0f546
    https://github.com/rectorphp/rector-src/commit/50798810910bbfa2971373b86946df9522d0f546 Update LNumber and DNumber to Int_ and Float_ (#7062)

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -1597,6 +1597,7 @@ return array(
     'Rector\\DowngradePhp83\\Rector\\ClassConst\\DowngradeTypedClassConstRector' => $vendorDir . '/rector/rector-downgrade-php/rules/DowngradePhp83/Rector/ClassConst/DowngradeTypedClassConstRector.php',
     'Rector\\DowngradePhp83\\Rector\\Class_\\DowngradeReadonlyAnonymousClassRector' => $vendorDir . '/rector/rector-downgrade-php/rules/DowngradePhp83/Rector/Class_/DowngradeReadonlyAnonymousClassRector.php',
     'Rector\\DowngradePhp84\\Rector\\Expression\\DowngradeArrayAllRector' => $vendorDir . '/rector/rector-downgrade-php/rules/DowngradePhp84/Rector/Expression/DowngradeArrayAllRector.php',
+    'Rector\\DowngradePhp84\\Rector\\Expression\\DowngradeArrayAnyRector' => $vendorDir . '/rector/rector-downgrade-php/rules/DowngradePhp84/Rector/Expression/DowngradeArrayAnyRector.php',
     'Rector\\DowngradePhp84\\Rector\\FuncCall\\DowngradeRoundingModeEnumRector' => $vendorDir . '/rector/rector-downgrade-php/rules/DowngradePhp84/Rector/FuncCall/DowngradeRoundingModeEnumRector.php',
     'Rector\\DowngradePhp84\\Rector\\MethodCall\\DowngradeNewMethodCallWithoutParenthesesRector' => $vendorDir . '/rector/rector-downgrade-php/rules/DowngradePhp84/Rector/MethodCall/DowngradeNewMethodCallWithoutParenthesesRector.php',
     'Rector\\DowngradePhp85\\Rector\\FuncCall\\DowngradeArrayFirstLastRector' => $vendorDir . '/rector/rector-downgrade-php/rules/DowngradePhp85/Rector/FuncCall/DowngradeArrayFirstLastRector.php',
---
+++ vendor/rector/rector-downgrade-php/rules/DowngradePhp84/Rector/Expression/DowngradeArrayAnyRector.php
@@ -0,0 +1,81 @@
+<?php
+
+declare (strict_types=1);
+namespace Rector\DowngradePhp84\Rector\Expression;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\ArrowFunction;
+use PhpParser\Node\Expr\Assign;
+use PhpParser\Node\Expr\ConstFetch;
+use PhpParser\Node\Expr\FuncCall;
+use PhpParser\Node\Name;
+use PhpParser\Node\Stmt;
+use PhpParser\Node\Stmt\Break_;
+use PhpParser\Node\Stmt\Expression;
+use PhpParser\Node\Stmt\Foreach_;
+use PhpParser\Node\Stmt\If_;
+use Rector\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+/**
+ * @changelog https://php.watch/versions/8.4/array_find-array_find_key-array_any-array_all
+ *
+ * @see \Rector\Tests\DowngradePhp84\Rector\Expression\DowngradeArrayAnyRector\DowngradeArrayAnyRectorTest
+ */
+final class DowngradeArrayAnyRector extends AbstractRector
+{
+    public function getNodeTypes() : array
+    {
+        return [Expression::class];
+    }
+    public function getRuleDefinition() : RuleDefinition
+    {
+        return new RuleDefinition('Downgrade array_any() to foreach loop', [new CodeSample(<<<'CODE_SAMPLE'
+$found = array_any($animals, fn($animal) => str_starts_with($animal, 'c'));
+CODE_SAMPLE
+, <<<'CODE_SAMPLE'
+$found = false;
+foreach ($animals as $animal) {
+    if (str_starts_with($animal, 'c')) {
+        $found = true;
+        break;
+    }
+}
+CODE_SAMPLE
+)]);
+    }
+    /**
+     * @param Expression $node
+     * @return Stmt[]|null
+     */
+    public function refactor(Node $node) : ?array
+    {
+        if (!$node->expr instanceof Assign) {
+            return null;
+        }
+        if (!$node->expr->expr instanceof FuncCall) {
+            return null;
+        }
+        if (!$this->isName($node->expr->expr, 'array_all')) {
+            return null;
+        }
+        if ($node->expr->expr->isFirstClassCallable()) {
+            return null;
+        }
+        $args = $node->expr->expr->getArgs();
+        if (\count($args) !== 2) {
+            return null;
+        }
+        if (!$args[1]->value instanceof ArrowFunction) {
+            return null;
+        }
+        $valueCond = $args[1]->value->expr;
+        $if = new If_($valueCond, ['stmts' => [new Expression(new Assign($node->expr->var, new ConstFetch(new Name('true')))), new Break_()]]);
+        return [
+            // init
+            new Expression(new Assign($node->expr->var, new ConstFetch(new Name('false')))),
+            // foreach loop
+            new Foreach_($args[0]->value, $args[1]->value->params[0]->var, isset($args[1]->value->params[1]->var) ? ['keyVar' => $args[1]->value->params[1]->var, 'stmts' => [$if]] : ['stmts' => [$if]]),
+        ];
+    }
+}
