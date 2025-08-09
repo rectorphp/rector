894518a2ba09dd50a62d8d42edbcd4e7aa2b36f3
Tomas Votruba <tomas.vot@gmail.com>
2025-08-09 07:21:22 +0000

    Updated Rector to commit cc8590972ead07373530f51758eaf37d616d1f7d
    https://github.com/rectorphp/rector-src/commit/cc8590972ead07373530f51758eaf37d616d1f7d [Php85] Rename socket_set_timeout function calls (#7121)

--- config/set/php85.php
+++ config/set/php85.php
@@ -5,6 +5,9 @@ namespace RectorPrefix202508;
 
 use Rector\Config\RectorConfig;
 use Rector\Php85\Rector\ArrayDimFetch\ArrayFirstLastRector;
+use Rector\Renaming\Rector\FuncCall\RenameFunctionRector;
 return static function (RectorConfig $rectorConfig) : void {
     $rectorConfig->rules([ArrayFirstLastRector::class]);
+    // https://wiki.php.net/rfc/deprecations_php_8_5#formally_deprecate_socket_set_timeout
+    $rectorConfig->ruleWithConfiguration(RenameFunctionRector::class, ['socket_set_timeout' => 'stream_set_timeout']);
 };
--- src/Application/VersionResolver.php
+++ src/Application/VersionResolver.php
@@ -19,12 +19,12 @@ final class VersionResolver
      * @api
      * @var string
      */
-    public const PACKAGE_VERSION = '65e3739ad8cda12d03b4abbf7f659fb8ea506d0d';
+    public const PACKAGE_VERSION = 'cc8590972ead07373530f51758eaf37d616d1f7d';
     /**
      * @api
      * @var string
      */
-    public const RELEASE_DATE = '2025-08-09 08:41:49';
+    public const RELEASE_DATE = '2025-08-09 14:18:08';
     /**
      * @var int
      */
