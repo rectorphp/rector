e2afa809bb8c0d064a392f0ca1d0d134b2e99077
Tomas Votruba <tomas.vot@gmail.com>
2025-09-10 13:26:03 +0000

    Updated Rector to commit 78fd064bd347d3a529883933c2e4afe4a76684c5
    https://github.com/rectorphp/rector-src/commit/78fd064bd347d3a529883933c2e4afe4a76684c5 [AutoImport] Add fixture to skip conflict no namespaced fqcn inside namespace (#7238)

--- src/Application/VersionResolver.php
+++ src/Application/VersionResolver.php
@@ -19,12 +19,12 @@ final class VersionResolver
      * @api
      * @var string
      */
-    public const PACKAGE_VERSION = '078f1936a7b1edd98cb984ade9bec02bc572a742';
+    public const PACKAGE_VERSION = '78fd064bd347d3a529883933c2e4afe4a76684c5';
     /**
      * @api
      * @var string
      */
-    public const RELEASE_DATE = '2025-09-10 19:18:29';
+    public const RELEASE_DATE = '2025-09-10 13:22:41';
     /**
      * @var int
      */
