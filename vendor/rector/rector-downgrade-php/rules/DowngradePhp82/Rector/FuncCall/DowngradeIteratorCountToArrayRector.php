116ac0ae5ee48e287962e768e8239dc69f993cfa
Tomas Votruba <tomas.vot@gmail.com>
2025-09-27 10:12:16 +0000

    Updated Rector to commit b2dcde56ae63793ef0d626c8ed5fe4b40a4ced90
    https://github.com/rectorphp/rector-src/commit/b2dcde56ae63793ef0d626c8ed5fe4b40a4ced90 Revert "[NodeTypeResolver] Apply logic Mixed inside Union check on TypeFactor…" (#7347)

--- vendor/rector/rector-downgrade-php/rules/DowngradePhp82/Rector/FuncCall/DowngradeIteratorCountToArrayRector.php
+++ vendor/rector/rector-downgrade-php/rules/DowngradePhp82/Rector/FuncCall/DowngradeIteratorCountToArrayRector.php
@@ -9,7 +9,7 @@ use PhpParser\Node\Expr\FuncCall;
 use PhpParser\Node\Expr\New_;
 use PhpParser\Node\Expr\Ternary;
 use PhpParser\Node\Name\FullyQualified;
-use PhpParser\NodeTraverser;
+use PhpParser\NodeVisitor;
 use PHPStan\Type\Type;
 use PHPStan\Type\TypeCombinator;
 use Rector\NodeAnalyzer\ArgsAnalyzer;
@@ -71,7 +71,7 @@ CODE_SAMPLE
         if ($node instanceof Ternary) {
             $hasIsArrayCheck = (bool) $this->betterNodeFinder->findFirst($node, fn(Node $subNode): bool => $subNode instanceof FuncCall && $this->isName($subNode, 'is_array'));
             if ($hasIsArrayCheck) {
-                return NodeTraverser::DONT_TRAVERSE_CHILDREN;
+                return NodeVisitor::DONT_TRAVERSE_CHILDREN;
             }
             return null;
         }
@@ -81,20 +81,18 @@ CODE_SAMPLE
         if ($node->isFirstClassCallable()) {
             return null;
         }
-        $args = $node->getArgs();
-        if ($this->argsAnalyzer->hasNamedArg($args)) {
+        $arg = $node->getArg('iterator', 0);
+        if (!$arg instanceof Arg) {
             return null;
         }
-        if (!isset($args[0])) {
-            return null;
-        }
-        $type = $this->nodeTypeResolver->getType($args[0]->value);
+        $type = $this->nodeTypeResolver->getType($arg->value);
         if ($this->shouldSkip($type)) {
             return null;
         }
-        Assert::isInstanceOf($node->args[0], Arg::class);
-        $firstValue = $node->args[0]->value;
-        $node->args[0]->value = new Ternary($this->nodeFactory->createFuncCall('is_array', [new Arg($firstValue)]), new New_(new FullyQualified('ArrayIterator'), [new Arg($firstValue)]), $firstValue);
+        $position = $this->argsAnalyzer->resolveArgPosition($node->getArgs(), 'iterator', 0);
+        Assert::isInstanceOf($node->args[$position], Arg::class);
+        $firstValue = $node->args[$position]->value;
+        $node->args[$position]->value = new Ternary($this->nodeFactory->createFuncCall('is_array', [new Arg($firstValue)]), new New_(new FullyQualified('ArrayIterator'), [new Arg($firstValue)]), $firstValue);
         return $node;
     }
     private function shouldSkip(Type $type): bool
