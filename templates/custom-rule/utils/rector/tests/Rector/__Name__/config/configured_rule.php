51122481bace61043385f564c8924f648950c8ff
Tomas Votruba <tomas.vot@gmail.com>
2025-10-11 23:40:33 +0200

    cleanup templates to allow rebuld

--- templates/custom-rule/utils/rector/tests/Rector/__Name__/config/configured_rule.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Rector\Config\RectorConfig;
-
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->rule(\Utils\Rector\Rector\__Name__::class);
-};
