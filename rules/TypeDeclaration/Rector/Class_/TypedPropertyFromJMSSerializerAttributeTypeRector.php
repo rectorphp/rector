f109ac85bb0bed4d8dd90e850616922b0df295d7
Tomas Votruba <tomas.vot@gmail.com>
2025-10-28 19:32:30 +0000

    Updated Rector to commit 2415d9d1c79bf7c6d1e7124ff14e725b9c214ca2
    https://github.com/rectorphp/rector-src/commit/2415d9d1c79bf7c6d1e7124ff14e725b9c214ca2 fallback to var float (#7601)

--- rules/TypeDeclaration/Rector/Class_/TypedPropertyFromJMSSerializerAttributeTypeRector.php
+++ rules/TypeDeclaration/Rector/Class_/TypedPropertyFromJMSSerializerAttributeTypeRector.php
@@ -12,10 +12,10 @@ use PhpParser\Node\NullableType;
 use PhpParser\Node\Stmt\Class_;
 use PhpParser\Node\Stmt\Property;
 use PHPStan\Reflection\ClassReflection;
+use PHPStan\Type\FloatType;
 use PHPStan\Type\MixedType;
 use PHPStan\Type\ObjectType;
 use PHPStan\Type\StringType;
-use PHPStan\Type\Type;
 use Rector\BetterPhpDocParser\PhpDocInfo\PhpDocInfo;
 use Rector\BetterPhpDocParser\PhpDocInfo\PhpDocInfoFactory;
 use Rector\DeadCode\PhpDoc\TagRemover\VarTagRemover;
@@ -122,7 +122,6 @@ CODE_SAMPLE
      */
     public function refactor(Node $node): ?Node
     {
-        $hasChanged = \false;
         if (!$this->hasAtLeastOneUntypedPropertyUsingJmsAttribute($node)) {
             return null;
         }
@@ -130,6 +129,7 @@ CODE_SAMPLE
         if (!$classReflection instanceof ClassReflection) {
             return null;
         }
+        $hasChanged = \false;
         foreach ($node->getProperties() as $property) {
             if ($this->shouldSkipProperty($property, $classReflection)) {
                 continue;
@@ -187,12 +187,18 @@ CODE_SAMPLE
     {
         if ($typeValue === 'float') {
             $propertyPhpDocInfo = $this->phpDocInfoFactory->createFromNode($property);
-            if ($propertyPhpDocInfo instanceof PhpDocInfo) {
-                // fallback to string, as most likely string representation of float
-                if ($propertyPhpDocInfo->getVarType() instanceof StringType) {
-                    $this->varTagRemover->removeVarTag($property);
-                    return new Identifier('string');
-                }
+            // fallback to string, as most likely string representation of float
+            if ($propertyPhpDocInfo instanceof PhpDocInfo && $propertyPhpDocInfo->getVarType() instanceof StringType) {
+                $this->varTagRemover->removeVarTag($property);
+                return new Identifier('string');
+            }
+        }
+        if ($typeValue === 'string') {
+            $propertyPhpDocInfo = $this->phpDocInfoFactory->createFromNode($property);
+            // fallback to string, as most likely string representation of float
+            if ($propertyPhpDocInfo instanceof PhpDocInfo && $propertyPhpDocInfo->getVarType() instanceof FloatType) {
+                $this->varTagRemover->removeVarTag($property);
+                return new Identifier('float');
             }
         }
         $type = $this->scalarStringToTypeMapper->mapScalarStringToType($typeValue);
--- src/Application/VersionResolver.php
+++ src/Application/VersionResolver.php
@@ -19,12 +19,12 @@ final class VersionResolver
      * @api
      * @var string
      */
-    public const PACKAGE_VERSION = '31f46dd2fd98fe1c4f371419dde0e6c7ab105e40';
+    public const PACKAGE_VERSION = '2415d9d1c79bf7c6d1e7124ff14e725b9c214ca2';
     /**
      * @api
      * @var string
      */
-    public const RELEASE_DATE = '2025-10-28 20:12:05';
+    public const RELEASE_DATE = '2025-10-28 20:28:59';
     /**
      * @var int
      */
