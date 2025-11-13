4ef6ec0fad0db1d3da9af60ba4179f8aadd6175f
Tomas Votruba <tomas.vot@gmail.com>
2025-11-13 15:51:22 +0000

    Updated Rector to commit 157504ceb2e315783606d7e976eb77d2ea21f28d
    https://github.com/rectorphp/rector-src/commit/157504ceb2e315783606d7e976eb77d2ea21f28d [TypeDeclaration] Skip no key generic object return on NarrowObjectReturnTypeRector (#7623)

--- vendor/rector/rector-symfony/rules/CodeQuality/Rector/Class_/ControllerMethodInjectionToConstructorRector.php
+++ vendor/rector/rector-symfony/rules/CodeQuality/Rector/Class_/ControllerMethodInjectionToConstructorRector.php
@@ -16,6 +16,7 @@ use Rector\StaticTypeMapper\StaticTypeMapper;
 use Rector\Symfony\Bridge\NodeAnalyzer\ControllerMethodAnalyzer;
 use Rector\Symfony\Enum\SymfonyClass;
 use Rector\Symfony\TypeAnalyzer\ControllerAnalyzer;
+use Rector\ValueObject\MethodName;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 /**
@@ -103,6 +104,9 @@ CODE_SAMPLE
         }
         $propertyMetadatas = [];
         foreach ($node->getMethods() as $classMethod) {
+            if ($classMethod->isMagic() && !$this->isName($classMethod->name, MethodName::INVOKE)) {
+                continue;
+            }
             if (!$this->controllerMethodAnalyzer->isAction($classMethod)) {
                 continue;
             }
