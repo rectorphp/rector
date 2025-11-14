947968c5b1e3c298f8319002ce5baf97428928a3
Tomas Votruba <tomas.vot@gmail.com>
2025-11-14 16:11:39 +0000

    Updated Rector to commit 8e15aadc7d2c37e8e5ccb4d3aad5937026de1224
    https://github.com/rectorphp/rector-src/commit/8e15aadc7d2c37e8e5ccb4d3aad5937026de1224 [Unambiguous] Use separate service for collect method calls of fluents (#7631)

--- vendor/rector/rector-phpunit/config/sets/phpunit-code-quality.php
+++ vendor/rector/rector-phpunit/config/sets/phpunit-code-quality.php
@@ -9,7 +9,6 @@ use Rector\PHPUnit\CodeQuality\Rector\Class_\AddReturnTypeToDependedRector;
 use Rector\PHPUnit\CodeQuality\Rector\Class_\ConstructClassMethodToSetUpTestCaseRector;
 use Rector\PHPUnit\CodeQuality\Rector\Class_\NarrowUnusedSetUpDefinedPropertyRector;
 use Rector\PHPUnit\CodeQuality\Rector\Class_\PreferPHPUnitThisCallRector;
-use Rector\PHPUnit\CodeQuality\Rector\Class_\RemoveDataProviderParamKeysRector;
 use Rector\PHPUnit\CodeQuality\Rector\Class_\SingleMockPropertyTypeRector;
 use Rector\PHPUnit\CodeQuality\Rector\Class_\TestWithToDataProviderRector;
 use Rector\PHPUnit\CodeQuality\Rector\Class_\TypeWillReturnCallableArrowFunctionRector;
@@ -66,8 +65,6 @@ return static function (RectorConfig $rectorConfig): void {
         TestWithToDataProviderRector::class,
         AssertEqualsOrAssertSameFloatParameterToSpecificMethodsTypeRector::class,
         DataProviderArrayItemsNewLinedRector::class,
-        // PHPUnit 11 reports warnings on typos + keys are rather noise than useful, fake system-keys for values
-        RemoveDataProviderParamKeysRector::class,
         FlipAssertRector::class,
         // narrow with consecutive
         NarrowIdenticalWithConsecutiveRector::class,
--- vendor/rector/rector-phpunit/rules/CodeQuality/Rector/Class_/RemoveDataProviderParamKeysRector.php
+++ vendor/rector/rector-phpunit/rules/CodeQuality/Rector/Class_/RemoveDataProviderParamKeysRector.php
@@ -4,35 +4,19 @@ declare (strict_types=1);
 namespace Rector\PHPUnit\CodeQuality\Rector\Class_;
 
 use PhpParser\Node;
-use PhpParser\Node\Expr;
-use PhpParser\Node\Expr\Array_;
-use PhpParser\Node\Expr\Yield_;
 use PhpParser\Node\Stmt\Class_;
-use PhpParser\Node\Stmt\Expression;
-use PhpParser\Node\Stmt\Return_;
-use Rector\PHPUnit\NodeAnalyzer\TestsNodeAnalyzer;
-use Rector\PHPUnit\NodeFinder\DataProviderClassMethodFinder;
+use Rector\Configuration\Deprecation\Contract\DeprecatedInterface;
+use Rector\Exception\ShouldNotHappenException;
 use Rector\Rector\AbstractRector;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 /**
- * @see \Rector\PHPUnit\Tests\CodeQuality\Rector\Class_\RemoveDataProviderParamKeysRector\RemoveDataProviderParamKeysRectorTest
+ * @deprecated as the keys can be useful to improve test readability
+ *
+ * @see https://github.com/rectorphp/rector-phpunit/pull/469#issuecomment-3533161836
  */
-final class RemoveDataProviderParamKeysRector extends AbstractRector
+final class RemoveDataProviderParamKeysRector extends AbstractRector implements DeprecatedInterface
 {
-    /**
-     * @readonly
-     */
-    private TestsNodeAnalyzer $testsNodeAnalyzer;
-    /**
-     * @readonly
-     */
-    private DataProviderClassMethodFinder $dataProviderClassMethodFinder;
-    public function __construct(TestsNodeAnalyzer $testsNodeAnalyzer, DataProviderClassMethodFinder $dataProviderClassMethodFinder)
-    {
-        $this->testsNodeAnalyzer = $testsNodeAnalyzer;
-        $this->dataProviderClassMethodFinder = $dataProviderClassMethodFinder;
-    }
     public function getRuleDefinition(): RuleDefinition
     {
         return new RuleDefinition('Remove data provider keys, that should match param names, as order is good enough', [new CodeSample(<<<'CODE_SAMPLE'
@@ -87,62 +71,6 @@ CODE_SAMPLE
      */
     public function refactor(Node $node): ?Node
     {
-        if (!$this->testsNodeAnalyzer->isInTestClass($node)) {
-            return null;
-        }
-        $dataProviderClassMethods = $this->dataProviderClassMethodFinder->find($node);
-        $hasChanged = \false;
-        foreach ($dataProviderClassMethods as $dataProviderClassMethod) {
-            // 1. remove array keys
-            foreach ((array) $dataProviderClassMethod->stmts as $classMethodStmt) {
-                if (!$classMethodStmt instanceof Return_) {
-                    continue;
-                }
-                if (!$classMethodStmt->expr instanceof Array_) {
-                    continue;
-                }
-                $returnedArray = $classMethodStmt->expr;
-                foreach ($returnedArray->items as $arrayItem) {
-                    if (!$arrayItem->value instanceof Array_) {
-                        continue;
-                    }
-                    $nestedArray = $arrayItem->value;
-                    foreach ($nestedArray->items as $nestedArrayItem) {
-                        if (!$nestedArrayItem->key instanceof Expr) {
-                            continue;
-                        }
-                        // remove key
-                        $nestedArrayItem->key = null;
-                        $hasChanged = \true;
-                    }
-                }
-            }
-            // 2. remove yield keys
-            foreach ((array) $dataProviderClassMethod->stmts as $classMethodStmt) {
-                if (!$classMethodStmt instanceof Expression) {
-                    continue;
-                }
-                if (!$classMethodStmt->expr instanceof Yield_) {
-                    continue;
-                }
-                $yield = $classMethodStmt->expr;
-                if (!$yield->value instanceof Array_) {
-                    continue;
-                }
-                $yieldArray = $yield->value;
-                foreach ($yieldArray->items as $yieldArrayItem) {
-                    if (!$yieldArrayItem->key instanceof Expr) {
-                        continue;
-                    }
-                    // remove key
-                    $yieldArrayItem->key = null;
-                    $hasChanged = \true;
-                }
-            }
-        }
-        if ($hasChanged === \false) {
-            return null;
-        }
-        return $node;
+        throw new ShouldNotHappenException(sprintf('"%s" is deprecated and should not be used anymore. Remove it from your config files.', self::class));
     }
 }
