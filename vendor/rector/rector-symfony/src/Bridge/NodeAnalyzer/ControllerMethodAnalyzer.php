472c7c028604d75d82449d2375f05c93b02cb8bc
Tomas Votruba <tomas.vot@gmail.com>
2025-11-22 15:17:46 +0000

    Updated Rector to commit 3cf1f519c544be931dff89694ed0e3b4af11eacc
    https://github.com/rectorphp/rector-src/commit/3cf1f519c544be931dff89694ed0e3b4af11eacc [EarlyReturn] Handle If, elseIf, else all returned on RemoveAlwaysElseRector (#7659)

--- vendor/rector/rector-symfony/src/Bridge/NodeAnalyzer/ControllerMethodAnalyzer.php
+++ vendor/rector/rector-symfony/src/Bridge/NodeAnalyzer/ControllerMethodAnalyzer.php
@@ -4,6 +4,10 @@ declare (strict_types=1);
 namespace Rector\Symfony\Bridge\NodeAnalyzer;
 
 use PhpParser\Node\Stmt\ClassMethod;
+use Rector\BetterPhpDocParser\PhpDocInfo\PhpDocInfo;
+use Rector\BetterPhpDocParser\PhpDocInfo\PhpDocInfoFactory;
+use Rector\Php80\NodeAnalyzer\PhpAttributeAnalyzer;
+use Rector\Symfony\Enum\SymfonyAttribute;
 use Rector\Symfony\TypeAnalyzer\ControllerAnalyzer;
 final class ControllerMethodAnalyzer
 {
@@ -11,9 +15,19 @@ final class ControllerMethodAnalyzer
      * @readonly
      */
     private ControllerAnalyzer $controllerAnalyzer;
-    public function __construct(ControllerAnalyzer $controllerAnalyzer)
+    /**
+     * @readonly
+     */
+    private PhpDocInfoFactory $phpDocInfoFactory;
+    /**
+     * @readonly
+     */
+    private PhpAttributeAnalyzer $phpAttributeAnalyzer;
+    public function __construct(ControllerAnalyzer $controllerAnalyzer, PhpDocInfoFactory $phpDocInfoFactory, PhpAttributeAnalyzer $phpAttributeAnalyzer)
     {
         $this->controllerAnalyzer = $controllerAnalyzer;
+        $this->phpDocInfoFactory = $phpDocInfoFactory;
+        $this->phpAttributeAnalyzer = $phpAttributeAnalyzer;
     }
     /**
      * Detect if is <some>Action() in Controller
@@ -23,6 +37,13 @@ final class ControllerMethodAnalyzer
         if (!$this->controllerAnalyzer->isInsideController($classMethod)) {
             return \false;
         }
-        return $classMethod->isPublic() && !$classMethod->isStatic();
+        if ($classMethod->isPublic() && !$classMethod->isStatic()) {
+            $phpDocInfo = $this->phpDocInfoFactory->createFromNode($classMethod);
+            if ($phpDocInfo instanceof PhpDocInfo && $phpDocInfo->hasByName('required')) {
+                return \false;
+            }
+            return !$this->phpAttributeAnalyzer->hasPhpAttribute($classMethod, SymfonyAttribute::REQUIRED);
+        }
+        return \false;
     }
 }
