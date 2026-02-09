e329b9ea0c96e4d08c2cdc8f734430f3c6848351
Tomas Votruba <tomas.vot@gmail.com>
2026-02-09 09:56:06 +0000

    Updated Rector to commit 4ed71aeee69c5f327243c840d2ead5cbf064e378
    https://github.com/rectorphp/rector-src/commit/4ed71aeee69c5f327243c840d2ead5cbf064e378 Skip ClosureToArrowFunctionRector when closure has @var docblock (#7885)

--- vendor/rector/rector-symfony/rules/Symfony73/Rector/Class_/ConstraintOptionsToNamedArgumentsRector.php
+++ vendor/rector/rector-symfony/rules/Symfony73/Rector/Class_/ConstraintOptionsToNamedArgumentsRector.php
@@ -84,6 +84,7 @@ CODE_SAMPLE
         }
         $array = $node->args[0]->value;
         $namedArgs = [];
+        $oldTokens = $this->file->getOldTokens();
         foreach ($array->items as $item) {
             if (!$item instanceof ArrayItem) {
                 continue;
@@ -98,6 +99,18 @@ CODE_SAMPLE
             $keyValue = $this->valueResolver->getValue($item->key);
             if (!is_string($keyValue)) {
                 continue;
+            }
+            $lastTokenKey = $item->key->getEndTokenPos();
+            $startTokenValue = $item->value->getStartTokenPos();
+            while ($lastTokenKey < $startTokenValue) {
+                ++$lastTokenKey;
+                if (!isset($oldTokens[$lastTokenKey])) {
+                    break;
+                }
+                $token = $oldTokens[$lastTokenKey];
+                if ($token->is([\T_DOC_COMMENT, \T_COMMENT])) {
+                    return null;
+                }
             }
             $arg = new Arg($item->value);
             $arg->name = new Identifier($keyValue);
