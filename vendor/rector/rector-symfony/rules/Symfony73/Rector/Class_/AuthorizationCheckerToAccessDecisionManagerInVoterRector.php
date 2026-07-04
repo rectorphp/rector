40c72161eac7200e8110cbd6e578d43d18f31a5e
Tomas Votruba <tomas.vot@gmail.com>
2026-07-04 23:05:35 +0000

    Updated Rector to commit eb3eb215f7594accf81a5c673c267770fbd5a8d0
    https://github.com/rectorphp/rector-src/commit/eb3eb215f7594accf81a5c673c267770fbd5a8d0 [TypeDeclaration] Add AddClosureParamTypeFromVariableCallRector (#8142)

--- vendor/rector/rector-symfony/rules/Symfony73/Rector/Class_/AuthorizationCheckerToAccessDecisionManagerInVoterRector.php
+++ vendor/rector/rector-symfony/rules/Symfony73/Rector/Class_/AuthorizationCheckerToAccessDecisionManagerInVoterRector.php
@@ -49,7 +49,7 @@ final class AuthorizationCheckerVoter extends Voter
 
     protected function voteOnAttribute(string $attribute, $subject, TokenInterface $token): bool
     {
-        return $this->authorizationChecker->isGranted('ROLE_ADMIN');
+        return $this->authorizationChecker->isGranted('ROLE_ADMIN', $subject);
     }
 }
 CODE_SAMPLE
@@ -66,7 +66,7 @@ final class AuthorizationCheckerVoter extends Voter
 
     protected function voteOnAttribute(string $attribute, $subject, TokenInterface $token): bool
     {
-        return $this->accessDecisionManager->decide($token, ['ROLE_ADMIN']);
+        return $this->accessDecisionManager->decide($token, ['ROLE_ADMIN'], $subject);
     }
 }
 CODE_SAMPLE
@@ -153,7 +153,12 @@ CODE_SAMPLE
                     return null;
                 }
                 $attributeExpr = $attributeArg->value;
-                $node->args = [new Arg($tokenVariable), new Arg(new Array_([new ArrayItem($attributeExpr)]))];
+                $args = [new Arg($tokenVariable), new Arg(new Array_([new ArrayItem($attributeExpr)]))];
+                $subjectArg = $node->args[1] ?? null;
+                if ($subjectArg instanceof Arg) {
+                    $args[] = new Arg($subjectArg->value);
+                }
+                $node->args = $args;
                 $hasChanged = \true;
                 return $node;
             });
