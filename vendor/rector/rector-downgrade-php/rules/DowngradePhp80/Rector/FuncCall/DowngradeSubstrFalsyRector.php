b53fabc838443d0b86ac7714a77f020535cc9ac7
Tomas Votruba <tomas.vot@gmail.com>
2026-08-29 17:50:46 +0000

    Updated Rector to commit 6af747ae61b0a57c9d050fac873b2050a35ad66a

--- vendor/rector/rector-downgrade-php/rules/DowngradePhp80/Rector/FuncCall/DowngradeSubstrFalsyRector.php
+++ vendor/rector/rector-downgrade-php/rules/DowngradePhp80/Rector/FuncCall/DowngradeSubstrFalsyRector.php
@@ -139,7 +139,6 @@ final class DowngradeSubstrFalsyRector extends AbstractRector
                 }
             }
         }
-        // @phpstan-ignore rector.rectorCheaperGuardsFirst (cannot hoist above the CallLike block, which marks IS_FALSY_UNCASTABLE on non-substr nodes)
         if (!$this->isName($node, 'substr')) {
             return null;
         }
