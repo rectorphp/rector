51a32428ab0731480c553c8f03332d27c3a62408
Tomas Votruba <tomas.vot@gmail.com>
2026-09-23 14:40:43 +0000

    Updated Rector to commit dd3bfc408e7b02b0d64158dbf4f654a15379ca6d
    https://github.com/rectorphp/rector-src/commit/dd3bfc408e7b02b0d64158dbf4f654a15379ca6d Point ReturnNullableTypeRectorPhp82Test to its own fixtures and config (#8517)

--- src/Application/VersionResolver.php
+++ src/Application/VersionResolver.php
@@ -19,12 +19,12 @@ final class VersionResolver
      * @api
      * @var string
      */
-    public const PACKAGE_VERSION = '3e701990a23d91a37906379311217036380bf501';
+    public const PACKAGE_VERSION = 'dd3bfc408e7b02b0d64158dbf4f654a15379ca6d';
     /**
      * @api
      * @var string
      */
-    public const RELEASE_DATE = '2026-09-23 16:32:54';
+    public const RELEASE_DATE = '2026-09-23 14:37:59';
     /**
      * @var int
      */
