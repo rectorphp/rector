db783ae357476f44f74fa156ab861d222cf7c954
Tomas Votruba <tomas.vot@gmail.com>
2026-09-24 07:26:23 +0000

    Updated Rector to commit ec9f135bd3a964f1022ae10fac954d48f1e8077e

--- vendor/rector/rector-phpunit/rules/CodeQuality/Rector/MethodCall/FlipAssertRector.php
+++ vendor/rector/rector-phpunit/rules/CodeQuality/Rector/MethodCall/FlipAssertRector.php
@@ -4,11 +4,26 @@ declare (strict_types=1);
 namespace Rector\PHPUnit\CodeQuality\Rector\MethodCall;
 
 use PhpParser\Node;
+use PhpParser\Node\Arg;
+use PhpParser\Node\ArrayItem;
 use PhpParser\Node\Expr;
+use PhpParser\Node\Expr\Array_;
+use PhpParser\Node\Expr\ArrayDimFetch;
+use PhpParser\Node\Expr\BinaryOp\Concat;
 use PhpParser\Node\Expr\ClassConstFetch;
 use PhpParser\Node\Expr\ConstFetch;
+use PhpParser\Node\Expr\FuncCall;
 use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Expr\New_;
+use PhpParser\Node\Expr\NullsafeMethodCall;
+use PhpParser\Node\Expr\NullsafePropertyFetch;
+use PhpParser\Node\Expr\PropertyFetch;
 use PhpParser\Node\Expr\StaticCall;
+use PhpParser\Node\Expr\StaticPropertyFetch;
+use PhpParser\Node\Expr\UnaryMinus;
+use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Identifier;
+use PhpParser\Node\Name;
 use PhpParser\Node\Scalar;
 use Rector\PHPUnit\NodeAnalyzer\TestsNodeAnalyzer;
 use Rector\Rector\AbstractRector;
@@ -26,7 +41,11 @@ final class FlipAssertRector extends AbstractRector
     /**
      * @var string[]
      */
-    private const METHOD_NAMES = ['assertSame', 'assertNotSame', 'assertNotEquals', 'assertEquals', 'assertStringContainsString'];
+    private const METHOD_NAMES = ['assertSame', 'assertNotSame', 'assertNotEquals', 'assertEquals', 'assertStringContainsString', 'assertEqualsCanonicalizing', 'assertNotEqualsCanonicalizing', 'assertEqualsIgnoringCase', 'assertNotEqualsIgnoringCase', 'assertEqualsWithDelta', 'assertNotEqualsWithDelta'];
+    /**
+     * @var array<class-string<Expr>>
+     */
+    private const COMPUTED_EXPR_CLASSES = [MethodCall::class, StaticCall::class, FuncCall::class, PropertyFetch::class, StaticPropertyFetch::class, NullsafeMethodCall::class, NullsafePropertyFetch::class, ArrayDimFetch::class];
     public function __construct(TestsNodeAnalyzer $testsNodeAnalyzer)
     {
         $this->testsNodeAnalyzer = $testsNodeAnalyzer;
@@ -81,11 +100,11 @@ CODE_SAMPLE
         }
         $firstArg = $node->getArgs()[0];
         $secondArg = $node->getArgs()[1];
-        // correct location
-        if ($this->isScalarValue($firstArg->value)) {
+        // named args keep their meaning regardless of position
+        if ($firstArg->name instanceof Identifier || $secondArg->name instanceof Identifier) {
             return null;
         }
-        if (!$this->isScalarValue($secondArg->value)) {
+        if (!$this->isFlipNeeded($firstArg->value, $secondArg->value)) {
             return null;
         }
         $oldArgs = $node->getArgs();
@@ -94,14 +113,91 @@ CODE_SAMPLE
         $node->args = $oldArgs;
         return $node;
     }
+    private function isFlipNeeded(Expr $firstExpr, Expr $secondExpr): bool
+    {
+        // correct location
+        if ($this->isExpectedValue($firstExpr)) {
+            return \false;
+        }
+        if ($this->isExpectedValue($secondExpr)) {
+            return \true;
+        }
+        // e.g. assertSame($obj->getValues(), [$a, $b])
+        return $secondExpr instanceof Array_ && $this->isComputedExpr($firstExpr) && $this->isLiteralArray($secondExpr, \true);
+    }
+    /**
+     * @param bool $allowVariables accept variables as array keys and values, not inside concat or new
+     */
+    private function isExpectedValue(Expr $expr, bool $allowVariables = \false): bool
+    {
+        if ($this->isScalarValue($expr) || $this->isEnumCaseValue($expr)) {
+            return \true;
+        }
+        if ($allowVariables && $expr instanceof Variable) {
+            return \true;
+        }
+        if ($expr instanceof New_) {
+            return $this->isNewWithLiteralArgs($expr);
+        }
+        if ($expr instanceof Concat) {
+            return $this->isExpectedValue($expr->left) && $this->isExpectedValue($expr->right);
+        }
+        return $expr instanceof Array_ && $this->isLiteralArray($expr, $allowVariables);
+    }
+    private function isNewWithLiteralArgs(New_ $new): bool
+    {
+        if (!$new->class instanceof Name) {
+            return \false;
+        }
+        $found = \true;
+        foreach ($new->getArgs() as $arg) {
+            if (!(!$arg->unpack && !$arg->name instanceof Identifier && $this->isExpectedValue($arg->value))) {
+                $found = \false;
+                break;
+            }
+        }
+        return $found;
+    }
+    private function isLiteralArray(Array_ $array, bool $allowVariables = \false): bool
+    {
+        $found = \true;
+        foreach ($array->items as $arrayItem) {
+            if (!(!$arrayItem->unpack && (!$arrayItem->key instanceof Expr || $this->isExpectedValue($arrayItem->key, $allowVariables)) && $this->isExpectedValue($arrayItem->value, $allowVariables))) {
+                $found = \false;
+                break;
+            }
+        }
+        return $found;
+    }
+    private function isComputedExpr(Expr $expr): bool
+    {
+        $found = \false;
+        foreach (self::COMPUTED_EXPR_CLASSES as $computedExprClass) {
+            if ($expr instanceof $computedExprClass) {
+                $found = \true;
+                break;
+            }
+        }
+        return $found;
+    }
+    /**
+     * e.g. Status::Active->value, Status::Active->name
+     */
+    private function isEnumCaseValue(Expr $expr): bool
+    {
+        if (!$expr instanceof PropertyFetch || !$this->isNames($expr->name, ['value', 'name'])) {
+            return \false;
+        }
+        return $expr->var instanceof ClassConstFetch && !$this->isName($expr->var->name, 'class');
+    }
+    /**
+     * Scalar or constant, optionally negated, e.g. 'value', -1, PHP_INT_MAX, -self::LIMIT
+     */
     private function isScalarValue(Expr $expr): bool
     {
-        if ($expr instanceof Scalar) {
-            return \true;
+        if ($expr instanceof UnaryMinus) {
+            return $this->isScalarValue($expr->expr);
         }
-        if ($expr instanceof ConstFetch) {
-            return \true;
-        }
-        return $expr instanceof ClassConstFetch;
+        return $expr instanceof Scalar || $expr instanceof ConstFetch || $expr instanceof ClassConstFetch;
     }
 }
